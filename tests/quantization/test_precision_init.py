a0412faae67b67ed82a56c71a90574c5531eb3b3
Lyalyushkin, Nikolay <nikolay.lyalyushkin@intel.com>
2020-06-05 13:09:49 +0300

    fixed tests

--- tests/quantization/test_precision_init.py
+++ tests/quantization/test_precision_init.py
@@ -44,7 +44,8 @@ from nncf.quantization.quantizer_id import WeightQuantizerId
 from nncf.utils import get_all_modules_by_type, safe_thread_call
 from tests.conftest import TEST_ROOT, EXAMPLES_DIR
 from tests.quantization.test_quantization_helpers import compare_multi_gpu_dump, \
-    get_quantization_config_without_range_init, distributed_init_test_default, post_compression_test_distr_init
+    get_quantization_config_without_range_init, distributed_init_test_default, post_compression_test_distr_init, \
+    get_squeezenet_quantization_config
 from tests.test_compressed_graph import check_graph
 from tests.test_helpers import create_compressed_model_and_algo_for_test, MockModel, create_conv, \
     create_mock_dataloader
