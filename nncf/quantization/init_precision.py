d32f71722483dc395cc6f048bef75b3fcf23decd
Nikolay <nikolay.lyalyushkin@intel.com>
2020-06-05 11:07:03 +0300

    HAWQ improvements (#24)
    - Optimized Hessian Trace calculation by skipping weight quantizer that have a single
    precision constraint.
    - Corrected mobilenet-v2 configs for manual setting of mixed precisions
    - Added check for manual precision init
    - Refactored HAWQDump
        - renamed to HAWQDebugger
        - dumps to log_dir
        - dumps bidwidth graph
    - Added behaviour tests for HAWQInitializer and HAWQDebugger

--- nncf/quantization/init_precision.py
+++ nncf/quantization/init_precision.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95ed109e67ecabd40ca85a7fa0dc8811bf2bf6c520d98a1023b5b8beb857cceb
-size 24157
+oid sha256:47abd3ff6ef21d71151e0702752de755fc69df1e86ed68b4aa05ffcfef21f3d2
+size 28737
--- tests/quantization/test_precision_init.py
+++ tests/quantization/test_precision_init.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fa4df9bf8aa11f6402c6992a9268a5247c42f5eb37b18f08c8686d9113d65fc
-size 19913
+oid sha256:f1e67a0700c2568c40d4954b34e940e7898970608132c0d9d3f6ca96f5b4f67b
+size 21607
