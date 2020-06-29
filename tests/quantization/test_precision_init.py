0a708225a686dbba70c4529e957ebe630cc41891
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2020-06-29 17:38:06 +0300

    Added latest configs for mixed precision by staged quantization (#42)

--- tests/quantization/test_precision_init.py
+++ tests/quantization/test_precision_init.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4094887ab0f68b573be4c3a46229ffec816f5593e5f343c1ae955edc7343517
+oid sha256:8d4a2c4fdcbe49ed8fe1be75d17732e5f50144e3c35fbc374c4c259d41148255
 size 23647
