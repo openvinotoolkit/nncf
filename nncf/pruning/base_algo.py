e4fbfbdf40c89473ba1380a92ea1c8d0e3269890
Vasily Shamporov <vasily.shamporov@intel.com>
2020-07-20 11:56:02 +0300

    Properly output weight shares in percent for pruning and sparsity (#54)

--- nncf/pruning/base_algo.py
+++ nncf/pruning/base_algo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fb7dddd3189eeedd71098f7541eda69608136401056f691bf0b6aa46045c84d
-size 12835
+oid sha256:bb6020b24f9c9ba9be213974a6ff436b9c21072e406a2729f80799edee6a9f57
+size 12841
