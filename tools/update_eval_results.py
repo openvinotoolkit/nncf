06415d8880bd6feddc6bec4960cb5d06211283db
Vasily Shamporov <vasily.shamporov@intel.com>
2020-07-21 19:48:56 +0300

    Remove redundant result entries in the top-level README (#60)

--- tools/update_eval_results.py
+++ tools/update_eval_results.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5c27f0969357cbc4f3a97d044641cf3ad8d77b3ff814cc8cf0efa9ee76274e5
-size 9949
+oid sha256:eb37e797d4551ac3eacf0ddcb36229e9b057780895501e8d95fca4eb0feb4c98
+size 9987
