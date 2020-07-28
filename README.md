3061e407d9794202c89b760538d6a75ad5221e82
Vasily Shamporov <vasily.shamporov@intel.com>
2020-07-28 19:02:54 +0300

    Add info on installation and PyPI install testing (#75)

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23fc8dec514e58c2f2ad8adab81bd299a2000a36777e6c9e7e7a40d86fafbc6c
-size 9348
+oid sha256:e4220599607218de3167990f770aa2ddbc03abf32ecdf1e9940bfda8c0d74075
+size 9484
--- tests/test_install.py
+++ tests/test_install.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:417d1a10a79edd74eff8819963b3639f896ad2c936b4b0d2e6a3ea04bd7b7eae
-size 2812
+oid sha256:d91a8cf3335f53f7dfef5f86e0403359bbda824f786ea65a5ec09b9de12e7bee
+size 2736
