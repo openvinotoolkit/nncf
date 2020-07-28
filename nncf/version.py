a561bee3193772640441918618022b20213648ff
Vasily Shamporov <vasily.shamporov@intel.com>
2020-07-28 14:09:32 +0300

    Release v1.4 of NNCF (#71)

--- nncf/version.py
+++ nncf/version.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e0280dd1a99bc2eec968f0c80bc9f7c21b025b437718e88ee850c16f18b5a9b
-size 22
+oid sha256:7cd88fe982dcf8612f0625eedb70008aa026d12a25a0926c89a7be17c2fc10ab
+size 20
