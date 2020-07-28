d93d92eff89e582a6400d06567e3b56b1d37b8bb
Vasily Shamporov <vasily.shamporov@intel.com>
2020-07-28 11:20:00 +0300

    Fix setup.py install mode (#70)
    Fixes: #69

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4dd19c0506e86829d60d18cd291644d7b1865ac7f25bfb776448bfc6fe4812c4
-size 5567
+oid sha256:9c647305a5420b145a43c2767efda3e1647badbbef188b6986c1b7baededfa3b
+size 5598
--- tests/test_install.py
+++ tests/test_install.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22232ad89c249d1590129deb303d989d922dc5ad4aa1958fbebc7e4654d2ca58
-size 2801
+oid sha256:417d1a10a79edd74eff8819963b3639f896ad2c936b4b0d2e6a3ea04bd7b7eae
+size 2812
