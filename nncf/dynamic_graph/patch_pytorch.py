4012d40e5e693d7b0abceafa7c624cd2b3c36fa8
Vasily Shamporov <vasily.shamporov@intel.com>
2020-08-05 14:01:47 +0300

    Omit nodes called during debugging from entering NNCF graph (#87)

--- nncf/dynamic_graph/patch_pytorch.py
+++ nncf/dynamic_graph/patch_pytorch.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a297dbe0dd061c28d6e03e03684281c4826d56dc1ba6f1dc7c66fc2fd724ce2
-size 8555
+oid sha256:3412ff3328cf40e6f911882dd3926a1b9040b1edcd8a4d10bfd9a06cbf898ec6
+size 8731
--- nncf/dynamic_graph/wrappers.py
+++ nncf/dynamic_graph/wrappers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:defdeba0929e256864f614ba0952067d5ba9c06a0bb36bc1a5f4964d485cd6dd
-size 2993
+oid sha256:c665ef2fdb20383c30b575ad05f0c021aa286315366bc4f99ea11ee865932714
+size 3402
