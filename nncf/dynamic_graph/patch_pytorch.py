d0f93e9a7c2a7202302929fcefbe364f25479f30
Vasily Shamporov <vasily.shamporov@intel.com>
2020-08-06 17:45:49 +0300

    Patch TracedTensor's __repr__ method instead of torch.Tensor's (#92)

--- nncf/dynamic_graph/patch_pytorch.py
+++ nncf/dynamic_graph/patch_pytorch.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3412ff3328cf40e6f911882dd3926a1b9040b1edcd8a4d10bfd9a06cbf898ec6
+oid sha256:08a36ebfa24daf59ae7d1270b1f739d25cb84c9100b39b4ae141debe9cc02284
 size 8731
