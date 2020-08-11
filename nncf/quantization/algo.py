a34b5256f4f75e5ae8428390f1f01e04fdeefc1d
Vasily Shamporov <vasily.shamporov@intel.com>
2020-08-11 15:46:06 +0300

    Fix input quantization in case of embeddings (#97)

--- nncf/quantization/algo.py
+++ nncf/quantization/algo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ded6605902bf86d1cbe09e4cbcfaee4148c053c651f19637b8ebeeef594bcbb3
-size 62836
+oid sha256:72177887dc418c2791d7c89c9e172c6f0181f9cfb05ca8d74996c5bd2ce66241
+size 62991
