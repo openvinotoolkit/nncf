24459c1cd8490dc65694b8ea7d7895eded537e49
Vasily Shamporov <vasily.shamporov@intel.com>
2020-08-14 15:32:06 +0300

    Fix bad rebase in asymmetric quantization ONNX export (#104)

--- nncf/quantization/layers.py
+++ nncf/quantization/layers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43d5093f4faf3bdc249ef68175f65c5e3749c3a6947f44afab903c0c12c8d6d5
-size 17248
+oid sha256:9ea0e5c0fbeb883dd851c0253b115120afd90ca7f46a8ddc15b7e9956efb0a60
+size 16819
