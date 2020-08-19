ecb1d95935ec83cba1999e6fa4c3f68066641e15
Vasily Shamporov <vasily.shamporov@intel.com>
2020-08-19 12:35:03 +0300

    Fix symmetric quantizer per-channel init for max values close to 0 (#109)

--- nncf/quantization/layers.py
+++ nncf/quantization/layers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ea0e5c0fbeb883dd851c0253b115120afd90ca7f46a8ddc15b7e9956efb0a60
-size 16819
+oid sha256:2c01fe4d4be716cf00bfc06181f5007424d3e1f2a0d5bc7c6bea000f3a862823
+size 16910
