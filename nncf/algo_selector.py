c063f98897cf798bec3a5de4ebc278b7847a04f2
Aleksei Kashapov <aleksei.kashapov@intel.com>
2020-09-23 17:36:57 +0300

    Fix bug with NoCompressionAlgorithmController (#150)

--- nncf/algo_selector.py
+++ nncf/algo_selector.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89dae392f50f615e5af1ea5ba8c946472d477145f3fa739aeca9720a1086e72c
-size 1072
+oid sha256:5b9b372db22966d68f8c8df27506f7a465c60a18db43d6ba86e3b77c980c8209
+size 1370
