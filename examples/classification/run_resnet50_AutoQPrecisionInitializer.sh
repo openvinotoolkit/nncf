7ad6754b3477ef5d22c8724e1c2631c1e9a54d88
Chua, Vui Seng <vui.seng.chua@intel.com>
2020-09-30 00:09:55 -0700

    Add sample config running resnet50 with AutoQPrecisionInitializer

---
+++ examples/classification/run_resnet50_AutoQPrecisionInitializer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export PYTHONPATH=/home/vchua/autox
+
+python main.py \
+    -m test \
+    --gpu-id 0 \
+    --config configs/quantization/resnet50_imagenet_mixed_int_AutoQ_0.15.json \
+    --data /data/dataset/imagenet/ilsvrc2012/torchvision \
+    --workers 16 \
+    --log-dir ./resnet50_AutoQPrecisionInitializer_run
+
