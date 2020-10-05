9d733ff03589ba18b99f9fef3689b92f7901fb3e
Aleksei Kashapov <aleksei.kashapov@intel.com>
2020-10-05 18:52:36 +0300

    Fix bug with different torch devices in get_scale_zp_from_input_low_input_high (#158)

--- nncf/quantization/quantize_functions.py
+++ nncf/quantization/quantize_functions.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d36b678836c08ced3fec90ee3716853927dfc81ed7474f707f8c6590ba869f59
-size 8239
+oid sha256:e819c80f6c9be59ba2b8a211529001e85a3cc01dcf104ebca65ae3b1f781b11c
+size 8459
