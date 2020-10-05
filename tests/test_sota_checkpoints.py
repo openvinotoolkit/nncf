3c6b5ac2617cbc0c793a8447aca604475736804f
Vasily Shamporov <vasily.shamporov@intel.com>
2020-10-05 18:52:07 +0300

    Improve eval test code (#160)

--- tests/test_sota_checkpoints.py
+++ tests/test_sota_checkpoints.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac1ff909bd98d676ff7e20f1c9565d49c942452e5ba884626c538400a845b5ec
-size 12719
+oid sha256:4e480cc3f681b264b071379e7496a42781ae2fbfedf3e4ff31f79477caf1d8d4
+size 16900
