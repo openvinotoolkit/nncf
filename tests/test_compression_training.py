f1b33b79ca335bdf3cc0bce809dc109ab1a97cc6
Vasily Shamporov <vasily.shamporov@intel.com>
2020-10-05 16:03:33 +0300

    Fix command line creation for test_compression_training (#167)

--- tests/test_compression_training.py
+++ tests/test_compression_training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:726c51dfb4e90a6aa28aabb5a17467057ffdb8210cd3fd83a0b93e6ff41cd4e9
-size 10932
+oid sha256:d41fe694975672cb04ae93f0eb6ab92cb78f3315ccab08193dce4289bbd45255
+size 10944
