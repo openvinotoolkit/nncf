460a996f9230171a70c1a3fa69311bb1530013e1
Vasily Shamporov <vasily.shamporov@intel.com>
2020-10-07 10:40:10 +0300

    Fix third_party_sanity and eval test bugs (#169)

--- tests/test_sota_checkpoints.py
+++ tests/test_sota_checkpoints.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e480cc3f681b264b071379e7496a42781ae2fbfedf3e4ff31f79477caf1d8d4
-size 16900
+oid sha256:9ca05d9f3668c6ea99d3e96f5100253ae58fc75bb64137521d12a9cd1e277bc6
+size 17200
