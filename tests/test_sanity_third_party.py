33102fc6b42c9a1fe85f8e5e482f50f1abfdb542
Vasily Shamporov <vasily.shamporov@intel.com>
2020-10-08 15:54:04 +0300

    Fix mmdetection dataset search path for SSD (#176)

--- tests/test_sanity_third_party.py
+++ tests/test_sanity_third_party.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e97a190201fb10d1b02968b6b182e54d4e18f96545426488cb24cd2611414665
-size 17877
+oid sha256:91fc6488fdac71a9a9479adddc098cbf7ecdc95ba483f9a021f63f310a0badae
+size 18053
