bedc8149ecd9c3054af5e0b52a411679bb9d9f6b
Vasily Shamporov <vasily.shamporov@intel.com>
2020-10-12 19:37:35 +0300

    Fix security issues with imports (#185)

--- examples/object_detection/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4665e8e4067d24eafb46b4ecabe76626e21f2b740458d68b358c98bc5d15b2a2
-size 852
--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b90b1aea8b505a48fec31b5f907f1d2b1220a237dfea0d345bb35e00dcb4b5f
-size 5748
+oid sha256:d273c426cbea6595b80d2575679acade72815f317fa3e1fe27d75ae17072ffa1
+size 5711
