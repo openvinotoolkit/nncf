c817436988db7d7b176f741df19f3ec89e91c645
Vasily Shamporov <vasily.shamporov@intel.com>
2020-10-12 20:29:58 +0300

    Fix paths to COCO in mmdetection third party sanity tests (#186)

--- tests/test_sanity_third_party.py
+++ tests/test_sanity_third_party.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91fc6488fdac71a9a9479adddc098cbf7ecdc95ba483f9a021f63f310a0badae
-size 18053
+oid sha256:09ac0ef34015b8ab2fb29b34c1f53554d355b834f0c087c73b07c96e7533a2e2
+size 18262
