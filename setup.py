42bfe7d64478add96f77f57111e804c90bf57728
Vasily Shamporov <vasily.shamporov@intel.com>
2020-10-12 14:17:14 +0300

    Refactor imports in setup.py (#182)
    Important for CI

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1d075ea8a9bd844bbe3e0d24f15034e542611f7d7859c47ef8180511cc97299
-size 5766
+oid sha256:0b90b1aea8b505a48fec31b5f907f1d2b1220a237dfea0d345bb35e00dcb4b5f
+size 5748
