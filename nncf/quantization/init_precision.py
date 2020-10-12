8e2cdec22e21e486a8d943e5695af58fd9e34ade
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2020-10-12 14:16:31 +0300

    Fixed disabling gradients of quantizers for HAWQ (#184)

--- nncf/quantization/init_precision.py
+++ nncf/quantization/init_precision.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a4dc49fdce5ea31e8542599521b2d74a83e8e8b83a34d79539f772e32cef597
-size 38275
+oid sha256:2d1fb1cfb8943a24ee1b0ee59e6a6344ff0a15b29fcd65c8c4fac5fedbcd38c1
+size 39515
--- tests/quantization/test_precision_init.py
+++ tests/quantization/test_precision_init.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba99213ae9e95c9996db587aedb96b40fb37300e210d9335170910bf898e30aa
-size 26103
+oid sha256:be731a9734debafaf1f50f2bc197ecf3b6226ae8322d1e55769809108d116d32
+size 26876
