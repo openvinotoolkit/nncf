4abf228b2101fd98444824f9dc9c686d7234af34
Vasily Shamporov <vasily.shamporov@intel.com>
2020-10-14 11:41:09 +0300

    Return zero-valued torch.Tensor in CompressionLoss by default instead of int (#190)

--- nncf/compression_method_api.py
+++ nncf/compression_method_api.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f0db0cac32024380fda3a0caa2f7443662a198ef9c7a72603f149ddfb256877
-size 10918
+oid sha256:ba1e1f918483525e774b6568c247d23f7c09611cc00c4dd572155e3dfce6a32a
+size 10932
