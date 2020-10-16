b99225509b4dc7449c8de5d375de5d780a248629
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2020-10-16 13:09:50 +0300

    Removed double logging to stdout (#198)

--- nncf/nncf_logger.py
+++ nncf/nncf_logger.py
@@ -4,15 +4,13 @@ import sys
 NNCF_LOGGER_NAME = "nncf"
 
 logger = logging.getLogger(NNCF_LOGGER_NAME)
-_LOGGER_INITIALIZED = False
 
-if not _LOGGER_INITIALIZED:
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    fmt = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
-    stdout_handler.setFormatter(fmt)
-    stdout_handler.setLevel(logging.INFO)
-    logger.addHandler(stdout_handler)
-    logger.setLevel(logging.INFO)
+stdout_handler = logging.StreamHandler(sys.stdout)
+fmt = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
+stdout_handler.setFormatter(fmt)
+stdout_handler.setLevel(logging.INFO)
+logger.addHandler(stdout_handler)
+logger.setLevel(logging.INFO)
 
 
 def set_log_level(level):
