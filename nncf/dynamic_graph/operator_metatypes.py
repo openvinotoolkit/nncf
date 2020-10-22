0004862a2f60bedccce2eddc5dbd08ecfdf9fd88
vuiseng9 <vuiseng9@gmail.com>
2020-10-22 18:28:34 +0800

    Add operator metatypes for 'sigmoid' and 'add' operator (#209)
    * Add operator metatypes for 'sigmoid' and 'add' operator
    
    * remove trailing spaces
    
    Co-authored-by: Chua, Vui Seng <vui.seng.chua@intel.com>

--- nncf/dynamic_graph/operator_metatypes.py
+++ nncf/dynamic_graph/operator_metatypes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee9763306ecd753e3352641450397abc57658f3f6041ed2cd6e79eaefd610fb7
-size 20459
+oid sha256:729c3411badd0c6a7bcd5484dffc71c857dd3f5b15c500122dda6d921a47ab19
+size 20555
