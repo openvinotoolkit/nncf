c01646a92744de62e4b3787ba89ff1ea44f529f7
Aleksei Kashapov <aleksei.kashapov@intel.com>
2020-10-28 23:33:06 +0300

    Change quantization levels for SymmetricQuantizer from 255 to 256

--- nncf/quantization/layers.py
+++ nncf/quantization/layers.py
@@ -257,8 +257,6 @@ class SymmetricQuantizer(BaseQuantizer):
             level_high = 2 ** num_bits - 1
             level_low = 0
         levels = 2 ** num_bits
-        if is_weights:
-            levels -= 1
         return level_high, level_low, levels
 
     @property
