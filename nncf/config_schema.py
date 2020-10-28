a08511e5678f32f09a08cc84d558a44b7006856f
Anastasia Senina <Anastasia.Senina@intel.com>
2020-10-28 12:30:46 +0300

    Update documentation (#219)
    * Update documentation.
    
    * Update docs. Add dependencies for param to json schema.

--- nncf/config_schema.py
+++ nncf/config_schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fa293f7dfe17c2afeae0fdecc80daa5e6d63aa8da393dca6eef3fb4dda46dfa
-size 39663
+oid sha256:bdd95cd96369f748d02212339106fee731a76dbbc42c4c507c416f8888d67994
+size 39829
