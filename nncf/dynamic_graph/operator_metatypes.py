1e48a441269cb66f309d6ead2ad3acf6998a3a43
Vasily Shamporov <vasily.shamporov@intel.com>
2020-10-29 09:37:18 +0300

    Fix register_operator (#224)

--- nncf/dynamic_graph/operator_metatypes.py
+++ nncf/dynamic_graph/operator_metatypes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:729c3411badd0c6a7bcd5484dffc71c857dd3f5b15c500122dda6d921a47ab19
-size 20555
+oid sha256:b3f2fffadb5b6dcfbd9f78e60cc75af16e46d2cd3d0b1c942d1b6b0602fecf0e
+size 20725
