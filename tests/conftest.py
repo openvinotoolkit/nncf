8dc1819197dae5e56a33653dc670bf9a1ad32b48
Pavel Finashov <66466565+pfinashx@users.noreply.github.com>
2020-10-30 13:18:53 +0300

    Added sota eval tests via AC (#142)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad8edea8a3c9f1cc6e9bbbf1ee68058d9b3a80abb2a80e29b82c5807ecd86d05
-size 3494
+oid sha256:57f3dbe75a428b2b3e743684f83aa538f124c9f3ee3ed9cc96cfcfcf95df446e
+size 3996
--- tests/test_sota_checkpoints.py
+++ tests/test_sota_checkpoints.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:126234c901867d879cdf80cab802cb4e0419425a16c2faaeafae7f85c807af2a
-size 17194
+oid sha256:9a90be9325c822129433cd934509ad7d99d9265bca2d836c4ad9d91e9f1d8fa7
+size 22701
