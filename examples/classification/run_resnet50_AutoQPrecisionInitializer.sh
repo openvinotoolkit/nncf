fb98d1def2ef0b047c735c6f7a8655be8c917f0b
Chua, Vui Seng <vui.seng.chua@intel.com>
2020-10-29 20:50:04 -0700

    Minor update on Resnet50 AutoQ Config and Runner Script

--- examples/classification/run_resnet50_AutoQPrecisionInitializer.sh
+++ examples/classification/run_resnet50_AutoQPrecisionInitializer.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH=~/workdir/nncf_pytorch:~/workdir/autox
+export PYTHONPATH=/path/to/autox
 
 python main.py \
     -m test \
     --gpu-id 0 \
     --config configs/quantization/resnet50_imagenet_mixed_int_AutoQ_0.15.json \
     --data /data/dataset/imagenet/ilsvrc2012/torchvision \
-    --workers 16 \
+    --workers 6 \
     --log-dir ./resnet50_AutoQPrecisionInitializer_run
 
