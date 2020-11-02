f4d7b22107cf68bb157ebf707c70aac383fea671
Kaglinskaya <maria.kaglinskaya@intel.com>
2020-11-02 15:56:26 +0300

    Fix prune batch norm default

--- nncf/pruning/base_algo.py
+++ nncf/pruning/base_algo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b60dc7c80deb9d744f9787e4ab29f1eba7ebb24ef049b40fbabf140b4d69a2d
-size 13283
+oid sha256:5270987fbc1fb6a116b61e416de43d541092e2fa2078d5b04dfa4846f6737aa5
+size 13282
