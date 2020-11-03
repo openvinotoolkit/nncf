cf1d9e0e15465e74dc46f22710bb7846213830d4
krodyush <konstantin.rodyushkin@intel.com>
2020-11-03 11:56:45 +0300

    fix quantization range initialization in case of 1 scale channel (#241)
    fix quantization range initialization in case of 1 scale channel to avoid initialization only by single slice of data (data[0]) and ignoring the other (data[1], data[2],.....)

--- nncf/quantization/init_range.py
+++ nncf/quantization/init_range.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb9f7e31cda59198b4a3e2089567621a80f7d78ec872c729400760d54cb0b4c3
-size 10951
+oid sha256:7ddd8518d0c7a5c4207f2cd284b5dd495e8abc214e8a657287330e4cfd350ede
+size 11017
