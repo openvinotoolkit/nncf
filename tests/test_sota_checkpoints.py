2c4990fe91cb08f868f304259904930e2038ddd0
Pavel Finashov <66466565+pfinashx@users.noreply.github.com>
2020-11-03 14:32:29 +0300

    Fixed skip Openvino tests and preinstall (#246)

--- tests/test_sota_checkpoints.py
+++ tests/test_sota_checkpoints.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a90be9325c822129433cd934509ad7d99d9265bca2d836c4ad9d91e9f1d8fa7
-size 22701
+oid sha256:d4512e49bc0714fb8e951f9d5a4890c3bd95f04340c9e0fde540114981d10791
+size 22785
