7ddce6623bc21bbfe503ea0cef1e00c6b6f3aa60
Anastasia Senina <Anastasia.Senina@intel.com>
2020-11-03 13:21:40 +0300

    Add DW-conv to input quantizable op. (#220)

--- nncf/quantization/quantizer_propagation.py
+++ nncf/quantization/quantizer_propagation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e62d97a8b0ee562e3267d0affcefe281ef00c714cdbe5b9b82f9eb58b2a491ad
-size 71235
+oid sha256:f290442c2328dd91a0492d6c6c9cc07b6f3d596526cb1deb3bffd7daf4d47209
+size 71299
