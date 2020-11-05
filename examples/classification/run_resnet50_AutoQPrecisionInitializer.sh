f28a18100cd611ea8ea69c0754ef2b48d94a1cd9
Chua, Vui Seng <vui.seng.chua@intel.com>
2020-11-05 04:19:21 -0800

    Revise launcher of Automated Mixed-Precision Sample

--- examples/classification/run_resnet50_AutoQPrecisionInitializer.sh
+++ examples/classification/run_resnet50_AutoQPrecisionInitializer.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-export PYTHONPATH=/path/to/autox
-
 python main.py \
     -m test \
     --gpu-id 0 \
     --config configs/quantization/resnet50_imagenet_mixed_int_AutoQ_0.15.json \
-    --data /data/dataset/imagenet/ilsvrc2012/torchvision \
+    --data /path/to/imagenet/dataset/ \
     --workers 6 \
     --log-dir ./resnet50_AutoQPrecisionInitializer_run
 
