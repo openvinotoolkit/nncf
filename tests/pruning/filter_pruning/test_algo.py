841b9b84da0c8752645d6f2e366c9df1f6c07410
Kaglinskaya <maria.kaglinskaya@intel.com>
2020-11-06 17:44:42 +0300

    Fix test

--- tests/pruning/filter_pruning/test_algo.py
+++ tests/pruning/filter_pruning/test_algo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6206d1ff535f6401330c600de146e455096b4c7c859700d12a9a7d7f7c1b39ec
-size 9880
+oid sha256:6c736272f9d020a80cf92a0275347d560bc7799b0ee55d6e7d1abc0076e38b48
+size 9878
