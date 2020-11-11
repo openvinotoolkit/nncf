70a1e0378ae0d8653530451119e24a3c67df6720
skholkin <holckin100@gmail.com>
2020-11-11 15:25:03 +0300

    kernel chainges

--- examples/object_detection/layers/extensions/nms/nms_kernel.cpp
+++ examples/object_detection/layers/extensions/nms/nms_kernel.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:598b80966ebeb7e7b9ec9f2f4333f6084f3bb99e14bc50fa6cd4a69dacd8508f
-size 2306
+oid sha256:c274a0f7c88fe5e98ecb9ae4334597a775cced1b952db6cae852ed928743a04b
+size 2648
--- examples/object_detection/main.py
+++ examples/object_detection/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:609e9364bac2312ecd69c8b65e7cf14ce20470d43682fa6e4b5417deda59df4e
-size 15478
+oid sha256:db0c29827bd4e9b237a6d57a6c0f690be814c090eb3aa264cb38b3625ba454f2
+size 15604
