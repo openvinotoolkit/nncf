2eea6abc46506af1e2aaf42dc1431d100462859f
krodyush <konstantin.rodyushkin@intel.com>
2020-11-11 13:00:59 +0300

    make customer happy to see param name that is wrong (#259)

--- nncf/dynamic_graph/input_wrapping.py
+++ nncf/dynamic_graph/input_wrapping.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6da60818a323cb90b7ee702933b44d9e5f160fff7e4fdc8dba61c689ab1da60
-size 5051
+oid sha256:5b823d978f9e4851a8beed13bc14309ebbf0340e5e76004a21338968825368f8
+size 5049
