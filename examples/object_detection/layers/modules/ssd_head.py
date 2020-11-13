82bd2b0fd43fc428104eeafe7448ed83e9d3ef8e
Chua, Vui Seng <vui.seng.chua@intel.com>
2020-11-13 02:42:20 -0800

    Fix last linter warnings

--- examples/object_detection/layers/modules/ssd_head.py
+++ examples/object_detection/layers/modules/ssd_head.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1af7f6f8f6a513dafe05905a379635469955413ac978655a2f903387657f607
-size 4276
+oid sha256:588b5a84df8ff33fb01b7514d2c55cec8cc0ee0973e42c6fc8f9e8bc680f32cb
+size 4269
