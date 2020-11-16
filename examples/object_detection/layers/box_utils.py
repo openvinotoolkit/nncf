a0cacd7846f7e18f51f0bf9ffc428fd86149260a
skholkin <holckin100@gmail.com>
2020-11-16 10:27:37 +0300

    is cuda

--- examples/object_detection/layers/box_utils.py
+++ examples/object_detection/layers/box_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cf8e2282bc13b68c2d7b59331ec5daf3f403b8172411dff69e8679139f1ff76
-size 8305
+oid sha256:45035c7bc324486ecda2b363dbbfb92d0db480e42db4c4523f4862f4c9fd1fae
+size 8296
