581f45ee1b68673b94fa67576a13ece2402b586a
Vasily Shamporov <vasily.shamporov@intel.com>
2020-11-23 16:36:24 +0300

    Remove __getattr__ from SampleConfig (#292)
    Newer `addict` version uses custom private attributes for internal
    working and __getattr__ disrupted it. It was quite useless anyway.

--- examples/common/sample_config.py
+++ examples/common/sample_config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec044d7b96bbbb2f91cd7bfcc38b995e968d2f0796bfbad5ae389a00faf02381
-size 3958
+oid sha256:682693f5480ddb6c68baa0e9ecf729374e2cd7da456b550e87fa4d9cf732eba3
+size 3784
