799551fb40f597a068555a9f2de1164e229b4245
Pavel Finashov <66466565+pfinashx@users.noreply.github.com>
2020-11-23 11:20:36 +0300

    Fixed collecting metrics.json for incomplete eval test (#279)

--- tests/test_sota_checkpoints.py
+++ tests/test_sota_checkpoints.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f83c055ea4836fa6267c6511daf8a7285ebbb6bed6a4b9c65b6f51a4df0fc83
-size 26236
+oid sha256:1a33a3284aabdace9c09389e1d8a8b0c1faddc4895d34201e8905f7d6e5fe8ec
+size 26906
