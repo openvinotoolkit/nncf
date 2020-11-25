0e914be0faeb6c25a2915cb5343af9ef77455f4d
Ivan Lazarevich <ivan.lazarevich@intel.com>
2020-11-25 13:01:40 +0300

    Proper BN momentum parameter and train mode setting in BN adaptation (#288)
    * proper BN momenta parameter and train mode setting in BN adaptation
    
    * use training mode switcher context maganer for BN adaptation inference

--- nncf/initialization.py
+++ nncf/initialization.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc76672b0239dad79fc353e8b8d211ffe92799451bc3eab1f2115be1a30c428b
-size 10462
+oid sha256:0e13051441b44ab51afacaee69d62b03513fddc23b1b259eebbc4079fe96516c
+size 11114
