e429fed4e38d9a434f0da0ea38ad5707be77d520
Vasily Shamporov <vasily.shamporov@intel.com>
2020-11-27 11:26:47 +0300

    Remove .dot extension in the HW config test case descriptor (#303)

--- tests/test_compressed_graph.py
+++ tests/test_compressed_graph.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96c62c8816da0eb9b17301cf38d1464efa52e8f6be9b4b7d62e123ed72665f45
-size 37357
+oid sha256:bcbc04c2f194dc2051af6c8adb1d7548bea2df45f97b1a0859343faa5d9064e2
+size 37345
