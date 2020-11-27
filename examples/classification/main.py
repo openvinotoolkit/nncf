4151d1dad802e832b41cd154e584a1f01c6d8dac
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2020-11-27 20:25:28 +0300

    Don't call MLFlow if it's not enabled (#304)
    Required to avoid mlflow.exceptions.MlflowException: Could not create run under non-active experiment with ID 0.

--- examples/classification/main.py
+++ examples/classification/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc782123bf8e18880b4cc9d84a2d50eebe3cecd3d94320245d99c65bb3f348be
-size 21715
+oid sha256:39080f713f4a20f234f6b487bade9490d128f568c8d4c01b41dbc60faac9459d
+size 21866
--- examples/common/utils.py
+++ examples/common/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f0b0067f62b5f1a36cfcaa183bdc891bbf40405a498a97653b08e1acb3ad941
+oid sha256:353bf97e9143c8c245828394139cb37a224ae28653c71ea6303395a3d7fb31e5
 size 7829
