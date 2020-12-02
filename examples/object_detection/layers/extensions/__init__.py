6d1921c54378837b92f735f1d6a9d8d30321919a
Vasily Shamporov <vasily.shamporov@intel.com>
2020-12-02 18:18:05 +0300

    Fix NMS CUDA extensions import for CPU only case (#316)

--- examples/object_detection/layers/extensions/__init__.py
+++ examples/object_detection/layers/extensions/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c370fe77c4be2ac5e079d277a0d959741c1e74c4f7ff850733409cd3ea37c005
-size 416
+oid sha256:80f410669eec0dc3390f3b58cda6a69b5a3a2476191a987bae6c7a4eeeb0f6c5
+size 510
