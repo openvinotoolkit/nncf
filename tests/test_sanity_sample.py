e06cc5780f8927c5b9258fb497437888a7d5322c
Vasily Shamporov <vasily.shamporov@intel.com>
2020-12-04 19:04:52 +0300

    Use mock 32x32 dataset instead of actual CIFAR for sanity test runs (#322)

--- tests/test_sanity_sample.py
+++ tests/test_sanity_sample.py
@@ -135,7 +135,7 @@ def create_command_line(args, sample_type):
 SAMPLE_TYPES = ["classification", "semantic_segmentation", "object_detection"]
 
 DATASETS = {
-    "classification": ["cifar10", "cifar100", "cifar10", "cifar10"],
+    "classification": ["mock_32x32", "mock_32x32", "mock_32x32", "mock_32x32"],
     "semantic_segmentation": ["camvid", "camvid"],
     "object_detection": ["voc"],
 }
