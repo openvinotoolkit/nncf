661adf03a2137167ef916d04e550aa63dd024e08
Vasily Shamporov <vasily.shamporov@intel.com>
2020-12-04 17:43:25 +0300

    WA for Python 3.6 on CI (#321)

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e50e0427ae75108064a0da4f7fe24502f3accce400fef02289b43ecd01eb77c8
-size 5437
+oid sha256:f03de874cbc8e04dbb2a7ca05e5c6ebd11e0872946b9fdcb4c095c4be654607e
+size 5443
