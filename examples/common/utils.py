112baf084fc500523037869ee805a18630108bb1
Anastasia Senina <Anastasia.Senina@intel.com>
2020-12-08 10:19:36 +0300

    Fix calculate statistics in local mode sparsity. (#337)

--- examples/common/utils.py
+++ examples/common/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50d1fe2f30e45cf7cbb13eacfa784066075bfea11a25258c05fea6ba05d4917d
-size 9403
+oid sha256:6c4b9f5f56bf2037bc01d8c901180bf7105e1aec21b7878a85bc8a71698e2953
+size 9417
--- nncf/sparsity/magnitude/algo.py
+++ nncf/sparsity/magnitude/algo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8135a5692746b5fa381a8945b2cfac73367bbd5fce9cf4bb843527b55a0544c8
-size 5277
+oid sha256:4612de35c8dba00e265d6e2aade916692b3af57649a03adb773415106698b336
+size 5997
