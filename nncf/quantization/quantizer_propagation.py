a4a4a120f722c73030fc6e6e5d04cbddeb47f887
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2020-12-08 17:50:37 +0300

    Corrected grouping of activation quantizers (#339)
    Not merged FQ's for activations should be in different groups, if unmerged activation FQ on the branch goes directly after another FQ for activation (common input for different branches).
    start->FQ_A  Conv
            \   /
           POST_HOOK
             /    \
      PRE_HOOK    PRE_HOOK
        |           \
      div          MaxPool   here->|FQ_A|
                      \     /
                    POST_HOOK

--- nncf/quantization/quantizer_propagation.py
+++ nncf/quantization/quantizer_propagation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66685ba0ae6874ef6ade00c6a0c40d6772d2e3316d86fd58988786f1efbc95aa
-size 71584
+oid sha256:978c16eb404d1eeefa58e8ccde21d7e805742c8c58024c511d4a202503d0107f
+size 73240
