51b90f5b84dea6d6fb6d3e410ffe7f14d5dae8f0
Vasily Shamporov <vasily.shamporov@intel.com>
2020-12-09 13:10:49 +0300

    Revert "Fk/update packages versions (#338)" (#343)
    This reverts commit 8c17e0cd0b71dfddc2c33374804eec1f0434af56.

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f156c45153cb7e603e7f11d27a9c6bcf39787a5ab8830c4ac3889cddab8f39c9
-size 5609
+oid sha256:19900a30976b459d14ae98dfa498d42af278364d135191c0b6c69e050a1e6f85
+size 5449
