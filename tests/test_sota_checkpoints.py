fd1e462149104b07e1efd6428646e761cadbef1a
Pavel Finashov <66466565+pfinashx@users.noreply.github.com>
2020-12-10 17:21:16 +0300

    Added separated execution OV tests to start parraleling (#282)

--- tests/test_sota_checkpoints.py
+++ tests/test_sota_checkpoints.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:952182389642ccd43c740f834602b89094798401945e580edccfe9b07bd44a5a
-size 26906
+oid sha256:858d6351ed9511e3bf9a685d98b307ab9b3f2c1c0bd43eff15a3fc57e47a029b
+size 22933
