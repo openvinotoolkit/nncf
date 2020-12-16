941fdba7c80ee1f7506676c851edd59fd4105c49
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2020-12-16 15:15:50 +0300

    Renamed case-sensitive files to prevent git issue on Windows (#357)
    After checkout fresh develop, git confuses file with/without capital letter.
    As a result this file can't be discarded.
    `git config --global core.ignorecase true` doesn't work as well

--- tests/test_compressed_graph.py
+++ tests/test_compressed_graph.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:901a0b1c01bfec971883d7a0a156c38812813dee5abb9f647f470a94987fa21e
-size 38042
+oid sha256:bcbfcf4cdaa5956a21b507e8b8ccc772d2e849d552b42dd663476b95ef09b393
+size 38082
