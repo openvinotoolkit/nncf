7cff4589bdf317e494c0a0372b49a56a7bb75ab4
Aleksei Kashapov <aleksei.kashapov@intel.com>
2020-12-21 15:49:15 +0300

    Update mmdet patch (#354)
    * Update mmdet patch
    
    * Update configs and meta
    
    * Add export tests
    
    * Update test
    
    * Update package installation

--- tests/test_sanity_third_party.py
+++ tests/test_sanity_third_party.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d141925f56d82cafe5dc781c18b479886d4fd1ab158eef2fa4d2ca8a895d655e
-size 18927
+oid sha256:2431a81a5890d24c996e02806c80a6ec224263faaffb712497e7d693caebdab4
+size 20312
