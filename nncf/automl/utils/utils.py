a172c28a8eecbf5812d724a278e3b2bba42c8e13
Xian Fu, Wong <xian.fu.wong@intel.com>
2020-12-23 23:42:49 -0800

    Fix pylint error: nncf/automl/utils/utils.py

--- nncf/automl/utils/utils.py
+++ nncf/automl/utils/utils.py
@@ -1,17 +1,8 @@
-
 import torch
-import torch.nn as nn
-
-import math
-import numpy as np
-import pandas as pd
-
-from natsort import natsorted
-from collections import OrderedDict
+from torch.autograd import Variable
 
 USE_CUDA = torch.cuda.is_available()
 FLOAT = torch.cuda.FloatTensor if USE_CUDA else torch.FloatTensor
-from torch.autograd import Variable
 
 def to_numpy(var):
     # return var.cpu().data.numpy()
@@ -27,4 +18,3 @@ def to_tensor(ndarray, volatile=False, requires_grad=False, dtype=FLOAT):
 def sample_from_truncated_normal_distribution(lower, upper, mu, sigma, size=1):
     from scipy import stats
     return stats.truncnorm.rvs((lower-mu)/sigma, (upper-mu)/sigma, loc=mu, scale=sigma, size=size)
-
