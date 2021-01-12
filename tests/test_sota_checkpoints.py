b09eb35aa66317d398c6ee97f40b5415cc876a59
Pavel Finashov <pavelx.finashov@intel.com>
2021-01-12 11:01:42 +0300

    Added Q-DQ ONNX converting parameter (#369)

--- tests/test_sota_checkpoints.py
+++ tests/test_sota_checkpoints.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72dab201e6187e3ff35fbad72f1ce6f6f854f3bb37b0a49576a7c5a51233a1fe
-size 23260
+oid sha256:4c5a8aa4f8d4629fb27f23c1643e94cc6a374f4eded3deb7220b263fc42fb261
+size 25300
