59116450fc9b1fad3a482db8fdbfde3120631724
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2021-01-17 16:21:03 +0300

    Added proper license for DDPG-related code (#398)

--- nncf/automl/agent/ddpg/ddpg.py
+++ nncf/automl/agent/ddpg/ddpg.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:975276f6bb4dcf9f7fd9d180c6911a3d2a8c9d7c4e10477cdc9b15a3d91696b7
-size 10434
+oid sha256:7d1a07aa8b0793a8e5433e86b66c643afec73da84a663c8ac1260a0d3288dc73
+size 10234
--- nncf/automl/agent/ddpg/memory.py
+++ nncf/automl/agent/ddpg/memory.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d796153320640311eb3cee4fe28890f6aa3da8da89a7e335b7208515b0aedc52
-size 10463
+oid sha256:ec2700f55afc265dcfe99c4116e5a94ccb022b39930024b0efce810ff59626e8
+size 10335
