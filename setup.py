d2ec7b903bb95e012826d737ba24c77558cca958
Vasily Shamporov <vasily.shamporov@intel.com>
2021-01-19 11:26:16 +0300

    Return pandas to the list of requirements (#405)

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dcd7f37931ab51615e5cd550931f9aa0ad836c3644b89911ff3418ae6efa6c2
-size 5341
+oid sha256:229afcfc85e3b5ff129ac0cc24769c50d69d7f693bdf266ba99e23105ec185b5
+size 5378
