c538d5ca93d2b48275408479214e61b4c8f08286
Vasily Shamporov <vasily.shamporov@intel.com>
2021-01-19 23:26:27 +0300

    Relax requirements packages versions (#415)

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:229afcfc85e3b5ff129ac0cc24769c50d69d7f693bdf266ba99e23105ec185b5
-size 5378
+oid sha256:4a9965cc0dd4eb748e5e11546339e2ea9cb0443607ad1d7f210e312578819027
+size 5377
