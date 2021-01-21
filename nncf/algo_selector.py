7b5c46e42d02e611dc94862ace81977ed802dbff
Vasily Shamporov <vasily.shamporov@intel.com>
2021-01-21 14:43:23 +0300

    Fix NoCompressionAlgorithmBuilder (#426)

--- nncf/algo_selector.py
+++ nncf/algo_selector.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8584e97ceb31556aa39235ef4341a75f1694a28f7ada18c4b9f6e2c4dbbede43
-size 1410
+oid sha256:af21c30171f5142a63141cbdb8e4c6c18a0bf2e5437e7d2adfa2d26d8fe71da7
+size 1558
