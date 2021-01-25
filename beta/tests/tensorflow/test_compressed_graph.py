20ade1cec41b55c338996d1bddea7321b7e74eb9
Andrey Churkin <andrey.churkin@intel.com>
2021-01-25 16:40:52 +0300

    Graph test for Mask R-CNN was added

--- beta/tests/tensorflow/test_compressed_graph.py
+++ beta/tests/tensorflow/test_compressed_graph.py
@@ -16,6 +16,7 @@ import pytest
 from addict import Dict
 
 import tensorflow as tf
+import networkx as nx
 
 from beta.tests.tensorflow import test_models
 from beta.tests.tensorflow.helpers import get_empty_config, create_compressed_model_and_algo_for_test
@@ -36,22 +37,83 @@ def get_basic_quantization_config(qconfig, input_sample_sizes=None):
     return config
 
 
-def check_graph(graph, ref_graph_dir, ref_graph_file_name):
-    data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'reference_graphs')
-    graph_dir = os.path.join(data_dir, ref_graph_dir)
-    graph_path = os.path.abspath(os.path.join(graph_dir, ref_graph_file_name))
+def get_nx_graph_from_tf_graph(tf_graph: tf.Graph):
+    def _get_node_attributes(op: tf.Operation):
+        attr = {'op': op.type}
+        return attr
 
-    # validate file with graph manually!
-    if not os.path.exists(graph_path):
-        if not os.path.exists(graph_dir):
-            os.makedirs(graph_dir)
-        tf.io.write_graph(graph, graph_dir, ref_graph_file_name, as_text=False)
+    def _get_inbound_edges(op: tf.Operation):
+        inbound_edges = []
+        for input_tensor in op.inputs:
+            inbound_edges.append((input_tensor.op.name, op.name))
+        return inbound_edges
 
+    nodes = {}
+    edges = []
+    for op in tf_graph.get_operations():
+        op_name = op.name
+        nodes[op_name] = _get_node_attributes(op)
+        edges.extend(_get_inbound_edges(op))
+
+    nx_graph = nx.DiGraph()
+
+    for node_name, atrrs in nodes.items():
+        nx_graph.add_node(node_name, **atrrs)
+
+    for edge in edges:
+        nx_graph.add_edge(*edge)
+
+    return nx_graph
+
+
+def check_graph_def(graph_def, graph_path: str):
     expected_graph = tf.compat.v1.GraphDef()
     with open(graph_path, 'rb') as f:
         expected_graph.ParseFromString(f.read())
 
-    tf.test.assert_equal_graph_def(graph, expected_graph)
+    tf.test.assert_equal_graph_def(graph_def, expected_graph)
+
+
+def check_nx_graph(nx_graph: nx.DiGraph, graph_path: str):
+    expected_graph = nx.drawing.nx_pydot.read_dot(graph_path)
+
+    assert nx_graph.nodes.keys() == expected_graph.nodes.keys()
+
+    for node_name, node_attrs in nx_graph.nodes.items():
+        expected_attrs = expected_graph.nodes[node_name]
+        assert expected_attrs == node_attrs
+
+    assert nx.DiGraph(expected_graph).edges == nx_graph.edges
+
+
+def check_graph(tf_graph: tf.Graph, ref_graph_dir: str, ref_graph_filename: str):
+    data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'reference_graphs')
+    graph_dir = os.path.join(data_dir, ref_graph_dir)
+    graph_path = os.path.abspath(os.path.join(graph_dir, ref_graph_filename))
+
+    # validate file with graph manually!
+    ref_graph_not_exist = False
+    if not os.path.exists(graph_path):
+        if not os.path.exists(graph_dir):
+            os.makedirs(graph_dir)
+        ref_graph_not_exist = True
+
+    _, ref_graph_ext = os.path.splitext(ref_graph_filename)
+
+    if ref_graph_ext == '.pb':
+        graph_def = tf_graph.as_graph_def(add_shapes=True)
+
+        if ref_graph_not_exist:
+            tf.io.write_graph(graph_def, graph_dir, ref_graph_filename, as_text=False)
+
+        check_graph_def(graph_def, graph_path)
+    else:
+        nx_graph = get_nx_graph_from_tf_graph(tf_graph)
+
+        if ref_graph_not_exist:
+            nx.drawing.nx_pydot.write_dot(nx_graph, graph_path)
+
+        check_nx_graph(nx_graph, graph_path)
 
 
 class QuantizeTestCaseConfiguration:
@@ -94,17 +156,11 @@ def _sparsity_case_config(request):
 
 
 class ModelDesc:
-    def __init__(self, pb_filename: str, model_builder, input_sample_sizes):
-        self.model_name = self._get_model_name(pb_filename)
+    def __init__(self, ref_graph_filename: str, model_builder, input_sample_sizes):
+        self.model_name, _ = os.path.splitext(ref_graph_filename)
         self.model_builder = model_builder
-        self.pb_filename = pb_filename
+        self.ref_graph_filename = ref_graph_filename
         self.input_sample_sizes = input_sample_sizes
-
-    @staticmethod
-    def _get_model_name(dot_filename):
-        if isinstance(dot_filename, tuple):
-            dot_filename = dot_filename[0]
-        return dot_filename[:dot_filename.find('.pb')]
 
 
 SKIP_MAP = {
@@ -140,21 +196,22 @@ def get_test_models_desc(algorithm):
         ModelDesc('sequential_model.pb', test_models.SequentialModel, [1, 224, 224, 3]),
         ModelDesc('sequential_no_input_model.pb', test_models.SequentialModelNoInput, [1, 224, 224, 3]),
         ModelDesc('mobilenet_v3_small.pb', test_models.MobileNetV3Small, [1, 32, 32, 3]),
-        ModelDesc('shared_layers_model.pb', test_models.SharedLayersModel, [1, 30, 30, 3])
+        ModelDesc('shared_layers_model.pb', test_models.SharedLayersModel, [1, 30, 30, 3]),
+        ModelDesc('mask_rcnn.dot', test_models.MaskRCNN, [1, 1024, 1024, 3]),
     ]
 
 
-def keras_model_to_graph_def(model):
+def keras_model_to_tf_graph(model):
     input_signature = []
     for item in model.inputs:
         input_signature.append(tf.TensorSpec(item.shape, item.dtype))
     concrete_function = tf.function(model).get_concrete_function(input_signature)
-    return concrete_function.graph.as_graph_def(add_shapes=True)
+    return concrete_function.graph
 
 
-def check_model_graph(compressed_model, ref_graph_file_name, ref_graph_dir):
-    compressed_graph = keras_model_to_graph_def(compressed_model)
-    check_graph(compressed_graph, ref_graph_dir, ref_graph_file_name)
+def check_model_graph(compressed_model, ref_graph_filename, ref_graph_dir):
+    compressed_graph = keras_model_to_tf_graph(compressed_model)
+    check_graph(compressed_graph, ref_graph_dir, ref_graph_filename)
 
 
 class TestModelsGraph:
@@ -170,7 +227,7 @@ class TestModelsGraph:
                                                input_sample_sizes=desc.input_sample_sizes)
         compressed_model, _ = create_compressed_model_and_algo_for_test(model, config)
 
-        check_model_graph(compressed_model, desc.pb_filename, _quantization_case_config.graph_dir)
+        check_model_graph(compressed_model, desc.ref_graph_filename, _quantization_case_config.graph_dir)
 
     @pytest.mark.parametrize(
         'desc', get_test_models_desc('magnitude_sparsity'), ids=[
@@ -184,7 +241,7 @@ class TestModelsGraph:
         config['compression']['params'] = {'schedule': 'multistep'}
         compressed_model, _ = create_compressed_model_and_algo_for_test(model, config)
 
-        check_model_graph(compressed_model, desc.pb_filename, _sparsity_case_config.graph_dir)
+        check_model_graph(compressed_model, desc.ref_graph_filename, _sparsity_case_config.graph_dir)
 
 
 QUANTIZE_OUTPUTS = [
@@ -203,4 +260,4 @@ def test_quantize_outputs(desc: ModelDesc, _quantization_case_config):
     config['compression']['quantize_outputs'] = True
     compressed_model, _ = create_compressed_model_and_algo_for_test(model, config)
 
-    check_model_graph(compressed_model, desc.pb_filename, _quantization_case_config.graph_dir)
+    check_model_graph(compressed_model, desc.ref_graph_filename, _quantization_case_config.graph_dir)
