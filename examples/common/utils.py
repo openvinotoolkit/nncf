5c31d5b92d329ac939eedc560a0d7554b408e9f0
Pavel Finashov <pavelx.finashov@intel.com>
2021-01-26 11:11:12 +0300

    Changed working logic with json metrics (#447)

--- examples/common/utils.py
+++ examples/common/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b41fa2bf1c322bd0488577b4bda124bde5f4e5302a83ebce303549cd2adcd5b
-size 9998
+oid sha256:809fd5b415a4371cac998b57de443782270c55c316c0abf8772b5c01bc2b9216
+size 9882
