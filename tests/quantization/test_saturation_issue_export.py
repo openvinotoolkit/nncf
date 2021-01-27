01d9b30dc96d71165f2611e9993cfeb5d7848e80
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-01-27 23:09:15 +0300

    Fix PyLint

--- tests/quantization/test_saturation_issue_export.py
+++ tests/quantization/test_saturation_issue_export.py
@@ -145,14 +145,14 @@ class EightConvTestModel(nn.Module):
     def __init__(self, in_out_ch):
         super().__init__()
         self.features = []
-        self.features.append(nn.Sequential(create_conv(*in_out_ch[0], 2, -1, -2)))
-        self.features.append(nn.Sequential(create_conv(*in_out_ch[1], 5, 1, 1)))
-        self.features.append(nn.Sequential(create_conv(*in_out_ch[2], 1, 2, 2)))
-        self.features.append(nn.Sequential(create_conv(*in_out_ch[3], 9, -1, 0)))
-        self.features.append(nn.Sequential(create_conv(*reversed(in_out_ch[3]), 3, 0, 1)))
-        self.features.append(nn.Sequential(create_conv(*reversed(in_out_ch[2]), 1, -1, 9)))
-        self.features.append(nn.Sequential(create_conv(*reversed(in_out_ch[1]), 2, 10, 1)))
-        self.features.append(nn.Sequential(create_conv(*reversed(in_out_ch[0]), 1, 1, 1)))
+        self.features.append(create_conv(*in_out_ch[0], 2, -1, -2))
+        self.features.append(create_conv(*in_out_ch[1], 5, 1, 1))
+        self.features.append(create_conv(*in_out_ch[2], 1, 2, 2))
+        self.features.append(create_conv(*in_out_ch[3], 9, -1, 0))
+        self.features.append(create_conv(*reversed(in_out_ch[3]), 3, 0, 1))
+        self.features.append(create_conv(*reversed(in_out_ch[2]), 1, -1, 9))
+        self.features.append(create_conv(*reversed(in_out_ch[1]), 2, 10, 1))
+        self.features.append(create_conv(*reversed(in_out_ch[0]), 1, 1, 1))
         self.features = nn.Sequential(*self.features)
 
     def forward(self, x):
@@ -196,13 +196,13 @@ def test_are_fq_exported_per_channel_weights_tensors_clipped(tmp_path):
                 if node.attribute[0].t.dims[0] == 1 and node.attribute[0].t.dims[1] == 3 and \
                         node.attribute[0].t.dims[2] == 1 and node.attribute[0].t.dims[3] == 1:
                     onnx_conv_weight_tensors.append(numpy_helper.to_array(node.attribute[0].t))
-        except:
+        except (AttributeError, IndexError):
             continue
 
     level_positive_negative_ratio = 64. / 63.
 
-    for quantizer, filter in zip(quantizers, onnx_conv_weight_tensors):
-        for scale, channel in zip(quantizer.quantizer_module_ref.scale, filter):
+    for quantizer, conv_filter in zip(quantizers, onnx_conv_weight_tensors):
+        for scale, channel in zip(quantizer.quantizer_module_ref.scale, conv_filter):
             negative_max_val = level_positive_negative_ratio * scale
             assert scale >= np.max(channel)
             assert -negative_max_val <= np.min(channel)
@@ -230,7 +230,7 @@ def test_are_qdq_exported_per_tensor_weights_tensors_clipped(tmp_path):
     second_quantizer.quantized_module.weight.data = 2000 * torch.ones_like(
         second_quantizer.quantized_module.weight)
 
-    onnx_checkpoint_path = str('/home/aleksei/model.onnx')
+    onnx_checkpoint_path = str(tmp_path / 'model.onnx')
     compression_ctrl.export_model(onnx_checkpoint_path, input_names=['input'])
 
     onnx_model = onnx.load(onnx_checkpoint_path)
@@ -285,4 +285,4 @@ def test_is_pytorch_output_the_same_as_onnx_qdq_saturation_fix_applied(tmp_path)
         input_name = sess.get_inputs()[0].name
         onnx_out = sess.run(None, {input_name: input_tensor.astype(np.float32)})[0]
 
-        assert (np.allclose(torch_out.numpy(), onnx_out))
+        assert np.allclose(torch_out.numpy(), onnx_out)
