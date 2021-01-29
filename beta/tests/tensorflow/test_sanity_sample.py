5fde8c5861acb424ae818130c5ed11271f07d845
Alexander Suslov <alexander.suslov@intel.com>
2021-01-29 14:09:24 +0300

    disable the sanity test for segmentation example (#460)

--- beta/tests/tensorflow/test_sanity_sample.py
+++ beta/tests/tensorflow/test_sanity_sample.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf187fe3a81de8113ad2a179cdecbeb6f57f5db90c2bf055cfdceb2f0865f065
-size 12411
+oid sha256:f2a04d8bf61fad4525f16ba18a07d2f898757fa093d7795b74aacd7f493a37f2
+size 12525
