b2efe44ce13425cd45cdf4d97e6707121fd4a24b
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-02-03 13:30:02 +0300

    Improve a test (#481)

--- tests/quantization/test_onnx_export.py
+++ tests/quantization/test_onnx_export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7912310eb4475537bb8fbfd3445f39686af70b7f7b71ed2a9689f28a4ee6ee4
-size 6191
+oid sha256:42371dcd6f8e0196ba29311c549077d0c2bac1d02d76e4b510b034bf08df2ffc
+size 6193
