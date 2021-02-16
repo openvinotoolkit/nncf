21485b83b1464f9c877c4217c9d74e9aa155b446
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2021-02-16 19:11:55 +0300

    Fixed pylint issue in DDPG (#508)
    Issue message: [E1130(invalid-unary-operand-type), DDPG.update_policy] bad operand type for unary -: tuple

--- nncf/automl/agent/ddpg/ddpg.py
+++ nncf/automl/agent/ddpg/ddpg.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad2e08ab73b54f8ba9b046f8a22fe62c9458343817893cbde54273a0e00d4e6b
-size 10223
+oid sha256:6879198b3563410590a472f41d24ad2c14996275e325ed6ffb3f1bf1130a767d
+size 10229
