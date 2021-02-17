44416bce55219442d3cf055abeb787aa907e7ab0
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2021-02-17 17:21:10 +0300

    Added headers file to the snapshot for correct kernels building

--- beta/examples/tensorflow/common/utils.py
+++ beta/examples/tensorflow/common/utils.py
@@ -106,7 +106,7 @@ def configure_logging(sample_logger, config):
     nncf_logger.addHandler(nncf_log_file_handler)
 
 
-def create_code_snapshot(root, dst_path, extensions=(".py", ".json", ".cpp", ".cu")):
+def create_code_snapshot(root, dst_path, extensions=(".py", ".json", ".cpp", ".cu", "h", ".cuh")):
     """Creates tarball with the source code"""
     with tarfile.open(str(dst_path), "w:gz") as tar:
         for path in Path(root).rglob("*"):
