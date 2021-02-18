23180a5350feb1ac4668852dfab544730776b98c
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2021-02-18 23:16:01 +0300

    Added headers file to the snapshot for correct kernels building (#515)

--- beta/examples/tensorflow/common/utils.py
+++ beta/examples/tensorflow/common/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b353efdbb398d4ab3c68fd1c4423baa396ff26a603307bc412034b8f56cc0544
-size 6896
+oid sha256:f04bba12f53f74ff2afacd5f2a2d90a829798605cc7cda5735eacfc3fc6e584a
+size 6909
--- examples/common/utils.py
+++ examples/common/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b203954158e90c62f05845536d439fa57f276ff08f917b0a58121bd87445893
-size 9890
+oid sha256:f1ce95b689902adf66827732a0a9b1efc7aad43bbb7fd3fc62512b8ac3b5b5c3
+size 9904
