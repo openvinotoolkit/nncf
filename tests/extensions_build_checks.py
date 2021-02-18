b9acb7b7ed1293085638d507a2fe08be739bcf97
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-02-18 11:26:36 +0300

    Add a fixture creating venv

--- tests/extensions_build_checks.py
+++ tests/extensions_build_checks.py
@@ -1,8 +1,27 @@
+import os
 import sys
-from .test_extensions_build import test_if_cuda_was_built_without_gpu
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        raise RuntimeError("Must be run with torch extension build directory")
-    torch_build_dir = sys.argv[1]
-    test_if_cuda_was_built_without_gpu(torch_build_dir)
+        raise RuntimeError('Must be run with target extensions build mode')
+    mode = sys.argv[1]
+    if mode == 'cpu':
+        # Do not remove - the import here is for testing purposes.
+        # pylint: disable=wrong-import-position
+        from nncf import force_build_cpu_extensions
+
+        # pylint: enable=wrong-import-position
+
+        force_build_cpu_extensions()
+    elif mode == 'cuda':
+        # pylint: disable=wrong-import-position
+        from nncf import force_build_cuda_extensions
+
+        # pylint: enable=wrong-import-position
+
+        # Set CUDA Architecture
+        # See cmake/Modules_CUDA_fix/upstream/FindCUDA/select_compute_arch.cmake
+        os.environ['TORCH_CUDA_ARCH_LIST'] = '7.5+PTX'
+        force_build_cuda_extensions()
+    else:
+        raise RuntimeError('Invalid mode type!')
--- tests/test_extensions_build.py
+++ tests/test_extensions_build.py
@@ -1,57 +1,55 @@
-import os
 import subprocess
-import sys
+import pytest
 import shutil
 import pathlib
 
-from tests.conftest import TEST_ROOT, PROJECT_ROOT
+from tests.conftest import TEST_ROOT
 
 EXTENSIONS_BUILD_FILENAME = 'extensions_build_checks.py'
 
 
-def test_force_cuda_build(tmp_path):
-    venv_path = tmp_path / 'venv'
-    venv_path.mkdir()
-
-    ext_path = tmp_path / 'extensions'
-    export_env_variables = "export CUDA_VISIBLE_DEVICES='' export TORCH_EXTENSIONS_DIR={}".format(ext_path)
-
-    python_executable_with_venv = ". {0}/bin/activate && {1} && {0}/bin/python".format(venv_path, export_env_variables)
-
-    version_string = "{}.{}".format(sys.version_info[0], sys.version_info[1])
-
-    subprocess.call("virtualenv -ppython{} {}".format(version_string, venv_path), shell=True)
-
-    subprocess.run(
-        "{python} {nncf_repo_root}/setup.py develop".format(
-            python=python_executable_with_venv,
-            nncf_repo_root=PROJECT_ROOT),
-        check=True,
-        shell=True,
-        cwd=PROJECT_ROOT)
-
-    run_path = tmp_path / 'run'
-    run_path.mkdir()
-
-    shutil.copy(TEST_ROOT / EXTENSIONS_BUILD_FILENAME, run_path)
-
-    subprocess.run(
-        "{} {}/extensions_build_checks.py {}".format(python_executable_with_venv, run_path, ext_path),
-        check=True, shell=True, cwd=run_path)
+@pytest.fixture(name="venv_type",
+                params=["venv"])
+def venv_type_(request):
+    return request.param
 
 
-def test_if_cuda_was_built_without_gpu(torch_build_dir):
+@pytest.fixture(name="package_type",
+                params=["develop"])
+def package_type_(request):
+    return request.param
+
+@pytest.fixture(name="install_type",
+                params=["cuda"])
+def install_type_(request):
+    return request.param
+
+
+def test_force_cuda_build(create_venv, install_type, tmp_path, package_type):
     '''
     Check that CUDA Extensions weren't initially built and \
     then with TORCH_CUDA_ARCH_LIST were forced to be built
     '''
+
+    venv_path = tmp_path / 'venv'
+
+    torch_build_dir = tmp_path / 'extensions'
+    export_env_variables = "export CUDA_VISIBLE_DEVICES='' export TORCH_EXTENSIONS_DIR={}".format(torch_build_dir)
+
+    python_executable_with_venv = ". {0}/bin/activate && {1} && {0}/bin/python".format(venv_path, export_env_variables)
+
+    run_path = tmp_path / 'run'
+
+    shutil.copy(TEST_ROOT / EXTENSIONS_BUILD_FILENAME, run_path)
+
     torch_ext_dir = pathlib.Path(torch_build_dir)
     assert not torch_ext_dir.exists()
 
-    # Do not remove - the import here is for testing purposes.
-    # pylint: disable=wrong-import-position
-    from nncf import force_build_cuda_extensions
-    # pylint: enable=wrong-import-position
+    mode = 'cpu'
+
+    subprocess.run(
+        "{} {}/extensions_build_checks.py {}".format(python_executable_with_venv, run_path, mode),
+        check=True, shell=True, cwd=run_path)
 
     cpu_ext_dir = (torch_ext_dir / 'quantized_functions_cpu')
     assert cpu_ext_dir.exists()
@@ -63,10 +61,11 @@ def test_if_cuda_was_built_without_gpu(torch_build_dir):
     cuda_ext_so = (cuda_ext_dir / 'quantized_functions_cuda.so')
     assert not cuda_ext_so.exists()
 
-    # Set CUDA Architecture
-    # See cmake/Modules_CUDA_fix/upstream/FindCUDA/select_compute_arch.cmake
-    os.environ['TORCH_CUDA_ARCH_LIST'] = '7.5+PTX'
-    force_build_cuda_extensions()
+    mode = 'cuda'
+
+    subprocess.run(
+        "{} {}/extensions_build_checks.py {}".format(python_executable_with_venv, run_path, mode),
+        check=True, shell=True, cwd=run_path)
 
     cuda_ext_dir = (torch_ext_dir / 'quantized_functions_cuda')
     assert cuda_ext_dir.exists()
