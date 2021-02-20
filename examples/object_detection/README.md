f5a6b5c92042a8a4a6b5d3db90c6f1b929c8943b
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-02-20 11:13:37 +0300

    Remove torchvision from dependency list (#504)
    * Remove torchvision from dependency list
    
    * Fix bug
    
    * Add installation part to samples readme
    
    * Update install instruction

--- examples/object_detection/README.md
+++ examples/object_detection/README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b7164746ab9007c77e899341be3ce5bfd968a24b4388feab9ec0b0cadd47bef
-size 5374
+oid sha256:70101f0ceac1d8fc88aa4656f54de2c5afa8bb45f985780cee9c4fae0c2a1d03
+size 5531
--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c23f96f7b55f532a27f491a09b77a6e4597ce07ea557c24cd515101af97de51
-size 5300
+oid sha256:6c7ae8387754d83dbcd976c1cba13f18396bdc03858651bffd6437d4d3e9b4af
+size 4738
