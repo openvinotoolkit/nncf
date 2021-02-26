fe8d0fac90ade7f2972a00d82784388da787aa8e
Timur Gadylshin <timur.gadylshin@intel.com>
2021-02-26 15:30:17 +0500

    Explicit SPARSITY_LAYERS

--- beta/nncf/tensorflow/sparsity/magnitude/algorithm.py
+++ beta/nncf/tensorflow/sparsity/magnitude/algorithm.py
@@ -33,7 +33,6 @@ from beta.nncf.tensorflow.graph.utils import collect_wrapped_layers
 from beta.nncf.tensorflow.graph.utils import get_custom_layers
 from beta.nncf.tensorflow.graph.utils import get_original_name_and_instance_index
 from beta.nncf.tensorflow.graph.utils import get_weight_node_name
-from beta.nncf.tensorflow.layers.common import LAYERS_WITH_WEIGHTS
 from beta.nncf.tensorflow.layers.common import WEIGHT_ATTR_NAME
 from beta.nncf.tensorflow.sparsity.magnitude.functions import calc_magnitude_binary_mask
 from beta.nncf.tensorflow.sparsity.magnitude.functions import WEIGHT_IMPORTANCE_FUNCTIONS
@@ -43,7 +42,18 @@ from beta.nncf.tensorflow.sparsity.utils import convert_raw_to_printable
 from beta.nncf.tensorflow.sparsity.utils import strip_model_from_masks
 from beta.nncf.tensorflow.utils.node import is_ignored
 
-SPARSITY_LAYERS = LAYERS_WITH_WEIGHTS
+SPARSITY_LAYERS = {
+    'Conv1D': {WEIGHT_ATTR_NAME: 'kernel'},
+    'Conv2D': {WEIGHT_ATTR_NAME: 'kernel'},
+    'DepthwiseConv2D': {WEIGHT_ATTR_NAME: 'depthwise_kernel'},
+    'Conv3D': {WEIGHT_ATTR_NAME: 'kernel'},
+    'Conv2DTranspose': {WEIGHT_ATTR_NAME: 'kernel'},
+    'Conv3DTranspose': {WEIGHT_ATTR_NAME: 'kernel'},
+    'Dense': {WEIGHT_ATTR_NAME: 'kernel'},
+    'Embedding': {WEIGHT_ATTR_NAME: 'embeddings'},
+    'LocallyConnected1D': {WEIGHT_ATTR_NAME: 'kernel'},
+    'LocallyConnected2D': {WEIGHT_ATTR_NAME: 'kernel'}
+}
 
 
 @TF_COMPRESSION_ALGORITHMS.register('magnitude_sparsity')
