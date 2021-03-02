1defe997feaf1394b3a6748e87fcf0016777a75b
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2021-03-02 11:57:48 +0300

    Optional parameters support on loading PT checkpoint

--- nncf/quantization/layers.py
+++ nncf/quantization/layers.py
@@ -19,6 +19,7 @@ import torch.nn as nn
 from functools import partial
 from torch import distributed
 
+from nncf.checkpoint_loading import OPTIONAL_PARAMETERS_REGISTRY
 from nncf.debug import is_debug
 from nncf.functions import clamp
 from nncf.common.utils.logger import logger as nncf_logger
@@ -72,12 +73,14 @@ class BaseQuantizer(nn.Module):
         self._signedness_to_force = qspec.signedness_to_force
         self._is_using_log_scale_storage = qspec.logarithm_scale
         self._num_bits = nn.Parameter(torch.IntTensor([qspec.num_bits]), requires_grad=False)
+        OPTIONAL_PARAMETERS_REGISTRY.register('_num_bits')
         self.level_high = None
         self.level_low = None
 
         self.levels = 0
-
-        self.register_buffer('enabled', torch.IntTensor([1]))
+        ENABLED_VAR_NAME = 'enabled'
+        self.register_buffer(ENABLED_VAR_NAME, torch.IntTensor([1]))
+        OPTIONAL_PARAMETERS_REGISTRY.register(ENABLED_VAR_NAME)
         self.initialized = False
         self.state_dict_name = None
         self.call_count = 0
