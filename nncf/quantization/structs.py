697f4dd375f4637d210883ece742e93950202038
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2021-03-04 22:45:13 +0300

    Removed QuantizersBetweenQuantizableLayers

--- nncf/quantization/structs.py
+++ nncf/quantization/structs.py
@@ -1,5 +1,6 @@
+from typing import List
+
 import torch
-from typing import List, Set
 
 from nncf.nncf_network import InsertionPoint
 from nncf.quantization.layers import BaseQuantizer
@@ -18,25 +19,3 @@ class WeightQuantizerInfo:
                  quantized_module: torch.nn.Module):
         self.quantizer_module_ref = quantizer_module_ref
         self.quantized_module = quantized_module
-
-
-class QuantizersBetweenQuantizableLayers:
-    """ Contains locations of quantizers between inputs quantizable layers: input agnostic operation execution context
-    for activations and scope - for quantized modules """
-
-    def __init__(self):
-        self.activation_quantizer_insertion_points = set()  # type: Set[InsertionPoint]
-        self.quantized_module_scopes = set()  # type: Set['Scope']
-
-    def add_activation_quantizer_insertion_point(self, ip: InsertionPoint):
-        self.activation_quantizer_insertion_points.add(ip)
-
-    def add_quantized_module_scope(self, scope: 'Scope'):
-        self.quantized_module_scopes.add(scope)
-
-    def __bool__(self) -> bool:
-        return bool(self.activation_quantizer_insertion_points) and bool(self.quantized_module_scopes)
-
-    def update(self, other: 'QuantizersBetweenQuantizableLayers'):
-        self.activation_quantizer_insertion_points.update(other.activation_quantizer_insertion_points)
-        self.quantized_module_scopes.update(other.quantized_module_scopes)
