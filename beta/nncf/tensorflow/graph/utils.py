1f1e1cd0264e71224d93da87301cc4d6b6d71ee0
Evgeny Tsykunov <evgeny.tsykunov@intel.com>
2021-03-09 12:34:56 +0300

    [TF] Custom activation fix (#557)
    * fix for https://github.com/openvinotoolkit/nncf/issues/547

--- beta/nncf/tensorflow/graph/utils.py
+++ beta/nncf/tensorflow/graph/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43401c68d65e0f4fd6ceb14e6fe011d27b82298231a2341bfb6fecbe604eee19
-size 4347
+oid sha256:4031fcf4a6e85c23138e73ddaea352f39391713617d0046e072f90739a5f0c13
+size 4330
