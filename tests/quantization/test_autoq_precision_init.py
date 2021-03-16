b0d90116845fdd1140497fb760c7d052f8b0ae9b
Vasily Shamporov <vasily.shamporov@intel.com>
2021-03-16 13:59:38 +0300

    Fix model creation in AutoQ and HAWQ tests (#576)

--- tests/quantization/test_autoq_precision_init.py
+++ tests/quantization/test_autoq_precision_init.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa26273b9a2b4a301afcd8c236f0826850477d935531f158aa955c899a86ef52
-size 9005
+oid sha256:57cafc566661a99319a1639bea7aca538e9fae4b4d9f8f848e86ba9d5af1faca
+size 9063
