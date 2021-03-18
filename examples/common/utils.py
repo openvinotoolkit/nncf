281917ca48aade51dc4183d1b14382f3ffd30621
Pavel Finashov <pavelx.finashov@intel.com>
2021-03-18 10:48:34 +0300

    Fixed re-write metrics in json (#582)

--- examples/common/utils.py
+++ examples/common/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1ce95b689902adf66827732a0a9b1efc7aad43bbb7fd3fc62512b8ac3b5b5c3
-size 9904
+oid sha256:3a82798118ecdb76c556d90253ab2a63e75c272ee0597c23d060ee02d678ba78
+size 9759
