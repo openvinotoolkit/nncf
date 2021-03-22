1318b53a7bee0d9b1638568247d3ca07bbd466f5
Ivan Lazarevich <ivan.lazarevich@intel.com>
2021-03-22 15:42:33 +0300

    use torch.cdist function to compute pairwise distances (#590)

--- nncf/pruning/filter_pruning/functions.py
+++ nncf/pruning/filter_pruning/functions.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc0e82318ed938b923d16d7ab7d1dd05493738310feebdf922b3354ac7bf6090
-size 2379
+oid sha256:84e53e598cc2e41d6c15d5f0e988c3a0dcb8355eaef0d96021ba0a6259611db4
+size 2107
