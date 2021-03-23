ac98518ddffc63af8c0de2c4dfc45b1fbcfcff92
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-03-24 02:09:41 +0300

    Add skipping test if machine doesn't have CUDA (#595)

--- tests/test_extensions_build.py
+++ tests/test_extensions_build.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:817769d1e71e18145a65f3f69c82f38cda7e0e0fad8a963dff88f37db666915c
-size 2582
+oid sha256:ce148f62d7b668f27b02e3312828ad6d8a501aaf5eea6b2e6f6ea4d970079d3b
+size 2704
