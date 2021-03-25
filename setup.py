eb0244b3473d09ccb2f3c203bd714d4609196fe2
Alexey Ignatyev <aleksei.ignatev@intel.com>
2021-03-25 14:00:28 +0300

    Add pip support (#599)
    * Add pip support
    
    * Fix supported python version

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c23f96f7b55f532a27f491a09b77a6e4597ce07ea557c24cd515101af97de51
-size 5300
+oid sha256:21547d20d2a7c848a353f2abf2ae8cdb51a70b790801f441bb2c5fd40bc706eb
+size 5306
