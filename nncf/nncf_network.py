d0b309c536263534b765d092b704467ffc489d77
Vasily Shamporov <vasily.shamporov@intel.com>
2021-03-25 10:22:40 +0300

    Fix tensor stripping for torch <1.7.0 (#594)

--- nncf/nncf_network.py
+++ nncf/nncf_network.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbae278c42ce9907de9496f5f8f1770f098d095ac153ffee33be8d774e2f6b66
-size 40376
+oid sha256:b87d25d374a466844e09e0b82f84d1cf06d651950083c7007db994569ec1f71d
+size 40568
