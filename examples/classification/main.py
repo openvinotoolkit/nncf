e010c03120c188d88d9c8aa7a6aa0dcc1fff2257
Ivan Lazarevich <ivan.lazarevich@intel.com>
2021-03-26 18:57:48 +0300

    make training settings sensible for CIFAR10/100 (#602)

--- examples/classification/main.py
+++ examples/classification/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2215523f4c06d26f26c35bf80eef922f06f8d51dd4d44ae27a405aad39b7434d
-size 22547
+oid sha256:875cf4056f98d1b8c81a94e676b765b2713b88bf08640d065a178cf009af0d15
+size 22994
