712f3e67194f0893e5c26e97b35d5c98c959cd66
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-03-30 15:43:26 +0300

    Cuda extension fix (#598)
    * Fix test
    
    * Update test for only CPU-machine

--- tests/test_extensions_build.py
+++ tests/test_extensions_build.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce148f62d7b668f27b02e3312828ad6d8a501aaf5eea6b2e6f6ea4d970079d3b
-size 2704
+oid sha256:88758b5a99249b4549111d66623dfbca5c8700f267c0a581ad1f62f617d789cd
+size 3216
