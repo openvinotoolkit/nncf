e0a33d4d74d5c979f5445da89a0167d52ac35228
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2021-04-02 23:14:53 +0300

    linter

--- nncf/pruning/export_helpers.py
+++ nncf/pruning/export_helpers.py
@@ -10,7 +10,6 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 """
-from typing import Tuple
 from typing import Union
 from typing import List
 
