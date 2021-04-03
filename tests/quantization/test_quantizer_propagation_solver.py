dc68c17ec0764cf7cccc4879cb1adfa0f0b2f22a
Vasily Shamporov <vasily.shamporov@intel.com>
2021-04-03 10:01:12 +0300

    Switch gnc_graph calls in test to using positional args (#626)
    The random_index mechanism in networkx looks sketchy as it only
    inspects args for a seed value (aka "random_state"), and not kwargs.
    This is a workaround for "random_state_index is incorrect" errors.

--- tests/quantization/test_quantizer_propagation_solver.py
+++ tests/quantization/test_quantizer_propagation_solver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1775610a75d40bee3956550e495c02b89dd7fa7922b0dc8baf68e86dca154502
-size 90366
+oid sha256:336b37034ef14aad4d4db2231141a7f412afa0ad650ecc2a652db508629c8035
+size 90367
