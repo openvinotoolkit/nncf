078034fc42392ffa64c2029ec0a0497f088f196a
Andrey Churkin <andrey.churkin@intel.com>
2021-04-05 11:42:51 +0300

    Load optimizer state from the checkpoint only when the training mode is enabled (#632)

--- examples/classification/staged_quantization_worker.py
+++ examples/classification/staged_quantization_worker.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de25494baba9ed34203490ec9c6897b5e32e72a13a0c42ba59e9897d6758166c
-size 16222
+oid sha256:ef31523858b14b03eb4beadf32a78058ae6ad47da37e3e74dd25d36df539a851
+size 16230
