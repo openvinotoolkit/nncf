83580b36a0fa34e5ddef0021bff6faa14ad347e5
Andrey Churkin <andrey.churkin@intel.com>
2021-04-05 11:42:51 +0300

    Load optimizer state from the checkpoint only when the training mode is enabled (#632)

--- examples/classification/staged_quantization_worker.py
+++ examples/classification/staged_quantization_worker.py
@@ -183,9 +183,9 @@ def staged_quantization_main_worker(current_gpu, config):
         best_acc1 = resuming_checkpoint['best_acc1']
         kd_loss_calculator.original_model.load_state_dict(resuming_checkpoint['original_model_state_dict'])
         compression_ctrl.scheduler.load_state(resuming_checkpoint['compression_scheduler'])
-        optimizer.load_state_dict(resuming_checkpoint['optimizer'])
-        optimizer_scheduler.load_state_dict(resuming_checkpoint['optimizer_scheduler'])
         if config.mode.lower() == 'train':
+            optimizer.load_state_dict(resuming_checkpoint['optimizer'])
+            optimizer_scheduler.load_state_dict(resuming_checkpoint['optimizer_scheduler'])
             logger.info("=> loaded checkpoint '{}' (epoch: {}, best_acc1: {:.3f})"
                         .format(resuming_checkpoint_path, resuming_checkpoint['epoch'], best_acc1))
         else:
