f4187cc75fe2e72dc196a075afd8a1796f7c33e7
Vasily Shamporov <vasily.shamporov@intel.com>
2021-04-08 11:42:54 +0300

    Fix random in YOLOv4 sample (#641)

--- beta/examples/tensorflow/common/object_detection/utils/yolo_v4_utils.py
+++ beta/examples/tensorflow/common/object_detection/utils/yolo_v4_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cd0fb47b0acaf6e1770a91eda39268f179b2c2d12033ef0b2458d283a496c01
-size 13288
+oid sha256:ffb5d4688f4d0ffde8d0007774fc4104179e403a17c248f4206975b145311696
+size 13306
