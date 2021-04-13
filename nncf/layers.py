d633893cd9ace0da6d7d75bdba349aec6917334b
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2021-04-14 00:13:31 +0300

    Fixed optimizer issue related to the padding value as a parameter, not buffer (#653)

--- nncf/layers.py
+++ nncf/layers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42e8a4e456042bd56eb3d3e3bd7fdc95285f8a1e71cfa7e224ef8e94d4ff69d3
-size 28489
+oid sha256:eb06e164a76adea46721b956c371ab81adc07acfd418c50ccb8dd172df60a9f9
+size 28453
--- tests/quantization/test_hawq_precision_init.py
+++ tests/quantization/test_hawq_precision_init.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c164fc184e0b00b25a38341c1f28c2f35ecf00a79e33e3b3db304c68c93641ff
+oid sha256:a0dbf9cfc563638670b073b9641fd081e1e79e74f10e427c1b4e194353f03456
 size 35483
