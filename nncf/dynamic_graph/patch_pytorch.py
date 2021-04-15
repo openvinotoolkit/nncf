d8f56a4b5dbd1459e1cc82ef90798265744d19c3
Vasily Shamporov <vasily.shamporov@intel.com>
2021-04-15 11:43:32 +0300

    Work around PT 1.8 always rebuilding CUDA extensions (#664)

--- nncf/dynamic_graph/patch_pytorch.py
+++ nncf/dynamic_graph/patch_pytorch.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4d4b3ceff4284dfbec8fe20f7aa9c6fca0781c63cf2b4d671a059a75f9624e0
-size 9662
+oid sha256:1b3e6c6c4d4fc0916074274fb2cfe452d5b3bddaac541fc9191bd94bfae26805
+size 10878
