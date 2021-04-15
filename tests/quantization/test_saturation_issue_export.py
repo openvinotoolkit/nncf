20dff2c2b44b97000bb2c8c81360a2f8b9f1c185
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-04-16 02:12:00 +0300

    Update threshold values of test (#666)
    * Update threshold values of test_is_pytorch_output_the_same_as_onnx_qdq_saturation_fix_applied
    
    * Update x2

--- tests/quantization/test_saturation_issue_export.py
+++ tests/quantization/test_saturation_issue_export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31b3a46f0d9969b42ca6dbcfdb9cb8eb6d719dbbcf2d5d8953f6488a22040897
-size 17831
+oid sha256:200874367d5a803b1d3568a423f1eeb8a2d9db2ffe2b249f9630f48ecc06c8f6
+size 17834
