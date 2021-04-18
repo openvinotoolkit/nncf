2b0460cc6848cc7df39bce343aa47db62303720f
Vasily Shamporov <vasily.shamporov@intel.com>
2021-04-18 14:30:36 +0300

    Install torchvision in mmdetection sanity test setup (#671)

--- tests/test_sanity_third_party.py
+++ tests/test_sanity_third_party.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5f92303fcc5a8c07ae77b2531c1c9b425ac209e547d3066db7cceacef323b54
-size 22120
+oid sha256:31aa1a24fe4a306cbed8352aef07b7b4f1c994a6d9916f8749760785dd2e58d1
+size 22375
