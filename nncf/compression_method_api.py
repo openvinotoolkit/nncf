1d494164d06859018ef5e52136efc97987b183ce
Andrey Churkin <andrey.churkin@intel.com>
2021-04-21 16:41:29 +0300

    Update the signature of the export_model method (#676)
    * Signature of the export_model method was updated
    
    * Minor fix

--- nncf/compression_method_api.py
+++ nncf/compression_method_api.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf422d87d7efd6671afd97a6bcf74297813475e3ad675c883e2c176697eb4bb3
-size 12839
+oid sha256:708624194cc3a1a54c69ff92395f04c3bd95744ec26a70c6816b6f2ae15015a0
+size 13225
