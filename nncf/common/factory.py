63b88cb1a78c51d481820766cce5a378ac6c52c4
Andrey Churkin <andrey.churkin@intel.com>
2021-04-27 15:20:50 +0300

    Minor fix

--- nncf/common/factory.py
+++ nncf/common/factory.py
@@ -19,7 +19,7 @@ from nncf.common.utils.backend import __nncf_backend__
 if __nncf_backend__ == 'Torch':
     from nncf.exporter import PTExporter
 elif __nncf_backend__ == 'TensorFlow':
-    from beta.nncf.tensorflow.utils.exporter import TFExporter
+    from beta.nncf.tensorflow.exporter import TFExporter
 
 
 def create_exporter(model: ModelType,
