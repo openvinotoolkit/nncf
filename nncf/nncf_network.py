9238d4cd88f1edf03c24ce666ae8f799bab21642
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-04-27 10:30:55 +0300

    Fix typo x3; Update test

--- nncf/nncf_network.py
+++ nncf/nncf_network.py
@@ -505,7 +505,8 @@ class NNCFNetwork(nn.Module, PostGraphBuildActing):
         # WARNING: Will reset pre- and post-ops of the underlying model. Use save_nncf_module_additions
         # and load_nncf_module_additions to preserve these, or temporary_clean_view().
         from nncf.utils import save_module_training_state, load_module_training_state
-        saved_state = save_module_training_state(self)
+        saved_state = {}
+        save_module_training_state(self, saved_state)
         model_copy = NNCFNetwork(self.get_nncf_wrapped_model(), self.input_infos,
                     self._user_dummy_forward_fn, self._wrap_inputs_fn,
                     self.scopes_without_shape_matching, self.ignored_scopes, self.target_scopes,
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,10 +5,9 @@ from nncf.utils import training_mode_switcher
 from tests.helpers import BasicConvTestModel, TwoConvTestModel, MockModel
 from tests.quantization.test_saturation_issue_export import DepthWiseConvTestModel, EightConvTestModel
 
-
 @pytest.mark.parametrize('model', [BasicConvTestModel(), TwoConvTestModel(), MockModel(),
                                    DepthWiseConvTestModel(), EightConvTestModel()])
-def test_training_mode_switcher(model):
+def test_training_mode_switcher(model, _seed):
     saved_model_state = {}
 
     def save_model_training_state(module):
@@ -21,18 +20,17 @@ def test_training_mode_switcher(model):
             assert saved_model_state[ch] == ch.training
             compare_saved_model_state_and_current_model_state(ch)
 
-    def random_change_model_training_state(module):
+    def randomly_change_model_training_state(module):
         import random
         for ch in module.children():
             if random.uniform(0, 1) > 0.5:
                 ch.training = False
             else:
                 ch.training = True
-            random_change_model_training_state(ch)
+            randomly_change_model_training_state(ch)
 
-    random_change_model_training_state(model)
+    randomly_change_model_training_state(model)
 
-    saved_model_state = {}
     save_model_training_state(model)
 
     with training_mode_switcher(model, True):
