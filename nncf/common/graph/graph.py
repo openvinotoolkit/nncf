d6f2e047ef5664481843eeb929a0d7ebf1f541b4
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2021-04-28 11:43:58 +0300

    Reimplement get_input_edges to return dictionary of edges with description (#684)

--- nncf/common/graph/graph.py
+++ nncf/common/graph/graph.py
@@ -17,6 +17,9 @@ from typing import Tuple
 from typing import Any
 from typing import KeysView
 from typing import ValuesView
+from typing import Dict
+
+from collections import OrderedDict
 
 import networkx as nx
 
@@ -185,18 +188,18 @@ class NNCFGraph:
         nx_node_keys = self._nx_graph.pred[self._node_id_to_key_dict[node.node_id]]
         return [self._nx_node_to_nncf_node(self._nx_graph.nodes[key]) for key in nx_node_keys]
 
-    def get_previous_nodes_sorted_by_in_port(self, node: NNCFNode) -> List[NNCFNode]:
+    def get_input_edges(self, node: NNCFNode) -> Dict[Tuple[str, str], dict]:
         """
-        Returns producer nodes of provided node sorted by 'in_port'.
+        Returns edges of input tensors with description sorted by 'in_port'.
 
         :param node: Consumer node.
-        :return: List of producers nodes of provided node sorted by in_port.
+        :return: Dictionary of input edges for node sorted by in_port.
         """
-        in_edges = sorted(list(self._nx_graph.in_edges(node.data['key'])),
-                       key=lambda edge: self._nx_graph.edges[edge]['in_port'])
-        nx_node_keys = [p for p, _ in in_edges]
+        nx_node_key = self._node_id_to_key_dict[node.node_id]
+        input_edges = sorted(list(self._nx_graph.in_edges(nx_node_key)),
+                             key=lambda edge: self._nx_graph.edges[edge][NNCFGraph.IN_PORT_NAME_EDGE_ATTR])
 
-        return [self._nx_node_to_nncf_node(self._nx_graph.nodes[key]) for key in nx_node_keys]
+        return OrderedDict((edge, self._nx_graph.edges[edge]) for edge in input_edges)
 
     def traverse_graph(self,
                        curr_node: NNCFNode,
@@ -260,16 +263,3 @@ class NNCFGraph:
         :param path: Path to save.
         """
         nx.drawing.nx_pydot.write_dot(self._nx_graph, path)
-
-    def get_input_edges(self, node: NNCFNode) -> List[dict]:
-        """
-        Returns description of edge for input tensors.
-
-        :param node: Consumer node.
-        :return: List of input edges for node.
-        """
-        nx_node_key = self._node_id_to_key_dict[node.node_id]
-        input_edges = sorted(list(self._nx_graph.in_edges(nx_node_key)),
-                             key=lambda edge: self._nx_graph.edges[edge][NNCFGraph.IN_PORT_NAME_EDGE_ATTR])
-
-        return [self._nx_graph.edges[edge] for edge in input_edges]
