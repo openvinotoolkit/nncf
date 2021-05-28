Fix calculation of shared layers FLOPs (#677)
* proper calculation of FLOPs of shared layers

* pytorch demo

* shapes calculation using graph edges

* changes related to wrappers renaming

* fix rebase

* change tests

* add test

* fix tests

* tests: sort nodes in cluster before comparing

* comments: remove redundant attribute

* fix rebase

beta/nncf/tensorflow/pruning/filter_pruning/algorithm.py
@@ -23,7 +23,8 @@ from beta.nncf.tensorflow.graph.metatypes.common import LINEAR_LAYER_METATYPES
 from beta.nncf.tensorflow.graph.metatypes.common import GENERAL_CONV_LAYER_METATYPES
 from beta.nncf.tensorflow.graph.metatypes.matcher import get_keras_layer_metatype
 from beta.nncf.tensorflow.graph.utils import collect_wrapped_layers
-from beta.nncf.tensorflow.graph.utils import get_original_name
+from beta.nncf.tensorflow.graph.utils import get_layer_identifier
+from beta.nncf.tensorflow.graph.utils import get_original_name_and_instance_index
 from beta.nncf.tensorflow.graph.utils import unwrap_layer
 from beta.nncf.tensorflow.layers.data_layout import get_input_channel_axis
 from beta.nncf.tensorflow.layers.wrapper import NNCFWrapper
@@ -175,7 +176,7 @@ class FilterPruningController(BasePruningAlgoController):
 
         # 3. Initialize pruning quotas
         for cluster in self._pruned_layer_groups_info.get_all_clusters():
-            self._pruning_quotas[cluster.id] = floor(self._layers_out_channels[cluster.nodes[0].layer_name]
+            self._pruning_quotas[cluster.id] = floor(self._layers_out_channels[cluster.nodes[0].node_name]
                                                      * self.pruning_quota)
 
     def _flops_count_init(self):
@@ -183,32 +184,35 @@ class FilterPruningController(BasePruningAlgoController):
         Collects input/output shapes of convolutional and dense layers,
         calculates corresponding layerwise FLOPs
         """
-        for layer in self._model.layers:
-            layer_metatype = get_keras_layer_metatype(layer)
+        for node in self._original_graph.get_nodes_by_metatypes(GENERAL_CONV_LAYER_METATYPES):
+            node_name, node_index = get_original_name_and_instance_index(node.node_name)
+            layer = self._model.get_layer(node_name)
             layer_ = unwrap_layer(layer)
 
-            if layer_metatype in GENERAL_CONV_LAYER_METATYPES:
-                channel_axis = get_input_channel_axis(layer)
-                dims_slice = slice(channel_axis - layer_.rank, channel_axis) \
-                    if layer.data_format == 'channels_last' else slice(channel_axis + 1, None)
-                in_shape = layer.get_input_shape_at(0)[dims_slice]
-                out_shape = layer.get_output_shape_at(0)[dims_slice]
+            channel_axis = get_input_channel_axis(layer)
+            dims_slice = slice(channel_axis - layer_.rank, channel_axis) \
+                if layer.data_format == 'channels_last' else slice(channel_axis + 1, None)
+            in_shape = layer.get_input_shape_at(node_index)[dims_slice]
+            out_shape = layer.get_output_shape_at(node_index)[dims_slice]
 
-                if not is_valid_shape(in_shape) or not is_valid_shape(out_shape):
-                    raise RuntimeError(f'Input/output shape is not defined for layer `{layer.name}` ')
+            if not is_valid_shape(in_shape) or not is_valid_shape(out_shape):
+                raise RuntimeError(f'Input/output shape is not defined for layer `{layer.name}` ')
 
-                self._layers_in_shapes[layer.name] = in_shape
-                self._layers_out_shapes[layer.name] = out_shape
+            self._layers_in_shapes[node.node_name] = in_shape
+            self._layers_out_shapes[node.node_name] = out_shape
 
-            elif layer_metatype in LINEAR_LAYER_METATYPES:
-                in_shape = layer.get_input_shape_at(0)[1:]
-                out_shape = layer.get_output_shape_at(0)[1:]
+        for node in self._original_graph.get_nodes_by_metatypes(LINEAR_LAYER_METATYPES):
+            node_name, node_index = get_original_name_and_instance_index(node.node_name)
+            layer = self._model.get_layer(node_name)
 
-                if not is_valid_shape(in_shape) or not is_valid_shape(out_shape):
-                    raise RuntimeError(f'Input/output shape is not defined for layer `{layer.name}` ')
+            in_shape = layer.get_input_shape_at(node_index)[1:]
+            out_shape = layer.get_output_shape_at(node_index)[1:]
 
-                self._layers_in_shapes[layer.name] = in_shape
-                self._layers_out_shapes[layer.name] = out_shape
+            if not is_valid_shape(in_shape) or not is_valid_shape(out_shape):
+                raise RuntimeError(f'Input/output shape is not defined for layer `{layer.name}` ')
+
+            self._layers_in_shapes[node.node_name] = in_shape
+            self._layers_out_shapes[node.node_name] = out_shape
 
         self._nodes_flops = count_flops_for_nodes(self._original_graph,
                                                   self._layers_in_shapes,
@@ -227,8 +231,7 @@ class FilterPruningController(BasePruningAlgoController):
         # 1. Calculate masks
         for group in self._pruned_layer_groups_info.get_all_clusters():
             # a. Calculate the cumulative importance for all filters in the group
-            cumulative_filters_importance = \
-                self._calculate_filters_importance_in_group(group, wrapped_layers)
+            cumulative_filters_importance = self._calculate_filters_importance_in_group(group)
             filters_num = len(cumulative_filters_importance)
 
             # b. Calculate threshold
@@ -250,7 +253,7 @@ class FilterPruningController(BasePruningAlgoController):
         nncf_sorted_nodes = self._original_graph.topological_sort()
         for layer in wrapped_layers:
             nncf_node = [n for n in nncf_sorted_nodes
-                         if layer.name == get_original_name(n.node_name)][0]
+                         if layer.name == get_layer_identifier(n)][0]
             if nncf_node.data['output_mask'] is not None:
                 self._set_operation_masks([layer], nncf_node.data['output_mask'])
 
@@ -272,8 +275,7 @@ class FilterPruningController(BasePruningAlgoController):
         # 1. Calculate masks
         # a. Calculate importances for all groups of filters
         for group in self._pruned_layer_groups_info.get_all_clusters():
-            cumulative_filters_importance = \
-                self._calculate_filters_importance_in_group(group, wrapped_layers)
+            cumulative_filters_importance = self._calculate_filters_importance_in_group(group)
             filter_importances[group.id] = cumulative_filters_importance
 
         # b. Calculate one threshold for all weights
@@ -295,7 +297,7 @@ class FilterPruningController(BasePruningAlgoController):
         nncf_sorted_nodes = self._original_graph.topological_sort()
         for layer in wrapped_layers:
             nncf_node = [n for n in nncf_sorted_nodes
-                         if layer.name == get_original_name(n.node_name)][0]
+                         if layer.name == get_layer_identifier(n)][0]
             if nncf_node.data['output_mask'] is not None:
                 self._set_operation_masks([layer], nncf_node.data['output_mask'])
 
@@ -313,7 +315,7 @@ class FilterPruningController(BasePruningAlgoController):
         nncf_sorted_nodes = self._original_graph.topological_sort()
         for layer in wrapped_layers:
             nncf_node = [n for n in nncf_sorted_nodes
-                         if layer.name == get_original_name(n.node_name)][0]
+                         if layer.name == get_layer_identifier(n)][0]
             nncf_node.data['output_mask'] = tf.ones(get_filters_num(layer))
 
         # 1. Calculate importances for all groups of filters. Initialize masks.
@@ -321,9 +323,7 @@ class FilterPruningController(BasePruningAlgoController):
         group_indexes = []
         filter_indexes = []
         for group in self._pruned_layer_groups_info.get_all_clusters():
-            cumulative_filters_importance = \
-                self._calculate_filters_importance_in_group(group, wrapped_layers)
-
+            cumulative_filters_importance = self._calculate_filters_importance_in_group(group)
             filter_importances.extend(cumulative_filters_importance)
             filters_num = len(cumulative_filters_importance)
             group_indexes.extend([group.id] * filters_num)
@@ -344,7 +344,7 @@ class FilterPruningController(BasePruningAlgoController):
             # Update input/output shapes of pruned nodes
             group = self._pruned_layer_groups_info.get_cluster_by_id(group_id)
             for node in group.nodes:
-                tmp_out_channels[node.layer_name] -= 1
+                tmp_out_channels[node.node_name] -= 1
             for node_name in self._next_nodes[group_id]:
                 tmp_in_channels[node_name] -= 1
 
@@ -370,7 +370,7 @@ class FilterPruningController(BasePruningAlgoController):
                 nncf_sorted_nodes = self._original_graph.topological_sort()
                 for layer in wrapped_layers:
                     nncf_node = [n for n in nncf_sorted_nodes
-                                 if layer.name == get_original_name(n.node_name)][0]
+                                 if layer.name == get_layer_identifier(n)][0]
                     if nncf_node.data['output_mask'] is not None:
                         self._set_operation_masks([layer], nncf_node.data['output_mask'])
                 return
@@ -422,26 +422,28 @@ class FilterPruningController(BasePruningAlgoController):
                                           linear_op_metatypes=LINEAR_LAYER_METATYPES).values())
         return flops
 
-    def _calculate_filters_importance_in_group(self, group: NodesCluster,
-                                               wrapped_layers: List[tf.keras.layers.Layer]):
+    def _calculate_filters_importance_in_group(self, group: NodesCluster):
         """
         Calculates cumulative filters importance in the group.
         :param group: Nodes cluster
-        :param wrapped_layers: List of keras nodes wrapped by NNCFWrapper
         :return a list of filter importance scores
         """
-        group_layer_names = [node.layer_name for node in group.nodes]
-        group_filters_num = tf.constant([get_filters_num(wrapped_layer)
-                                         for wrapped_layer in wrapped_layers
-                                         if wrapped_layer.name in group_layer_names])
+        group_layers = [self._model.get_layer(node.layer_name) for node in group.nodes]
+        group_filters_num = tf.constant([get_filters_num(layer) for layer in group_layers])
         filters_num = group_filters_num[0]
         assert tf.reduce_all(group_filters_num == filters_num)
 
         cumulative_filters_importance = tf.zeros(filters_num)
         # Calculate cumulative importance for all filters in this group
+        shared_nodes = []
         for minfo in group.nodes:
-            layer = [layer for layer in wrapped_layers if layer.name == minfo.layer_name][0]
-            filters_importance = self._layer_filter_importance(layer)
+            layer_name = minfo.layer_name
+            if layer_name in shared_nodes:
+                continue
+            nncf_node = self._original_graph.get_node_by_id(minfo.nncf_node_id)
+            if nncf_node.data['is_shared']:
+                shared_nodes.append(layer_name)
+            filters_importance = self._layer_filter_importance(self._model.get_layer(layer_name))
             cumulative_filters_importance += filters_importance
 
         return cumulative_filters_importance

nncf/common/pruning/utils.py
@@ -14,7 +14,7 @@
 import math
 
 from functools import partial
-from typing import Dict, List, Optional, Tuple, Type, Union
+from typing import Dict, List, Optional, Tuple, Type
 
 import numpy as np
 
@@ -193,10 +193,6 @@ def get_previous_conv(graph: NNCFGraph, nncf_node: NNCFNode,
     return None
 
 
-def get_original_node_name(node_name: str):
-    return node_name.split('^')[0]
-
-
 def get_conv_in_out_channels(graph: NNCFGraph):
     """
     Collects the number of input and output channels for each convolution in the graph.
@@ -210,8 +206,7 @@ def get_conv_in_out_channels(graph: NNCFGraph):
     in_channels, out_channels = {}, {}
     for node in graph.get_all_nodes():
         if isinstance(node.module_attributes, ConvolutionModuleAttributes):
-            name = node.ia_op_exec_context.scope_in_model if hasattr(node, 'ia_op_exec_context') \
-                else get_original_node_name(node.node_name)
+            name = node.node_name
             if name in in_channels and name in out_channels:
                 continue
             in_channels[name] = node.module_attributes.in_channels
@@ -235,14 +230,10 @@ def get_cluster_next_nodes(graph: NNCFGraph, pruned_groups_info,
         cluster_nodes = set()
         for cluster_node in cluster.nodes:
             nncf_cluster_node = graph.get_node_by_id(cluster_node.nncf_node_id)
-            nncf_cluster_node_scope = nncf_cluster_node.ia_op_exec_context.scope_in_model \
-                if hasattr(nncf_cluster_node, 'ia_op_exec_context') \
-                else get_original_node_name(nncf_cluster_node.node_name)
-            cluster_nodes.add(nncf_cluster_node_scope)
+            cluster_nodes.add(nncf_cluster_node.node_name)
             curr_next_nodes = get_next_nodes_of_types(graph, nncf_cluster_node, prunable_types)
 
-            next_nodes_idxs = [n.ia_op_exec_context.scope_in_model if hasattr(n, 'ia_op_exec_context')
-                               else get_original_node_name(n.node_name) for n in curr_next_nodes]
+            next_nodes_idxs = [n.node_name for n in curr_next_nodes]
             next_nodes_cluster = next_nodes_cluster.union(next_nodes_idxs)
         next_nodes[cluster.id] = list(next_nodes_cluster - cluster_nodes)
     return next_nodes
@@ -262,8 +253,8 @@ def count_flops_for_nodes(graph: NNCFGraph,
                          fully connected layers. E.g {node_name: (height, width)}
     :param output_shapes: Dictionary of output dimension shapes for convolutions and
                           fully connected layers. E.g {node_name: (height, width)}
-    :param conv_op_types: List of metatypes defining convolution operations.
-    :param linear_op_types: List of metatypes defining linear/fully connected operations.
+    :param conv_op_metatypes: List of metatypes defining convolution operations.
+    :param linear_op_metatypes: List of metatypes defining linear/fully connected operations.
     :param input_channels: Dictionary of input channels number in convolutions.
                            If not specified, taken from the graph. {node_name: channels_num}
     :param output_channels: Dictionary of output channels number in convolutions.
@@ -274,26 +265,22 @@ def count_flops_for_nodes(graph: NNCFGraph,
     input_channels = input_channels or {}
     output_channels = output_channels or {}
     for node in graph.get_nodes_by_metatypes(conv_op_metatypes):
-        name = node.ia_op_exec_context.scope_in_model if hasattr(node, 'ia_op_exec_context') \
-            else get_original_node_name(node.node_name)
-        if name in flops:
-            continue
+        name = node.node_name
         num_in_channels = input_channels.get(name, node.module_attributes.in_channels)
         num_out_channels = output_channels.get(name, node.module_attributes.out_channels)
         flops[name] = 2 * np.prod(node.module_attributes.kernel_size) * \
                       num_in_channels * num_out_channels * np.prod(output_shapes[name])
 
     for node in graph.get_nodes_by_metatypes(linear_op_metatypes):
-        name = node.ia_op_exec_context.scope_in_model if hasattr(node, 'ia_op_exec_context') \
-            else get_original_node_name(node.node_name)
+        name = node.node_name
         flops[name] = 2 * np.prod(input_shapes[name]) * np.prod(output_shapes[name])
 
     return flops
 
 
 def calculate_in_out_channels_in_uniformly_pruned_model(pruning_groups, pruning_rate: float,
-                                                        full_input_channels: Dict[Union[str, 'Scope'], int],
-                                                        full_output_channels: Dict[Union[str, 'Scope'], int],
+                                                        full_input_channels: Dict[str, int],
+                                                        full_output_channels: Dict[str, int],
                                                         pruning_groups_next_nodes: Dict[int, List[str]]):
     """
     Imitates filters pruning by removing `pruning_rate` percent of output filters in each pruning group
@@ -311,8 +298,8 @@ def calculate_in_out_channels_in_uniformly_pruned_model(pruning_groups, pruning_
     tmp_out_channels = full_output_channels.copy()
 
     for group in pruning_groups:
-        layer_name = group.nodes[0].key
-        assert all(tmp_out_channels[layer_name] == tmp_out_channels[node.key] for node in
+        layer_name = group.nodes[0].node_name
+        assert all(tmp_out_channels[layer_name] == tmp_out_channels[node.node_name] for node in
                    group.nodes)
         # Prune all nodes in cluster (by output channels)
         old_out_channels = full_output_channels[layer_name]
@@ -320,7 +307,7 @@ def calculate_in_out_channels_in_uniformly_pruned_model(pruning_groups, pruning_
         new_out_channels_num = old_out_channels - num_of_sparse_elems
 
         for node in group.nodes:
-            tmp_out_channels[node.key] = new_out_channels_num
+            tmp_out_channels[node.node_name] = new_out_channels_num
 
         # Prune in_channels in all next nodes of cluster
         for node_name in pruning_groups_next_nodes[group.id]:

tests/pruning/test_model_pruning_analysis.py
@@ -13,6 +13,7 @@
 from typing import Callable
 
 import pytest
+from torch import nn
 
 from nncf.torch.layers import NNCF_PRUNING_MODULES_DICT
 from nncf.torch.dynamic_graph.graph_tracer import ModelInputInfo
@@ -27,10 +28,11 @@ from nncf.common.pruning.model_analysis import NodesCluster
 from nncf.common.pruning.model_analysis import Clusterization
 from nncf.common.pruning.model_analysis import cluster_special_ops
 from nncf.common.pruning.model_analysis import ModelAnalyzer
-from tests.helpers import create_compressed_model_and_algo_for_test, create_nncf_model_and_algo_builder
+from tests.helpers import create_compressed_model_and_algo_for_test, create_nncf_model_and_algo_builder, \
+    module_scope_from_node_name
 from tests.pruning.helpers import PruningTestModelEltwise, get_basic_pruning_config, TestModelBranching, \
     TestModelResidualConnection, TestModelEltwiseCombination, TestModelDiffConvs, \
-    TestModelShuffleNetUnit, TestModelShuffleNetUnitDW
+    TestModelShuffleNetUnit, TestModelShuffleNetUnitDW, PruningTestModelSharedConvs
 
 
 # pylint: disable=protected-access
@@ -47,9 +49,9 @@ def create_nncf_model_and_builder(model, config_params):
 
 
 class GroupPruningModulesTestStruct:
-    def __init__(self, model, not_pruned_modules, pruned_groups, pruned_groups_by_node_id, prune_params):
+    def __init__(self, model, not_pruned_nodes, pruned_groups, pruned_groups_by_node_id, prune_params):
         self.model = model
-        self.not_pruned_modules = not_pruned_modules
+        self.not_pruned_nodes = not_pruned_nodes
         self.pruned_groups = pruned_groups
         self.pruned_groups_by_node_id = pruned_groups_by_node_id
         self.prune_params = prune_params
@@ -57,65 +59,69 @@ class GroupPruningModulesTestStruct:
 
 GROUP_PRUNING_MODULES_TEST_CASES = [
     GroupPruningModulesTestStruct(model=PruningTestModelEltwise,
-                                  not_pruned_modules=['PruningTestModelEltwise/NNCFConv2d[conv1]',
-                                                      'PruningTestModelEltwise/NNCFConv2d[conv4]'],
-                                  pruned_groups=[['PruningTestModelEltwise/NNCFConv2d[conv2]',
-                                                  'PruningTestModelEltwise/NNCFConv2d[conv3]']],
+                                  not_pruned_nodes=['1 PruningTestModelEltwise/NNCFConv2d[conv1]/conv2d_0',
+                                                    '7 PruningTestModelEltwise/NNCFConv2d[conv4]/conv2d_0'],
+                                  pruned_groups=[['3 PruningTestModelEltwise/NNCFConv2d[conv2]/conv2d_0',
+                                                  '4 PruningTestModelEltwise/NNCFConv2d[conv3]/conv2d_0']],
                                   pruned_groups_by_node_id=[[3, 4]],
                                   prune_params=(False, False, False)),
     GroupPruningModulesTestStruct(model=PruningTestModelEltwise,
-                                  not_pruned_modules=[],
-                                  pruned_groups=[['PruningTestModelEltwise/NNCFConv2d[conv1]'],
-                                                 ['PruningTestModelEltwise/NNCFConv2d[conv4]'],
-                                                 ['PruningTestModelEltwise/NNCFConv2d[conv2]',
-                                                  'PruningTestModelEltwise/NNCFConv2d[conv3]']],
+                                  not_pruned_nodes=[],
+                                  pruned_groups=[['1 PruningTestModelEltwise/NNCFConv2d[conv1]/conv2d_0'],
+                                                 ['7 PruningTestModelEltwise/NNCFConv2d[conv4]/conv2d_0'],
+                                                 ['3 PruningTestModelEltwise/NNCFConv2d[conv2]/conv2d_0',
+                                                  '4 PruningTestModelEltwise/NNCFConv2d[conv3]/conv2d_0']],
                                   pruned_groups_by_node_id=[[1], [7], [3, 4]],
                                   prune_params=(True, True, False)),
     GroupPruningModulesTestStruct(model=TestModelBranching,
-                                  not_pruned_modules=[],
-                                  pruned_groups=[['TestModelBranching/NNCFConv2d[conv1]',
-                                                  'TestModelBranching/NNCFConv2d[conv2]',
-                                                  'TestModelBranching/NNCFConv2d[conv3]']],
+                                  not_pruned_nodes=[],
+                                  pruned_groups=[['1 TestModelBranching/NNCFConv2d[conv1]/conv2d_0',
+                                                  '2 TestModelBranching/NNCFConv2d[conv2]/conv2d_0',
+                                                  '4 TestModelBranching/NNCFConv2d[conv3]/conv2d_0']],
                                   pruned_groups_by_node_id=[[1, 2, 4]],
                                   prune_params=(True, True, False)),
     GroupPruningModulesTestStruct(model=TestModelBranching,
-                                  not_pruned_modules=['TestModelBranching/NNCFConv2d[conv1]',
-                                                      'TestModelBranching/NNCFConv2d[conv2]',
-                                                      'TestModelBranching/NNCFConv2d[conv3]'],
-                                  pruned_groups=[['TestModelBranching/NNCFConv2d[conv4]',
-                                                  'TestModelBranching/NNCFConv2d[conv5]']],
+                                  not_pruned_nodes=['1 TestModelBranching/NNCFConv2d[conv1]/conv2d_0',
+                                                    '2 TestModelBranching/NNCFConv2d[conv2]/conv2d_0',
+                                                    '4 TestModelBranching/NNCFConv2d[conv3]/conv2d_0'],
+                                  pruned_groups=[['7 TestModelBranching/NNCFConv2d[conv4]/conv2d_0',
+                                                  '8 TestModelBranching/NNCFConv2d[conv5]/conv2d_0']],
                                   pruned_groups_by_node_id=[[7, 8]],
                                   prune_params=(False, True, False)),
     GroupPruningModulesTestStruct(model=TestModelBranching,
-                                  not_pruned_modules=['TestModelBranching/NNCFConv2d[conv4]',
-                                                      'TestModelBranching/NNCFConv2d[conv5]'],
-                                  pruned_groups=[['TestModelBranching/NNCFConv2d[conv1]',
-                                                  'TestModelBranching/NNCFConv2d[conv2]',
-                                                  'TestModelBranching/NNCFConv2d[conv3]']],
+                                  not_pruned_nodes=['7 TestModelBranching/NNCFConv2d[conv4]/conv2d_0',
+                                                    '8 TestModelBranching/NNCFConv2d[conv5]/conv2d_0'],
+                                  pruned_groups=[['1 TestModelBranching/NNCFConv2d[conv1]/conv2d_0',
+                                                  '2 TestModelBranching/NNCFConv2d[conv2]/conv2d_0',
+                                                  '4 TestModelBranching/NNCFConv2d[conv3]/conv2d_0']],
                                   pruned_groups_by_node_id=[[1, 2, 4]],
                                   prune_params=(True, False, False)),
     GroupPruningModulesTestStruct(model=TestModelResidualConnection,
-                                  not_pruned_modules=['TestModelResidualConnection/NNCFConv2d[conv4]',
-                                                      'TestModelResidualConnection/NNCFConv2d[conv5]'],
-                                  pruned_groups=[['TestModelResidualConnection/NNCFConv2d[conv1]',
-                                                  'TestModelResidualConnection/NNCFConv2d[conv2]',
-                                                  'TestModelResidualConnection/NNCFConv2d[conv3]']],
+                                  not_pruned_nodes=['7 TestModelResidualConnection/NNCFConv2d[conv4]/conv2d_0',
+                                                    '8 TestModelResidualConnection/NNCFConv2d[conv5]/conv2d_0'],
+                                  pruned_groups=[['1 TestModelResidualConnection/NNCFConv2d[conv1]/conv2d_0',
+                                                  '2 TestModelResidualConnection/NNCFConv2d[conv2]/conv2d_0',
+                                                  '4 TestModelResidualConnection/NNCFConv2d[conv3]/conv2d_0']],
                                   pruned_groups_by_node_id=[[1, 2, 4]],
-                                  prune_params=(True, True, False),
-
-                                  ),
+                                  prune_params=(True, True, False)),
     GroupPruningModulesTestStruct(model=TestModelEltwiseCombination,
-                                  not_pruned_modules=[],
-                                  pruned_groups=[['TestModelEltwiseCombination/NNCFConv2d[conv1]',
-                                                  'TestModelEltwiseCombination/NNCFConv2d[conv2]',
-                                                  'TestModelEltwiseCombination/NNCFConv2d[conv3]',
-                                                  'TestModelEltwiseCombination/NNCFConv2d[conv4]'],
-                                                 ['TestModelEltwiseCombination/NNCFConv2d[conv5]',
-                                                  'TestModelEltwiseCombination/NNCFConv2d[conv6]']],
+                                  not_pruned_nodes=[],
+                                  pruned_groups=[['1 TestModelEltwiseCombination/NNCFConv2d[conv1]/conv2d_0',
+                                                  '2 TestModelEltwiseCombination/NNCFConv2d[conv2]/conv2d_0',
+                                                  '4 TestModelEltwiseCombination/NNCFConv2d[conv3]/conv2d_0',
+                                                  '6 TestModelEltwiseCombination/NNCFConv2d[conv4]/conv2d_0'],
+                                                 ['8 TestModelEltwiseCombination/NNCFConv2d[conv5]/conv2d_0',
+                                                  '9 TestModelEltwiseCombination/NNCFConv2d[conv6]/conv2d_0']],
                                   pruned_groups_by_node_id=[[1, 2, 4, 6], [8, 9]],
-                                  prune_params=(True, True, False),
-
-                                  )
+                                  prune_params=(True, True, False)),
+    GroupPruningModulesTestStruct(model=PruningTestModelSharedConvs,
+                                  not_pruned_nodes=['1 PruningTestModelSharedConvs/NNCFConv2d[conv1]/conv2d_0',
+                                                    '5 PruningTestModelSharedConvs/NNCFConv2d[conv3]/conv2d_0',
+                                                    '6 PruningTestModelSharedConvs/NNCFConv2d[conv3]/conv2d_1'],
+                                  pruned_groups=[['3 PruningTestModelSharedConvs/NNCFConv2d[conv2]/conv2d_0',
+                                                  '4 PruningTestModelSharedConvs/NNCFConv2d[conv2]/conv2d_1']],
+                                  pruned_groups_by_node_id=[[3, 4]],
+                                  prune_params=(False, False, False))
 ]
 
 
@@ -126,7 +132,7 @@ def test_input_info_struct(request):
 
 def test_groups(test_input_info_struct_: GroupPruningModulesTestStruct):
     model = test_input_info_struct_.model
-    not_pruned_modules = test_input_info_struct_.not_pruned_modules
+    not_pruned_nodes = test_input_info_struct_.not_pruned_nodes
     pruned_groups = test_input_info_struct_.pruned_groups
     prune_first, prune_last, prune_downsample = test_input_info_struct_.prune_params
 
@@ -144,23 +150,24 @@ def test_groups(test_input_info_struct_: GroupPruningModulesTestStruct):
     all_pruned_modules_info = clusters.get_all_nodes()
     all_pruned_modules = [info.module for info in all_pruned_modules_info]
     print([minfo.module_scope for minfo in all_pruned_modules_info])
-    for module_name in not_pruned_modules:
-        module = compressed_model.get_module_by_scope(Scope.from_str(module_name))
-        assert module not in all_pruned_modules
+    for node_name in not_pruned_nodes:
+        module = compressed_model.get_module_by_scope(module_scope_from_node_name(node_name))
+        assert module is not None and module not in all_pruned_modules
 
     # 2. Check that all pruned groups are valid
     for group in pruned_groups:
-        first_node_scope = Scope.from_str(group[0])
+        first_node_scope = group[0]
         cluster = clusters.get_cluster_by_node_id(first_node_scope)
         cluster_modules = [n.module for n in cluster.nodes]
-        group_modules = [compressed_model.get_module_by_scope(Scope.from_str(module_scope)) for module_scope in group]
+        group_modules = [compressed_model.get_module_by_scope(module_scope_from_node_name(node_name))
+                         for node_name in group]
 
         assert cluster_modules == group_modules
 
 
 def test_pruning_node_selector(test_input_info_struct_: GroupPruningModulesTestStruct):
     model = test_input_info_struct_.model
-    not_pruned_modules = test_input_info_struct_.not_pruned_modules
+    not_pruned_nodes = test_input_info_struct_.not_pruned_nodes
     pruned_groups_by_node_id = test_input_info_struct_.pruned_groups_by_node_id
     prune_first, prune_last, prune_downsample = test_input_info_struct_.prune_params
 
@@ -184,15 +191,16 @@ def test_pruning_node_selector(test_input_info_struct_: GroupPruningModulesTestS
     all_pruned_nodes = pruning_groups.get_all_nodes()
     all_pruned_modules = [nncf_network.get_module_by_scope(node.ia_op_exec_context.scope_in_model)
                           for node in all_pruned_nodes]
-    for module_name in not_pruned_modules:
-        module = nncf_network.get_module_by_scope(Scope.from_str(module_name))
-        assert module not in all_pruned_modules
+    for node_name in not_pruned_nodes:
+        module = nncf_network.get_module_by_scope(module_scope_from_node_name(node_name))
+        assert module is not None and module not in all_pruned_modules
 
     # 2. Check that all pruned groups are valid
     for group_by_id in pruned_groups_by_node_id:
         first_node_id = group_by_id[0]
         cluster = pruning_groups.get_cluster_by_node_id(first_node_id)
         cluster_node_ids = [n.node_id for n in cluster.nodes]
+        cluster_node_ids.sort()
 
         assert cluster_node_ids == group_by_id
 
@@ -237,7 +245,7 @@ def test_group_special_nodes(test_special_ops_struct: GroupSpecialModulesTestStr
 
 
 class ModelAnalyserTestStruct:
-    def __init__(self, model: NNCFNetwork, ref_can_prune: dict):
+    def __init__(self, model: nn.Module, ref_can_prune: dict):
         self.model = model
         self.ref_can_prune = ref_can_prune
 
@@ -267,7 +275,7 @@ def test_model_analyzer(test_struct: GroupSpecialModulesTestStruct):
 
 
 class ModulePrunableTestStruct:
-    def __init__(self, model: NNCFNetwork, config_params: dict, is_module_prunable: dict):
+    def __init__(self, model: nn.Module, config_params: dict, is_module_prunable: dict):
         self.model = model
         self.config_params = config_params
         self.is_module_prunable = is_module_prunable