c25e2a32dfb06deb898bacf8b5ca349c44a91284
Evgeny Tsykunov <e.tsykunov@gmail.com>
2021-06-01 09:05:48 +0300

    pylint fix

--- beta/nncf/tensorflow/initialization.py
+++ beta/nncf/tensorflow/initialization.py
@@ -132,5 +132,6 @@ class DataLoaderBNAdaptationRunner:
 
 
 def register_default_init_args(nncf_config, train_loader, device = None):
-    nncf_config.register_extra_structs([BNAdaptationInitArgs(data_loader=InitializingDataLoader(train_loader), device=device)])
+    nncf_config.register_extra_structs([BNAdaptationInitArgs(data_loader=InitializingDataLoader(train_loader), 
+                                                             device=device)])
     return nncf_config
