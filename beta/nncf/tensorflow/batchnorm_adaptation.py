40a51957bca67cd0743ebf9d7027873f1639571a
Evgeny Tsykunov <e.tsykunov@gmail.com>
2021-06-02 16:38:52 +0300

    keep the logic within TFBatchnormAdaptationAlgorithmImpl

--- beta/nncf/tensorflow/batchnorm_adaptation.py
+++ beta/nncf/tensorflow/batchnorm_adaptation.py
@@ -11,8 +11,16 @@
  limitations under the License.
 """
 
+from functools import partial
+from contextlib import contextmanager
+from typing import Dict
+
+import tensorflow as tf
+
 from nncf.common.batchnorm_adaptation import BatchnormAdaptationAlgorithmImpl
-from beta.nncf.tensorflow.initialization import DataLoaderBNAdaptationRunner
+from beta.nncf.tensorflow.graph.metatypes.keras_layers import TFBatchNormalizationLayerMetatype
+from beta.nncf.tensorflow.graph.metatypes.matcher import get_keras_layer_metatype
+from nncf.common.utils.progress_bar import ProgressBar
 
 
 class TFBatchnormAdaptationAlgorithmImpl(BatchnormAdaptationAlgorithmImpl):
@@ -20,12 +28,93 @@ class TFBatchnormAdaptationAlgorithmImpl(BatchnormAdaptationAlgorithmImpl):
     Implementation of the batch-norm statistics adaptation algorithm for the TensorFlow backend.
     """
 
-    def run(self, model) -> None:
+    @staticmethod
+    def _apply_to_batchnorms(func):
+        def func_apply_to_bns(layer):
+            if get_keras_layer_metatype(layer) == TFBatchNormalizationLayerMetatype:
+                func(layer)
+
+        return func_apply_to_bns
+
+    def _apply_to_model(self, func):
+        for layer in self._model.layers:
+            func(layer)
+
+    @contextmanager
+    def _bn_training_state_switcher(self) -> None:
+        def save_original_bn_training_state(module):
+            self.original_training_state[module] = module.trainable
+
+        def set_bn_training_state(module, state: Dict[str, bool]):
+            module.trainable = state
+
+        def restore_original_bn_training_state(module):
+            module.trainable = self.original_training_state[module]
+
+        self._apply_to_model(self._apply_to_batchnorms(save_original_bn_training_state))
+        self._apply_to_model(self._apply_to_batchnorms(partial(set_bn_training_state, state=True)))
+
+        try:
+            yield
+        finally:
+            self._apply_to_model(self._apply_to_batchnorms(restore_original_bn_training_state))
+
+    @contextmanager
+    def _bn_momentum_switcher(self) -> None:
+        def set_bn_momentum(module, momentum_value):
+            module.momentum = momentum_value
+
+        def save_original_bn_momentum(module):
+            self.original_momenta_values[module] = module.momentum
+
+        def restore_original_bn_momentum(module):
+            module.momentum = self.original_momenta_values[module]
+
+        self._apply_to_model(self._apply_to_batchnorms(save_original_bn_momentum))
+        self._apply_to_model(self._apply_to_batchnorms(partial(set_bn_momentum,
+                                                               momentum_value=0.1)))
+        try:
+            yield
+        finally:
+            self._apply_to_model(self._apply_to_batchnorms(restore_original_bn_momentum))
+
+    def _infer_batch(self, x: tf.Tensor) -> None:
+        """
+        Run the forward pass of the model in train mode.
+        BatchNormalization moving statistics are getting updated.
+        """
+        self._model(x, training=True)
+
+    def _run_model_inference(self):
+        self.original_momenta_values = {}
+        self.original_training_state = {}
+        num_bn_adaptation_steps = self._num_bn_adaptation_steps
+        num_bn_forget_steps = self._num_bn_forget_steps
+        with self._bn_training_state_switcher():
+            if num_bn_forget_steps is not None and num_bn_forget_steps > 0:
+                with self._bn_momentum_switcher():
+                    for i, (x, _) in ProgressBar(
+                            enumerate(self._data_loader),
+                            total=num_bn_forget_steps,
+                            desc='BatchNorm statistics forget'
+                    ):
+                        if i >= num_bn_forget_steps:
+                            break
+                        self._infer_batch(x)
+            for i, (x, _) in ProgressBar(
+                    enumerate(self._data_loader),
+                    total=num_bn_adaptation_steps,
+                    desc='BatchNorm statistics adaptation'
+            ):
+                if num_bn_adaptation_steps is not None and i >= num_bn_adaptation_steps:
+                    break
+                self._infer_batch(x)
+
+    def run(self, model: tf.keras.Model) -> None:
         """
         Runs the batch-norm statistics adaptation algorithm.
 
         :param model: A model for which the algorithm will be applied.
         """
-
-        bn_adaptation_runner = DataLoaderBNAdaptationRunner(model, self._device, self._num_bn_forget_steps)
-        bn_adaptation_runner.run(self._data_loader, self._num_bn_adaptation_steps)
+        self._model = model
+        self._run_model_inference()
--- beta/nncf/tensorflow/initialization.py
+++ beta/nncf/tensorflow/initialization.py
@@ -11,18 +11,13 @@
  limitations under the License.
 """
 
-from functools import partial
-from contextlib import contextmanager
-from typing import Dict, Union
+from typing import Union
 
 import tensorflow as tf
 
-from nncf.common.utils.progress_bar import ProgressBar
 from nncf.common.initialization import NNCFDataLoader
 from nncf.config.structure import BNAdaptationInitArgs
 from nncf.config.config import NNCFConfig
-from beta.nncf.tensorflow.graph.metatypes.keras_layers import TFBatchNormalizationLayerMetatype
-from beta.nncf.tensorflow.graph.metatypes.matcher import get_keras_layer_metatype
 
 
 class InitializingDataLoader(NNCFDataLoader):
@@ -30,112 +25,16 @@ class InitializingDataLoader(NNCFDataLoader):
     This class wraps the tf.data.Dataset class.
     This is required for proper initialization of certain compression algorithms.
     """
-
-    def __init__(self, data_loader: tf.data.Dataset):
+    def __init__(self, data_loader: tf.data.Dataset, batch_size):
         self._data_loader = data_loader
+        self._batch_size = batch_size
 
     @property
     def batch_size(self) -> int:
-        for (x, _) in self._data_loader:
-            batch_size = x.shape[0]
-            return batch_size
+        return self._batch_size
 
     def __iter__(self):
         return iter(self._data_loader)
-
-
-class DataLoaderBNAdaptationRunner:
-    def __init__(self, model: tf.keras.Model, init_device: str, num_bn_forget_steps: float):
-        self.model = model
-        self.init_device = init_device
-        self.progressbar_adaptation_description = 'BatchNorm statistics adaptation'
-        self.progressbar_forget_description = 'BatchNorm statistics forget'
-        self.num_bn_forget_steps = num_bn_forget_steps
-        self.momentum_bn_forget = 0.1
-        self.original_momenta_values = {}
-        self.original_training_state = {}
-
-    @staticmethod
-    def _apply_to_batchnorms(func):
-        def func_apply_to_bns(layer):
-            if get_keras_layer_metatype(layer) == TFBatchNormalizationLayerMetatype:
-                func(layer)
-
-        return func_apply_to_bns
-
-    def _apply_to_model(self, func):
-        for layer in self.model.layers:
-            func(layer)
-
-    @contextmanager
-    def _bn_training_state_switcher(self) -> None:
-        def save_original_bn_training_state(module):
-            self.original_training_state[module] = module.trainable
-
-        def set_bn_training_state(module, state: Dict[str, bool]):
-            module.trainable = state
-
-        def restore_original_bn_training_state(module):
-            module.trainable = self.original_training_state[module]
-
-        self._apply_to_model(self._apply_to_batchnorms(save_original_bn_training_state))
-        self._apply_to_model(self._apply_to_batchnorms(partial(set_bn_training_state, state=True)))
-
-        try:
-            yield
-        finally:
-            self._apply_to_model(self._apply_to_batchnorms(restore_original_bn_training_state))
-
-    @contextmanager
-    def _bn_momentum_switcher(self) -> None:
-        def set_bn_momentum(module, momentum_value):
-            module.momentum = momentum_value
-
-        def save_original_bn_momentum(module):
-            self.original_momenta_values[module] = module.momentum
-
-        def restore_original_bn_momentum(module):
-            module.momentum = self.original_momenta_values[module]
-
-        self._apply_to_model(self._apply_to_batchnorms(save_original_bn_momentum))
-        self._apply_to_model(self._apply_to_batchnorms(partial(set_bn_momentum,
-                                                               momentum_value=self.momentum_bn_forget)))
-        try:
-            yield
-        finally:
-            self._apply_to_model(self._apply_to_batchnorms(restore_original_bn_momentum))
-
-    def _infer_batch(self, x: tf.Tensor) -> None:
-        """
-        Run the forward pass of the model in train mode.
-        BatchNormalization moving statistics are getting updated.
-        """
-        self.model(x, training=True)
-
-    def _run_model_inference(self, data_loader: InitializingDataLoader, num_init_steps: float):
-        num_bn_forget_steps = self.num_bn_forget_steps
-        with self._bn_training_state_switcher():
-            if num_bn_forget_steps is not None and num_bn_forget_steps > 0:
-                with self._bn_momentum_switcher():
-                    for i, (x, _) in ProgressBar(
-                            enumerate(data_loader),
-                            total=num_bn_forget_steps,
-                            desc=self.progressbar_forget_description
-                    ):
-                        if i >= num_bn_forget_steps:
-                            break
-                        self._infer_batch(x)
-            for i, (x, _) in ProgressBar(
-                    enumerate(data_loader),
-                    total=num_init_steps,
-                    desc=self.progressbar_adaptation_description
-            ):
-                if num_init_steps is not None and i >= num_init_steps:
-                    break
-                self._infer_batch(x)
-
-    def run(self, data_loader: InitializingDataLoader, num_init_steps: float):
-        self._run_model_inference(data_loader, num_init_steps)
 
 
 def register_default_init_args(nncf_config: NNCFConfig,
@@ -147,10 +46,11 @@ def register_default_init_args(nncf_config: NNCFConfig,
     :param nncf_config: NNCF config without extra structures.
     :param data_loader: Dataset used for initialization.
     :param device: Device to perform initialization. If `device` is `None` then the device
-            of the model parameters will be used.
+        of the model parameters will be used.
     :return: NNCF config with extra structures.
     """
     nncf_config.register_extra_structs([
-        BNAdaptationInitArgs(data_loader=InitializingDataLoader(data_loader), device=device)
+        BNAdaptationInitArgs(data_loader=InitializingDataLoader(data_loader, nncf_config['batch_size']),
+                             device=device)
     ])
     return nncf_config
