cbb381efc5b22ca1539d218754436df52d0d078f
Daniil Lyakhov <daniil.lyakhov@intel.com>
2021-06-10 11:45:17 +0300

    Save compression controller state together with model (#763)

--- beta/nncf/tensorflow/callbacks/statistics_callback.py
+++ beta/nncf/tensorflow/callbacks/statistics_callback.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b95808846fb32b72717fa182369033c62057e2cbdae4662d1afcb8a521320a10
-size 2856
+oid sha256:316a7bbc59fd1ffe4e0f8e03d5196d8ac8f138fbb3c485400538dd1c519bc4a1
+size 2855
