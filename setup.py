4a9dd7a26fe19b6b44d9a79c4153e76f0e89f32c
Alexander Suslov <alexander.suslov@intel.com>
2021-06-16 09:34:31 +0300

    Python >=3.6.2 (#776)
    * Python >=3.6.2
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1ef423ff5560480ec94ce162dc5abeac8067fbc4b55183fd0b2ddf4965a40da
-size 4084
+oid sha256:62f38439ed4e905f5045efbc10bdcd269890312577e45b2036ed11db9a48df09
+size 4085
