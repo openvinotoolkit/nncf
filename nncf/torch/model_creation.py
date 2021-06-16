b38e57ca735eda76c4c1980ff12a37c52057e069
asenina <anastasia.senina@intel.com>
2021-06-16 10:39:01 +0300

    Fix reference graphs after rebase.

--- nncf/torch/model_creation.py
+++ nncf/torch/model_creation.py
@@ -132,17 +132,8 @@ def create_compressed_model(model: Module, config: NNCFConfig,
             load_state(compressed_model, resuming_state_dict, is_resume=True)
     finally:
         if dump_graphs and is_main_process():
-            if dummy_forward_fn is None:
-                compressed_graph_builder = GraphBuilder(custom_forward_fn=
-                                                        create_dummy_forward_fn(input_info_list,
-                                                                                with_input_tracing=False,
-                                                                                with_output_tracing=False))
-            else:
-                compressed_graph_builder = GraphBuilder(custom_forward_fn=dummy_forward_fn)
-
-            graph = compressed_graph_builder.build_graph(compressed_model,
-                                                         compressed_model.get_tracing_context())
-            graph.visualize_graph(osp.join(config.get("log_dir", "."), "compressed_graph.dot"))
+            compressed_graph = compressed_model.get_graph()
+            compressed_graph.visualize_graph(osp.join(config.get("log_dir", "."), "compressed_graph.dot"))
 
     # Synchronize all processes if run in distributed mode
     if is_dist_avail_and_initialized():
