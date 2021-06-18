0f246f5a5c7ec1cb39958e7fd260036c7316f8c5
Daniil Lyakhov <daniil.lyakhov@intel.com>
2021-06-18 10:26:03 +0300

    Fix import order (#786)

--- examples/tensorflow/segmentation/train.py
+++ examples/tensorflow/segmentation/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6b1ff4ec10cecc815f7e7a6511d9a3b24210e316ed8107e0f2e2ad573e29e1e
+oid sha256:d92b3fcf5cef58c0792b064bb392f10829f31cd2e37f078c20dc980c8c256938
 size 12031
