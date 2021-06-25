8aea28b866e0cbfe2c4d13fc52dca17b8818097c
Daniil Lyakhov <daniil.lyakhov@intel.com>
2021-06-25 10:57:06 +0300

    Unify prepare_checkpoint.py (#797)
    Add param --model-type to make it possible to choose between object detection and segmentation models

--- examples/tensorflow/common/prepare_checkpoint.py
+++ examples/tensorflow/common/prepare_checkpoint.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a7472e706d063919de005303af0d82c8640ae1a43d2ab7f668e9ed783a5acc0
-size 3825
+oid sha256:0f2223e069f15bd81e9e14b9f2d5cf8ad111605a4e90c377f2b2c06fba0b548d
+size 6127
--- tests/tensorflow/test_sanity_sample.py
+++ tests/tensorflow/test_sanity_sample.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb0126815e066b040fc54959a4d0e3a88ecce3d9c091c75f3b563996ade33adc
-size 12377
+oid sha256:fce29d55627d215855a845fecdea1a70fef57ff56a5a75c999a84612697f8aa6
+size 14897
