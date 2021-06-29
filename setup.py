84f58b247c985b65bca36bb6f47ec741ab45644d
Vasily Shamporov <vasily.shamporov@intel.com>
2021-06-29 11:34:36 +0300

    Remove graphviz dependency (#804)
    Not actually necessary to use graphviz package - we render to .dot instead.

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac8f8ddcc46a514db4e5a248df0e29ab57cf40dd742e51c6d5f379b97fe55a8e
-size 4425
+oid sha256:9d50a0b493075da2ffe7798a587505b3dee28a97c6742b31c5ddf1a185940953
+size 4387
