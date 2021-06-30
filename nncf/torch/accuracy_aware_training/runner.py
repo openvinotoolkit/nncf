192b7adfaf94af5c574e8f4e16ff9ef0f4a15a1b
Ivan Lazarevich <ivan.lazarevich@intel.com>
2021-06-30 15:43:57 +0300

    Remove tensorboard dependency (#811)

--- nncf/torch/accuracy_aware_training/runner.py
+++ nncf/torch/accuracy_aware_training/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d4aa3c778fbe427ff0ba761f43c4a8381b62af70e12f8eba84e8b635a8f782d
-size 9937
+oid sha256:dd756053979ce40afd20523a32b5d3adc381bc11eff41c04bc43d0c910e2b051
+size 10106
--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d50a0b493075da2ffe7798a587505b3dee28a97c6742b31c5ddf1a185940953
-size 4387
+oid sha256:818e200fc796ac6f36f9bfbb1748fa0af8d214aa904fa21d9251e9e1f668a233
+size 4347
