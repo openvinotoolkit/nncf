a54ed4e4692d10cc449b9f1b96b8850737d66dda
Daniil Lyakhov <daniil.lyakhov@intel.com>
2021-07-01 08:55:16 +0300

    Fix quantized values miss exact zero (#809)
    `tf.quantization.fake_quant_with_min_max_vars` sometimes returns small values on zero valued inputs due to arithmetic error. Updated export save exact zero value indexes and after saturation issue fix applied restore exact zeros on saved indexes

--- nncf/tensorflow/quantization/utils.py
+++ nncf/tensorflow/quantization/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:512a9c8cdc141565d32dcd6509db6232c0f6ec024d087a915f56b5b9a27090ba
-size 1566
+oid sha256:3ae98287c2194e927a833587f4c6b6f85897ee3de066587f99feb7b40e5cb436
+size 1867
