c6b8ba75cd604bcad2cd75bd0eb349c15c47c07f
Liubov Batanina <piccione-mail@yandex.ru>
2021-07-02 20:05:40 +0300

    Fix validation_freq (#818)

--- examples/tensorflow/classification/main.py
+++ examples/tensorflow/classification/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3049c928e364c8e7b1d3059101fb9c5b69bf688b408157683c73773dbcfa71fb
-size 12966
+oid sha256:26b207b6fc16e70120d2f98900aea60464dfa5c2b2c045dbc5137b958a42c72a
+size 12991
