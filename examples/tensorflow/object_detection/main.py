f9ba8ed97816067a88adb84bb8b0fa07edf12325
Ivan Lazarevich <ivan.lazarevich@intel.com>
2021-07-03 11:56:41 +0300

    Infer NNCF backend from model type in accuracy-aware training (#794)

--- examples/tensorflow/object_detection/main.py
+++ examples/tensorflow/object_detection/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ce55f1208871e009654f802d14b02e6ed6c94495489ef884609dae2b73a64e9
-size 17625
+oid sha256:9b73beec07a9d996ec789bf19c661815d51dfcb6238b7c2adc124e6f602e9a21
+size 17385
--- nncf/common/utils/backend.py
+++ nncf/common/utils/backend.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a54522458c045143c650a64bf381e18aff2b89c34a23d82a78aa36047f909910
-size 606
+oid sha256:10be91db173a5a26fc774e4ed407e8d0e081dd13eb0913c28d290f67fa3ce7ab
+size 1605
