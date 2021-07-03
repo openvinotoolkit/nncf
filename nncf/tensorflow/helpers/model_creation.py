f68bf952d370df9f10f0ebd77fc243e9cf4dee4c
Andrey Churkin <andrey.churkin@intel.com>
2021-07-03 09:15:46 +0300

    Refactoring (#816)
    The `create_compression_algorithm_builder()` method never returns `None` because the `extract_compression_algorithm_configs()` handles this case.

--- nncf/tensorflow/helpers/model_creation.py
+++ nncf/tensorflow/helpers/model_creation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a4e5b826b7c86dbe12e050291eee5320b34642d462af1c71f5d6afb1ee69f5d
-size 3990
+oid sha256:e6c8cd8c8645dc64e2cddee5cd95d3c6e0df4b8e102949324bb840ea70dd505b
+size 4162
