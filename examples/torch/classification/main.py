4ff02bb9069365325be10df15eedc907643ea994
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2021-07-07 10:50:52 +0300

    Accuracy validation doesn't depend on batch size and number of GPU (#829)
    Previously, last data point in all DataLoaders were dropped to prevent issue with BN (#379)
    But it's actual for training only, on eval it shouldn't happen.

--- examples/torch/classification/main.py
+++ examples/torch/classification/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afc7fded01d591560d2e8b9089ec401aeb4baaa93eafebe3300cd9bef8e543c2
-size 27989
+oid sha256:bebb89853444a713806d57405c2a7052c6beb40030f7be0f3642e8c64bcb15bb
+size 27990
--- examples/torch/semantic_segmentation/main.py
+++ examples/torch/semantic_segmentation/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38d8a5cbae5e15f203f8f318fd2c1514241def370a09bc9b33cf218733ab8f14
-size 25976
+oid sha256:fcba675e423b8e90d22cb74584e76f789563acac0baf5967e59d9213a9d6699c
+size 25977
