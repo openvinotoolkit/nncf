4da6e1ea7ea975e6a3fcb45b5ee871989263d186
Alexander Suslov <alexander.suslov@intel.com>
2021-07-14 09:05:13 +0300

    Updated update_eval_results.py relative to # 830 PR (#843)

--- tools/update_eval_results.py
+++ tools/update_eval_results.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3149eb54582883aee7f14e08b52fa441043403d47cae732cd7c1b1624838560
-size 12922
+oid sha256:fe1f1d60bda34053e978bab36219c15d8a30fe204c4813aa9acf8562a5f770b2
+size 13443
