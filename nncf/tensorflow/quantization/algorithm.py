b610b2ab587652f6d3fe8e39a63d5c01e49ada49
Alexander Suslov <alexander.suslov@intel.com>
2021-07-22 13:32:11 +0300

    Fixed statistic collection for the algo-mixing scenario (#866)
    Ref 60535

--- nncf/tensorflow/quantization/algorithm.py
+++ nncf/tensorflow/quantization/algorithm.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b5bdb8c8e307094e17538a9172f57cb56c40d7b23bc2cf9166dc0f20f1be5d7
-size 29922
+oid sha256:900da16c57e087801a2e32b17d34ad36af71ea98b5e6a9a6679afb1fdfc794d1
+size 29969
