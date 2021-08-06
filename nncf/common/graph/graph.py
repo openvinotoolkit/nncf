68c3ecb17361fb6d6d7ba7a8ac0db4869092a268
Vasily Shamporov <vasily.shamporov@intel.com>
2021-08-06 14:58:30 +0300

    Do not fail if PNG rendering failed (#882)

--- nncf/common/graph/graph.py
+++ nncf/common/graph/graph.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6fa9fb916a60e814d5c63089ec4439e4c456fe02e3609f738062e4896c83fcc
-size 26721
+oid sha256:fd8d19e6b4392038f7d69d1110c79d27152e35c24d0c4584f899d5b0e3ecec94
+size 26841
--- nncf/tensorflow/api/compression.py
+++ nncf/tensorflow/api/compression.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9570445afc8be2ccd211914d8ce0c5f5157361c1b85699caad405e5925fdb88f
-size 2310
+oid sha256:4c67b020c524d213d114c96b48ceeaa8dc9e52d4d637c5dde1836b299e0ce0de
+size 2309
