e2364ae54b22cd386cc0b7dda5238b6d1c824253
Vasily Shamporov <vasily.shamporov@intel.com>
2021-08-13 11:53:32 +0300

    Fix embedding sparsity (#891)

--- nncf/torch/sparsity/collector.py
+++ nncf/torch/sparsity/collector.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a7ae0c9a7e08e8374370fc46957059df2d6efb2b45a8f1407985dacdc042b4f
-size 2968
+oid sha256:35971f50d565e8548a5054a9305bbaeeb39755dd36bef5a6b3f4e24d24dca0af
+size 3002
--- tests/torch/sparsity/test_common.py
+++ tests/torch/sparsity/test_common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7b9a6a2d5a34425ee20e5249e870f93c9d9b6444807ead4a53679850b9d2749
-size 16517
+oid sha256:e039c4816baa82535ee90b2a82afbe2c271f258180434e73e2442dbad7317a87
+size 17354
