a0fad232c094c0811261d2b3c9efc5c2c35029b9
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2021-08-19 19:26:57 +0300

    Add group_norm + relu pattern (#900)

--- nncf/torch/hardware/fused_patterns.py
+++ nncf/torch/hardware/fused_patterns.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbde56b5a0e91eeb62c3db56dfb892d5fa9b2bd3581b990c7893a5ba7fc69409
-size 2106
+oid sha256:46f0b0b5a262183c3bbc02aa483128a912c2bacda62d22ea568202427011ef11
+size 2476
