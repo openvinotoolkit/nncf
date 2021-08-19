82829fded29c643a1a74f47bbf74f03fab33b5ce
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2021-08-19 19:26:57 +0300

    Add group_norm + relu pattern (#900)

--- nncf/torch/hardware/fused_patterns.py
+++ nncf/torch/hardware/fused_patterns.py
@@ -1,9 +1,11 @@
 from nncf.common.graph.patterns import GraphPattern
 from nncf.common.graph.patterns import HWFusedPatterns
-from nncf.torch.graph.pattern_operations import ATOMIC_ACTIVATIONS_OPERATIONS
 from nncf.torch.graph.pattern_operations import ARITHMETIC_OPERATIONS
+from nncf.torch.graph.pattern_operations import ATOMIC_ACTIVATIONS_OPERATIONS
 from nncf.torch.graph.pattern_operations import BATCH_NORMALIZATION_OPERATIONS
+from nncf.torch.graph.pattern_operations import GROUP_NORMALIZATION_OPERATIONS
 from nncf.torch.graph.pattern_operations import LINEAR_OPERATIONS
+from nncf.torch.graph.pattern_operations import RELU_OPERATIONS
 from nncf.torch.graph.patterns import create_h_sigmoid_act
 from nncf.torch.graph.patterns import create_h_swish_act
 from nncf.torch.graph.patterns import create_swish_act
@@ -39,6 +41,13 @@ def _get_torch_hw_fused_patterns() -> HWFusedPatterns:
     retval.register(activations + batch_norm, 'ACTIVATIONS + BN', match=True)
     retval.register(arithmetic_ops + batch_norm_activations_permutation,
                     'ARITHMETIC + BN_ACT_PERM', match=True)
+
+    group_norm = GraphPattern()
+    group_norm.add_node(**GROUP_NORMALIZATION_OPERATIONS)
+    relu = GraphPattern()
+    relu.add_node(**RELU_OPERATIONS)
+    retval.register(group_norm + relu, 'GROUP_NORM + RELU', match=True)
+
     return retval
 
 
