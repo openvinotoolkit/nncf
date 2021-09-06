b606690a272509b977a7559b29c97a453ade1401
Kaglinskaya <maria.kaglinskaya@intel.com>
2021-09-06 17:11:50 +0300

    Small fixes

--- tests/torch/run_examples_for_test_sota.py
+++ tests/torch/run_examples_for_test_sota.py
@@ -5,12 +5,12 @@ from examples.torch.object_detection import main as od_main
 from examples.torch.semantic_segmentation import main as seg_main
 from tests.torch.helpers import create_dataloader_with_num_workers
 
-cls_main.create_datasets = create_dataloader_with_num_workers(cls_main.create_datasets, num_workers=0,
+cls_main.create_data_loaders = create_dataloader_with_num_workers(cls_main.create_data_loaders, num_workers=0,
                                                               sample_type='classification')
 od_main.create_dataloaders = create_dataloader_with_num_workers(od_main.create_dataloaders, num_workers=0,
                                                                 sample_type='object_detection')
 seg_main.load_dataset = create_dataloader_with_num_workers(seg_main.load_dataset, num_workers=0,
-                                                           sample_type='semntic_segmentation')
+                                                           sample_type='semantic_segmentation')
 
 SAMPLES = {
     'classification': cls_main.main,
