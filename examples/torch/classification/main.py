029b1fca4c6613a11102522454b9c845e2d48488
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-09-15 10:46:09 +0300

    Remove random transformations from mock datasets in PT classification (#929)
    This will allow to make results with mock datasets more stable

--- examples/torch/classification/main.py
+++ examples/torch/classification/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5786b323eb8abcb4728a5497d5a0e427a98c33e00d549aa175921e67d2f5b074
-size 28120
+oid sha256:2b43cbc4803e198c04dce1a114f3a7dcb80b6cc5deb38b6f500e86371275f0e3
+size 28573
