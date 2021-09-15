244185df33d1ab67dc8cbd06f046025023df8ddb
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-09-15 11:54:06 +0300

    Fix bug in __getattr__ of NNCFNetwork (#914)
    Add a capability to change the bound functions of a wrapped module by passing NNCFNetwork instance as a first argument

--- nncf/torch/nncf_network.py
+++ nncf/torch/nncf_network.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09e811ff8176c516a5ae5308a7c3e58cc0c911c0f5837f1741325b092f928c87
-size 35162
+oid sha256:8f3ab9e3b39968d59eac93d6849c9b4b162802d8432e2e7fbb29c74d3fe44c70
+size 36042
--- tests/torch/test_nncf_network.py
+++ tests/torch/test_nncf_network.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22bb08acf52a20357dd4b1b366406af41ed0e69d3ba6e7b03f972267655b09d6
-size 40404
+oid sha256:d985d7cfd9615e6c347b803b838852e45d188019ce9c7d24d69263db4ef80181
+size 41773
