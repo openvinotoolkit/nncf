eaaf6c97f948f94d4807bf4e8523fda4cbfd282e
Vasily Shamporov <vasily.shamporov@intel.com>
2021-09-17 12:29:55 +0300

    Fix patch:

--- tests/torch/test_sanity_third_party.py
+++ tests/torch/test_sanity_third_party.py
@@ -103,7 +103,7 @@ class TestTransformers:
     def test_xnli_train(self, temp_folder):
         com_line = "examples/pytorch/text-classification/run_xnli.py --model_name_or_path bert-base-chinese" \
                    " --language zh --train_language zh --do_train --per_gpu_train_batch_size 24" \
-                   " --learning_rate 5e-5 --num_train_epochs 1.0 --max_seq_length 128 --output_dir {}" \
+                   " --learning_rate 5e-5 --num_train_epochs 0.0001 --max_seq_length 128 --output_dir {}" \
                    " --save_steps 200 --nncf_config nncf_bert_config_xnli.json" \
             .format(os.path.join(temp_folder["models"], "xnli"))
         runner = Command(create_command_line(com_line, self.VENV_ACTIVATE, self.PYTHON_EXECUTABLE,
@@ -116,6 +116,7 @@ class TestTransformers:
         com_line = "examples/pytorch/text-classification/run_xnli.py --model_name_or_path {output}" \
                    " --language zh --do_eval --learning_rate 5e-5 --max_seq_length 128 --output_dir" \
                    " {output} --nncf_config nncf_bert_config_xnli.json --per_gpu_eval_batch_size 24" \
+                   " --max_eval_samples 10" \
             .format(output=os.path.join(temp_folder["models"], "xnli"))
         runner = Command(create_command_line(com_line, self.VENV_ACTIVATE, self.PYTHON_EXECUTABLE,
                                              self.CUDA_VISIBLE_STRING), self.TRANSFORMERS_REPO_PATH)
@@ -124,11 +125,10 @@ class TestTransformers:
     @pytest.mark.dependency(depends=['install_trans'], name='squad_train')
     def test_squad_train(self, temp_folder):
         com_line = "examples/pytorch/question-answering/run_qa.py --model_name_or_path " \
-                   "bert-large-uncased-whole-word-masking-finetuned-squad --do_train " \
-                   "--train_file {}/squad/train-v1.1.json" \
-                   " --learning_rate 3e-5 --num_train_epochs 1 --max_seq_length 384 --doc_stride 128 --output_dir " \
-                   "{} --per_gpu_train_batch_size=1 --save_steps=200 --nncf_config" \
-                   " nncf_bert_config_squad.json".format(DATASET_PATH, os.path.join(temp_folder["models"], "squad"))
+                   "bert-large-uncased-whole-word-masking-finetuned-squad --dataset_name squad --do_train " \
+                   " --learning_rate 3e-5 --num_train_epochs 0.0001 --max_seq_length 384 --doc_stride 128 " \
+                   " --output_dir {} --per_gpu_train_batch_size=1 --save_steps=200 --nncf_config" \
+                   " nncf_bert_config_squad.json".format(os.path.join(temp_folder["models"], "squad"))
         runner = Command(create_command_line(com_line, self.VENV_ACTIVATE, self.PYTHON_EXECUTABLE,
                                              self.CUDA_VISIBLE_STRING), self.TRANSFORMERS_REPO_PATH)
         runner.run()
@@ -137,10 +137,11 @@ class TestTransformers:
     @pytest.mark.dependency(depends=['install_trans', 'squad_train'])
     def test_squad_eval(self, temp_folder):
         com_line = "examples/pytorch/question-answering/run_qa.py --model_name_or_path {output}" \
-                   " --do_eval --do_lower_case  --validation_file {}/squad/dev-v1.1.json --learning_rate 3e-5" \
+                   " --do_eval --dataset_name squad  --learning_rate 3e-5" \
                    " --max_seq_length 384 --doc_stride 128 --per_gpu_eval_batch_size=4 --output_dir {output} " \
-                   "--nncf_config nncf_bert_config_squad.json" \
-            .format(DATASET_PATH, output=os.path.join(temp_folder["models"], "squad"))
+                   " --max_eval_samples 10" \
+                   " --nncf_config nncf_bert_config_squad.json" \
+            .format(output=os.path.join(temp_folder["models"], "squad"))
         runner = Command(create_command_line(com_line, self.VENV_ACTIVATE, self.PYTHON_EXECUTABLE,
                                              self.CUDA_VISIBLE_STRING), self.TRANSFORMERS_REPO_PATH)
         runner.run()
@@ -148,11 +149,11 @@ class TestTransformers:
     @pytest.mark.dependency(depends=['install_trans'], name='glue_roberta_train')
     def test_glue_train(self, temp_folder):
         com_line = "examples/pytorch/text-classification/run_glue.py --model_name_or_path" \
-                   " roberta-large-mnli --task_name mnli --do_train --train_file {}/glue/glue_data/MNLI/train.tsv" \
-                   " --per_gpu_train_batch_size 4 --learning_rate 2e-5 --num_train_epochs 1.0 --max_seq_length 128 " \
-                   "--output_dir {} --save_steps 200 --nncf_config" \
+                   " roberta-large-mnli --task_name mnli --do_train " \
+                   " --per_gpu_train_batch_size 4 --learning_rate 2e-5 --num_train_epochs 0.001 --max_seq_length 128 " \
+                   " --output_dir {} --save_steps 200 --nncf_config" \
                    " nncf_roberta_config_mnli.json" \
-            .format(DATASET_PATH, os.path.join(temp_folder["models"], "roberta_mnli"))
+            .format(os.path.join(temp_folder["models"], "roberta_mnli"))
         runner = Command(create_command_line(com_line, self.VENV_ACTIVATE, self.PYTHON_EXECUTABLE,
                                              self.CUDA_VISIBLE_STRING), self.TRANSFORMERS_REPO_PATH)
         runner.run()
@@ -162,8 +163,9 @@ class TestTransformers:
     def test_glue_eval(self, temp_folder):
         com_line = "examples/pytorch/text-classification/run_glue.py --model_name_or_path {output}" \
                    " --task_name mnli --do_eval --validation_file {}/glue/glue_data/MNLI/dev_matched.tsv " \
-                   "--learning_rate 2e-5" \
-                   " --num_train_epochs 1.0 --max_seq_length 128 --output_dir {output}" \
+                   " --learning_rate 2e-5" \
+                   " --max_seq_length 128 --output_dir {output}" \
+                   " --max_eval_samples 10" \
                    " --nncf_config nncf_roberta_config_mnli.json" \
             .format(DATASET_PATH, output=os.path.join(temp_folder["models"], "roberta_mnli"))
         runner = Command(create_command_line(com_line, self.VENV_ACTIVATE, self.PYTHON_EXECUTABLE,
@@ -175,7 +177,7 @@ class TestTransformers:
         com_line = "examples/pytorch/text-classification/run_glue.py --model_name_or_path" \
                    " distilbert-base-uncased --train_file {}/glue/glue_data/SST-2/train.tsv" \
                    " --task_name sst2 --do_train --max_seq_length 128 --per_gpu_train_batch_size 8" \
-                   " --learning_rate 5e-5 --num_train_epochs 3.0" \
+                   " --learning_rate 5e-5 --num_train_epochs 0.001" \
                    " --output_dir {} --save_steps 200 --nncf_config" \
                    " nncf_distilbert_config_sst2.json".format(DATASET_PATH, os.path.join(temp_folder["models"],
                                                                                          "distilbert_output"))
@@ -189,6 +191,7 @@ class TestTransformers:
         com_line = "examples/pytorch/text-classification/run_glue.py --model_name_or_path {output}" \
                    " --task_name sst2 --do_eval --max_seq_length 128" \
                    " --output_dir {output} --validation_file {}/glue/glue_data/SST-2/test.tsv" \
+                   " --max_eval_samples 10" \
                    " --nncf_config nncf_distilbert_config_sst2.json" \
             .format(DATASET_PATH, output=os.path.join(temp_folder["models"], "distilbert_output"))
         runner = Command(create_command_line(com_line, self.VENV_ACTIVATE, self.PYTHON_EXECUTABLE,
@@ -198,11 +201,12 @@ class TestTransformers:
     @pytest.mark.dependency(depends=['install_trans'], name='lm_train')
     def test_lm_train(self, temp_folder):
         com_line = "examples/pytorch/language-modeling/run_clm.py --model_name_or_path gpt2" \
-                   " --do_train --per_gpu_train_batch_size 8" \
-                   " --train_data_file {}/wikitext-2-raw/wiki.train.raw " \
+                   " --do_train --per_gpu_train_batch_size 2" \
+                   " --dataset_name wikitext --dataset_config_name wikitext-2-raw-v1 " \
+                   " --num_train_epochs 0.001" \
                    " --output_dir {} --nncf_config" \
-                   " nncf_gpt2_config_wikitext_hw_config.json".format(DATASET_PATH, os.path.join(temp_folder["models"],
-                                                                                                 "lm_output"))
+                   " nncf_gpt2_config_wikitext_hw_config.json".format(os.path.join(temp_folder["models"],
+                                                                                   "lm_output"))
         runner = Command(create_command_line(com_line, self.VENV_ACTIVATE, self.PYTHON_EXECUTABLE,
                                              self.CUDA_VISIBLE_STRING), self.TRANSFORMERS_REPO_PATH)
         runner.run()
@@ -211,10 +215,11 @@ class TestTransformers:
     @pytest.mark.dependency(depends=['install_trans', 'lm_train'])
     def test_lm_eval(self, temp_folder):
         com_line = "examples/language-modeling/run_clm.py " \
-                   "--model_name_or_path {output} --do_eval " \
-                   " --output_dir {output} --eval_data_file {}/wikitext-2-raw/wiki.train.raw" \
+                   " --model_name_or_path {output} --do_eval " \
+                   " --output_dir {output} --dataset_name wikitext --dataset_config_name wikitext-2-raw-v1" \
+                   " --max_eval_samples 10" \
                    " --nncf_config nncf_gpt2_config_wikitext_hw_config.json" \
-            .format(DATASET_PATH, output=os.path.join(temp_folder["models"], "lm_output"))
+            .format(output=os.path.join(temp_folder["models"], "lm_output"))
         runner = Command(create_command_line(com_line, self.VENV_ACTIVATE, self.PYTHON_EXECUTABLE,
                                              self.CUDA_VISIBLE_STRING), self.TRANSFORMERS_REPO_PATH)
         runner.run()
@@ -222,10 +227,13 @@ class TestTransformers:
     @pytest.mark.dependency(depends=['install_trans'])
     def test_convert_to_onnx(self, temp_folder):
         com_line = "examples/pytorch/question-answering/run_qa.py --model_name_or_path {output} " \
-                   " --validation_file {}/squad/dev-v1.1.json" \
+                   " --do_eval" \
+                   " --dataset_name squad " \
+                   " --max_eval_samples 10" \
                    " --output_dir {output}" \
-                   " --to_onnx {output}/model.onnx".format(DATASET_PATH,
-                                                           output=os.path.join(temp_folder["models"], "squad"))
+                   " --to_onnx {output}/model.onnx" \
+                   " --nncf_config nncf_bert_config_squad.json".format(output=os.path.join(temp_folder["models"],
+                                                                                           "squad"))
         runner = Command(create_command_line(com_line, self.VENV_ACTIVATE, self.PYTHON_EXECUTABLE,
                                              self.CUDA_VISIBLE_STRING), self.TRANSFORMERS_REPO_PATH)
         runner.run()
