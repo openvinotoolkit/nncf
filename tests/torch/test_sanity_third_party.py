371ba34762ad2bafd87263c54c4d4a9931c6de7f
Vasily Shamporov <vasily.shamporov@intel.com>
2021-09-22 12:56:23 +0300

    Update transformers patch to transformers v4.9.1 (#883)
    * Update transformers patch to transformers v4.9.1
    
    * Refactor third_party_sanity test code
    
    * Fix patch
    
    * Restore dataset paths
    
    * Update patch
    
    * Install onnx
    
    * Fix patch:

--- tests/torch/test_sanity_third_party.py
+++ tests/torch/test_sanity_third_party.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2b8620c0d8ff26b357d2287ae2f0ca7fe41b58463eb346cacadb5c0977e4e59
-size 14089
+oid sha256:8ce3c054932b5efda71554328138026979fdef22e8bfef915e40ad58813373c1
+size 14767
--- third_party_integration/huggingface_transformers/README.md
+++ third_party_integration/huggingface_transformers/README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ab30aa6f5b36d1028fb81606a49d67fb4339bf956053c5da48a2279b62e68ac
+oid sha256:50e200faea31e1e94af7a0d21d80ba646d0bc588a8502c661961e474a68e1313
 size 5841
