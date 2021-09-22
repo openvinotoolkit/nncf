8fb4f86c7d7041dccfb0e40b636eef05010dd2e3
Vasily Shamporov <vasily.shamporov@intel.com>
2021-09-22 17:28:43 +0300

    Fix view with size (#932)
    * Fix view with size in BERT
    
    * Renamings
    
    * Fix equalities

--- nncf/torch/dynamic_graph/graph.py
+++ nncf/torch/dynamic_graph/graph.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:899453da3534e6bcfe3cc15cc92b3ea6c095f73f19a21680ccce82de962e54e4
-size 26584
+oid sha256:cb658c8053e9282f07054f1f571f351e418bc610e846c78565cc7c462088c4fa
+size 28133
