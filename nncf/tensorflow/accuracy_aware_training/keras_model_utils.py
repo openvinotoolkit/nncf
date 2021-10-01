a7936fa08640f5fc95b782d73a048294fd26be57
Evgeny Tsykunov <e.tsykunov@gmail.com>
2021-10-01 15:53:00 +0300

    refactor keras_model_utils.py

--- nncf/tensorflow/accuracy_aware_training/keras_model_utils.py
+++ nncf/tensorflow/accuracy_aware_training/keras_model_utils.py
@@ -16,7 +16,6 @@ from tensorflow.python.keras.engine import training_utils
 from tensorflow.python.profiler import trace
 from tensorflow.python.eager import context
 from tensorflow.python.keras import callbacks as callbacks_module
-from tensorflow.python.keras.callbacks import History
 from tensorflow.python.keras.engine import data_adapter
 
 from nncf.common.accuracy_aware_training import create_accuracy_aware_training_loop
@@ -30,52 +29,44 @@ def accuracy_aware_fit(cls_instance, train_dataset, compression_ctrl,
     if result_dict_to_val_metric_fn is None:
         result_dict_to_val_metric_fn = lambda metric: metric
 
-    def train_epoch_fn(compression_ctrl, model, epoch):
-        model.reset_metrics()
-
-        with model.distribute_strategy.scope(), \
-            training_utils.RespectCompiledTrainableState(model):
-            # pylint: disable=protected-access
-            data_handler = data_adapter.DataHandler(
-                x=train_dataset,
-                y=None,
-                sample_weight=None,
-                batch_size=batch_size,
-                steps_per_epoch=steps_per_epoch,
-                initial_epoch=initial_epoch,
-                epochs=1,
-                shuffle=True,
-                class_weight=None,
-                max_queue_size=10,
-                workers=1,
-                use_multiprocessing=False,
-                model=model,
-                steps_per_execution=model._steps_per_execution)
+    with cls_instance.distribute_strategy.scope(), \
+        training_utils.RespectCompiledTrainableState(cls_instance):
+        # pylint: disable=protected-access
+        data_handler = data_adapter.DataHandler(
+            x=train_dataset,
+            y=None,
+            sample_weight=None,
+            batch_size=batch_size,
+            steps_per_epoch=steps_per_epoch,
+            initial_epoch=initial_epoch,
+            epochs=1,
+            shuffle=True,
+            class_weight=None,
+            max_queue_size=10,
+            workers=1,
+            use_multiprocessing=False,
+            model=cls_instance,
+            steps_per_execution=cls_instance._steps_per_execution)
 
         if not isinstance(callbacks, callbacks_module.CallbackList):
-            callbacks_container = callbacks_module.CallbackList(
+            callbacks = callbacks_module.CallbackList(
                 callbacks,
                 add_history=True,
-                model=model,
+                model=cls_instance,
                 epochs=1,
                 verbose=1,
                 add_progbar=True,
                 steps=data_handler.inferred_steps
-            )
+                )
 
-        # Check if callbacks.on_train_begin() has been called before the first epoch.
-        # If it is not - call it.
-        for callback in callbacks_container:
-            if isinstance(callback, History):
-                if not hasattr(callback, 'epoch'):
-                    callbacks_container.on_train_begin()
-                    break
+    def train_epoch_fn(compression_ctrl, model, epoch):
+        model.reset_metrics()
 
         if model.train_function is None:
             model.train_function = model.make_train_function()
         _, iterator = next(data_handler.enumerate_epochs())
 
-        callbacks_container.on_epoch_begin(epoch)
+        callbacks.on_epoch_begin(epoch)
         with data_handler.catch_stop_iteration():
             for step in data_handler.steps():
                 with trace.Trace(
@@ -84,20 +75,20 @@ def accuracy_aware_fit(cls_instance, train_dataset, compression_ctrl,
                     step_num=step,
                     batch_size=None,
                     _r=1):
-                    callbacks_container.on_train_batch_begin(step)
+                    callbacks.on_train_batch_begin(step)
                     tmp_logs = model.train_function(iterator)
                     if data_handler.should_sync:
                         context.async_wait()
                     logs = tmp_logs
                     end_step = step + data_handler.step_increment
-                    callbacks_container.on_train_batch_end(end_step, logs)
+                    callbacks.on_train_batch_end(end_step, logs)
                     if model.stop_training:
                         break
 
         if logs is None:
             raise ValueError('Expect x to be a non-empty array or dataset.')
         epoch_logs = copy.copy(logs)
-        callbacks_container.on_epoch_end(epoch, epoch_logs)
+        callbacks.on_epoch_end(epoch, epoch_logs)
 
     if validation_data is None:
         validation_data = train_dataset
@@ -116,6 +107,7 @@ def accuracy_aware_fit(cls_instance, train_dataset, compression_ctrl,
         return result_dict_to_val_metric_fn(val_logs)
 
 
+    callbacks.on_train_begin()
     cls_instance.original_model_accuracy = uncompressed_model_accuracy
     acc_aware_training_loop = create_accuracy_aware_training_loop(nncf_config, compression_ctrl)
     cls_instance = acc_aware_training_loop.run(cls_instance,
@@ -123,3 +115,4 @@ def accuracy_aware_fit(cls_instance, train_dataset, compression_ctrl,
                                                validate_fn=validate_fn,
                                                tensorboard_writer=tensorboard_writer,
                                                log_dir=log_dir)
+    callbacks.on_train_end()
