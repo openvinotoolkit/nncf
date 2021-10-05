53c2a590ad70f3b47345fcf12044dfa1ee2b7d19
Maria Kaglinskaya <maria.kaglinskaya@intel.com>
2021-10-05 10:57:00 +0300

    Test compression training fix  (#962)
    Fixed test for compression training for LeGR algorithm. Ticket - 62532.
    The number of training epochs and potential accuracy diff has been increased.
    
    @ljaljushkin @vshampor

--- tests/torch/test_compression_training.py
+++ tests/torch/test_compression_training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dadd5419c3d41151e6bf5b00ca7ee1c0d908c99cd334e0d5fda1699991ab2c1
+oid sha256:c0a6fa254beeb9a22459fa24d7b6eb4306bd82c33018c363af8513836f9c6a9d
 size 13239
