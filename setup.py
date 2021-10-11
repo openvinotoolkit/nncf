8bab7b2671419c7cdedd8df8f11b10d6def7689b
Evgeny Tsykunov <e.tsykunov@gmail.com>
2021-10-11 13:47:00 +0300

    uodate setup, tests are passed

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ if python_version < (3, 6, 2):
 version_string = "{}{}".format(sys.version_info[0], sys.version_info[1])
 
 _extra_deps = [
-    "tensorflow==2.4.2",
+    "tensorflow==2.4.3",
     "torch>=1.5.0, <=1.9.1, !=1.8.0",
 ]
 
