be2aab8764e752d6f818b80533ae6bd5467462fe
Alexander Suslov <alexander.suslov@intel.com>
2021-10-13 11:26:32 +0300

    Revert scikit-learn version to align with OpenVINO components (#980)
    ### Changes
    
    Revert scikit-learn>=1.0 -> scikit-learn>=0.24.0 for python_version>='3.7'
    
    ### Reason for changes
    
    Adjust scikit-learn version with OpenVINO components
    
    ### Related tickets
    
    Bug from the validation team
    
    ### Tests
    
    N/A

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:802834d9db316ffb4aec1a966ff702cfbfd1c1d97c381d9855fe6c7d4f1804aa
-size 4979
+oid sha256:09a4bcac66eea9eb60fd1178953fd95aa9e506b96ba738f2ee325efd79ae2a43
+size 4982
