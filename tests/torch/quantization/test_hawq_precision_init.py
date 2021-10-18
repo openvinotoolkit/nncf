2a9fe48b4ec72b202c2b7b5e014dd6c63bbf0eb8
Sergei Kholkin <holckin100@gmail.com>
2021-10-18 15:28:26 +0300

    Fix test_hawq_on_single_conv_without_quantizers (#981)
    ### Changes
    
    Changing references tolerance values for test_hawq_on_single_conv_without_quantizers 
    
    ### Reason for changes
    
    Because of changes that came through time and environment changes (such as torch version changes) actual and reference test results started to diverge. Which lead to failing of cpu precommit. Increase of tolerance values should protect cpu and cuda parts of test from failing through environment changes or etc.
    
    ### Related tickets
    
    CVS-61609
    
    ### Tests

--- tests/torch/quantization/test_hawq_precision_init.py
+++ tests/torch/quantization/test_hawq_precision_init.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d81d9698451bf5ffc4fb88a480c209913fd7d60c88607c5bbde83851f62f5578
+oid sha256:1ad171698b82c721501d285d4a885593fe22bd81279bd7aaf6af6e45648c7046
 size 36905
