6fcaf9beb026e6bfd58693edae0bbc973161a8b9
Sergei Kholkin <holckin100@gmail.com>
2021-10-20 10:54:33 +0300

    CI test_compression_training.py timeout error fix (#971)
    * CI mobilenet cifar CPU only timeout fix
    * Fix torch 1.9.1 deterministic problem with asym quantization
    * Mobilenet v2 sym int8 to cut off epochs to 1
    * Fix eval part of mobilenet asym int8 compression training test
    * Change eval tolerance through possible non deterministic divergence

--- tests/torch/test_compression_training.py
+++ tests/torch/test_compression_training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0a6fa254beeb9a22459fa24d7b6eb4306bd82c33018c363af8513836f9c6a9d
-size 13239
+oid sha256:ce8a8ce4eb6018aa94f8b80d9e9fa3ef85ea06426db78a4e9943ba7b76480a74
+size 13713
