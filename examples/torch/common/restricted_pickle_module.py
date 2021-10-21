77c50d44a4906edf646735a2dfd3d68a0671a170
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-10-21 15:51:40 +0300

    Add additional func (#989)

--- examples/torch/common/restricted_pickle_module.py
+++ examples/torch/common/restricted_pickle_module.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18b9de6c8b30325aa98ea1457f293f6e01ee846e4eda240528a64b76f653aff0
-size 2737
+oid sha256:24ab601dd221ae4866a79c764814af9bf02f7ce35105907f7957ef57297f0cce
+size 2756
