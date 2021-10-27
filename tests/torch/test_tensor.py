d97c3761086b6378ca5663d5afa7d02f21d5f312
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-10-27 16:18:52 +0300

    Fix bug

--- tests/torch/test_tensor.py
+++ tests/torch/test_tensor.py
@@ -3,13 +3,15 @@ from nncf.torch.tensor import PTNNCFTensorProcessor
 import torch
 
 
-@pytest.mark.parametrize('device', (torch.device('cpu'), torch.device('cuda:0')))
+@pytest.mark.parametrize('device', (torch.device('cpu'), 'cuda'))
 def test_create_tensor(device):
     if not torch.cuda.is_available():
-        if device == torch.device('cuda'):
+        if device == 'cuda':
             pytest.skip('There are no available CUDA devices')
     shape = [1, 3, 10, 100]
     tensor = PTNNCFTensorProcessor.ones(shape, device)
     assert torch.is_tensor(tensor.tensor)
+    if device == 'cuda':
+        device = torch.device('cuda:0')
     assert tensor.device == device
     assert list(tensor.tensor.shape) == shape
