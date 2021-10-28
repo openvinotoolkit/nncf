e3b13ed92442ba93618cedaa7bb232eac769800b
Vasily Shamporov <vasily.shamporov@intel.com>
2021-10-28 21:50:59 +0300

    Freeze pyparsing version (#996)
    ### Changes
    
    The pyparsing version has been frozen at 2.x.
    
    ### Reason for changes
    
    The recent major version update of pyparsing breaks the loading/matching of reference .dot files representing the NNCF graph structure which are used in tests.
    
    ### Related tickets
    
    69520
    
    ### Tests
    
    test_build_graph and related precommit failures should be passing now.

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6a8a626e0835c269dcfc4327b22d021343500949d08c813465da5de07cea9d2
-size 4982
+oid sha256:08626b118aaeb9c52dec04b1bb8e7954ecbea0410b0541d05a7d90da53692eef
+size 5392
