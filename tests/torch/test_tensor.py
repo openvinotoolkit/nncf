c86d68d372cb860e08b43cf6b3c2436a16c812a3
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-10-28 11:12:58 +0300

    Now check device.type which is independent of GPU number

--- tests/torch/test_tensor.py
+++ tests/torch/test_tensor.py
@@ -3,15 +3,13 @@ from nncf.torch.tensor import PTNNCFTensorProcessor
 import torch
 
 
-@pytest.mark.parametrize('device', (torch.device('cpu'), 'cuda'))
+@pytest.mark.parametrize('device', (torch.device('cpu'), torch.device('cuda')))
 def test_create_tensor(device):
     if not torch.cuda.is_available():
-        if device == 'cuda':
+        if device == torch.device('cuda'):
             pytest.skip('There are no available CUDA devices')
     shape = [1, 3, 10, 100]
     tensor = PTNNCFTensorProcessor.ones(shape, device)
     assert torch.is_tensor(tensor.tensor)
-    if device == 'cuda':
-        device = torch.device('cuda:0')
-    assert tensor.device == device
+    assert tensor.device.type == device.type
     assert list(tensor.tensor.shape) == shape
