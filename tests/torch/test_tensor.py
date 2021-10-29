7b609f373d9de1ea22e87e7c0f5159518270c887
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-10-29 11:25:01 +0300

    Fix bug in test (#995)
    * Fix bug
    
    * Now check device.type which is independent of GPU number

--- tests/torch/test_tensor.py
+++ tests/torch/test_tensor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:372b98e212335537a43aba84dc40f8c9756075cc5ed34387c5137b45e4863c12
-size 545
+oid sha256:9875e078ddb5c536a87cb38e08df2f4f30b08ccec5fd964872ff70b82fa034b2
+size 553
