b6bb3702b3d9bae90dd42209a4701c647acb198a
Daniil Lyakhov <daniil.lyakhov@intel.com>
2021-11-08 13:25:48 +0300

    Add leaky relu metatype (#1005)

--- nncf/torch/pruning/operations.py
+++ nncf/torch/pruning/operations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe4ede141e7643757fcac181273d2879c9da8e7cd71db00d0cb39775bf15ac5f
-size 13557
+oid sha256:1993deee947e0305eafe5cb03ba2f5bceb2bf2b918d0a7fa1e305cf2a1f37ee2
+size 13599
