22e68138ffcf3d3d6880fa68d814412b523f41b1
Alexander Suslov <alexander.suslov@intel.com>
2021-11-17 21:26:48 +0300

    turned off downloading weights for models from the ncf_keras_graph_equality tests (#1016)

--- tests/tensorflow/test_nncf_keras_graph_equality.py
+++ tests/tensorflow/test_nncf_keras_graph_equality.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77b5aff19ff5694290bf4cc113e5918f65fc4710ed8df6bd636937ed12593d88
-size 2730
+oid sha256:036ff70626c1152cd7a130d3cdccb4a1ab1d1a6b6e0d7bd671d837815e4e2aa2
+size 2742
