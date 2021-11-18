1317def1e5e242f4859ac1cb64c1075c3c848632
Alexander Suslov <alexander.suslov@intel.com>
2021-11-18 12:43:51 +0300

    Bump tensorflow from ==2.4.3 to ~=2.4.3 (#1015)
    Bump tensorflow from ==2.4.3 to ~=2.4.3
    
    Alignment with OpenVINO 2.4.2
    
    ref: 70577
    
    E2E 355
    Manual Precommit 7

--- nncf/tensorflow/__init__.py
+++ nncf/tensorflow/__init__.py
@@ -16,7 +16,9 @@ from nncf.version import BKC_TF_VERSION
 
 import tensorflow
 from pkg_resources import parse_version
-if parse_version(BKC_TF_VERSION).base_version != parse_version(tensorflow.__version__).base_version:
+
+tensorflow_version = parse_version(tensorflow.__version__).base_version
+if not tensorflow_version.startswith(BKC_TF_VERSION[:-2]):
     raise RuntimeError(
          'NNCF only supports tensorflow=={bkc}, while current tensorflow version is {curr}'.format(
          bkc=BKC_TF_VERSION,
