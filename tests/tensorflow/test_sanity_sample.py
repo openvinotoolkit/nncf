28878fd990c25cf7ff212ceff8659935367f6f00
Aleksei Kashapov <aleksei.kashapov@intel.com>
2021-11-25 17:46:03 +0300

    Make mocking datasets only for test_sanity_sample (#1022)

--- tests/tensorflow/test_sanity_sample.py
+++ tests/tensorflow/test_sanity_sample.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3912c7fbdd5f6b3c268bd7472c747ceed6cc171414ae5975f8dec5eba3682209
-size 17053
+oid sha256:1461c688fba4a55e0a6ef4d231be03281d808dca1d7ccdfdab4c51e548db062d
+size 17734
