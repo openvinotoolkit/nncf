c4c4171c9c8652656bae827002babfc2fd19127e
asenina <anastasia.senina@intel.com>
2021-11-26 11:07:50 +0300

    Remove extra comment.

--- nncf/torch/model_creation.py
+++ nncf/torch/model_creation.py
@@ -139,11 +139,6 @@ def create_compressed_model(model: Module,
                                    original_model_accuracy=original_model_accuracy)
 
     should_init = compression_state is None
-    #from nncf.torch.nas.seacrh_blocks import get_building_blocks
-    #blocks, group = get_building_blocks(compressed_model)
-    #nncf_logger.info("blocks {}".format(blocks))
-    #print(blocks)
-    #print(group)
     builder = create_compression_algorithm_builder(config, should_init)
     is_state_loadable = not is_legacy_model_state_dict and compression_state is not None
     if is_state_loadable:
