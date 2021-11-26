a923d1b48372ef33bf39b830b88d596763416f55
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2021-11-26 11:25:16 +0300

    Fix error in determine_subtype for custom module (#1025)
    ### Changes
    
    Check layer_attributes before check subtypes.
    
    
    ### Reason for changes
    
    Incorrect works with custom modules like
    ```Python
    class CustomMoudule(nn.Module):
        def __init__(self, in_features, out_features):
            ...
            self.in_features = in_features
            self.out_features = out_features
            self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
            ...
    
        def forward(self, x):
            weight = normalize(self.weight, dim=1, p=2).view(self.out_features, self.in_features, 1, 1)
            out = F.conv2d(x, weight)
            return out
    ```
    
    In with case layer_attributes is `GenericWeightedLayerAttributes` and `operator_name` is `conv2d` as result 
    ```
      File "nncf/torch/graph/operator_metatypes.py", line 87, in determine_subtype
        functions_kwargs):
      File "nncf/torch/graph/operator_metatypes.py", line 166, in matches
        if layer_attributes.groups == layer_attributes.in_channels and layer_attributes.in_channels > 1:
    AttributeError: 'GenericWeightedLayerAttributes' object has no attribute 'groups'
    ```

--- nncf/torch/graph/graph_builder.py
+++ nncf/torch/graph/graph_builder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84999b7c6e62cb96647054138a68fe769f416056480cb77aa2c67581aab0d987
-size 6351
+oid sha256:a714ec9f9a386947dd29726bc028cff58ea51bacc3c29753c24fdd3344e8ed0a
+size 6539
