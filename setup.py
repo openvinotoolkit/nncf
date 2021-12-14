0ffdd04ebc5413c8084d4b6f2a0a667ac0930920
Pavel Finashov <pavelx.finashov@intel.com>
2021-12-14 10:58:15 +0300

    Changed scipy version, to avoid 1.8.0rc1 (#1039)

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:beea0c53b9fd7f646764f084928cadef21ef0da1b56eca2f5562d4f5dc42a0a6
-size 5394
+oid sha256:edb33b43fc695b1c43ac2df8a7a55b50d0a19c238c5b0220d4fbb459bc8e1db7
+size 5400
