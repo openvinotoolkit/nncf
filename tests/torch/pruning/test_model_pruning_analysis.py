8495a2144cba4ccf7b0385a9cc91079e50ca396b
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2021-12-17 17:25:26 +0300

    Add test GroupNorm in model_pruning_analysis (#1043)
    ### Changes
    
    Add test for model with GroupNorm modules

--- tests/torch/pruning/test_model_pruning_analysis.py
+++ tests/torch/pruning/test_model_pruning_analysis.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4596179bd250773de9e022d0f84f37a270f02ee0adc102143ad1e8ebc3519f2c
-size 42073
+oid sha256:54093dc064021ecb8617b31263eca9bd94a4acaafd2b7f5d8415c83061e252db
+size 42605
