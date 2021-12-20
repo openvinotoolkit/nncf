82b21c0fbda116b7d3a699423353f02cd46c15d9
Vasily Shamporov <vasily.shamporov@intel.com>
2021-12-20 14:04:47 +0300

    Formulate PT OD configs in terms of epochs only (#1019)
    * Formulate PT OD configs in terms of epochs only
    
    * Recalculate epochs correctly

--- examples/torch/object_detection/main.py
+++ examples/torch/object_detection/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34584c4d88b1cdac88a0a52fcd16defbf16bd1be12bfaa118bf0e8cf4b4cb400
-size 22450
+oid sha256:a6b1ad0dfc9d293c02aa7e5fd789383b6e2605a30f0dd6c9978252c23eaa0470
+size 22169
