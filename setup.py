b411595bc5ea6632c1061d28ffb17ddc2f0c1c2c
Vasily Shamporov <vasily.shamporov@intel.com>
2021-12-22 14:13:51 +0300

    Fix issues (#1052)
    ### Changes
    
    <!--- What was changed (briefly), how to reproduce (if applicable), what the reviewers should focus on -->
    
    ### Reason for changes
    
    <!--- Why should the change be applied -->
    
    ### Related tickets
    
    <!--- Post the numerical ID of the ticket, if available -->
    
    ### Tests
    
    <!--- How was the correctness of changes tested and whether new tests were added -->

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbf4d1f19d8bf52e51158ceb410e94d11070a0e6cd45ae3c0a328978ee1bce18
-size 6195
+oid sha256:51be17ccecd14fbf23871d1156f237e4a63145203c74069f69a85b662210c7fd
+size 6214
