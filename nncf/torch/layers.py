a029f36f9c7963dee36f2833f138b4e18e2b5816
Vasily Shamporov <vasily.shamporov@intel.com>
2022-01-03 17:46:58 +0300

    Fix weight norm applied to modules (#1058)
    * Add test for WeightNorm-ed Conv2d wrapping
    
    * Handle WeightNorm-ed modules when wrapping NNCF* modules

--- nncf/torch/layers.py
+++ nncf/torch/layers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa3e90e48e598e1d8366b9a886af2db6e1a05feaaaa1839524b0a95794545948
-size 30870
+oid sha256:802a930e77ac9a87470fb86ce6835ccc7837d1c9214b479e35872bddbb985fa0
+size 31954
--- tests/torch/test_nncf_network.py
+++ tests/torch/test_nncf_network.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75b1af38229cb77499d5fd2440966ef0cc6fbea705b2658a7e81419db780deab
-size 42457
+oid sha256:6bb7663c1ca3c05615b9ab06cdb39fd2a0fff6a41d34bcd9a4f1138065b96164
+size 43321
