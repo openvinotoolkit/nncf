ea7fda635ad72259954d9e9ca37673dc6243bdb8
Vasily Shamporov <vasily.shamporov@intel.com>
2022-01-12 01:00:08 +0300

    Freeze pandas version to exclude the broken 1.4.0rc0 and higher (#1065)

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51be17ccecd14fbf23871d1156f237e4a63145203c74069f69a85b662210c7fd
-size 6214
+oid sha256:de8e2abb5fdbf115224b33d2de4bde845dd8bd912444afc0b4281418bccb1c47
+size 6224
