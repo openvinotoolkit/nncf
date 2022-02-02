3133baef6d4228ee8e6008db2a1afd3fc697970e
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2022-02-02 11:46:43 +0300

    Changed normalization parameters for CIFAR-10 images (#1091)
    ### Changes
    
    Changed mean/std values for pre-processing input images from CIFAR10 dataset
    
    ### Reason for changes
    
    It helps to reuse models pretrained on CIFAR10 from:
    https://github.com/huyvnphan/PyTorch_CIFAR10/blob/master/data.py#L16
    This models are used for e2e validation of BootstrapNAS feature #1079 
    
    ### Related tickets
    
    ### Tests
    
    It affects backward compatibility tests that compare actual test accuracy with 'best_acc1' value in the checkpoint.
    The `best_acc1` value was overridden to correspond to the introduced pre-processing.

--- examples/torch/classification/main.py
+++ examples/torch/classification/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ad776121022dcb4b41d2410875beb76914d03446af6db6c24610361757e0e44
-size 28566
+oid sha256:386a0787fbd5754feb5e27091be6df645cab5a399fbdc506b3f13632d1a7a68d
+size 28736
