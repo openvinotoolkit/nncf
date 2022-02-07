564299da8bbcbc16ba17a1644d1ce724743efafb
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2022-02-07 11:24:33 +0300

    Corrected typo to fix broken TF pre-commit (#1102)
    ### Changes
    
    Corrected typo in a TF test to fix degradation in the TF precommit with new version of pytest.
    Seems like previous runs was with old version of pytest (6.2.5) and it ignored this typo.
    Currently, CI uses newer version - 7.0.0 and it's failing.
    
    ### Reason for changes
    
    to fix degradation in the TF precommit
    
    ### Related tickets
    
    ### Tests
    
    tf precommit should be green

--- tests/tensorflow/test_compressed_graph.py
+++ tests/tensorflow/test_compressed_graph.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7e3cb33fec4fe9dd69e09d87e8e128329ca9f558124899a5c70ba6b47252585
-size 20982
+oid sha256:c7263547fc25e2a8663313ffcd96a7e53949a09760c7f8d1a85b33644093ebf4
+size 20981
