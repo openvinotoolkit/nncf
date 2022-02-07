e29b42709a20183903adc16938eb53b40688abaf
Evgeny Tsykunov <e.tsykunov@gmail.com>
2022-02-07 20:59:50 +0300

    too-many-statements

--- examples/tensorflow/object_detection/main.py
+++ examples/tensorflow/object_detection/main.py
@@ -380,7 +380,7 @@ def run(config):
         logger.info("Saved to {}".format(save_path))
 
     # Due to https://github.com/tensorflow/tensorflow/issues/50487
-    # pylint: disable=protected-access
+    # pylint: disable=protected-access, too-many-statements
     atexit.register(strategy._extended._collective_ops._pool.close)
 
 
