8f376485208be4d677322af8c485fc59017c9049
Meshalkin Nikita <nikita.meshalkin@intel.com>
2022-02-11 14:38:28 +0300

    Adding --disable-compression command line option

--- examples/torch/common/sample_config.py
+++ examples/torch/common/sample_config.py
@@ -1,5 +1,5 @@
 """
- Copyright (c) 2020 Intel Corporation
+ Copyright (c) 2022 Intel Corporation
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
@@ -115,7 +115,9 @@ def create_sample_config(args, parser) -> SampleConfig:
     if sample_config.get("target_device") is not None:
         target_device = sample_config.pop("target_device")
         loaded_json["target_device"] = target_device
-
+    
     nncf_config = NNCFConfig.from_dict(loaded_json)
+    if args.disable_compression and 'compression' in nncf_config:
+        del nncf_config['compression']
     sample_config.nncf_config = nncf_config
     return sample_config
