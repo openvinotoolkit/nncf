8649ba31f7a6c78276cf0dd2e075ec030addcd00
Evgeny Tsykunov <e.tsykunov@gmail.com>
2022-02-24 16:15:44 +0300

    TF precommit on CPU (#1116)
    Changes
    
    Graph test updated to support CPU device.
    Reason for changes
    
    Support pre-commit on CPU
    Related tickets
    
    76181

--- tests/tensorflow/data/reference_graphs/2.5/quantized/hw/CPU/inception_v3.pb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f9409cd909ca631d0ee4f3b560c4a1706aaf28e5718656eada0bf5099cf77423
-size 699647
--- tests/tensorflow/test_compressed_graph.py
+++ tests/tensorflow/test_compressed_graph.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:035b1eba6d18d57615144470145af22568df53eb921bc28779690a1cd0f48b69
-size 21476
+oid sha256:0e4b2faf68ccd9488dad25c76c907ec3a70c39b47a86db28507b5367f6800066
+size 21468
