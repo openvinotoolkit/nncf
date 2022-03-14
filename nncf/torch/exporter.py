ea9bd9c679de7b613556935be6d34e38f04524f6
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2022-03-14 11:10:22 +0300

    Add option to set onnx opeset version  (#1133)
    * Add option to set onnx opeset version
    
    * Add test_exported_version

--- nncf/torch/exporter.py
+++ nncf/torch/exporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e134256e7a4a8af170a0de1bd2b80cacaabfa2468f721f148aaaebc4bf3ae742
-size 4782
+oid sha256:1c7c09119dcecf7758f45f8a23fe3589e927aa5abc5ec43791c93bda9fc46c8f
+size 6362
