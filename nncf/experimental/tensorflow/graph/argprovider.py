449170c1667a38bdefb5698e8e318dc58598579c
Evgeny Tsykunov <e.tsykunov@gmail.com>
2022-03-18 12:14:12 +0300

    ArgProvider update (#1138)
    * argprovider update
    
    * minor

--- nncf/experimental/tensorflow/graph/argprovider.py
+++ nncf/experimental/tensorflow/graph/argprovider.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8d2552a45424763a9a725e78b7b8b9e86882b9af2bb7b13f7370e21d925bf4b
-size 8510
+oid sha256:07717416f08ad7d9c6d060e0006489be13b6eb1d1dea9c8930769133d5772a31
+size 9340
