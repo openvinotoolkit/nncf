898c2aa6da953f22191cd9757dde74b4aeae4b64
Sergei Kholkin <holckin100@gmail.com>
2022-03-21 10:45:27 +0300

    Fixed test_mapping_to_zero test for cpu only case (#1139)

--- tests/torch/quantization/test_functions.py
+++ tests/torch/quantization/test_functions.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e897509f23e1f117cfcab70a702a07564d1cb05fd2ef5262544c3f16f53741de
-size 18317
+oid sha256:7d403c6fa4403525d7e15a7a49095eb89262e8c4d27c36072c59ac093e9c9d77
+size 18445
