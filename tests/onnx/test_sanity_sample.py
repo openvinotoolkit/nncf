778ae557eee94ce04dd936c7a1b401a987149c74
Aleksei Kashapov <aleksei.kashapov@intel.com>
2022-03-23 15:23:43 +0300

    Add licences; Add mock dataset to test_sanity_sample.py

--- tests/onnx/test_sanity_sample.py
+++ tests/onnx/test_sanity_sample.py
@@ -1,4 +1,21 @@
+"""
+ Copyright (c) 2022 Intel Corporation
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+from typing import List
+from typing import Tuple
+
 import pytest
+from unittest.mock import patch
 
 import os
 
@@ -8,6 +25,7 @@ import onnxruntime as rt
 import numpy as np
 
 from examples.experimental.onnx_ptq_classification import run
+from nncf.experimental.post_training.api.dataloader import DataLoader
 from tests.common.helpers import TEST_ROOT
 
 MODELS = [
@@ -29,8 +47,26 @@ INPUT_SHAPES = [
 ]
 
 
+class TestDataloader(DataLoader):
+    def __init__(self, samples: List[Tuple[np.ndarray, int]]):
+        super().__init__(shuffle=False)
+        self.samples = samples
+
+    def __getitem__(self, item):
+        return self.samples[item]
+
+    def __len__(self):
+        return 1
+
+
+def mock_dataloader_creator(dataset_path, input_shape, batch_size, shuffle):
+    return TestDataloader([(np.zeros(input_shape[1:]), 0), ])
+
+
 @pytest.mark.parametrize(("model, input_shape"),
                          zip(MODELS, INPUT_SHAPES))
+@patch('examples.experimental.onnx_ptq_classification.create_dataloader_from_imagenet_torch_dataset',
+       new=mock_dataloader_creator)
 def test_sanity_quantize_sample(tmp_path, model, input_shape):
     model_name = str(model.__class__)
     onnx_model_dir = str(TEST_ROOT.joinpath('onnx', 'data', 'models'))
@@ -39,17 +75,10 @@ def test_sanity_quantize_sample(tmp_path, model, input_shape):
         os.mkdir(onnx_model_dir)
     x = torch.randn(input_shape, requires_grad=False)
     torch.onnx.export(model, x, onnx_model_path, opset_version=13)
-
-    dataset_path = '/home/aleksei/datasetsimagenet/'
     onnx_output_model_path = str(tmp_path / model_name)
-    batch_size = 1
-    shuffle = True
-    num_init_samples = 1
-    ignored_scopes = None
-
-    run(onnx_model_path, onnx_output_model_path, dataset_path,
-        batch_size, shuffle, num_init_samples,
-        input_shape, ignored_scopes)
+    run(onnx_model_path, onnx_output_model_path, 'none',
+        batch_size=1, shuffle=True, num_init_samples=1,
+        input_shape=input_shape, ignored_scopes=None)
 
     sess = rt.InferenceSession(onnx_output_model_path, providers=['OpenVINOExecutionProvider'])
     _input = np.random.random(input_shape)
