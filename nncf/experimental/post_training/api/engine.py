1b9a02579a56dd9bf49063d18d3714205d09619a
Malinin, Nikita <nikita.malinin@intel.com>
2022-04-02 09:37:53 +0300

    Update Engine API

--- nncf/experimental/post_training/api/engine.py
+++ nncf/experimental/post_training/api/engine.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1efa2e2d4d450ba04a89c1170182e1ccf762ddab42d82818225cb9513a250ed6
-size 3286
+oid sha256:62ac3460e9d52f8f675f73baba8c2fac936349db669a4af11edc6d574f9cf304
+size 5168
