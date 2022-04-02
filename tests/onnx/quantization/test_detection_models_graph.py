c45dfd11434f17fc000d7ebded2dd1655e0d3ae1
Aleksei Kashapov <aleksei.kashapov@intel.com>
2022-04-02 15:48:16 +0300

    Add functional test of ONNX PTQ of detection models (#1155)
    ### Changes
    
    - Add hardware fusing pattern for input preprocessing
    - Add graph test for yolov2, tiny-yolov2. Currently, there are some issues with SSD models
    
          https://github.com/onnx/onnx/issues/4101
          https://github.com/onnx/onnx/issues/4102
    
    ### Reason for changes
    
    Extend support of ONNX PTQ models
    
    ### Related tickets
    
    83146
    
    ### Tests
    
    As stated in the title

---
+++ tests/onnx/quantization/test_detection_models_graph.py
@@ -0,0 +1,80 @@
+"""
+ Copyright (c) 2022 Intel Corporation
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+import pytest
+
+import os
+import requests
+
+import onnx
+
+from tests.common.helpers import TEST_ROOT
+from tests.onnx.test_nncf_graph_builder import check_nx_graph
+
+from tests.onnx.quantization.test_classification_models_graph import TestDataloader
+
+from nncf.experimental.post_training.compression_builder import CompressionBuilder
+from nncf.experimental.onnx.algorithms.quantization.min_max_quantization import ONNXMinMaxQuantization
+from nncf.experimental.post_training.algorithms.quantization import MinMaxQuantizationParameters
+
+from nncf.experimental.onnx.graph.nncf_graph_builder import GraphConverter
+
+MODELS_NAME = [
+    'yolov2-coco-9',
+    'tiny-yolov2',
+]
+
+MODELS_URL = [
+    'https://github.com/onnx/models/raw/main/vision/object_detection_segmentation/yolov2-coco/model/yolov2-coco-9.onnx',
+    'https://github.com/onnx/models/raw/main/vision/object_detection_segmentation/tiny-yolov2/model/tinyyolov2-7.onnx',
+]
+
+PATH_REF_GRAPHS = [
+    'yolov2-coco-9.dot',
+    'tiny-yolov2.dot',
+]
+
+INPUT_SHAPES = [
+    [1, 3, 416, 416],
+    [1, 3, 416, 416],
+]
+
+REFERENCE_GRAPHS_TEST_ROOT = 'data/reference_graphs/quantization'
+
+
+@pytest.mark.parametrize(('model_name', 'model_url', 'path_ref_graph', 'input_shape'),
+                         zip(MODELS_NAME, MODELS_URL, PATH_REF_GRAPHS, INPUT_SHAPES))
+def test_min_max_quantization_graph(tmp_path, model_name, model_url, path_ref_graph, input_shape):
+    onnx_model_dir = str(TEST_ROOT.joinpath('onnx', 'data', 'models'))
+    onnx_model_path = str(TEST_ROOT.joinpath(onnx_model_dir, model_name))
+    if not os.path.isdir(onnx_model_dir):
+        os.mkdir(onnx_model_dir)
+
+    r = requests.get(model_url)
+    with open(onnx_model_path, 'wb') as f:
+        f.write(r.content)
+
+    original_model = onnx.load(onnx_model_path)
+
+    dataloader = TestDataloader(input_shape)
+    builder = CompressionBuilder()
+    builder.add_algorithm(ONNXMinMaxQuantization(MinMaxQuantizationParameters(number_samples=1)))
+    quantized_model = builder.apply(original_model, dataloader)
+
+    nncf_graph = GraphConverter.create_nncf_graph(quantized_model)
+    nx_graph = nncf_graph.get_graph_for_structure_analysis(extended=True)
+
+    data_dir = os.path.join(TEST_ROOT, 'onnx', REFERENCE_GRAPHS_TEST_ROOT)
+    path_to_dot = os.path.abspath(os.path.join(data_dir, path_ref_graph))
+
+    check_nx_graph(nx_graph, path_to_dot, True)
