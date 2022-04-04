e65733b1691b07036fa5342161ebb746d7c29b7a
Sergei Kholkin <holckin100@gmail.com>
2022-04-04 15:40:22 +0300

    Adding saturation fix parameters to compression state for PT (#1128)
    * Trying to debug autoq tests failure
    
    * First working iteration
    
    * New compression state loading and backward compatibility
    
    * Rebased
    
    * Test NotImplementedError deleted
    
    * Little clean up
    
    * Compression State versioning
    
    * Comparison operators
    
    * nas test fix
    
    * Composite compression loading
    
    * Enum PT compression state versioning
    
    * Remove accidentally added files
    
    * Changed version to local quantizaer builder one
    
    * Fixed pylint
    
    * Fixes

--- nncf/torch/quantization/layers.py
+++ nncf/torch/quantization/layers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c88b00df2aa588e01d6f59e284e8ec9ec7a75101bcf84e9d3fc95cf5331eafd1
-size 30220
+oid sha256:6364a202dc4eb1e887b7b990243fed0fa1e7fb5bbf71b98a99a673e4632ae906
+size 37339
