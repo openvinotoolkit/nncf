b3c6d13004cb2a116bc637129e675c0415a1d654
J. Pablo Munoz <jpablomch@gmail.com>
2022-04-25 18:52:14 -0700

    BNAS Search

---
+++ nncf/experimental/torch/nas/bootstrapNAS/search/evaluator.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa8f76e57c307649ca417fe9f1a9144c224a1b634ea2738660d1535d2aaa268e
+size 8190
---
+++ nncf/experimental/torch/nas/bootstrapNAS/search/evaluator_handler.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed6a2e0c59600fa642b42a552e9cc71da10ac25df0cf8ae5bc4f39de997dbace
+size 3750
