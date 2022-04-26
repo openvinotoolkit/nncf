518fad6bc3a1b807f3e3f3a8695117e1b553b406
J. Pablo Munoz <jpablomch@gmail.com>
2022-04-25 19:32:25 -0700

    Remove test EfficientNet

--- tests/torch/test_compression_training.py
+++ tests/torch/test_compression_training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63ac47e4c76c2c6e650ba2b3804e17cbd1ee9301b48e7cb9d2116c1ffe490547
-size 20433
+oid sha256:eeea561004581771e92857db50f0a1a91815c836e50340df621953da3e32dc12
+size 20449
