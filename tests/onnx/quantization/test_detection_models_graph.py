f8ae51933634b465471d0acdd4b8e54e08bdf32b
Kim, Vinnam <vinnam.kim@intel.com>
2022-05-03 00:58:03 +0900

    Revert tests/onnx/quantization to 3826d8d
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- tests/onnx/quantization/test_detection_models_graph.py
+++ tests/onnx/quantization/test_detection_models_graph.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:276d740927c85cf98d0811356c8674a921aff8aa3c6972ea79d01f3f4e60ccde
-size 1732
+oid sha256:1ec7e5bd4feb5578f83e55061f3e033a94e78438bfe3033e937fe188f3fc16e3
+size 2922
