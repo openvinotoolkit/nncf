b3789607e2453cf9fbf5c79db061e6812c1cef4f
Kim, Vinnam <vinnam.kim@intel.com>
2022-05-03 11:33:10 +0900

    Fix argument help string
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- examples/experimental/onnx/onnx_ptq_classification.py
+++ examples/experimental/onnx/onnx_ptq_classification.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b157b87052a183ecfd2bf721b35c7785fd7dc58aebe819cfe17508bcb444be6
-size 6254
+oid sha256:2742a70cc28409a16ab6648ac0f486147fac2d2c6b0d9692f7d69a165e59b0e4
+size 6259
