eba0e33b634ed8df94cf50734255c59975fb5830
Kim, Vinnam <vinnam.kim@intel.com>
2022-05-03 11:28:24 +0900

    Revert --shuffle default=True
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- examples/experimental/onnx/onnx_ptq_classification.py
+++ examples/experimental/onnx/onnx_ptq_classification.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b58aa4d16c29346613076e5579af212367b971a3413a564f5bbf00edbcc3ad02
-size 6255
+oid sha256:5b157b87052a183ecfd2bf721b35c7785fd7dc58aebe819cfe17508bcb444be6
+size 6254
