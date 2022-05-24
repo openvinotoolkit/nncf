21c016dd78d0be899cff68f98c74698aae63d781
Vinnam Kim <vinnam.kim@intel.com>
2022-05-24 14:44:05 +0900

    Fix version issue for Python3.6 (#1185)
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:918275f4f088c5ee3b256b93c11e7c3a72a0a02d2ad2076fb7e88580cb5e3512
-size 6314
+oid sha256:ce8c5fcd90c2362ff59e648c99f4ab2ae795d5262b687fca013228116ccb3042
+size 6396
