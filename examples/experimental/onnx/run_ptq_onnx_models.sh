0b43fb9367507ea93f738de1271758bfe06a6d61
Kim, Vinnam <vinnam.kim@intel.com>
2022-05-30 18:17:43 +0900

    Use dataset_definitions.yml for classification models
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- examples/experimental/onnx/run_ptq_onnx_models.sh
+++ examples/experimental/onnx/run_ptq_onnx_models.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b924a3cce25c1bd9ee8839cc7ad55955c7ef3ad5e37d743e01e6599d90a0f840
-size 2797
+oid sha256:f3cd06c4ce79cb576311d45d81c1547bcd0d80a6a573a06685783be406452bb9
+size 3070
