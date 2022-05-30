1a2fcc0e70668288ce6eb10195724e51cc6b0e37
Kim, Vinnam <vinnam.kim@intel.com>
2022-05-30 15:11:48 +0900

    Update run_ptq_onnx_models.sh to support det_and_seg
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- examples/experimental/onnx/run_ptq_onnx_models.sh
+++ examples/experimental/onnx/run_ptq_onnx_models.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08655006b99ef4845e1e07e23723e03a4adb4f29cc7bed344681f76b87963b20
-size 2216
+oid sha256:b924a3cce25c1bd9ee8839cc7ad55955c7ef3ad5e37d743e01e6599d90a0f840
+size 2797
