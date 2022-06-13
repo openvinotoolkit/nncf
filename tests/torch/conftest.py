6374535167638a02d7689578faa3255a4ec8ba3b
dlyakhov <daniil.lyakhov@intel.com>
2022-06-13 21:54:52 +0900

    TF32 presicison disabled in tests

--- tests/torch/conftest.py
+++ tests/torch/conftest.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:553e181639374ceb2480f15ff982771dc6cba24f01a276fcd60d0573ecb543d9
-size 7022
+oid sha256:85d58a2c84c8fd20f68ee95ff4ef0d2813d6cca8a4cebf881ffa7ca7b2666bf2
+size 7264
