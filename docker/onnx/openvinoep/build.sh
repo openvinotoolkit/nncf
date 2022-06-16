ac5b5a3b8e60253e19bad5196d229c368a167330
Kim, Vinnam <vinnam.kim@intel.com>
2022-06-16 11:54:09 +0900

    Fix onnx stack installation
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- docker/onnx/openvinoep/build.sh
+++ docker/onnx/openvinoep/build.sh
@@ -5,6 +5,7 @@ WORK_DIR="${SCRIPT_DIR}/../../../"
 
 cd $WORK_DIR && echo "WORK_DIR=$PWD"
 
+# TODO: Replace this image with Dockerfile.dev
 docker build -t onnx_ptq_experimental:latest                \
     --build-arg http_proxy=$http_proxy                      \
     --build-arg https_proxy=$https_proxy                    \
@@ -12,4 +13,13 @@ docker build -t onnx_ptq_experimental:latest                \
     --build-arg PIP_EXTRA_INDEX_URL=$PIP_EXTRA_INDEX_URL    \
     --build-arg PIP_TRUSTED_HOST=$PIP_TRUSTED_HOST          \
     -f docker/onnx/openvinoep/Dockerfile                    \
+    .
+
+docker build -t onnx_ptq_experimental:dev                   \
+    --build-arg http_proxy=$http_proxy                      \
+    --build-arg https_proxy=$https_proxy                    \
+    --build-arg no_proxy=$no_proxy                          \
+    --build-arg PIP_EXTRA_INDEX_URL=$PIP_EXTRA_INDEX_URL    \
+    --build-arg PIP_TRUSTED_HOST=$PIP_TRUSTED_HOST          \
+    -f docker/onnx/openvinoep/Dockerfile.dev                \
     .
