59188bcd115cd8898b645fa3110f89e9101e72b5
Evgeny Tsykunov <e.tsykunov@gmail.com>
2022-06-21 02:33:35 +0200

    limit the protobuf version - tensorflow requirement (#1199)

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce8c5fcd90c2362ff59e648c99f4ab2ae795d5262b687fca013228116ccb3042
-size 6396
+oid sha256:535663fa5d03cb63e3d9b0fbda8411accb8dd1a0d9f02dd6980a27056c61bdcd
+size 6599
