a98b1a99fe4e9b887bcd861391225e0979b3fb47
Evgeny Tsykunov <e.tsykunov@gmail.com>
2022-06-23 14:50:54 +0200

    more relaxation (#1205)

--- tests/torch/test_compression_training.py
+++ tests/torch/test_compression_training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39ca6d7c9796f11ea69d9a489a3426bb0df77dc91b5558a64fde8b3d6d5ed158
-size 20280
+oid sha256:cccd00e27875456e93bfad32e1ed06f1e9e5aa31ccefdd90307399686ac9a036
+size 20272
