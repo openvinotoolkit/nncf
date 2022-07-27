ff26e2e841c6ba1ed5cecc4f25c75e55c05b4b72
Vinnam Kim <vinnam.kim@gmail.com>
2022-07-27 18:53:05 +0900

    Revert "Implement FracBitsQuantizers (#1231)" (#1237)
    This reverts commit 53df688545a7dd3a31ac840f2fb6ddbdb138b9ca.

--- nncf/experimental/torch/fracbits/quantizer.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a38fe3c3cfedafa641cfd3e6aa6a199f4730780a4aba9dc29f3d15f95b8f70c2
-size 8715
--- nncf/experimental/torch/fracbits/structs.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4a10dfa268db8e418580a62ebd573ffbfb759418168f51964a8ee7d08e8f634d
-size 687
