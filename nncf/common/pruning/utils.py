1bda253269251df91664e00148a756e9a074f6af
sooahleex <sooah.lee@intel.com>
2022-08-24 19:46:01 +0900

    cover empty input mask case and revert decision

--- nncf/common/pruning/utils.py
+++ nncf/common/pruning/utils.py
@@ -594,6 +594,8 @@ def get_input_masks(node: NNCFNode, graph: NNCFGraph) -> List[Optional[NNCFTenso
     input_masks = [input_node.data['output_mask'] for input_node in graph.get_previous_nodes(node)]
     for input_mask in input_masks:
         retval.append(input_mask[node.node_name] if isinstance(input_mask, dict) else input_mask)
+    if not retval:
+        return None
     return retval
 
 
