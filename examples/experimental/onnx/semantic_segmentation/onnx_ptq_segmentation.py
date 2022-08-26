05350fa869baff4b5744479be38e39a3514b8152
Vasily Shamporov <vasily.shamporov@intel.com>
2022-08-26 13:16:23 +0200

    Fix missing import (#1260)

--- examples/experimental/onnx/semantic_segmentation/onnx_ptq_segmentation.py
+++ examples/experimental/onnx/semantic_segmentation/onnx_ptq_segmentation.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49b0dc2165c84a8bbec3af075381e197df7089a52d01366805d1a86f229de636
-size 3850
+oid sha256:f26c7dd0cffdf4f8b0a3b09d64329049e8e34d3fd6725050dc9182a9579035a7
+size 3860
