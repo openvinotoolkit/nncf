8af18cdaed9e1e3ab6b7e1f266ac18c20b866fd6
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2022-08-31 16:22:41 +0200

    Corrected ModelAnalyzer to prune not only conv, but linear (#1262)

--- nncf/common/pruning/node_selector.py
+++ nncf/common/pruning/node_selector.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c1c4bf9e6f9e38ebdeb1257a7fc7322bb83651e02223348e1c4c1ea62f77785
-size 17002
+oid sha256:ccd5518fd03880f55e7b5ddb8e2c2ac158c326c01921a67c77634413ea5e0747
+size 17007
--- tests/torch/pruning/test_model_pruning_analysis.py
+++ tests/torch/pruning/test_model_pruning_analysis.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d362194be6239fee715afacb41419a9952d81c2f54cbe54b740336685f6f494
-size 48600
+oid sha256:0d602315bc1fbb54a4350e4dbb575b2657e200437c4a5d8fe06b6ca6f607e6e0
+size 49806
