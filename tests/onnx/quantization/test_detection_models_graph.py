16d28a20acdb25635290c960e3d4eda49c9ec864
kshpv <aleksei.kashapov@intel.com>
2022-09-01 19:00:33 +0200

    Fix Mask RCNN

--- tests/onnx/quantization/test_detection_models_graph.py
+++ tests/onnx/quantization/test_detection_models_graph.py
@@ -53,7 +53,8 @@ def test_min_max_quantization_graph(tmp_path, model_to_test):
         convert_opset_version = False
         # TODO: need to investigate disallowed_op_types for Mask RCNN
         ignored_scopes += find_ignored_scopes(
-            ["Concat", "Mul", "Add", "Sub", "Sigmoid", "Softmax", "Floor", "RoiAlign", "Resize", 'Div'], original_model)
+            ["Concat", "Mul", "Add", "Sub", "Sigmoid", "Softmax", "Floor", "RoiAlign", "Resize", 'Div', 'Cast',
+             'ScatterElements'], original_model)
 
     quantized_model = min_max_quantize_model(model_to_test.input_shape, original_model,
                                              convert_opset_version=convert_opset_version,
