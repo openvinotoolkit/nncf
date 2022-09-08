209a26c0ad326683d180a54374a5b7d6c4130875
kshpv <aleksei.kashapov@intel.com>
2022-09-08 13:24:51 +0200

    Fix logs

--- nncf/experimental/onnx/model_normalizer.py
+++ nncf/experimental/onnx/model_normalizer.py
@@ -140,8 +140,8 @@ class ONNXModelNormalizer:
             modified_model = onnx.helper.make_model(modified_model.graph, ir_version=ir_version, opset_imports=[op])
             onnx.checker.check_model(modified_model)
             nncf_logger.info(
-                'The model was successfully converted  to the Opset Version = {}'.format(
-                    modified_model.opset_import[0].version))
+                'The model was successfully converted  to the IR Version = {}'.format(
+                    modified_model.ir_version))
             return modified_model
 
         return modified_model
