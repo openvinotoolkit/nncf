b5c3c44b19dd4c53caaadfa9c5c93d353d0658e2
kshpv <aleksei.kashapov@intel.com>
2022-09-19 14:08:43 +0200

    Update NNCFGraph get_graph_for_structure_analysis() codestyle

--- nncf/common/graph/graph.py
+++ nncf/common/graph/graph.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9054b47c89f44073edd3297826fcd10ad2dc86dbe2886ca0b2c832b53ef4b5e
-size 30030
+oid sha256:f31b6b21db06835bcbff77cf0c9a475bdf3dbc1a8ee81a07341e256404012f9a
+size 30081
