308a0bcf172f3681fbca66e8e5699ab2fe750096
kshpv <aleksei.kashapov@intel.com>
2022-09-26 16:38:13 +0200

    Add activation layers in stop COMPLEX_COMPUTATIONAL_LAYERS_METATYPES; Remove models from xfail in e2e

--- nncf/experimental/onnx/graph/metatypes/onnx_metatypes.py
+++ nncf/experimental/onnx/graph/metatypes/onnx_metatypes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:658dcd4837f8f98e2c241b4920cf4bf97d774ab88b8bb80180c45ef224902670
-size 11060
+oid sha256:2273d1b2a16e316d63e164ea25bf345d59087ed3114dfe09320c81c93b708c8f
+size 11406
