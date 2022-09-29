fc0ef043cafb192c66aa884b52cc623c8da8fcc5
kshpv <aleksei.kashapov@intel.com>
2022-09-29 14:10:34 +0200

    Add Quantization Preset

--- nncf/experimental/post_training/algorithms/quantization/algorithm.py
+++ nncf/experimental/post_training/algorithms/quantization/algorithm.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19ea7555f56eefc4c6b7ba005497c52d1670cbd87408df38353e26d6a2820068
-size 4776
+oid sha256:2e915159d95ca390eb9e10e99924f6b7e9c480417a61858c148fba31ceae6e4d
+size 4774
--- nncf/experimental/post_training/algorithms/quantization/min_max_quantization.py
+++ nncf/experimental/post_training/algorithms/quantization/min_max_quantization.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc6a430d7c730b077c708e3852557e38b9ee870ea61feaf5671c3b58e85c1272
-size 4551
+oid sha256:b573b85b5199a96cb927ea9a6e29671500a318faa54596c8e65aaae73c836c1e
+size 4723
