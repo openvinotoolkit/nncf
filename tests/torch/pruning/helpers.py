07efe62f697c81d0300c7b2a069967b8f5dd590d
Daniil Lyakhov <daniil.lyakhov@intel.com>
2022-10-05 10:21:02 +0200

    Minor: make HRNetBlock actually prunable (#1277)
    ### Changes
    
    Amount of channels and initialization of conv layers of HRNetBlock in pruning tests was updated to make is possible to actually prune and export this block. It's needed to start Ngraph pruning transformation experimenting
    
    ### Tickets
    88441

--- tests/torch/pruning/helpers.py
+++ tests/torch/pruning/helpers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f14376dee5f60c6c6a22d3a115e8d563607ce388c2c7643bdee2b5569d4d038b
-size 34601
+oid sha256:8e5043d13533a6028ed694907eb8475439953370fd57f37cc0e95e6a6ec4e8bc
+size 34763
