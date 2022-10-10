b6327b3f0331af6f3acfc986999585ef1dc6486c
kshpv <aleksei.kashapov@intel.com>
2022-10-10 18:06:08 +0200

    Apply comments

--- nncf/common/quantization/quantizer_propagation/solver.py
+++ nncf/common/quantization/quantizer_propagation/solver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c6a9c58588c886fb14ad14c715d9025016008c0acab6a9e4b095b5a609e9d28
-size 89555
+oid sha256:b8472b2731e41e5a02cb4118f98ac2dd5f071c580786e13a841b9c5d3812f0b0
+size 89432
--- tests/common/quantization/test_ignore_post_processing_nodes.py
+++ tests/common/quantization/test_ignore_post_processing_nodes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8ca561b2f5651a1d13f2d0b0cb7ef7e974fdb508bd4791763bd94eab02a765d
-size 9907
+oid sha256:d5a7e05d9e3333def58830364ff091284672c0c7387108a4185d3d2339391f8e
+size 11003
