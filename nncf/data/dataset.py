a8929170cca5a49beb76d82d58e2ddca7f7be7f9
Andrey Churkin <andrey.churkin@intel.com>
2022-10-14 12:05:01 +0100

    Docstring fot the nncf.Dataset was added

--- nncf/data/dataset.py
+++ nncf/data/dataset.py
@@ -18,25 +18,72 @@ from typing import Optional
 from typing import List
 
 
+DataItem = Any
+
+
+ModelInput = Any
+
+
 class Dataset:
+    """
+    The `nncf.Dataset` class defines the interface by which compression algorithms
+    retrieve data items from the passed data source object. These data items are used
+    for different purposes, for example, model inference and model validation. It depends
+    on the compression algorithm.
+
+    If the data item has been returned from the data source per iteration and it cannot be
+    used as input for model inference, the transformation function is used to extract the
+    model's input from this data item. For example, in supervised learning, the data item
+    usually contains both examples and labels. So transformation function should extract
+    the examples from the data item.
+    """
 
     def __init__(self,
-                 data_source: Iterable[Any],
-                 transform_func: Optional[Callable[[Any], Any]] = None):
+                 data_source: Iterable[DataItem],
+                 transform_func: Optional[Callable[[DataItem], ModelInput]] = None):
+        """
+        Initializes the `nncf.Dataset` object.
+
+        :param data_source: It is the iterable object where data items are from.
+        :param transform_func: The function that is used to extract the model's input
+            from the data item. The data item here is the data item that is returned from
+            the data source per iteration. This function should be passed when
+            the data item cannot be used as model's input. The identity function is used
+            by default.
+        """
         self._data_source = data_source
         self._transform_func = transform_func
 
-    def get_data(self, indices: Optional[List[int]] = None) -> Iterable[Any]:
+    def get_data(self, indices: Optional[List[int]] = None) -> Iterable[DataItem]:
+        """
+        Returns the iterable object that contains selected data items from the data source as-is.
+
+        :param indices: The zero-based indices of data items that should be selected from
+            the data source. The indices should be sorted in ascending order. If indices are
+            not passed all data items are selected from the data source.
+        :return: The iterable object that contains selected data items from the data source as-is.
+        """
         return DataProvider(self._data_source, None, indices)
 
-    def get_inference_data(self, indices: Optional[List[int]] = None) -> Iterable[Any]:
+    def get_inference_data(self, indices: Optional[List[int]] = None) -> Iterable[ModelInput]:
+        """
+        Returns the iterable object that contains selected data items from the data source, for which
+        the transformation function was applied. The item, which was returned per iteration from this
+        iterable, can be used as the model's input for model inference.
+
+        :param indices: The zero-based indices of data items that should be selected from
+            the data source. The indices should be sorted in ascending order. If indices are
+            not passed all data items are selected from the data source.
+        :return: The iterable object that contains selected data items from the data source, for which
+            the transformation function was applied.
+        """
         return DataProvider(self._data_source, self._transform_func, indices)
 
 
 class DataProvider:
     def __init__(self,
-                 data_source: Iterable[Any],
-                 transform_func: Callable[[Any], Any],
+                 data_source: Iterable[DataItem],
+                 transform_func: Callable[[DataItem], ModelInput],
                  indices: Optional[Iterable[int]] = None):
         self._data_source = data_source
         if transform_func is None:
