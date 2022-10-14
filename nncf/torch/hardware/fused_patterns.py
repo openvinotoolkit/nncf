039bdb843e12849960d8bbaedc67cc0c4a3d92d7
Liubov Talamanova <liubov.talamanova@intel.com>
2022-10-14 11:33:00 +0200

    Merge MATMUL and LINEAR ops

--- nncf/torch/hardware/fused_patterns.py
+++ nncf/torch/hardware/fused_patterns.py
@@ -5,7 +5,6 @@ from nncf.torch.graph.pattern_operations import ATOMIC_ACTIVATIONS_OPERATIONS
 from nncf.torch.graph.pattern_operations import BATCH_NORMALIZATION_OPERATIONS
 from nncf.torch.graph.pattern_operations import GROUP_NORMALIZATION_OPERATIONS
 from nncf.torch.graph.pattern_operations import LINEAR_OPERATIONS
-from nncf.torch.graph.pattern_operations import MATMUL_OPERATIONS
 from nncf.torch.graph.pattern_operations import RELU_OPERATIONS
 from nncf.torch.graph.patterns import create_fc_conv_mul
 from nncf.torch.graph.patterns import create_h_sigmoid_act
@@ -19,10 +18,6 @@ def _get_torch_hw_fused_patterns() -> HWFusedPatterns:
     linear_ops = GraphPattern()
     linear_ops.add_node(**LINEAR_OPERATIONS)
     retval.register(linear_ops, LINEAR_OPERATIONS['label'], match=False)
-
-    matmul_ops = GraphPattern()
-    matmul_ops.add_node(**MATMUL_OPERATIONS)
-    retval.register(matmul_ops, MATMUL_OPERATIONS['label'], match=False)
 
     batch_norm = GraphPattern()
     batch_norm.add_node(**BATCH_NORMALIZATION_OPERATIONS)
@@ -42,9 +37,8 @@ def _get_torch_hw_fused_patterns() -> HWFusedPatterns:
 
     batch_norm_activations_permutation = batch_norm + activations | activations + batch_norm | batch_norm | activations
 
+    retval.register(linear_ops + arithmetic_ops, 'LINEAR + ARITHMETIC', match=True)
     retval.register(linear_ops + batch_norm_activations_permutation, 'LINEAR + BN_ACT_PERM',
-                    match=True)
-    retval.register(matmul_ops + arithmetic_ops, 'MATMUL + ARITHMETIC',
                     match=True)
     retval.register(batch_norm + activations, 'BN + ACTIVATIONS', match=True)
     retval.register(activations + batch_norm, 'ACTIVATIONS + BN', match=True)
