eecd5df2e096441856b8bf31aaefe002c83e6370
Alexander <kozzzloff@list.ru>
2022-11-04 12:06:35 +0400

    Removed limitations of Q/DQ per-channel export

--- nncf/torch/quantization/layers.py
+++ nncf/torch/quantization/layers.py
@@ -448,13 +448,6 @@ class BaseQuantizer(nn.Module):
                                                           input_high)
             if self._export_mode == QuantizerExportMode.ONNX_QUANTIZE_DEQUANTIZE_PAIRS:
                 x, y_scale, y_zero_point = self._prepare_qdq_export_quantization(x)
-                if self.per_channel and y_zero_point.numel() > 1:
-                    if torch.allclose(y_scale - y_scale[0], torch.zeros_like(y_scale)) and \
-                            torch.allclose(y_zero_point - y_zero_point[0], torch.zeros_like(y_zero_point)):
-                        y_scale, y_zero_point = y_scale[0], y_zero_point[0]
-                        return ExportQuantizeToONNXQuantDequant.apply(x, y_scale, y_zero_point)
-                    raise RuntimeError("PyTorch export to ONNX using QuantizeLinear-DequantizeLinear "
-                                       "doesn't support per channel quantization")
                 return ExportQuantizeToONNXQuantDequant.apply(x, y_scale, y_zero_point)
         raise RuntimeError('Unknown export mode')
 
