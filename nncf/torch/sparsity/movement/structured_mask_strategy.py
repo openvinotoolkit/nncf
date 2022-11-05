1e6b5ef09e9bfe6d0c6c51071649a138d057a3ea
Vui Seng Chua <vui.seng.chua@intel.com>
2022-11-05 16:16:14 -0700

    Add structured masking support for HuggingFace's Swin and refactor auto model family detection

--- nncf/torch/sparsity/movement/structured_mask_strategy.py
+++ nncf/torch/sparsity/movement/structured_mask_strategy.py
@@ -1,3 +1,4 @@
+import inspect
 from typing import Dict, List, Optional, Sequence, Tuple, Union
 
 from nncf.common.utils.registry import Registry
@@ -8,10 +9,13 @@ from nncf.torch.nncf_network import NNCFNetwork
 STRUCTURED_MASK_STRATEGY = Registry("structured_mask_strategy")
 
 def detect_supported_model_family(model):
-    # TODO: need discussion on how to implement this
-    for name, _ in model.named_modules():
-        if 'bert' in name.lower():
-            return 'huggingface_bert'
+    # TODO: review and discuss
+    model_pymodule = inspect.getmodule(model.get_nncf_wrapped_model()).__name__.split(".")
+    if model_pymodule[0] == 'transformers':
+        # the case of input model defined by HuggingFace's transformers
+        model_family = f'huggingface_{model_pymodule[2]}'
+        if  model_family in STRUCTURED_MASK_STRATEGY.registry_dict:
+            return model_family
     return None
     
 class StructuredMaskRule:
@@ -84,6 +88,66 @@ class HuggingFaceBertStructuredMaskStrategy(BaseStructuredMaskStrategy):
                     keywords=[self.MHSA_O],
                     prune_by_row=False,
                     prune_grid=(-1, self.hidden_dim // self.num_heads),
+                ),
+            ],
+            BuildingBlockType.FF: [
+                StructuredMaskRule(
+                    keywords=[self.FFN_I],
+                    prune_by_row=True,
+                    prune_grid=(1, -1),
+                ),
+                StructuredMaskRule(
+                    keywords=[self.FFN_O],
+                    prune_by_row=False,
+                    prune_grid=(-1, 1),
+                ),
+            ],
+        }
+        return config
+
+
+@STRUCTURED_MASK_STRATEGY.register("huggingface_swin")
+class HuggingFaceBertStructuredMaskStrategy(BaseStructuredMaskStrategy):
+    MHSA_Q: str = "query"
+    MHSA_K: str = "key"
+    MHSA_V: str = "value"
+    MHSA_O: str = "SwinSelfOutput"
+    FFN_I: str = "SwinIntermediate"
+    FFN_O: str = "SwinOutput"
+
+    # Description of config in HuggingFace's Swin
+    # swin.config.depth: a list specifiying the number of Swin Transformer Block in each stage. 
+    #                    E.g. depth of Swin-b is [2, 2, 18, 2], meaning there 4 stages.
+    #
+    # swin.config.num_heads: a list of specifiying the number of self-attention head in each stage/
+    #                        E.g. Swin-b has [4, 8, 16, 32]. Together with depth config, stage 3 has 18 Swin Transformer Blocks,
+    #                        each Swin transformer block in this stage has 16 attention heads.
+    #
+    # swin.config.encoder_stride: synonymous to the number of output dimension in each self-attention head, dim_per_head
+    
+    def __init__(self, dim_per_head: int) -> None:
+        super().__init__()
+        self.dim_per_head = dim_per_head
+
+    @classmethod
+    def from_compressed_model(cls, compressed_model: NNCFNetwork):
+        return cls(
+            dim_per_head=compressed_model.nncf_module.swin.config.encoder_stride
+        )
+
+    @property
+    def strategy_by_group_type(self) -> Dict[str, List[StructuredMaskRule]]:
+        config = {
+            BuildingBlockType.MSHA: [
+                StructuredMaskRule(
+                    keywords=[self.MHSA_Q, self.MHSA_K, self.MHSA_V],
+                    prune_by_row=True,
+                    prune_grid=(self.dim_per_head, -1),
+                ),
+                StructuredMaskRule(
+                    keywords=[self.MHSA_O],
+                    prune_by_row=False,
+                    prune_grid=(-1, self.dim_per_head),
                 ),
             ],
             BuildingBlockType.FF: [
