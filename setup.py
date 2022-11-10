7de2913d351d3eebdcc4c0544b7696076e078279
Aleksei Kashapov <aleksei.kashapov@intel.com>
2022-11-10 10:36:31 +0100

    Remove shape_inference from initialization of ONNXGraph (#1353)
    ### Changes
    
    Remove shape_inference call from initialization of **ONNXGraph**. 
    Also, remove unnecessary dependency for ONNX -  **skl2onnx**
    
    ### Reason for changes
    
    **ONNXGraph** aims to provide the interface for obtaining necessary information from the ONNX model. 
    
    One of the functionality is to provide the information about activation tensors. Typically, the ONNX model doesn't contain any info about activatiion tensors. To get it this is essential to do shape inference. 
    Currently, the shape inference is done for every ONNX model during initialization. This PR implements a lazy initialization for ONNXGraph, meaning to do shape_inference only when it is needed. 
    
    This speeds up the quantization pipeline much.
    
    ### Related tickets
    
    No
    
    ### Tests
    
    No

--- setup.py
+++ setup.py
@@ -127,7 +127,6 @@ EXTRAS_REQUIRE = {
         "torch==1.9.1",
         "torchvision==0.10.1",
         "onnx==1.11.0",
-        "skl2onnx==1.9.3",
         "protobuf==3.20.1",
         "onnxruntime-openvino==1.11.0",
     ],
