c348044502aba492b66f0e4964c835317503cf31
Alexander <kozzzloff@list.ru>
2022-11-15 18:40:42 +0400

    Improved test robastness

--- tests/post_training/conftest.py
+++ tests/post_training/conftest.py
@@ -14,13 +14,11 @@ def pytest_configure(config):
 def pytest_runtest_makereport(item, call):
     outcome = yield
     result = outcome.get_result()
-    print(f"outcome: {outcome}")
-    print(f"result: {result}")
 
     if result.when == 'call':
         table = item.config._test_result
         header = ["Model name", "FP32 top-1", "Torch INT8 top-1", "ONNX INT8 top-1", "OV INT8 top-1",
             "FP32 FPS", "Torch INT8 FPS", "ONNX INT8 FPS", "OV INT8 FPS"]
         output_folder = Path(item.config.getoption("--output"))
-        
+        output_folder.mkdir(parents=True, exist_ok=True)        
         np.savetxt(output_folder / "results.csv", table, delimiter=",", fmt='%s', header=','.join(header))
