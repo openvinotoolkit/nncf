3faea867487c227fff210cbd9dcfccb55f31e1be
Alexander <kozzzloff@list.ru>
2022-11-16 10:53:23 +0400

    Added quantization options and defined model scope

--- tests/post_training/test_quantize_conformance.py
+++ tests/post_training/test_quantize_conformance.py
@@ -20,17 +20,10 @@ from sklearn.metrics import accuracy_score
 
 import openvino.runtime as ov
 
+from model_scope import get_validation_scope
+
 NOT_AVAILABLE_MESSAGE = 'N/A'
 
-def get_model_list():
-    full_list = timm.list_models()
-    #model_list = [m for m in full_list if "mobile" in m] # Shold be revised in the future
-    #model_list += [m for m in full_list if "vit" in m]
-    #model_list = [m for m in full_list if "levit" in m]
-    #model_list = ["adv_inception_v3", "bat_resnext26ts", "beit_base_patch16_224","botnet26t_256", "cait_m36_384", "coat_lite_mini", "convit_tiny", "convmixer_768_32",  "convnext_base","crossvit_9_240", "cspdarknet53", "darknet53", "deit_base_distilled_patch16_224","densenet121", "dla34",  "dm_nfnet_f0",  "dpn68", "eca_botnext26ts_256","ecaresnet26t", "efficientnet_b0","efficientnet_el_pruned","efficientnet_lite0", "efficientnetv2_l", "ese_vovnet19b_dw","fbnetc_100", "gcresnet33ts", "gernet_l", "gernet_m", "gernet_s", "ghostnet_050","gluon_senet154", "gluon_seresnext50_32x4d", "gluon_xception65", "gmixer_12_224",  "gmlp_b16_224","halo2botnet50ts_256", "hardcorenas_a","hrnet_w18", "ig_resnext101_32x8d", "inception_resnet_v2", "inception_v3", "inception_v4", "jx_nest_base","lambda_resnet26rpt_256","lcnet_035","levit_128", "mixer_b16_224","mnasnet_050", "mobilenetv2_035", "mobilenetv2_050", "mobilenetv2_075", "mobilenetv2_100","mobilenetv3_large_075", "mobilenetv3_large_100","nasnetalarge", "nest_base","nf_ecaresnet26", "nf_ecaresnet50", "nf_regnet_b0","nf_seresnet50", "nfnet_f2s","pit_b_distilled_224", "pit_s_224",  "pnasnet5large", "regnetx_002","regnety_002",  "regnetz_b16","repvgg_a2", "repvgg_b2",  "res2net50_14w_8s","resmlp_12_224","resmlp_36_224","resnest14d","resnet18", "resnetblur18","resnetrs50","resnetv2_50d", "resnetv2_50x1_bitm_in21k","resnext26ts","rexnetr_130","sebotnet33ts_256", "sehalonet33ts", "selecsls42","semnasnet_050",  "senet154", "seresnet18", "skresnet18", "spnasnet_100", "ssl_resnet18","swin_base_patch4_window7_224","swsl_resnet18","tresnet_m", "tv_resnet34", "twins_pcpvt_base","vgg11", "visformer_small","vit_base_patch16_224","wide_resnet101_2", "xception","xcit_large_24_p8_224"]
-    model_list = ["mobilenetv2_050", "resnet18", "mobilenetv3_large_075", "xception", "resnet18","seresnet18", "vgg11", "repvgg_a2","efficientnet_b0","efficientnet_el_pruned","efficientnet_lite0", "efficientnetv2_l"]
-    #model_list = ["mobilenetv2_050"]
-    return model_list
 
 def create_timm_model(name):
     model = timm.create_model(name, num_classes=1000, in_chans=3, pretrained=True, checkpoint_path="")
@@ -195,11 +188,15 @@ def output(pytestconfig):
 def result(pytestconfig):
     return pytestconfig._test_result
 
-@pytest.mark.parametrize("model_name", 
-            get_model_list())
-def test_ptq_timm(data, output, result, model_name):
+@pytest.mark.parametrize("model_args", 
+            get_validation_scope())
+def test_ptq_timm(data, output, result, model_args):
+    output_folder = Path(output)
+    output_folder.mkdir(parents=True, exist_ok=True) 
     torch.multiprocessing.set_sharing_strategy('file_system') # W/A to avoid RuntimeError
 
+    model_name = model_args["name"]
+    model_quantization_params = model_args["quantization_params"]
     try:
         ouput_folder = Path(output)
 
@@ -218,47 +215,62 @@ def test_ptq_timm(data, output, result, model_name):
         calibration_dataset = nncf.Dataset(val_dataloader, transform_fn)
 
         # quantize PyTorch model
-        torch_quantized_model = nncf.quantize(model, calibration_dataset)
-        # benchmark quantized torch model
-        torch_output_path = ouput_folder/ "torch"
-        torch_output_path.mkdir(parents=True, exist_ok=True)
-        q_torch_model_name = model_name + "_torch_int8"
-        q_torch_perf, q_torch_acc = benchmark_torch_model(torch_quantized_model, batch_one_dataloader, q_torch_model_name, torch_output_path)
+        try:
+            torch_quantized_model = nncf.quantize(model, calibration_dataset, **model_quantization_params)
+            # benchmark quantized torch model
+            torch_output_path = ouput_folder/ "torch"
+            torch_output_path.mkdir(parents=True, exist_ok=True)
+            q_torch_model_name = model_name + "_torch_int8"
+            q_torch_perf, q_torch_acc = benchmark_torch_model(torch_quantized_model, batch_one_dataloader, q_torch_model_name, torch_output_path)
+        except BaseException as error:
+            q_torch_perf = str(error)
+            q_torch_acc = str(error)
 
         # quantize ONNX model
-        onnx_model_path = ouput_folder / (model_name + ".onnx")
-        onnx_model = onnx.load(onnx_model_path)
-        onnx_input_name = onnx_model.graph.input[0].name
-        def onnx_transform_fn(data_item):
-            images, _ = data_item
-            return {onnx_input_name: images.numpy()}
-        onnx_calibration_dataset = nncf.Dataset(batch_one_dataloader, onnx_transform_fn)
+        try:
+            onnx_model_path = ouput_folder / (model_name + ".onnx")
+            onnx_model = onnx.load(onnx_model_path)
+            onnx_input_name = onnx_model.graph.input[0].name
+            def onnx_transform_fn(data_item):
+                images, _ = data_item
+                return {onnx_input_name: images.numpy()}
+            onnx_calibration_dataset = nncf.Dataset(batch_one_dataloader, onnx_transform_fn)
 
-        onnx_quantized_model = nncf.quantize(onnx_model, onnx_calibration_dataset)
+            onnx_quantized_model = nncf.quantize(onnx_model, onnx_calibration_dataset, **model_quantization_params)
 
-        onnx_output_path = ouput_folder/ "onnx"
-        onnx_output_path.mkdir(parents=True, exist_ok=True)
-        q_onnx_model_name = model_name + "_onnx_int8"
-        q_onnx_perf, q_onnx_acc = benchmark_onnx_model(onnx_quantized_model, batch_one_dataloader, q_onnx_model_name, onnx_output_path)
+            onnx_output_path = ouput_folder/ "onnx"
+            onnx_output_path.mkdir(parents=True, exist_ok=True)
+            q_onnx_model_name = model_name + "_onnx_int8"
+            q_onnx_perf, q_onnx_acc = benchmark_onnx_model(onnx_quantized_model, batch_one_dataloader, q_onnx_model_name, onnx_output_path)
+        except BaseException as error:
+            q_onnx_perf = str(error)
+            q_onnx_acc = str(error)
 
         # quantize OpenVINO model
-        def ov_transform_fn(data_item):
-            images, _ = data_item
-            return images.numpy()
-        ov_calibration_dataset = nncf.Dataset(batch_one_dataloader, ov_transform_fn)
+        try:
+            def ov_transform_fn(data_item):
+                images, _ = data_item
+                return images.numpy()
+            ov_calibration_dataset = nncf.Dataset(batch_one_dataloader, ov_transform_fn)
 
-        ov_model_path = ouput_folder / (model_name + ".xml")
-        core = ov.Core()
-        ov_model = core.read_model(ov_model_path)
-        ov_quantized_model = nncf.quantize(ov_model, ov_calibration_dataset)
+            ov_model_path = ouput_folder / (model_name + ".xml")
+            core = ov.Core()
+            ov_model = core.read_model(ov_model_path)
+            ov_quantized_model = nncf.quantize(ov_model, ov_calibration_dataset, **model_quantization_params)
 
-        ov_output_path = ouput_folder/ "openvino"
-        ov_output_path.mkdir(parents=True, exist_ok=True)
-        q_ov_model_name = model_name + "_ov_int8"
-        q_ov_perf, q_ov_acc = benchmark_ov_model(ov_quantized_model, batch_one_dataloader, q_ov_model_name, ov_output_path)
+            ov_output_path = ouput_folder/ "openvino"
+            ov_output_path.mkdir(parents=True, exist_ok=True)
+            q_ov_model_name = model_name + "_ov_int8"
+            q_ov_perf, q_ov_acc = benchmark_ov_model(ov_quantized_model, batch_one_dataloader, q_ov_model_name, ov_output_path)
+        except BaseException as error:
+            q_ov_perf = str(error)
+            q_ov_acc = str(error)
 
 
         result.append([model_name, orig_acc, q_torch_acc, q_onnx_acc, q_ov_acc,
                 orig_perf, q_torch_perf, q_onnx_perf, q_ov_perf])
     except BaseException as error:
+        result.append([model_name, error, "-", "-", "-",
+                "-", "-", "-", "-"])
         logging.error(f"Error when running test for model: {model_name}. Error: {error}")
+        assert False
