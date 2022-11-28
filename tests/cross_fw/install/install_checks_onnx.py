e3f3b08d1cffe2711290700b2bbc469f46b7331f
kshpv <aleksei.kashapov@intel.com>
2022-11-28 16:00:28 +0100

    Update test

--- tests/cross_fw/install/install_checks_onnx.py
+++ tests/cross_fw/install/install_checks_onnx.py
@@ -11,34 +11,17 @@
  limitations under the License.
 """
 
-from typing import List
+from tests.cross_fw.install.common import load_nncf_modules
 
-import numpy as np
+EXCLUDED_MODULES_PATTERNS = (
+    'nncf\\.openvino.*',
+    'nncf\\.tensorflow.*',
+    'nncf\\.torch.*',
+    'nncf\\.experimental\\.tensorflow.*',
+    'nncf\\.experimental\\.torch.*',
+    '.*?openvino_[^\\.]*',
+    '.*?torch_[^\\.]*',
+    '.*?tf_[^\\.]*'
+)
 
-from nncf.quantization.algorithms.post_training.algorithm import PostTrainingQuantization
-from nncf.quantization.algorithms.post_training.algorithm import PostTrainingQuantizationParameters
-from nncf.data.dataset import Dataset
-from tests.onnx.models import LinearModel
-
-
-class DatasetToTest:
-    def __init__(self, input_shape: List[int], input_name: str):
-        self._input_shape = input_shape
-        self._input_name = input_name
-
-    def __iter__(self):
-        yield {self._input_name: np.random.random(self._input_shape).astype(np.float32)}
-
-    def __len__(self):
-        return 1
-
-
-model = LinearModel()
-onnx_model = model.onnx_model
-input_shape = model.input_shape[0]
-input_name = onnx_model.graph.input[0].name
-dataset_to_test = DatasetToTest(input_shape, input_name)
-
-quantization_parameters = PostTrainingQuantizationParameters()
-quantization = PostTrainingQuantization(quantization_parameters)
-quantized_model = quantization.apply(onnx_model, dataset=Dataset(dataset_to_test))
+load_nncf_modules(EXCLUDED_MODULES_PATTERNS, True)
