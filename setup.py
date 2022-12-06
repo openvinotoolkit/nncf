fe17f5242ba8a565f846a25ae585e2a82732b9c0
Vasily Shamporov <vasily.shamporov@intel.com>
2022-12-06 16:12:00 +0100

    Relax patch version req'ts

--- setup.py
+++ setup.py
@@ -132,10 +132,10 @@ EXTRAS_REQUIRE = {
         "tensorflow~=2.8.4",
     ],
     "torch": [
-        "torch==1.13.0",
+        "torch~=1.13.0",
     ],
     "onnx": [
-        "torch==1.13.0",
+        "torch~=1.13.0",
         "torchvision==0.13.1",
         "onnx==1.12.0",
         "onnxruntime-openvino==1.13.1"
