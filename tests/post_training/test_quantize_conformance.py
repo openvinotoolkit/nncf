58dc6f4d4005c1d0aaf4b4eae25a51eda349e1c9
Alexander <kozzzloff@list.ru>
2022-12-09 14:19:09 +0400

    Refactored torch and onnx to factory

--- tests/post_training/test_quantize_conformance.py
+++ tests/post_training/test_quantize_conformance.py
@@ -1,3 +1,6 @@
+from abc import ABCMeta, abstractmethod
+from typing import Callable, Any
+
 import os
 import re
 import logging
@@ -6,94 +9,139 @@ import copy
 import numpy as np
 import pytest
 
-import torch
 import nncf
+import torch
 import onnx
-from torchvision import datasets
-from torchvision import transforms
-from torchvision.transforms import InterpolationMode
 
-import timm
 from sklearn.metrics import accuracy_score
 
 import openvino.runtime as ov
 
-from tests.shared.command import Command
 from model_scope import get_validation_scope
+from utils import create_timm_model
+from utils import get_model_transform
+from utils import get_torch_dataloader
+from utils import export_to_onnx
+from utils import export_to_ir
+from utils import run_benchmark
 
 
 NOT_AVAILABLE_MESSAGE = "N/A"
 DEFAULT_VAL_THREADS = 4
 ILSVRC_VALIDATION_SUBSET_SIZE = 50000
 
+class QuantizerBase(metaclass=ABCMeta):
+    """ Base class for an executor """
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
 
-def create_timm_model(name):
-    model = timm.create_model(
-        name, num_classes=1000, in_chans=3, pretrained=True, checkpoint_path=""
-    )
-    return model
+    @abstractmethod
+    def quantize(self, ):
+        """ Abstract method to quantize the model in a certain framework """
+        return False
+ 
+class QuantizerFactory:
+    """ The factory class for creating quantizers"""
+ 
+    registry = {}
+    """ Internal registry for available quantizers """
+ 
+    @classmethod
+    def register(cls, target_type=None) -> Callable:
+        def inner_wrapper(wrapped_class: QuantizerBase) -> Callable:
+            registered_name = wrapped_class.__name__ if target_type == None else target_type
+            if registered_name in cls.registry:
+                logging.warning('Executor %s already exists. Will replace it', registered_name)
+            cls.registry[registered_name] = wrapped_class
+            return wrapped_class
+ 
+        return inner_wrapper
+ 
+    @classmethod
+    def quantize(cls, quantizer_name, *args, **kwargs):
+        if quantizer_name not in cls.registry:
+            raise ValueError("Quantizer type does not exist")
+ 
+        quantizer = cls.registry[quantizer_name](quantizer_name)
+        return quantizer.quantize(*args, **kwargs)
+
+    @classmethod
+    def validate(cls, quantizer_name, *args, **kwargs):
+        if quantizer_name not in cls.registry:
+            raise ValueError("Quantizer type does not exist")
+ 
+        quantizer = cls.registry[quantizer_name](quantizer_name)
+        return quantizer.validate(*args, **kwargs)
+
+@QuantizerFactory.register("torch")
+class TorchQuantizer(QuantizerBase):
+    """ Torch quantizer class"""
+
+    def quantize(self, model, dataloader, quantization_params):
+        def transform_fn(data_item):
+                images, _ = data_item
+                return images
+
+        calibration_dataset = nncf.Dataset(dataloader, transform_fn)
+
+        quantized_model = nncf.quantize(
+            model, calibration_dataset, **quantization_params
+        )
+        return quantized_model
+
+    def validate(self, model, dataloader, model_name, output_folder, quantized=True):    
+        torch_model_name = model_name
+        torch_output_path = output_folder
+
+        if quantized:
+            torch_output_path = torch_output_path / "torch"
+            torch_output_path.mkdir(parents=True, exist_ok=True)
+            torch_model_name = torch_model_name + "_torch"
+            torch_model_name = torch_model_name + "_int8"
+
+        data_sample, _ = next(iter(dataloader))
+        # Dump model
+        onnx_path = Path(torch_output_path) / (torch_model_name + ".onnx")
+        export_to_onnx(model, onnx_path, data_sample)
+        ov_path = Path(torch_output_path) / (torch_model_name + ".xml")
+        export_to_ir(onnx_path, torch_output_path, torch_model_name)
+        # Validate accuracy
+        accuracy = validate_accuracy(ov_path, dataloader)
+        return ov_path, accuracy, torch_model_name
 
 
-def get_model_transform(model):
-    config = model.default_cfg
-    normalize = transforms.Normalize(mean=config["mean"], std=config["std"])
-    input_size = config["input_size"]
-    resize_size = tuple(int(x / config["crop_pct"]) for x in input_size[-2:])
+@QuantizerFactory.register("onnx")
+class ONNXQuantizer(QuantizerBase):
+    """ ONNX quantizer class"""
 
-    RESIZE_MODE_MAP = {
-        "bilinear": InterpolationMode.BILINEAR,
-        "bicubic": InterpolationMode.BICUBIC,
-        "nearest": InterpolationMode.NEAREST,
-    }
+    def quantize(self, model, dataloader, quantization_params):
+        onnx_input_name = model.graph.input[0].name
 
-    transform = transforms.Compose(
-        [
-            transforms.Resize(
-                resize_size, interpolation=RESIZE_MODE_MAP[config["interpolation"]]
-            ),
-            transforms.CenterCrop(input_size[-2:]),
-            transforms.ToTensor(),
-            normalize,
-        ]
-    )
-    return transform
+        def transform_fn(data_item):
+                images, _ = data_item
+                return {onnx_input_name: images.numpy()}
 
+        calibration_dataset = nncf.Dataset(dataloader, transform_fn)
 
-def get_torch_dataloader(folder, transform, batch_size=1):
-    val_dataset = datasets.ImageFolder(root=folder, transform=transform)
-    val_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=batch_size, num_workers=2, shuffle=False
-    )
-    return val_loader
+        quantized_model = nncf.quantize(
+            model, calibration_dataset, **quantization_params
+        )
+        return quantized_model
 
+    def validate(self, model, dataloader, model_name, output_folder):
+        output_path = output_folder / "onnx"
+        output_path.mkdir(parents=True, exist_ok=True)
+        q_model_name = model_name + "_onnx_int8"
 
-def export_to_onnx(model, save_path, data_sample):
-    torch.onnx.export(
-        model,
-        data_sample,
-        save_path,
-        export_params=True,
-        opset_version=13,
-        do_constant_folding=False,
-    )
-
-
-def export_to_ir(model_path, save_path, model_name):
-    runner = Command(f"mo -m {model_path} -o {save_path} -n {model_name}")
-    runner.run()
-
-
-def run_benchmark(model_path):
-    runner = Command(f"benchmark_app -m {model_path} -d CPU -niter 300")
-    runner.run()
-    cmd_output = " ".join(runner.output)
-
-    match = re.search(r"Throughput\: (.+?) FPS", cmd_output)
-    if match is not None:
-        fps = match.group(1)
-        return float(fps), cmd_output
-
-    return None, cmd_output
+        # Dump model
+        onnx_path = Path(output_path) / (q_model_name + ".onnx")
+        onnx.save(model, onnx_path)
+        ov_path = Path(output_path) / (q_model_name + ".xml")
+        export_to_ir(onnx_path, output_path, q_model_name)
+        # Validate accuracy
+        accuracy = validate_accuracy(ov_path, dataloader)
+        return ov_path, accuracy, q_model_name
 
 
 def benchmark_performance(model_path, model_name):
@@ -107,12 +155,12 @@ def benchmark_performance(model_path, model_name):
         model_perf, bench_output = run_benchmark(model_path)
 
         if model_perf is None:
-            logging.info(
-                f"Cannot measure performance for the model: {model_name}\nDetails: {bench_output}\n"
+            logging.error(
+                f"Cannot measure performance for the model: {model_name}.\nDetails: {bench_output}\n"
             )
             model_perf = NOT_AVAILABLE_MESSAGE
     except BaseException as error:
-        logging.error(f"Error when becnhmarking the model: model_name Details: {error}")
+        logging.error(f"Error when becnhmarking the model: model_name. Details: {error}")
 
     return model_perf
 
@@ -149,31 +197,6 @@ def validate_accuracy(model_path, val_loader):
     references = np.concatenate(references, axis=0)
 
     return accuracy_score(predictions, references)
-
-
-def validate_torch_model(model, dataloader, model_name, output_path):
-    data_sample, _ = next(iter(dataloader))
-    # Dump model
-    onnx_path = Path(output_path) / (model_name + ".onnx")
-    export_to_onnx(model, onnx_path, data_sample)
-    ov_path = Path(output_path) / (model_name + ".xml")
-    export_to_ir(onnx_path, output_path, model_name)
-
-    # Validate accuracy
-    accuracy = validate_accuracy(ov_path, dataloader)
-    return ov_path, accuracy
-
-
-def validate_onnx_model(model, dataloader, model_name, output_path):
-    # Dump model
-    onnx_path = Path(output_path) / (model_name + ".onnx")
-    onnx.save(model, onnx_path)
-    ov_path = Path(output_path) / (model_name + ".xml")
-    export_to_ir(onnx_path, output_path, model_name)
-
-    # Validate accuracy
-    accuracy = validate_accuracy(ov_path, dataloader)
-    return ov_path, accuracy
 
 
 def validate_ov_model(model, dataloader, model_name, output_path):
@@ -228,10 +251,10 @@ def test_ptq_timm(data, output, subset, result, model_args): # pylint: disable=W
     logging.getLogger().setLevel(logging.INFO)
 
     model_name = model_args["name"]
-    model_quantization_params = model_args["quantization_params"]
+    quantization_params = model_args["quantization_params"]
 
     logging.info(f"Quantizing model: '{model_name}', with parameters: "
-                 f"{model_quantization_params}")
+                 f"{quantization_params}")
 
     try:
         output_folder = Path(output)
@@ -244,9 +267,13 @@ def test_ptq_timm(data, output, subset, result, model_args): # pylint: disable=W
         batch_one_dataloader = get_torch_dataloader(data, transform, batch_size=1)
         val_dataloader = get_torch_dataloader(data, transform, batch_size=128)
         # benchmark original models (once)
-        orig_model_path, orig_acc = validate_torch_model(
-            model, batch_one_dataloader, model_name, output_folder
-        )
+        #orig_model_path, orig_acc = validate_torch_model(
+        #    model, batch_one_dataloader, model_name, output_folder
+        #)
+
+        orig_model_path, orig_acc, _ = QuantizerFactory.validate(
+                                                "torch", model, batch_one_dataloader,
+                                                model_name, output_folder, False)
 
         # keep paths and names for performance benchmarking
         ov_model_paths = [orig_model_path]
@@ -254,25 +281,11 @@ def test_ptq_timm(data, output, subset, result, model_args): # pylint: disable=W
 
         # quantize PyTorch model
         try:
-            def transform_fn(data_item):
-                images, _ = data_item
-                return images
+            q_torch_model = QuantizerFactory.quantize("torch", model, val_dataloader, quantization_params)
+            ov_model_path, q_torch_acc, q_torch_model_name = QuantizerFactory.validate(
+                                                "torch", q_torch_model, batch_one_dataloader,
+                                                model_name, output_folder)
 
-            calibration_dataset = nncf.Dataset(val_dataloader, transform_fn)
-
-            torch_quantized_model = nncf.quantize(
-                model, calibration_dataset, **model_quantization_params
-            )
-            # benchmark quantized torch model
-            torch_output_path = output_folder / "torch"
-            torch_output_path.mkdir(parents=True, exist_ok=True)
-            q_torch_model_name = model_name + "_torch_int8"
-            ov_model_path, q_torch_acc = validate_torch_model(
-                torch_quantized_model,
-                batch_one_dataloader,
-                q_torch_model_name,
-                torch_output_path,
-            )
             ov_model_paths.append(ov_model_path)
             model_names.append(q_torch_model_name)
 
@@ -285,31 +298,14 @@ def test_ptq_timm(data, output, subset, result, model_args): # pylint: disable=W
         try:
             onnx_model_path = output_folder / (model_name + ".onnx")
             onnx_model = onnx.load(onnx_model_path)
-            onnx_input_name = onnx_model.graph.input[0].name
 
-            def onnx_transform_fn(data_item):
-                images, _ = data_item
-                return {onnx_input_name: images.numpy()}
+            q_onnx_model = QuantizerFactory.quantize("onnx", onnx_model, batch_one_dataloader, quantization_params)
+            ov_model_path, q_onnx_acc, q_onnx_model_name = QuantizerFactory.validate(
+                                                "onnx", q_onnx_model, batch_one_dataloader,
+                                                model_name, output_folder)
 
-            onnx_calibration_dataset = nncf.Dataset(
-                batch_one_dataloader, onnx_transform_fn
-            )
-
-            onnx_quantized_model = nncf.quantize(
-                onnx_model, onnx_calibration_dataset, **model_quantization_params
-            )
-
-            onnx_output_path = output_folder / "onnx"
-            onnx_output_path.mkdir(parents=True, exist_ok=True)
-            q_onnx_model_name = model_name + "_onnx_int8"
-            ov_model_path, q_onnx_acc = validate_onnx_model(
-                onnx_quantized_model,
-                batch_one_dataloader,
-                q_onnx_model_name,
-                onnx_output_path,
-            )
             ov_model_paths.append(ov_model_path)
-            model_names.append(q_torch_model_name)
+            model_names.append(q_onnx_model_name)
         except Exception as error:
             q_onnx_acc = re.escape(str(error))
             ov_model_paths.append(None)
@@ -328,7 +324,7 @@ def test_ptq_timm(data, output, subset, result, model_args): # pylint: disable=W
             core = ov.Core()
             ov_model = core.read_model(ov_model_path)
             ov_quantized_model = nncf.quantize(
-                ov_model, ov_calibration_dataset, **model_quantization_params
+                ov_model, ov_calibration_dataset, **quantization_params
             )
 
             ov_output_path = output_folder / "openvino"
