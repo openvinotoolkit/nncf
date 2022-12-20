3f07ca7b7c277af490201251d7b3f773b45439f1
Vasily Shamporov <vasily.shamporov@intel.com>
2022-12-20 13:16:18 +0100

    Extend deprecated decorator to apply to classes

--- nncf/internal/deprecation.py
+++ nncf/internal/deprecation.py
@@ -10,8 +10,12 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 """
+import functools
+import inspect
 import warnings
 from typing import Callable
+from typing import Type
+from typing import TypeVar
 
 
 def warning_deprecated(msg):
@@ -19,10 +23,14 @@ def warning_deprecated(msg):
     warnings.warn(msg, FutureWarning, stacklevel=2)
 
 
-class deprecated_call:
+ClassOrFn = TypeVar('ClassOrFn', Callable, Type)
+
+
+class deprecated:
     """
-    A decorator for marking function calls as deprecated. A call to the marked function will
-    trigger a `FutureWarning`.
+    A decorator for marking function calls or class instantiations as deprecated. A call to the marked function or an
+    instantiation of an object of the marked class will trigger a `FutureWarning`. If a class is marked as
+    @deprecated, only the instantiations will trigger a warning, but static attribute accesses or method calls will not.
     """
     def __init__(self, msg: str = None):
         """
@@ -30,13 +38,19 @@ class deprecated_call:
         """
         self.msg = msg
 
-    def __call__(self, fn: Callable) -> Callable:
-        name = fn.__module__ + '.' + fn.__name__
+    def __call__(self, fn_or_class: ClassOrFn) -> ClassOrFn:
+        name = fn_or_class.__module__ + '.' + fn_or_class.__name__
+        if inspect.isclass(fn_or_class):
+            fn_or_class.__init__ = self._get_wrapper(fn_or_class.__init__, name)
+            return fn_or_class
+        return self._get_wrapper(fn_or_class, name)
 
+    def _get_wrapper(self, fn_to_wrap: Callable, name: str) -> Callable:
+        @functools.wraps(fn_to_wrap)
         def wrapped(*args, **kwargs):
             msg = f"Usage of {name} is deprecated and will be removed in a future NNCF version."
             if self.msg is not None:
                 msg += self.msg
             warning_deprecated(msg)
-            return fn(*args, **kwargs)
+            return fn_to_wrap(*args, **kwargs)
         return wrapped
