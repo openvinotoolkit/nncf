Fix FQ calculation and add tests

nncf/experimental/openvino_native/quantization/quantizer_parameters.py
@@ -11,7 +11,7 @@
  limitations under the License.
 """
 
-from typing import Optional
+from typing import Optional, Tuple
 from dataclasses import dataclass
 import numpy as np
 
@@ -38,6 +38,87 @@ class OVQuantizerLayerParameters:
     levels: int
 
 
+def compute_levels(quantizer_config: QuantizerConfig, is_weights: bool) -> int:
+    def_levels = 2 ** quantizer_config.num_bits
+
+    if is_weights and quantizer_config.mode == QuantizationMode.SYMMETRIC:
+        level_low = -def_levels / 2 + 1
+    else:
+        level_low = -def_levels / 2
+    level_high = def_levels / 2 - 1
+    return int(abs(level_high) + abs(level_low) + 1)
+
+
+def fix_zero_filters_symmetric(max_level: np.ndarray, eps: float = 0.01) -> np.ndarray:
+    max_range = np.max(max_level)
+    lower_threshold = np.maximum(8e-5, eps * max_range)
+    return np.maximum(lower_threshold, max_level)
+
+
+def fix_zero_filters_asymmetric(max_level: np.ndarray, min_level: np.ndarray,
+                                eps: float = 1e-8) -> Tuple[np.ndarray, np.ndarray]:
+    ranges = max_level - min_level
+    ranges = ranges if isinstance(ranges, np.ndarray) else np.array([ranges])
+    min_correction = 8 * 10e-5
+    corrections = [(np.maximum(eps * rng, rng) - rng) * 0.5 if rng > min_correction
+                   else min_correction for rng in ranges]
+    max_level = max_level + corrections
+    min_level = min_level - corrections
+    return max_level, min_level
+
+
+def tune_range(left_border: np.ndarray, right_border: np.ndarray, num_bits: int) -> Tuple[np.ndarray, np.ndarray]:
+    """ Tunes asymmetric quantization range to set zero quant precisely to zero value.
+    Function moves left or right borders to do this and doesn't make left border higher or
+    right border lesser than its original values
+    :param left_border: range left border
+    :param right_border: range right border
+    :param num_bits: number of bits to perform quantization
+    :return tuple with recomputed ranges
+    """
+    level_high = 2 ** num_bits - 1
+    s = level_high / (right_border - left_border)
+    fval = -left_border * s
+    qval = np.round(fval)
+
+    with np.errstate(invalid='ignore', divide='ignore'):
+        ra = np.where(qval < level_high, qval / (qval - level_high) * right_border, left_border)
+        rb = np.where(qval > 0.0, (qval - level_high) / qval * left_border, right_border)
+
+    range_a = right_border - ra
+    range_b = rb - left_border
+
+    mask = np.where(range_a > range_b, 1.0, 0.0)
+    inv_mask = np.abs(1.0 - mask)
+
+    ra = mask * ra + inv_mask * left_border
+    rb = inv_mask * rb + mask * right_border
+
+    return ra, rb
+
+
+def symmetric_range(min_values: np.ndarray, max_values: np.ndarray, levels: int,
+                    quantizer_config: QuantizerConfig, is_weights: bool) -> Tuple[np.ndarray, np.ndarray]:
+    max_level = fix_zero_filters_symmetric(max_values)
+    if is_weights:
+        min_level = -max_level
+    else:
+        signed = quantizer_config.signedness_to_force
+        min_level = np.zeros(max_level.shape) if np.all(min_values >= 0) and not signed else \
+            -max_level * levels / (levels - 2)
+
+    return min_level, max_level
+
+
+def asymmetric_range(min_values: np.ndarray, max_values: np.ndarray,
+                     quantizer_config: QuantizerConfig) -> Tuple[np.ndarray, np.ndarray]:
+    max_level, min_level = fix_zero_filters_asymmetric(max_values, min_values)
+    min_level = np.where(min_level < 0.0, min_level, 0.0)
+    max_level = np.where(max_level > 0.0, max_level, 0.0)
+    min_level, max_level = tune_range(min_level, max_level, quantizer_config.num_bits)
+    return min_level, max_level
+
+
 def calculate_weight_quantizer_parameters(weight_tensor: np.ndarray, quantizer_config: QuantizerConfig,
                                           axis: Optional[int]) -> OVQuantizerLayerParameters:
     """
@@ -55,17 +136,17 @@ def calculate_weight_quantizer_parameters(weight_tensor: np.ndarray, quantizer_c
         axes = tuple(axes)
     else:
         axes = None
-    input_high = np.amax(weight_tensor, axis=axes, keepdims=True)
-    input_low = np.amin(weight_tensor, axis=axes, keepdims=True)
+    min_values = np.amin(weight_tensor, axis=axes, keepdims=True)
+    max_values = np.amax(weight_tensor, axis=axes, keepdims=True)
 
-    levels = 2 ** quantizer_config.num_bits
+    levels = compute_levels(quantizer_config, is_weights=True)
     if quantizer_config.mode == QuantizationMode.SYMMETRIC:
-        output_low = np.full_like(input_low, fill_value=-levels / 2)
-        output_high = np.full_like(input_high, fill_value=levels / 2 - 1)
+        min_level, max_level = symmetric_range(min_values, max_values, levels, quantizer_config, is_weights=True)
     else:
-        output_low = np.zeros_like(input_low)
-        output_high = np.full_like(input_high, fill_value=levels - 1)
-    return OVQuantizerLayerParameters(input_low, input_high, output_low, output_high, levels)
+        min_level, max_level = asymmetric_range(min_values, max_values, quantizer_config)
+
+    output_low, output_high = min_level, max_level
+    return OVQuantizerLayerParameters(min_values, max_values, output_low, output_high, levels)
 
 
 def calculate_activation_quantizer_parameters(statistics: MinMaxTensorStatistic,
@@ -77,15 +158,14 @@ def calculate_activation_quantizer_parameters(statistics: MinMaxTensorStatistic,
     :param quantizer_config: Config of the quantization configuration.
     :return: Parameters of the FakeQuantize layer.
     """
-    input_low = np.array(statistics.min_values)
-    input_high = np.array(statistics.max_values)
-    levels = 2 ** quantizer_config.num_bits
+    levels = compute_levels(quantizer_config, is_weights=False)
+    min_values = np.array(statistics.min_values)
+    max_values = np.array(statistics.max_values)
 
     if quantizer_config.mode == QuantizationMode.SYMMETRIC:
-        output_low = np.full_like(input_low, fill_value=-levels / 2)
-        output_high = np.full_like(input_high, fill_value=levels / 2 - 1)
+        min_level, max_level = symmetric_range(min_values, max_values, levels, quantizer_config, is_weights=False)
     else:
-        output_low = np.zeros_like(input_low)
-        output_high = np.full_like(input_high, fill_value=levels - 1)
+        min_level, max_level = asymmetric_range(min_values, max_values, quantizer_config)
 
-    return OVQuantizerLayerParameters(input_low, input_high, output_low, output_high, levels)
+    output_low, output_high = min_level, max_level
+    return OVQuantizerLayerParameters(min_level, max_level, output_low, output_high, levels)

tests/openvino/native/models.py
@@ -12,8 +12,13 @@
 """
 
 import numpy as np
+from abc import ABC, abstractmethod
 import openvino.runtime as ov
 from openvino.runtime import opset9 as opset
+
+from nncf.common.utils.registry import Registry
+
+SYNTHETIC_MODELS = Registry('OV_SYNTHETIC_MODELS')
 
 
 class OVReferenceModel:
@@ -22,31 +27,43 @@ class OVReferenceModel:
         self.ref_graph_name = f'{self.__class__.__name__}.dot'
 
 
-class LinearModel(OVReferenceModel):
+class OVReferenceModel(ABC):
     def __init__(self):
+        self._rng = np.random.default_rng(seed=0)
+        self.ref_graph_name = f'{self.__class__.__name__}.dot'
+        self.ov_model = self._create_ov_model()
+
+    @abstractmethod
+    def _create_ov_model(self) -> ov.Model:
+        pass
+
+
+@SYNTHETIC_MODELS.register()
+class LinearModel(OVReferenceModel):
+    def _create_ov_model(self):
         input_shape = [1, 3, 4, 2]
         input_1 = opset.parameter(input_shape, name="Input")
         reshape = opset.reshape(input_1, (1, 3, 2, 4), special_zero=False, name='Reshape')
-        data = np.random.rand(1, 3, 4, 5).astype(np.float32)
+        data = self._rng.random((1, 3, 4, 5)).astype(np.float32)
         matmul = opset.matmul(reshape, data, transpose_a=False, transpose_b=False, name="MatMul")
-        add = opset.add(reshape, np.random.rand(1, 3, 2, 4).astype(np.float32), name="Add")
+        add = opset.add(reshape, self._rng.random(1, 3, 2, 4).astype(np.float32), name="Add")
         r1 = opset.result(matmul, name="Result_MatMul")
         # TODO(KodiaqQ): Remove this after fix - CVS-100010
         r1.get_output_tensor(0).set_names(set(["Result_MatMul"]))
         r2 = opset.result(add, name="Result_Add")
         r2.get_output_tensor(0).set_names(set(["Result_Add"]))
         model = ov.Model([r1, r2], [input_1])
+        return model
 
-        super().__init__(model)
 
-
+@SYNTHETIC_MODELS.register()
 class ConvModel(OVReferenceModel):
-    def __init__(self):
+    def _create_ov_model(self):
         input_1 = opset.parameter([1, 3, 4, 2], name="Input_1")
-        mean = np.random.rand(1, 3, 1, 1).astype(np.float32)
-        scale = np.random.rand(1, 3, 1, 1).astype(np.float32) + 1e-4
+        mean = self._rng.random((1, 3, 1, 1)).astype(np.float32)
+        scale = self._rng.random((1, 3, 1, 1)).astype(np.float32) + 1e-4
         subtract = opset.subtract(input_1, mean, name="Sub")
-        kernel = np.random.rand(3, 3, 1, 1).astype(np.float32) / scale
+        kernel = self._rng.random((3, 3, 1, 1)).astype(np.float32) / scale
         strides = [1, 1]
         pads = [0, 0]
         dilations = [1, 1]
@@ -61,8 +78,7 @@ class ConvModel(OVReferenceModel):
         cat = opset.concat([relu, transpose], axis=0)
         result = opset.result(cat, name="Result")
         model = ov.Model([result], [input_1, input_2])
-
-        super().__init__(model)
+        return model
 
 
 class QuantizedModel(OVReferenceModel):
@@ -113,20 +129,20 @@ class QuantizedModel(OVReferenceModel):
         model = ov.Model([result], [input_1, input_2])
 
 class WeightsModel(OVReferenceModel):
-    def __init__(self):
+    def _create_ov_model(self):
         input_1 = opset.parameter([1, 3, 5, 5], name="Input_1")
-        kernel = np.random.rand(3, 3, 1, 1).astype(np.float32)
+        kernel = self._rng.random((3, 3, 1, 1)).astype(np.float32)
         strides = [1, 1]
         pads = [0, 0]
         dilations = [1, 1]
         conv = opset.convolution(input_1, kernel, strides, pads, pads, dilations, name="Conv")
-        kernel_2 = np.random.rand(3, 3, 1, 1).astype(np.float32)
+        kernel_2 = self._rng.random((3, 3, 1, 1)).astype(np.float32)
         output_shape = [1, 1]
         conv_tr = opset.convolution_backprop_data(conv, kernel_2, output_shape, strides, pads, pads, dilations, name="Conv_backprop")
 
-        weights_1 = np.random.rand(1, 3, 1, 4).astype(np.float32)
+        weights_1 = self._rng.random((1, 3, 1, 4)).astype(np.float32)
         matmul_1 = opset.matmul(conv_tr, weights_1, transpose_a=False, transpose_b=False, name="MatMul_1")
-        weights_0 = np.random.rand(1, 3, 1, 1).astype(np.float32)
+        weights_0 = self._rng.random((1, 3, 1, 1)).astype(np.float32)
         matmul_0 = opset.matmul(weights_0, matmul_1, transpose_a=False, transpose_b=False, name="MatMul_0")
         matmul = opset.matmul(matmul_0, matmul_1, transpose_a=False, transpose_b=True, name="MatMul")
         matmul_const = opset.matmul(weights_1, weights_0, transpose_a=True, transpose_b=False, name="MatMul_const")
@@ -134,18 +150,17 @@ class WeightsModel(OVReferenceModel):
         add = opset.add(matmul_const, matmul)
         result = opset.result(add, name="Result")
         model = ov.Model([result], [input_1])
+        return model
 
-        super().__init__(model)
 
-
+@SYNTHETIC_MODELS.register()
 class MatMul2DModel(OVReferenceModel):
-    def __init__(self):
+    def _create_ov_model(self):
         input_shape = [3, 5]
         input_1 = opset.parameter(input_shape, name="Input")
-        data = np.random.rand(5, 2).astype(np.float32)
+        data = self._rng.random((5, 2)).astype(np.float32)
         matmul = opset.matmul(input_1, data, transpose_a=False, transpose_b=False, name="MatMul")
-        add = opset.add(matmul, np.random.rand(1, 2).astype(np.float32), name="Add")
+        add = opset.add(matmul, self._rng.random((1, 2)).astype(np.float32), name="Add")
         result_1 = opset.result(add, name="Result")
-        model = ov.Model(result_1, input_1)
-
-        super().__init__(model)
+        model = ov.Model([result_1], [input_1])
+        return model

tests/openvino/native/test_statistics_aggregator.py
@@ -12,8 +12,9 @@
 """
 
 import pytest
-from typing import List
 import numpy as np
+from typing import Optional, Tuple, Union
+from dataclasses import dataclass
 
 from nncf import Dataset
 from nncf.common.graph.transformations.commands import TargetType
@@ -41,22 +42,16 @@ DATASET_SAMPLES[0][0, 2, 0, 0] = 128  # max
 DATASET_SAMPLES[0][0, 2, 0, 1] = -128  # min
 
 
+@dataclass
 class TestParameters:
-    def __init__(self, range_type, use_abs_max, reduction_shape, ref_max_val, ref_min_val):
-        self.range_type = range_type
-        self.use_abs_max = use_abs_max
-        self.reduction_shape = reduction_shape
-        self.ref_max_val = ref_max_val
-        self.ref_min_val = ref_min_val
+    range_type: RangeType
+    use_abs_max: bool
+    reduction_shape: Optional[Tuple[int]]
+    ref_max_val: Union[np.ndarray, float]
+    ref_min_val: Union[np.ndarray, float]
 
 
-def get_dataset_for_test(samples: List[np.ndarray], input_name: str) ->  Dataset:
-    def transform_fn(data_item):
-        return {input_name: data_item}
-    return Dataset(samples, transform_fn)
-
-
-@pytest.mark.parametrize('test_parameters, ',
+@pytest.mark.parametrize('test_parameters',
                          ((TestParameters(RangeType.MEAN_MINMAX, False, None, 64.5, -63.5)),
                           (TestParameters(RangeType.MEAN_MINMAX, False, (0, 2, 3), np.array((1, 0.55, 64.5)),
                                           np.array((-4.5, 0, -63.5)))),
@@ -71,8 +66,9 @@ def get_dataset_for_test(samples: List[np.ndarray], input_name: str) ->  Dataset
                           )
                          )
 def test_statistics_aggregator(test_parameters):
+    target_node_name = 'Input'
     model = LinearModel().ov_model
-    dataset = get_dataset_for_test(DATASET_SAMPLES, "Input")
+    dataset = Dataset(DATASET_SAMPLES, transform_func=lambda data: {target_node_name: data})
 
     statistics_aggregator = OVStatisticsAggregator(dataset)
     statistics_points = StatisticPointsContainer()
@@ -83,7 +79,6 @@ def test_statistics_aggregator(test_parameters):
         tensor_collector = OVMeanMinMaxStatisticCollector(False, test_parameters.use_abs_max,
                                                             test_parameters.reduction_shape,
                                                             num_samples=len(DATASET_SAMPLES))
-    target_node_name = 'Input'
     algorithm_name = 'TestAlgo'
     statistic_point_type = TargetType.POST_LAYER_OPERATION
     target_point = OVTargetPoint(statistic_point_type, target_node_name, 0)