fe2cd359d1ae67c8d2f568ae2e161d72212b9ac0
Vasily Shamporov <vasily.shamporov@intel.com>
2023-01-20 14:01:11 +0100

    Use parse_voc_xml in a backwards-compatible way with old torchvision (#1515)
    ### Changes
    
    As stated in the title.
    
    ### Reason for changes
    Fixes some of the issues in PT compat E2E tests for 1.8.2 and 1.9.1
    
    ### Related tickets
    
    N/A
    
    ### Tests
    e2e compat PT tests

--- examples/torch/object_detection/datasets/voc0712.py
+++ examples/torch/object_detection/datasets/voc0712.py
@@ -189,7 +189,10 @@ class VOCDetection(data.Dataset):
         img_name = self.ids[index]
         anno = ET.parse(self._annopath % img_name).getroot()
         _, gt_res = self.target_transform(None,
-                                          datasets.VOCDetection.parse_voc_xml(node=anno),
+                                          # Instantiating an object is required here for backwards compatibility with
+                                          # pre-torchvision 0.13 versions, where `parse_voc_xml` was not yet a static
+                                          # method
+                                          datasets.VOCDetection(self.root).parse_voc_xml(node=anno),
                                           pull=True)
         return img_name[1], gt_res
 
