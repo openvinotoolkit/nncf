2c47966ef7bdf07207cb1de7d4fcdf0015354ee1
Aleksei Kashapov <aleksei.kashapov@intel.com>
2023-01-26 11:05:25 +0100

    ONNX Add new pattern for densenet (#1513)
    ### Changes
    
    Introduce new patterns - **BatchNorm + Arithmetic Operation + Arithmetic Operation + Activation** 
    **LINEAR + BatchNorm + Arithmetic Operation + Arithmetic Operation + Activation** 
    
    ![image](https://user-images.githubusercontent.com/32935044/213189442-724d1aab-dfeb-47b9-b8ef-496132bc5991.png)
    
    
    ### Reason for changes
    
    To enable correct quantization scheme for densenet121 without ignored_scopes.
    
    ### Related tickets
    
    No
    
    ### Tests
    
    Add graph test for densenet121 from ONNX Model ZOO
    
    Co-authored-by: Liubov Talamanova  <piccione-mail@yandex.ru>
    Co-authored-by: Alexander Suslov <alexander.suslov@intel.com>

--- tests/onnx/quantization/test_classification_models_graph.py
+++ tests/onnx/quantization/test_classification_models_graph.py
@@ -17,34 +17,50 @@ import torch
 from torchvision import models
 import onnx
 # pylint: disable=no-member
-
+from tests.onnx.conftest import ONNX_MODEL_DIR
 from tests.onnx.quantization.common import ModelToTest
 from tests.onnx.quantization.common import min_max_quantize_model
 from tests.onnx.quantization.common import compare_nncf_graph
 from tests.onnx.quantization.common import mock_collect_statistics
+from tests.onnx.weightless_model import load_model_topology_with_zeros_weights
+
+TORCHVISION_TEST_DATA = [(ModelToTest('resnet18', [1, 3, 224, 224]), models.resnet18(pretrained=True)),
+                         (ModelToTest('mobilenet_v2', [1, 3, 224, 224]), models.mobilenet_v2(pretrained=True)),
+                         (ModelToTest('mobilenet_v3_small', [1, 3, 224, 224]),
+                          models.mobilenet_v3_small(pretrained=True)),
+                         (ModelToTest('inception_v3', [1, 3, 224, 224]), models.inception_v3(pretrained=True)),
+                         (ModelToTest('googlenet', [1, 3, 224, 224]), models.googlenet(pretrained=True)),
+                         (ModelToTest('vgg16', [1, 3, 224, 224]), models.vgg16(pretrained=True)),
+                         (ModelToTest('shufflenet_v2_x1_0', [1, 3, 224, 224]),
+                          models.shufflenet_v2_x1_0(pretrained=True)),
+                         (ModelToTest('squeezenet1_0', [1, 3, 224, 224]), models.squeezenet1_0(pretrained=True)),
+                         (ModelToTest('densenet121', [1, 3, 224, 224]), models.densenet121(pretrained=True)),
+                         (ModelToTest('mnasnet0_5', [1, 3, 224, 224]), models.mnasnet0_5(pretrained=True)),
+                         ]
 
 
-@pytest.mark.parametrize(('model_to_test', 'model'),
-                         [(ModelToTest('resnet18', [1, 3, 224, 224]), models.resnet18(pretrained=True)),
-                          (ModelToTest('mobilenet_v2', [1, 3, 224, 224]), models.mobilenet_v2(pretrained=True)),
-                          (ModelToTest('mobilenet_v3_small', [1, 3, 224, 224]),
-                           models.mobilenet_v3_small(pretrained=True)),
-                          (ModelToTest('inception_v3', [1, 3, 224, 224]), models.inception_v3(pretrained=True)),
-                          (ModelToTest('googlenet', [1, 3, 224, 224]), models.googlenet(pretrained=True)),
-                          (ModelToTest('vgg16', [1, 3, 224, 224]), models.vgg16(pretrained=True)),
-                          (ModelToTest('shufflenet_v2_x1_0', [1, 3, 224, 224]),
-                           models.shufflenet_v2_x1_0(pretrained=True)),
-                          (ModelToTest('squeezenet1_0', [1, 3, 224, 224]), models.squeezenet1_0(pretrained=True)),
-                          (ModelToTest('densenet121', [1, 3, 224, 224]), models.densenet121(pretrained=True)),
-                          (ModelToTest('mnasnet0_5', [1, 3, 224, 224]), models.mnasnet0_5(pretrained=True)),
-                          ]
-                         )
-def test_min_max_quantization_graph(tmp_path, mocker, model_to_test, model):
+@pytest.mark.parametrize(('model_to_test', 'model'), TORCHVISION_TEST_DATA,
+                         ids=[model_to_test[0].model_name for model_to_test in TORCHVISION_TEST_DATA])
+def test_min_max_quantization_graph_torchvision_models(tmp_path, mocker, model_to_test, model):
     mock_collect_statistics(mocker)
     onnx_model_path = tmp_path / model_to_test.model_name
     x = torch.randn(model_to_test.input_shape, requires_grad=False)
     torch.onnx.export(model, x, onnx_model_path, opset_version=13)
 
     original_model = onnx.load(onnx_model_path)
+    quantized_model = min_max_quantize_model(model_to_test.input_shape, original_model)
+    compare_nncf_graph(quantized_model, model_to_test.path_ref_graph)
+
+
+ONNX_TEST_DATA = [ModelToTest('densenet-12', [1, 3, 224, 224])]
+
+
+@pytest.mark.parametrize(('model_to_test'), ONNX_TEST_DATA,
+                         ids=[model_to_test.model_name for model_to_test in ONNX_TEST_DATA])
+def test_min_max_quantization_graph_onnx_model(tmp_path, mocker, model_to_test):
+    mock_collect_statistics(mocker)
+    onnx_model_path = ONNX_MODEL_DIR / (model_to_test.model_name + '.onnx')
+    original_model = load_model_topology_with_zeros_weights(onnx_model_path)
+
     quantized_model = min_max_quantize_model(model_to_test.input_shape, original_model)
     compare_nncf_graph(quantized_model, model_to_test.path_ref_graph)
