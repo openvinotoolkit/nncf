fc54446d1fe45b5544b6360a03ea59772e3e10b0
kshpv <aleksei.kashapov@intel.com>
2023-01-26 11:48:54 +0100

    Add mobielenet_v2 reg test

---
+++ tests/onnx/quantization/test_ptq_regression.py
@@ -0,0 +1,41 @@
+"""
+Copyright (c) 2023 Intel Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import pytest
+
+import subprocess
+import sys
+from tests.shared.paths import PROJECT_ROOT
+from tests.shared.paths import EXAMPLES_DIR
+
+
+def test_mobilenet_v2_regression(verbose: bool = False):
+    script_path = EXAMPLES_DIR / 'post_training_quantization' / 'onnx' / 'mobilenet_v2' / 'main.py'
+    command = f'{sys.executable} {script_path}'
+    print(f"Run command: {command}")
+    with subprocess.Popen(command,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.STDOUT,
+                          cwd=PROJECT_ROOT) as result:
+        outs, _ = result.communicate()
+        accuracy_drop = outs.decode("utf-8").splitlines()[-3].split(' ')[-1]
+        assert 100 * float(accuracy_drop) < 0.3
+        if result.returncode != 0:
+            print(outs.decode('utf-8'))
+            pytest.fail()
+            return
+        if verbose:
+            print(outs.decode('utf-8'))
