5eb0abda16ee2d250379c466c96413c4fcfc8bf2
dlyakhov <daniil.lyakhov@intel.com>
2023-02-15 10:46:39 +0100

    Remove progress bar during val step

--- tests/post_training/test_quantize_conformance.py
+++ tests/post_training/test_quantize_conformance.py
@@ -147,7 +147,9 @@ def validate_accuracy(model_path, val_loader):
 
     infer_queue.set_callback(process_result)
 
-    for i, (images, target) in tqdm(enumerate(val_loader)):
+    print(f'Validation dataset size: {dataset_size}')
+    print('Start validation process...')
+    for i, (images, target) in enumerate(val_loader):
         # W/A for memory leaks when using torch DataLoader and OpenVINO
         image_copies = copy.deepcopy(images.numpy())
         infer_queue.start_async(image_copies, userdata=i)
