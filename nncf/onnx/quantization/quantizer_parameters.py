Fix rebase; Update formula for OV

nncf/onnx/quantization/quantizer_parameters.py
@@ -86,16 +86,16 @@ def calculate_scale_zero_point(max_val: np.ndarray, min_val: np.ndarray, level_l
 
 
 def calculate_weight_quantizer_parameters(statistics: MinMaxTensorStatistic, quantizer_config: QuantizerConfig,
-                                          axis: Optional[int], half_range: bool) -> ONNXQuantizerLayerParameters:
+                                          half_range: bool, axis: Optional[int]) -> ONNXQuantizerLayerParameters:
     """
     Calculates Quantizer/Dequantizer layer attributes for weight quantizer such as scale, zero_points and
     quantization mode: symmetric, asymmetric.
 
     :param statistics: Collected statistics for the quantized insertion.
     :param quantizer_config: Config of Quantizer.
-    :param axis: In per-channel case - the axis for the quantization. In per-tensor - ignored.
     :param half_range: If ``True`` effectively only a half of a quantizer range are used.
         False - the full range are used.
+    :param axis: In per-channel case - the axis for the quantization. In per-tensor - ignored.
     :return: Parameters of Quantizer.
     """
     # The weight is restricted to have only signed range.
@@ -105,7 +105,7 @@ def calculate_weight_quantizer_parameters(statistics: MinMaxTensorStatistic, qua
                          'while the quantizer configuration for weights contains signedness_to_force=False.')
     input_low = np.array(statistics.min_values)
     input_high = np.array(statistics.max_values)
-    return calculate_quantizer_parameters(input_low, input_high, tensor_type, quantizer_config, axis)
+    return calculate_quantizer_parameters(input_low, input_high, tensor_type, quantizer_config, half_range, axis)
 
 
 def calculate_activation_quantizer_parameters(statistics: MinMaxTensorStatistic, quantizer_config: QuantizerConfig,
@@ -124,8 +124,7 @@ def calculate_activation_quantizer_parameters(statistics: MinMaxTensorStatistic,
     tensor_type = np.uint8 if np.all(input_low >= 0) else np.int8
     if quantizer_config.signedness_to_force is not None:
         tensor_type = np.int8 if quantizer_config.signedness_to_force else np.uint8
-
-    return calculate_quantizer_parameters(input_low, input_high, tensor_type, quantizer_config, axis)
+    return calculate_quantizer_parameters(input_low, input_high, tensor_type, quantizer_config, False, axis)
 
 
 def calculate_quantizer_parameters(input_low: np.ndarray, input_high: np.ndarray,
@@ -144,10 +143,8 @@ def calculate_quantizer_parameters(input_low: np.ndarray, input_high: np.ndarray
     :param axis: Axis of the quantization. None in a per-tensor quantization case.
     :return: Parameters of Quantizer.
     """
-    if half_range:
-        level_low, level_high = get_level_low_level_high(tensor_type, quantizer_config.num_bits - 1)
-    else:
-        level_low, level_high = get_level_low_level_high(tensor_type, quantizer_config.num_bits)
+    num_bits = quantizer_config.num_bits - 1 if half_range else quantizer_config.num_bits
+    level_low, level_high = get_level_low_level_high(tensor_type, num_bits)
     scales, zero_points = calculate_scale_zero_point(input_high, input_low, level_low, level_high,
                                                      quantizer_config.mode, tensor_type)
     return ONNXQuantizerLayerParameters(scales, zero_points, quantizer_config.mode, axis, tensor_type)

tests/onnx/quantization/test_calculation_quantizer_params.py
@@ -132,19 +132,24 @@ CASES_FOR_TEST = (CaseToTestActivationQParams(num_bits=8,
                                               ref_scale=0.0784313725490196 * np.ones((3, 3)),
                                               ref_zero_point=np.zeros((3, 3)),
                                               ref_tensor_type=np.int8),
-                                              # Check activation_signed work
-                                              CaseToTestActivationQParams(num_bits=8,
-                                                                          mode=QuantizationMode.SYMMETRIC,
-                                                                          activations_signed=False,
-                                                                          per_channel=False,
-                                                                          axis=None,
-                                                                          ref_tensor_type=np.uint8),
-                                              CaseToTestActivationQParams(num_bits=8,
-                                                                          mode=QuantizationMode.SYMMETRIC,
-                                                                          activations_signed=True,
-                                                                          per_channel=False,
-                                                                          axis=None,
-                                                                          ref_tensor_type=np.int8),
+                  CaseToTestActivationQParams(num_bits=8,
+                                              mode=QuantizationMode.SYMMETRIC,
+                                              signedness_to_force=False,
+                                              per_channel=False,
+                                              axis=None,
+                                              statistics=ONNXMinMaxTensorStatistic(-1, 10),
+                                              ref_scale=np.array((0.0392156862745098)),
+                                              ref_zero_point=np.array((0.0)),
+                                              ref_tensor_type=np.uint8),
+                  CaseToTestActivationQParams(num_bits=8,
+                                              mode=QuantizationMode.SYMMETRIC,
+                                              signedness_to_force=True,
+                                              per_channel=False,
+                                              axis=None,
+                                              statistics=ONNXMinMaxTensorStatistic(-1, 10),
+                                              ref_scale=np.array((0.0784313725490196)),
+                                              ref_zero_point=np.array((0.0)),
+                                              ref_tensor_type=np.int8),
                   )
 
 
@@ -187,7 +192,7 @@ CASES_FOR_TEST = [
                      signedness_to_force=None,
                      per_channel=False,
                      axis=None,
-                     weight_tensor=np.array([[0, 1, 2], [-1, 0, 0]]),
+                     weight_tensor=ONNXMinMaxTensorStatistic(min_values=-1, max_values=2),
                      is_half_range=False,
                      ref_scale=np.array((0.01568627450980392)),
                      ref_zero_point=np.array((0)),
@@ -198,7 +203,7 @@ CASES_FOR_TEST = [
                      signedness_to_force=None,
                      per_channel=False,
                      axis=None,
-                     weight_tensor=np.array([[0, 1, 2], [-1, 0, 0]]),
+                     weight_tensor=ONNXMinMaxTensorStatistic(min_values=-1, max_values=2),
                      is_half_range=True,
                      ref_scale=np.array((0.031496062992125984)),
                      ref_zero_point=np.array((0)),
@@ -209,7 +214,7 @@ CASES_FOR_TEST = [
                      signedness_to_force=None,
                      per_channel=False,
                      axis=None,
-                     weight_tensor=np.array([[0, 1, 2], [-1, 0, 0]]),
+                     weight_tensor=ONNXMinMaxTensorStatistic(min_values=-1, max_values=2),
                      is_half_range=False,
                      ref_scale=np.array((0.011764705882352941)),
                      ref_zero_point=np.array((-43)),
@@ -220,7 +225,7 @@ CASES_FOR_TEST = [
                      signedness_to_force=None,
                      per_channel=False,
                      axis=None,
-                     weight_tensor=np.array([[0, 1, 2], [-1, 0, 0]]),
+                     weight_tensor=ONNXMinMaxTensorStatistic(min_values=-1, max_values=2),
                      is_half_range=True,
                      ref_scale=np.array((0.023622047244094488)),
                      ref_zero_point=np.array((-22)),
@@ -231,7 +236,7 @@ CASES_FOR_TEST = [
                      signedness_to_force=None,
                      per_channel=True,
                      axis=1,
-                     weight_tensor=np.array([[0, 1, 2], [0, 1, 3]]),
+                     weight_tensor=ONNXMinMaxTensorStatistic(min_values=np.array((0, 1, 2)), max_values=np.array((0, 1, 3))),
                      is_half_range=True,
                      ref_scale=np.array((0, 0.015748031496062992, 0.047244094488188976)),
                      ref_zero_point=np.array((0, 0, 0)),
@@ -242,7 +247,7 @@ CASES_FOR_TEST = [
                      signedness_to_force=False,
                      per_channel=True,
                      axis=1,
-                     weight_tensor=np.array([[0, 1, 2], [0, 1, 3]]),
+                     weight_tensor=ONNXMinMaxTensorStatistic(min_values=np.array((0, 1, 2)), max_values=np.array((0, 1, 3))),
                      is_half_range=True,
                      ref_scale=np.array((0, 0.015748031496062992, 0.047244094488188976)),
                      ref_zero_point=np.array((0, 0, 0)),
@@ -253,7 +258,6 @@ CASES_FOR_TEST = [
 
 @pytest.mark.parametrize('case_to_test', (CASES_FOR_TEST))
 def test_calculate_weight_quantizer_parameters(case_to_test: CaseToTestActivationQParams):
-    statistics = ONNXMinMaxTensorStatistic(-1 * np.ones((3, 10, 10)), np.ones((3, 10, 10)))
     qconfig = QuantizerConfig(num_bits=case_to_test.num_bits,
                               mode=case_to_test.mode,
                               signedness_to_force=case_to_test.signedness_to_force,
@@ -265,12 +269,12 @@ def test_calculate_weight_quantizer_parameters(case_to_test: CaseToTestActivatio
                                                        tensor_type=case_to_test.ref_tensor_type)
     if case_to_test.signedness_to_force is not None and not case_to_test.signedness_to_force:
         with pytest.raises(Exception):
-            calculate_weight_quantizer_parameters(case_to_test.weight_tensor, qconfig, case_to_test.axis,
-                                                  case_to_test.is_half_range)
+            calculate_weight_quantizer_parameters(case_to_test.weight_tensor, qconfig,
+                                                  case_to_test.is_half_range, case_to_test.axis)
         return
 
-    quantize_params = calculate_weight_quantizer_parameters(case_to_test.weight_tensor, qconfig, case_to_test.axis,
-                                                            case_to_test.is_half_range)
+    quantize_params = calculate_weight_quantizer_parameters(case_to_test.weight_tensor, qconfig,
+                                                            case_to_test.is_half_range, case_to_test.axis)
     assert np.array_equal(ref_quantize_params.scale, quantize_params.scale)
     assert np.array_equal(ref_quantize_params.zero_point, quantize_params.zero_point)
     assert ref_quantize_params.mode == quantize_params.mode

tests/openvino/native/quantization/test_fq_configurations.py
@@ -119,7 +119,7 @@ def test_calculate_activation_quantizer_parameters(case_to_test):
 
     statistics = OVMinMaxTensorStatistic(min_values, max_values)
     qconfig = QuantizerConfig(num_bits=8, mode=mode, signedness_to_force=sign, per_channel=per_ch)
-    quantize_params = calculate_quantizer_parameters(statistics, qconfig, QuantizerGroup.ACTIVATIONS)
+    quantize_params = calculate_quantizer_parameters(statistics, qconfig, False, QuantizerGroup.ACTIVATIONS)
 
     compare_fq_parameters(ref_quantize_params, quantize_params)
 
@@ -136,12 +136,12 @@ def test_calculate_weight_quantizer_parameters(case_to_test):
     qconfig = QuantizerConfig(num_bits=8, mode=mode, signedness_to_force=sign, per_channel=per_ch)
     axes = None
     if qconfig.per_channel:
-        bounds_shape = get_weight_stats_shape(data.shape, half_range, None)
+        bounds_shape = get_weight_stats_shape(data.shape, None)
         axes = tuple(i for i, dim in enumerate(bounds_shape) if dim == 1)
 
     min_values = np.amin(data, axis=axes, keepdims=qconfig.per_channel)
     max_values = np.amax(np.abs(data), axis=axes, keepdims=qconfig.per_channel)
     statistics = OVMinMaxTensorStatistic(min_values, max_values)
-    quantize_params = calculate_quantizer_parameters(statistics, qconfig, QuantizerGroup.WEIGHTS)
+    quantize_params = calculate_quantizer_parameters(statistics, qconfig, half_range, QuantizerGroup.WEIGHTS)
 
     compare_fq_parameters(ref_quantize_params, quantize_params)