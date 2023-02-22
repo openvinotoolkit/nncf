b099b66a1d3c91ebe3001c6fef8a57351dd4ef3f
kshpv <aleksei.kashapov@intel.com>
2023-02-22 16:50:26 +0100

    Pylint

--- nncf/onnx/graph/model_transformer.py
+++ nncf/onnx/graph/model_transformer.py
@@ -10,7 +10,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 """
-from typing import List, Optional, Tuple
+from typing import List, Tuple
 
 from copy import deepcopy
 from collections import Counter
