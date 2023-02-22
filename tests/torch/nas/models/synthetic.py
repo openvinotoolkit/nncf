bb4eef354eedda68b2eb980e8d286a4961a31155
Vasily Shamporov <vasily.shamporov@intel.com>
2023-02-22 14:07:54 +0100

    Fix NAS tests

--- tests/torch/nas/models/synthetic.py
+++ tests/torch/nas/models/synthetic.py
@@ -269,7 +269,9 @@ class TwoConvAddConvTestModel(nn.Module):
 
     def get_minimal_subnet_output_without_reorg(self, x):
         o = (self.V11 * x + self.V11) + (self.V21 * x + self.V21)
-        return self.last_conv(o)
+        ref_weights = self.last_conv.weight[:, :1, :, :]
+        ref_bias = self.last_conv.bias[:1]
+        return do_conv2d(self.last_conv, o, weight=ref_weights, bias=ref_bias)
 
     def forward(self, x):
         o = self.conv1(x) + self.conv2(x)
