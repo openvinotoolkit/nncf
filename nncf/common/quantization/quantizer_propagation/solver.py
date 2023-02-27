2f330f560f1bf967564cae1d5016132aed13ad1f
Nikita Malinin <nikita.malinin@intel.com>
2023-02-27 18:38:34 +0100

    Fix dynamic shapes & DetOutput

--- nncf/common/quantization/quantizer_propagation/solver.py
+++ nncf/common/quantization/quantizer_propagation/solver.py
@@ -293,7 +293,7 @@ class PostprocessingNodeLocator:
             if self._is_node_has_underlying_weights(node_key):
                 visited_nodes.add(node_key)
                 return True, output
-            if node_metatype in [InputNoopMetatype, OutputNoopMetatype]:
+            if node_metatype in list(OUTPUT_NOOP_METATYPES.values()) + list(INPUT_NOOP_METATYPES.values()):
                 visited_nodes.add(node_key)
                 return False, output
             self._check_if_postprocessing(node_metatype)
@@ -313,7 +313,12 @@ class PostprocessingNodeLocator:
 
         partial_backward_traverse_function = partial(backward_traverse_function, visited_nodes=visited_nodes)
         output = []
-        for start_node_key in self._quant_prop_graph.get_node_keys_by_metatype(OutputNoopMetatype):
+
+        output_nodes = []
+        for output_metatype in OUTPUT_NOOP_METATYPES.values():
+            output_nodes.extend(self._quant_prop_graph.get_node_keys_by_metatype(output_metatype))
+
+        for start_node_key in output_nodes:
             self._post_processing_marker_encountered = False
             node_keys = self._quant_prop_graph.traverse_graph(start_node_key, partial_backward_traverse_function,
                                                               output=[], traverse_forward=False)
