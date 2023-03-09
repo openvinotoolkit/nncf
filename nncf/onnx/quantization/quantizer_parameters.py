Fix ONNX ZP calculation

nncf/onnx/quantization/quantizer_parameters.py
@@ -36,7 +36,6 @@ class ONNXQuantizerLayerParameters:
 
 def convert_fq_params_to_onnx_params(parameters: FakeQuantizeParameters,
                                      num_bits: int,
-                                     mode: QuantizationMode,
                                      tensor_type: np.dtype,
                                      axis: Optional[int] = None) -> ONNXQuantizerLayerParameters:
     """
@@ -44,11 +43,14 @@ def convert_fq_params_to_onnx_params(parameters: FakeQuantizeParameters,
 
     :param parameters: FakeQuantizeParameters representation.
     :param num_bits: Number of quantizer bits.
-    :param mode: Symmetric or Asymmetric mode.
     :param tensor_type: Value type of the tensor. Could be INT8 or UINT8.
     :param axis: Axis for per-channel quantization. Should be none in case of per-tensor.
     :return: Quantizer layer attributes.
     """
+    levels = parameters.levels
+    if levels not in [255, 256]:
+        raise ValueError('Can only export to INT8/UIN8 256-level ONNX Quantize/Dequantize pairs.')
+
     input_low, input_high = parameters.input_low, parameters.input_high
     output_low, output_high = parameters.output_low, parameters.output_high
     if not np.allclose(input_high, output_high) or not np.allclose(input_low, output_low):
@@ -56,11 +58,7 @@ def convert_fq_params_to_onnx_params(parameters: FakeQuantizeParameters,
                          ' input_high == output_high and input_low == output_low.')
 
     level_low, level_high = get_level_low_level_high(tensor_type, num_bits)
-    levels = level_high - level_low + 1
-    if levels not in [255, 256]:
-        raise ValueError('Can only export to INT8/UIN8 256-level ONNX Quantize/Dequantize pairs.')
-
-    scale, zero_point = calculate_scale_zero_point(input_low, input_high, level_low, level_high, mode)
+    scale, zero_point = calculate_scale_zero_point(input_low, input_high, level_low, level_high, levels)
     return ONNXQuantizerLayerParameters(scale, zero_point, tensor_type, axis)
 
 
@@ -78,8 +76,8 @@ def get_level_low_level_high(tensor_type: np.dtype, num_bits: int) -> Tuple[int,
     return - (2 ** num_bits) // 2, (2 ** num_bits) // 2 - 1
 
 
-def calculate_scale_zero_point(input_low: np.ndarray, input_high: np.ndarray, level_low: int, level_high: int,
-                               mode: QuantizationMode, eps: float = 1e-6) -> Tuple[np.ndarray, np.ndarray]:
+def calculate_scale_zero_point(input_low: np.ndarray, input_high: np.ndarray,
+                               level_low: int, level_high: int, levels: int) -> Tuple[np.ndarray, np.ndarray]:
     """
     Calculates Quantizer/Dequantizer layer scale level.
     Returns scale and zero_point values for the quantizer.
@@ -90,25 +88,14 @@ def calculate_scale_zero_point(input_low: np.ndarray, input_high: np.ndarray, le
         The default is "0" for an unsigned range, and "-2^(bit-1)" for a signed one .
     :param level_high: The maximum level in the integer range to quantize.
         The default is "2^bits-1" for an unsigned range, and "2^(bit-1)-1" for a signed one.
-    :param mode: Symmetric or Asymmetric mode.
-    :param eps: The correction value for scale to avoid division by zero.
+    :param levels: Number of quantization levels.
     :return: Scale and Zero point values.
     """
-    scale, zero_point = None, None
-    if mode == QuantizationMode.SYMMETRIC:
-        scale = np.array((input_high - input_low) / (level_high - level_low))
-        zero_point = np.zeros_like(scale, dtype=np.int32)
-    elif mode == QuantizationMode.ASYMMETRIC:
-        scale = np.array((input_high - input_low) / (level_high - level_low))
-        zero_point = np.round(level_low - input_low / np.maximum(scale, eps)).astype(np.int32)
-
-        level_low *= np.ones_like(zero_point, dtype=np.int32)
-        level_high *= np.ones_like(zero_point, dtype=np.int32)
-
-        zero_point = np.maximum(zero_point, level_low)
-        zero_point = np.minimum(zero_point, level_high)
-
+    scale = np.array((input_high - input_low) / (levels - 1))
+    exact_level_low = -levels / 2 if level_low < 0 else 0
+    zero_point = np.round((exact_level_low - input_low / scale).astype(np.float32))
+    zero_point = np.minimum(np.maximum(zero_point.astype(np.int32), level_low), level_high)
     scale = np.squeeze(scale).astype(np.float32)
-    zero_point = np.squeeze(zero_point).astype(np.int32)
+    zero_point = np.squeeze(zero_point)
 
     return scale, zero_point

nncf/quantization/algorithms/min_max/onnx_backend.py
@@ -80,8 +80,7 @@ class ONNXMinMaxAlgoBackend(MinMaxAlgoBackend):
         nncf_input_node_next_nodes = ONNXMinMaxAlgoBackend._get_input_edges_mapping(nncf_graph)
         axis = ONNXMinMaxAlgoBackend._get_axis(nncf_graph, target_point, quantizer_config)
         tensor_type = np.int8 if quantizer_config.signedness_to_force or np.any(parameters.input_low < 0) else np.uint8
-        onnx_parameters = convert_fq_params_to_onnx_params(
-            parameters, quantizer_config.num_bits, quantizer_config.mode, tensor_type, axis)
+        onnx_parameters = convert_fq_params_to_onnx_params(parameters, quantizer_config.num_bits, tensor_type, axis)
         return ONNXQuantizerInsertionCommand(target_point, nncf_input_node_next_nodes, onnx_parameters)
 
     @staticmethod
@@ -97,8 +96,7 @@ class ONNXMinMaxAlgoBackend(MinMaxAlgoBackend):
         tensor_type = np.int8  # The weight is restricted to have only signed range
         nncf_input_node_next_nodes = ONNXMinMaxAlgoBackend._get_input_edges_mapping(nncf_graph)
         axis = ONNXMinMaxAlgoBackend._get_axis(nncf_graph, target_point, quantizer_config)
-        onnx_parameters = convert_fq_params_to_onnx_params(
-            parameters, quantizer_config.num_bits, quantizer_config.mode, tensor_type, axis)
+        onnx_parameters = convert_fq_params_to_onnx_params(parameters, quantizer_config.num_bits, tensor_type, axis)
         return ONNXQuantizerInsertionCommand(target_point, nncf_input_node_next_nodes, onnx_parameters)
 
     @staticmethod

nncf/quantization/algorithms/min_max/torch_backend.py
@@ -243,7 +243,8 @@ class PTMinMaxAlgoBackend(MinMaxAlgoBackend):
                 torch.nn.Parameter(torch.from_numpy(np.array(parameters.input_high - parameters.input_low)))
         else:
             quantizer.signed = np.any(parameters.input_low < 0)
-            quantizer.scale = torch.nn.Parameter(torch.from_numpy(parameters.input_high))
+            scale = np.maximum(np.abs(parameters.input_high), np.abs(parameters.input_low))
+            quantizer.scale = torch.nn.Parameter(torch.from_numpy(scale))
 
     @staticmethod
     def _create_quantizer_insertion_command(nncf_graph: NNCFGraph,

tests/onnx/quantization/test_calculation_quantizer_params.py
@@ -13,47 +13,27 @@
 
 import pytest
 import numpy as np
-from nncf.common.quantization.structs import QuantizationMode
 from nncf.onnx.quantization.quantizer_parameters import calculate_scale_zero_point
 from nncf.onnx.quantization.quantizer_parameters import get_level_low_level_high
 
 
-@pytest.mark.parametrize(('inp_high, inp_low, level_low, level_high, mode, ref_scale, ref_zero_point'),
-                         ((np.zeros((10, 10)), np.zeros((10, 10)), -128, 127, QuantizationMode.SYMMETRIC,
-                           np.zeros((10, 10)),
-                           np.zeros((10, 10), dtype=np.int32)),
-
-                          (np.zeros((10, 10)), np.zeros((10, 10)), -128, 127, QuantizationMode.ASYMMETRIC,
-                           np.zeros((10, 10)),
-                           -128 * np.ones((10, 10), dtype=np.int32)),
-
-                          (np.ones((10, 10)), np.zeros((10, 10)), 10, 999, QuantizationMode.SYMMETRIC,
-                           0.00101112 * np.ones((10, 10)),
-                           np.zeros((10, 10), dtype=np.int32)),
-
-                          (np.ones((10, 10)), np.zeros((10, 10)), 10, 999, QuantizationMode.ASYMMETRIC,
-                           0.001011122 * np.ones((10, 10)),
-                           10 * np.ones((10, 10), dtype=np.int32)),
-
-                          (10 * np.ones((10, 10)), -np.ones((10, 10)), 0, 1000, QuantizationMode.SYMMETRIC,
-                           0.011 * np.ones((10, 10)),
-                           np.zeros((10, 10), dtype=np.int32)),
-
-                          (10 * np.ones((10, 10)), -np.ones((10, 10)), 0, 1000, QuantizationMode.ASYMMETRIC,
-                           0.011 * np.ones((10, 10)),
-                           91 * np.ones((10, 10), dtype=np.int32)),
-
-                          (10 * np.ones((10, 10)), -np.ones((10, 10)), -10, -1, QuantizationMode.SYMMETRIC,
-                           1.2222222 * np.ones((10, 10)),
-                           np.zeros((10, 10), dtype=np.int32)),
-
-                          (10 * np.ones((10, 10)), -np.ones((10, 10)), -10, -1, QuantizationMode.ASYMMETRIC,
-                           1.22222222 * np.ones((10, 10)),
-                           -9 * np.ones((10, 10), dtype=np.int32))
+@pytest.mark.parametrize(('inp_low, inp_high, level_low, level_high, levels, ref_scale, ref_zero_point'),
+                         ((-8e-05, 8e-05, -128, 127, 255, 6.3e-7, 0),
+                          (-0.0008062992, 0.0008, 0, 255, 256, 6.3e-6, 128),
+                          (0, 1, 10, 999, 1024, 0.00097752, 10),
+                          (-10.019569, 10, 0, 1023, 1024, 0.01956947, 512),
+                          (0, 1, 0, 255, 256, 0.00392157, 0),
+                          (-1, 10, 0, 1023, 1024, 0.01075269, 93),
+                          (-11.428572, 10, 0, 15, 16, 1.4285715, 8),
+                          (-10, 10, -512, 511, 1024, 0.01955034, 0),
+                          (-10, 10, -128, 127, 255, 0.07874016, 0),
+                          (0, 25, 0, 15, 16, 1.6666666, 0),
                           )
                          )
-def test_calculate_scale_zero_point(inp_high, inp_low, level_low, level_high, mode, ref_scale, ref_zero_point):
-    scale, zero_point = calculate_scale_zero_point(inp_low, inp_high, level_low, level_high, mode)
+def test_calculate_scale_zero_point(inp_low, inp_high, level_low, level_high, levels, ref_scale, ref_zero_point):
+    inp_low, inp_high = np.array(inp_low), np.array(inp_high)
+    scale, zero_point = calculate_scale_zero_point(inp_low, inp_high, level_low, level_high, levels)
+    ref_zero_point = np.array(ref_zero_point, dtype=np.int32)
     assert np.allclose(ref_scale, scale)
     assert np.allclose(ref_zero_point, zero_point)
 