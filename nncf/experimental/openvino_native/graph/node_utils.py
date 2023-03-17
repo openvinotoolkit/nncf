Fix broadcasting bias in FBC

nncf/experimental/openvino_native/graph/node_utils.py
@@ -22,14 +22,12 @@ from nncf.experimental.openvino_native.graph.metatypes.openvino_metatypes import
 from nncf.experimental.openvino_native.graph.metatypes.openvino_metatypes import OVConvertMetatype
 from nncf.experimental.openvino_native.graph.metatypes.openvino_metatypes import OVConstantMetatype
 from nncf.experimental.openvino_native.graph.metatypes.openvino_metatypes import OPERATIONS_WITH_BIAS_METATYPES
-from nncf.experimental.openvino_native.graph.metatypes.openvino_metatypes import METATYPE_TO_CHANNEL_AXIS
 from nncf.experimental.openvino_native.graph.nncf_graph_builder import OVConstantLayerAttributes
 
 
 def is_node_with_bias(node: NNCFNode, nncf_graph: NNCFGraph) -> bool:
     """
     Checks if the node has a bias or not.
-    Bias is a tensor with the shape 1 at every dim except batch and channel axis of output tensor.
 
     :param node: The node to check.
     :param nncf_graph: NNCFGraph instance.
@@ -45,22 +43,7 @@ def is_node_with_bias(node: NNCFNode, nncf_graph: NNCFGraph) -> bool:
         return False
 
     bias_constant = get_node_with_bias_value(add_node, nncf_graph)
-    if bias_constant is None:
-        return False
-
-    channel_axis = METATYPE_TO_CHANNEL_AXIS[node.metatype]
-    probable_bias_shape = add_node.layer_attributes.const_shape
-    probable_bias_ndim = len(probable_bias_shape)
-    positive_channel_axis = range(probable_bias_ndim)[channel_axis]
-    # Checks whether all indices except 0 and channel_axis equal 1
-    for i in range(1, probable_bias_ndim):
-        if i == positive_channel_axis:
-            continue
-        if probable_bias_shape[i] != 1:
-            return False
-
-    edge = nncf_graph.get_edge(node, add_node)
-    return edge.tensor_shape[positive_channel_axis] == probable_bias_shape[positive_channel_axis]
+    return bias_constant is not None
 
 
 def get_bias_value(node_with_bias: NNCFNode, nncf_graph: NNCFGraph, model: ov.Model) -> np.ndarray:
@@ -77,7 +60,9 @@ def get_bias_value(node_with_bias: NNCFNode, nncf_graph: NNCFGraph, model: ov.Mo
     add_node = nncf_graph.get_next_nodes(node_with_bias)[0]
     bias_constant = get_node_with_bias_value(add_node , nncf_graph)
     ov_bias_constant = ops_dict[bias_constant.node_name]
-    return ov_bias_constant.get_vector()
+    flatten_vector = ov_bias_constant.constant.data
+    vector = np.reshape(flatten_vector, newshape=ov_bias_constant.get_output_shape(0))
+    return vector
 
 
 def get_weight_value(node_with_weight: NNCFNode, nncf_graph: NNCFGraph, model: ov.Model) -> np.ndarray:

nncf/experimental/openvino_native/quantization/algorithms/fast_bias_correction/openvino_backend.py
@@ -81,11 +81,10 @@ class OVFastBiasCorrectionAlgoBackend(FastBiasCorrectionAlgoBackend):
         return subgraph.inputs[0].node.friendly_name, subgraph.outputs[0].node.friendly_name
 
     @staticmethod
-    def create_blob(shape: Tuple[int], data: List[np.ndarray], channel_axis: int) -> np.ndarray:
+    def create_blob(shape: Tuple[int], data: List[float]) -> np.ndarray:
         blob = np.zeros(shape)
-        for j, idx in enumerate(np.ndindex(blob.shape[channel_axis])):
-            index = tuple(slice(None) if i != channel_axis else idx for i in range(blob.ndim))
-            blob[index] = data[j]
+        for i, value in enumerate(data):
+            blob[:, i] = value
         blob = blob.astype(data[0].dtype)
         return blob
 

nncf/quantization/algorithms/fast_bias_correction/algorithm.py
@@ -144,15 +144,13 @@ class FastBiasCorrection(Algorithm):
             channel_axis = self._backend_entity.channel_axis_by_types[node.metatype]
             input_blob = self._create_input_data(input_shape,
                                                  input_fp,
-                                                 sub_input_name,
-                                                 channel_axis)
+                                                 sub_input_name)
             bias_shift = self._get_bias_shift(
                 model=extracted_model,
                 input_blob=input_blob,
                 channel_axis=channel_axis,
                 output_fp=output_fp,
                 output_name=sub_output_name)
-
             updated_bias = bias_value + bias_shift
             magnitude = self._get_bias_shift_magnitude(bias_value, updated_bias)
 
@@ -250,25 +248,23 @@ class FastBiasCorrection(Algorithm):
     def _create_input_data(self,
                            input_shape: Tuple[int],
                            input_fp: List[np.ndarray],
-                           input_name: str,
-                           channel_axis: int) -> Dict[str, NNCFTensor]:
+                           input_name: str) -> Dict[str, NNCFTensor]:
         """
         Creates input blob for the bias shift calculation.
 
         :param input_shape: Input shape for the blob.
         :param input_fp: Input data for the blob.
         :param input_name: Name for the output dictionary.
-        :param channel_axis: Axis to fill the blob with provided data.
         :return: The dictionary of the blob by input name.
         """
-        input_blob = self._backend_entity.create_blob(input_shape, input_fp, channel_axis)
+        input_blob = self._backend_entity.create_blob(input_shape, input_fp)
         input_data = {input_name: input_blob}
         return input_data
 
     def _get_bias_shift(self,
                         model: TModel,
                         input_blob: Dict[str, NNCFTensor],
-                        channel_axis: Tuple[int],
+                        channel_axis: int,
                         output_fp: List[np.ndarray],
                         output_name: str) -> np.ndarray:
         """
@@ -285,8 +281,11 @@ class FastBiasCorrection(Algorithm):
         engine = EngineFactory.create(model)
         raw_output = engine.infer(input_blob)
         q_outputs = self._backend_entity.process_model_output(raw_output, output_name)
-        q_outputs = self._backend_entity.tensor_processor.mean_per_channel(q_outputs, channel_axis).tensor
-        bias_shift = np.array(output_fp) - q_outputs
+        q_outputs_mean = self._backend_entity.tensor_processor.mean_per_channel(q_outputs, channel_axis).tensor
+        bias_shift = np.array(output_fp) - q_outputs_mean
+        for i in range(q_outputs.tensor.ndim):
+            if i != channel_axis: 
+                bias_shift = np.expand_dims(bias_shift, i)
         return bias_shift
 
     @staticmethod

nncf/quantization/algorithms/fast_bias_correction/onnx_backend.py
@@ -81,14 +81,13 @@ class ONNXFastBiasCorrectionAlgoBackend(FastBiasCorrectionAlgoBackend):
         return subgraph.graph.input[0].name, subgraph.graph.output[0].name
 
     @staticmethod
-    def create_blob(shape: Tuple[int], data: List[np.ndarray], channel_axis: int) -> np.ndarray:
+    def create_blob(shape: Tuple[int], data: List[float]) -> np.ndarray:
         blob = np.zeros(shape)
-        for j, idx in enumerate(np.ndindex(blob.shape[channel_axis])):
-            index = tuple(slice(None) if i != channel_axis else idx for i in range(blob.ndim))
-            blob[index] = data[j]
+        for i, value in enumerate(data):
+            blob[:, i] = value
         blob = blob.astype(data[0].dtype)
         return blob
-
+        
     @staticmethod
     def get_bias_value(node: NNCFNode, nncf_graph: NNCFGraph, model: onnx.ModelProto) -> np.ndarray:
         return get_bias_value(node, model)