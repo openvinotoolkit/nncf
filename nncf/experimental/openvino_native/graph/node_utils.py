Fix bias broadcasting

nncf/experimental/openvino_native/graph/node_utils.py
@@ -61,7 +61,7 @@ def get_bias_value(node_with_bias: NNCFNode, nncf_graph: NNCFGraph, model: ov.Mo
     bias_constant = get_node_with_bias_value(add_node , nncf_graph)
     ov_bias_constant = ops_dict[bias_constant.node_name]
     flatten_vector = ov_bias_constant.get_vector()
-    vector = np.reshape(flatten_vector, newshape=ov_bias_constant.get_output_shape(0))
+    vector = np.reshape(flatten_vector, newshape=ov_bias_constant.get_data().shape)
     return vector
 
 

nncf/quantization/algorithms/fast_bias_correction/backend.py
@@ -116,12 +116,12 @@ class FastBiasCorrectionAlgoBackend(ABC):
 
     @staticmethod
     @abstractmethod
-    def create_blob(shape: Tuple[int], data: List[float]) -> np.ndarray:
+    def create_blob(shape: Tuple[int], data: List[np.ndarray], channel_axis: int) -> np.ndarray:
         """
         Creates the backend-specific (because of layout) blob.
-
         :param shape: Shape of the blob.
         :param data: Data to fill the blob.
+        :param channel_axis: Axis to fill the blob with provided data.
         :return: np.ndarray blob.
         """
 