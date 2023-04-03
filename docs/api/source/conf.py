c7a9f1873adc32b72be94b29e8bacf23cff9fb26
Vasily Shamporov <vasily.shamporov@intel.com>
2023-04-03 16:18:43 +0200

    Custom API entity collector

--- docs/api/source/conf.py
+++ docs/api/source/conf.py
@@ -20,50 +20,15 @@ release = 'v2.4.0'
 
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.autosummary']
+
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
-templates_path = ['_templates']
+templates_path = ['_templates_']
 exclude_patterns = []
 
 module_fqn_with_api_fns_memo = set()
 
 _memo = {}
-
-def _has_api_members(module, memo):
-    if module in memo:
-        return memo[module]
-    modules = []
-    funcs_and_classes = []
-    has_api = False
-    for obj in inspect.getmembers(module):
-        if inspect.isfunction(obj) or inspect.isclass(obj):
-            funcs_and_classes.append(obj)
-        if inspect.ismodule(obj):
-            modules.append(obj)
-    for fc in funcs_and_classes:
-        if hasattr(fc, "_nncf_api_marker"):
-            has_api = True
-            break
-    for submodule in modules:
-        if _has_api_members(submodule, memo):
-            has_api = True
-            break
-    memo[module] = has_api
-    return has_api
-
-def skip_non_api(app, what, name, obj, skip, options):
-    if what == "module" and _has_api_members(obj, _memo):
-        print(f"VSHAMPOR: not skipping module {name}, has API")
-        return False
-    elif hasattr(obj, "_nncf_api_marker"):
-        print(f"VSHAMPOR: not skipping object {name}, is API")
-        return False
-
-    print(f"VSHAMPOR: skipping {what} {name}, not API")
-    return True
-
-def setup(sphinx):
-   sphinx.connect("autodoc-skip-member", skip_non_api)
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
