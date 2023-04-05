594dadef343394cd987f47bf9ee648903c27402e
Liubov Talamanova <liubov.talamanova@intel.com>
2023-04-05 20:41:44 +0200

    Add DeformConv for ONNX, Torch

--- nncf/onnx/graph/metatypes/onnx_metatypes.py
+++ nncf/onnx/graph/metatypes/onnx_metatypes.py
@@ -470,6 +470,12 @@ class ONNXDequantizeLinearMetatype(ONNXOpMetatype):
     op_names = ['DequantizeLinear']
 
 
+@ONNX_OPERATION_METATYPES.register()
+class ONNXDeformableConvolutionMetatype(ONNXOpMetatype):
+    name = 'DeformConvOp'
+    op_names = ['DeformConv']
+
+
 WEIGHT_LAYER_METATYPES = [ONNXConvolutionMetatype,
                           ONNXDepthwiseConvolutionMetatype,
                           ONNXConvolutionTransposeMetatype,
