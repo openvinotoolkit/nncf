1ed968f38656fccecf19ecaa39ff262d75368d53
Andrey Churkin <andrey.churkin@intel.com>
2023-04-11 15:55:09 +0100

    Test was updated

--- tests/common/accuracy_control/test_ranking.py
+++ tests/common/accuracy_control/test_ranking.py
@@ -19,15 +19,12 @@ import numpy as np
 from nncf.data.dataset import Dataset
 from nncf.quantization.algorithms.accuracy_control.rank_functions import normalized_mse
 from nncf.quantization.algorithms.accuracy_control.ranker import get_ranking_subset_indices
-from nncf.quantization.algorithms.accuracy_control.ranker import MetricBasedRanker
-from nncf.quantization.algorithms.accuracy_control.ranker import LogitsBasedRanker
-from nncf.quantization.algorithms.accuracy_control.algorithm import QuantizationAccuracyRestorer
+from nncf.quantization.algorithms.accuracy_control.evaluator import Output
+from nncf.quantization.algorithms.accuracy_control.evaluator import Evaluator
 
 
 def create_fp32_tensor_1d(items):
-    return {
-        'output': np.array(items, dtype=np.float32)
-    }
+    return np.array(items, dtype=np.float32)
 
 
 @pytest.mark.parametrize(
@@ -59,7 +56,12 @@ def create_fp32_tensor_1d(items):
     ]
 )
 def test_normalized_mse(x_ref: np.ndarray, x_approx: np.ndarray, expected_nmse: float):
-    actual_nmse = normalized_mse(x_ref, x_approx)
+    output_ref = Output()
+    output_ref.register(x_ref)
+    output_approx = Output()
+    output_approx.register(x_approx)
+
+    actual_nmse = normalized_mse(output_ref, output_approx)
     assert np.allclose(expected_nmse, actual_nmse)
 
 
@@ -132,11 +134,13 @@ def test_get_ranking_subset_indices(errors: List[float], subset_size: int, expec
 
 
 def _validation_fn_with_error(model, val_dataset) -> float:
-    raise RuntimeError
+    if len(list(val_dataset)) < 3:
+        raise RuntimeError
+    return 0.1, [0.1]
 
 
 def _validation_fn(model, val_dataset) -> float:
-    return 0.1
+    return 0.1, [0.1]
 
 
 class DummyAccuracyControlAlgoBackend:
@@ -148,14 +152,14 @@ class DummyAccuracyControlAlgoBackend:
 def test_create_logits_ranker():
     algo_backend = DummyAccuracyControlAlgoBackend()
     dataset = Dataset([0, 1, 2])
-    # pylint:disable=protected-access
-    ranker = QuantizationAccuracyRestorer._create_ranker(None, _validation_fn_with_error, dataset, 300, algo_backend)
-    assert isinstance(ranker, LogitsBasedRanker)
+    evaluator = Evaluator(_validation_fn_with_error, algo_backend)
+    evaluator.validate(None, dataset)
+    assert not evaluator.is_metric_mode()
 
 
 def test_create_metric_ranker():
     algo_backend = DummyAccuracyControlAlgoBackend()
     dataset = Dataset([0, 1, 2])
-    # pylint:disable=protected-access
-    ranker = QuantizationAccuracyRestorer._create_ranker(None, _validation_fn, dataset, 300, algo_backend)
-    assert isinstance(ranker, MetricBasedRanker)
+    evaluator = Evaluator(_validation_fn, algo_backend)
+    evaluator.validate(None, dataset)
+    assert evaluator.is_metric_mode()
