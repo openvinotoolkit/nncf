fb328eda20ae5e10865ab2c825ed0b6b65f40cfb
Vasily Shamporov <vasily.shamporov@intel.com>
2023-04-21 11:27:44 +0200

    Fix module hiding for quantize

--- nncf/quantization/__init__.py
+++ nncf/quantization/__init__.py
@@ -12,5 +12,5 @@
 """
 
 from nncf.common.quantization.structs import QuantizationPreset
-from nncf.quantization.quantize import quantize
-from nncf.quantization.quantize import quantize_with_accuracy_control
+from nncf.quantization.quantize_fns import quantize
+from nncf.quantization.quantize_fns import quantize_with_accuracy_control
