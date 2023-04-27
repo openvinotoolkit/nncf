62bfe5a0921dcec392fad64ecb10a8feac7f7f25
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2023-04-27 14:49:29 +0300

    too-many-lines

--- nncf/torch/nncf_network.py
+++ nncf/torch/nncf_network.py
@@ -10,7 +10,7 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 """
-
+# pylint: disable=too-many-lines
 import functools
 import inspect
 import types
