85b962635b854607c8c37c2f331ece5fc754f0e9
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2023-05-18 20:49:58 +0300

    Copyright for torch and common

--- tests/common/quantization/test_quantizer_propagation_solver.py
+++ tests/common/quantization/test_quantizer_propagation_solver.py
@@ -1,4 +1,3 @@
-# pylint:disable=too-many-lines
 # Copyright (c) 2023 Intel Corporation
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -9,6 +8,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# pylint:disable=too-many-lines
 from collections import Counter
 from collections import namedtuple
 from itertools import permutations
