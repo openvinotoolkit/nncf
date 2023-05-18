46ab06dabb3f96d5c1cbb2097f099ed40313c62b
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2023-05-19 02:41:01 +0300

    rework replace custom module

--- nncf/experimental/torch/replace_custom_modules/timm_custom_modules.py
+++ nncf/experimental/torch/replace_custom_modules/timm_custom_modules.py
@@ -18,8 +18,10 @@ from timm.models.layers.norm_act import GroupNormAct
 from timm.models.layers.norm_act import LayerNormAct
 from torch import nn
 
+from nncf.torch.nncf_module_replacement import replace_modules_by_nncf_modules
 
-def copy_parameters(src_module: nn.Module, trg_module: nn.Module):
+
+def _copy_parameters(src_module: nn.Module, trg_module: nn.Module):
     """
     Copies parameters of a source module to a target module.
     :param src_module: The source module to copy parameters from.
@@ -29,7 +31,7 @@ def copy_parameters(src_module: nn.Module, trg_module: nn.Module):
         setattr(trg_module, name, deepcopy(param))
 
 
-def convert_linear(module: Linear) -> nn.Linear:
+def _convert_linear(module: Linear) -> nn.Linear:
     """
     Convert Linear module to torch.nn.Linear.
 
@@ -44,11 +46,11 @@ def convert_linear(module: Linear) -> nn.Linear:
         device=module.weight.device,
         dtype=module.weight.dtype,
     )
-    copy_parameters(module, new_ln)
+    _copy_parameters(module, new_ln)
     return new_ln
 
 
-def convert_batch_norm_act_2d(module: BatchNormAct2d) -> nn.Sequential:
+def _convert_batch_norm_act_2d(module: BatchNormAct2d) -> nn.Sequential:
     """
     Converts a BatchNormAct2d module to an nn.Sequential module that contains nn.BatchNorm2d,
     followed by dropout and activation functions.
@@ -65,7 +67,7 @@ def convert_batch_norm_act_2d(module: BatchNormAct2d) -> nn.Sequential:
         device=module.weight.device,
         dtype=module.weight.dtype,
     )
-    copy_parameters(module, new_bn)
+    _copy_parameters(module, new_bn)
     new_bn.running_mean = deepcopy(module.running_mean)
     new_bn.running_var = deepcopy(module.running_var)
 
@@ -74,7 +76,7 @@ def convert_batch_norm_act_2d(module: BatchNormAct2d) -> nn.Sequential:
     return nn.Sequential(new_bn, new_drop, new_act)
 
 
-def convert_group_norm_act(module: GroupNormAct) -> nn.Sequential:
+def _convert_group_norm_act(module: GroupNormAct) -> nn.Sequential:
     """
     Converts a GroupNormAct module to an nn.Sequential module that contains nn.GroupNorm,
     followed by dropout and activation functions.
@@ -90,13 +92,13 @@ def convert_group_norm_act(module: GroupNormAct) -> nn.Sequential:
         device=module.weight.device,
         dtype=module.weight.dtype,
     )
-    copy_parameters(module, new_gn)
+    _copy_parameters(module, new_gn)
     new_drop = deepcopy(module.drop)
     new_act = deepcopy(module.act)
     return nn.Sequential(new_gn, new_drop, new_act)
 
 
-def convert_layer_norm_act(module: LayerNormAct) -> nn.Sequential:
+def _convert_layer_norm_act(module: LayerNormAct) -> nn.Sequential:
     """
     Converts a LayerNormAct module to an nn.Sequential module that contains nn.LayerNorm,
     followed by dropout and activation functions.
@@ -111,28 +113,59 @@ def convert_layer_norm_act(module: LayerNormAct) -> nn.Sequential:
         device=module.weight.device,
         dtype=module.weight.dtype,
     )
-    copy_parameters(module, new_norm)
+    _copy_parameters(module, new_norm)
     new_drop = deepcopy(module.drop)
     new_act = deepcopy(module.act)
     return nn.Sequential(new_norm, new_drop, new_act)
 
 
-REPLACE_FN_MAP = {
-    BatchNormAct2d: convert_batch_norm_act_2d,
-    GroupNormAct: convert_group_norm_act,
-    LayerNormAct: convert_layer_norm_act,
-    Linear: convert_linear,
+CONVERT_FN_MAP = {
+    BatchNormAct2d: _convert_batch_norm_act_2d,
+    GroupNormAct: _convert_group_norm_act,
+    LayerNormAct: _convert_layer_norm_act,
+    Linear: _convert_linear,
 }
 
 
-def replace_timm_modules(module: nn.Module) -> Optional[nn.Module]:
+def is_timm_custom_module(module: nn.Module):
+    """
+    Check that module is timm custom module and can be converted.
+
+    :param module: The module.
+    :return: `True` if module is custom module, otherwise `False`
+    """
+    return type(module) in list(CONVERT_FN_MAP.keys())
+
+
+def convert_timm_custom_modules(module: nn.Module) -> Optional[nn.Module]:
     """
     Replaces the given module with a PyTorch native module if possible.
 
     :param module: The module to replace.
     :return: The replaced module if replacement is possible, None otherwise.
     """
-    convert_fn = REPLACE_FN_MAP.get(type(module))
+    module_type = type(module)
+    convert_fn = CONVERT_FN_MAP.get(module_type)
     if convert_fn is None:
-        return None
+        raise TypeError(
+            f"The type of module {module_type} should be one of the following: {list(CONVERT_FN_MAP.keys())}"
+        )
     return convert_fn(module)
+
+
+def replace_timm_custom_modules_with_torch_native(model: nn.Module) -> nn.Module:
+    """
+    Replace custom module that can not be operated by NNCF to torch native modules.
+
+    :param model: The target model.
+    :return nn.Module: Transformed model.
+    """
+    model_copy = deepcopy(model)
+
+    model_copy, _ = replace_modules_by_nncf_modules(
+        model=model_copy,
+        custom_replacer=convert_timm_custom_modules,
+        predicate_fn=is_timm_custom_module,
+    )
+
+    return model_copy
