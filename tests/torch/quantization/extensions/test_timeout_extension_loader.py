4a9e940995f1f321a4497d571a1e3d372a3db925
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2023-05-19 14:47:39 +0300

    Add timeout for load extension (#1708)
    ### Changes
    
    Add timeout for for load extensions. 
    If loading hangs raise exception with message:
    ```
    nncf.torch.extensions.ExtensionLoaderTimeoutException: The extension load function failed to execute within 1 seconds.
    This may be due to leftover lock files from the PyTorch C++ extension build process.
    If this is the case, running the following command should help:
        rm -rf ~/.cache/torch_extensions/binarized_functions_cuda/1.13.1+cu117
    For a machine with poor performance, you may try increasing the time limit by setting the environment variable:
        NNCF_EXTENSION_LOAD_TIMEOUT=180
    Or disable timeout by set:
        NNCF_EXTENSION_LOAD_TIMEOUT=0
    For more information, see FAQ entry at: https://github.com/openvinotoolkit/nncf/blob/develop/docs/FAQ.md#importing-anything-from-nncftorch-hangs
    ```
    
    ### Reason for changes
    
    Loading extensions may hang if lock file exists.
    
    ### Related tickets
    
    107820
    
    ### Tests
    ```
    torch.binarization.test_timeout_extension_loader[cpu]
    torch.binarization.test_timeout_extension_loader[cuda]
    torch.quantization.extensions.test_timeout_extension_loader[cpu]
    torch.quantization.extensions.test_timeout_extension_loader[cuda]
    ```
    
    TODO: 
    - [x] collect statistics of times to build extension
    - [x] Mac OS, win?

---
+++ tests/torch/quantization/extensions/test_timeout_extension_loader.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+
+import pytest
+import torch
+
+from nncf.torch.extensions import EXTENSION_LOAD_TIMEOUT_ENV_VAR
+from nncf.torch.extensions import ExtensionLoaderTimeoutException
+from nncf.torch.quantization.extensions import QuantizedFunctionsCPU
+from nncf.torch.quantization.extensions import QuantizedFunctionsCUDA
+from tests.shared.isolation_runner import ISOLATION_RUN_ENV_VAR
+from tests.shared.isolation_runner import run_pytest_case_function_in_separate_process
+
+
+@pytest.mark.skipif(ISOLATION_RUN_ENV_VAR not in os.environ, reason="Should be run via isolation proxy")
+def test_timeout_extension_loader_isolated(tmp_path, use_cuda):
+    if not torch.cuda.is_available() and use_cuda is True:
+        pytest.skip("Skipping CUDA test cases for CPU only setups")
+
+    quant_func = QuantizedFunctionsCUDA if use_cuda else QuantizedFunctionsCPU
+
+    os.environ[EXTENSION_LOAD_TIMEOUT_ENV_VAR] = "1"
+    os.environ["TORCH_EXTENSIONS_DIR"] = tmp_path.as_posix()
+
+    # pylint: disable=protected-access
+    build_dir = Path(quant_func._loader.get_build_dir())
+    lock_file = build_dir / "lock"
+    lock_file.touch()
+    with pytest.raises(ExtensionLoaderTimeoutException):
+        quant_func.get("Quantize_forward")
+
+
+def test_timeout_extension_loader():
+    run_pytest_case_function_in_separate_process(test_timeout_extension_loader_isolated)
