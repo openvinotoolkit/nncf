1376e78d114928fc66d57e477d15f11ca91df82b
Alexander Suslov <alexander.suslov@intel.com>
2023-06-02 10:29:28 +0400

    typo in tests

--- tests/openvino/native/quantization/test_fq_params_calculation.py
+++ tests/openvino/native/quantization/test_fq_params_calculation.py
@@ -141,9 +141,8 @@ def test_omz_models_fq_scales(model_name, preset, inplace_statistics, tmp_path):
     ref_stats_path = REFERENCE_SCALES_DIR / ref_stats_name
 
     # Uncomment lines below to generate reference for new models.
-    from tests.shared.helpers import dump_to_json
-
-    dump_to_json(ref_stats_path, nodes)
+    # from tests.shared.helpers import dump_to_json
+    # dump_to_json(ref_stats_path, nodes)
 
     ref_nodes = load_json(ref_stats_path)
     params = ["input_low", "input_high", "output_low", "output_high"]
