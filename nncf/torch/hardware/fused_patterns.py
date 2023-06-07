4ab8079e4783b4613c21e1d18f801f4f010adc5a
Aleksei Kashapov <aleksei.kashapov@intel.com>
2023-06-07 13:41:08 +0200

    fix torch

--- nncf/torch/hardware/fused_patterns.py
+++ nncf/torch/hardware/fused_patterns.py
@@ -46,35 +46,6 @@ def create_l2_norm_operations() -> GraphPattern:
     return pattern
 
 
-@PT_HW_FUSED_PATTERNS.register(HWFusedPatternNames.MATMUL_SOFTMAX_MATMUL)
-def create_matmul_softmax_matmul() -> GraphPattern:
-    matmul_aliases = ["linear", "addmm", "matmul", "bmm", "mm", "baddbmm"]
-    pattern = GraphPattern()
-    softmax_1 = pattern.add_node(label="SOFTMAX", type="softmax")
-    mat_mul_1_1 = pattern.add_node(label="MATMUL_1", type=matmul_aliases)
-    mat_mul_2_1 = pattern.add_node(label="MATMUL_2", type=matmul_aliases)
-
-    any_1 = pattern.add_node(label="ANY", type=GraphPattern.NON_PATTERN_NODE_TYPE)
-
-    pattern.add_edge(mat_mul_1_1, softmax_1)
-    pattern.add_edge(softmax_1, mat_mul_2_1)
-    pattern.add_edge(any_1, mat_mul_2_1)
-
-    softmax_2 = pattern.add_node(label="SOFTMAX", type="softmax")
-    add_2 = pattern.add_node(label="ADD", type=["add", "__add__", "__iadd__", "__radd__"])
-    mat_mul_1_2 = pattern.add_node(label="MATMUL_1", type=matmul_aliases)
-    mat_mul_2_2 = pattern.add_node(label="MATMUL_2", type=matmul_aliases)
-
-    any_2 = pattern.add_node(label="ANY", type=GraphPattern.NON_PATTERN_NODE_TYPE)
-
-    pattern.add_edge(mat_mul_1_2, add_2)
-    pattern.add_edge(add_2, softmax_2)
-    pattern.add_edge(softmax_2, mat_mul_2_2)
-    pattern.add_edge(any_2, mat_mul_2_2)
-
-    return pattern
-
-
 # COMBINATIONS
 
 
