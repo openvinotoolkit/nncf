98754aa5d31e5e4072155f8f3ae538fbad16242f
Alexander Suslov <alexander.suslov@intel.com>
2023-06-14 12:01:50 +0400

    Fixed quantization OpenVINO MatMul operation with weights rank>2 (#1902)
    ### Changes
    
    Fixed quantization OpenVINO MatMul operation with weights rank>2
    
    ### Reason for changes
    
    MatMul operation with weights rank>2 is not quantized.
    
    ### Related tickets
    
    https://github.com/openvinotoolkit/openvino/issues/17389
    
    ### Tests
    
    N/A

--- nncf/openvino/graph/metatypes/openvino_metatypes.py
+++ nncf/openvino/graph/metatypes/openvino_metatypes.py
@@ -106,7 +106,7 @@ class OVMatMulMetatype(OVOpMetatype):
     name = "MatMulOp"
     op_names = ["MatMul"]
     hw_config_names = [HWConfigOpName.MATMUL]
-    const_channel_axis = [1]  # const layout: [Y, X]
+    const_channel_axis = [-1]  # const layout: [B, ..., Y, X]
     output_channel_axis = -1
 
 
