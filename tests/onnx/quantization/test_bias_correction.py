61b93193339dc2a9e25669eacd5611670b774fa4
Nikita Malinin <nikita.malinin@intel.com>
2023-06-20 10:00:17 +0200

    Fix pre-commit

--- tests/onnx/quantization/test_bias_correction.py
+++ tests/onnx/quantization/test_bias_correction.py
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List, Dict
+from typing import Dict, List
 
 import numpy as np
 import onnx
