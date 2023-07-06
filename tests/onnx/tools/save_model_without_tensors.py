9d008b4c230943b3c2863c56611bd2f21f2c1d7c
Aleksei Kashapov <aleksei.kashapov@intel.com>
2023-07-06 12:15:04 +0200

    Add description of a tool

--- tests/onnx/tools/save_model_without_tensors.py
+++ tests/onnx/tools/save_model_without_tensors.py
@@ -8,6 +8,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""
+This script is used to save only model topology without weights.
+Usage example: to save model topology in tests/onnx/data/models for future usage in graph tests.
+"""
 from argparse import ArgumentParser
 
 import onnx
