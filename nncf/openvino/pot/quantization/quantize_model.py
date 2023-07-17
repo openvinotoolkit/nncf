ef6b506c878e05e09beb9e64972324a02b493c72
Nikita Malinin <nikita.malinin@intel.com>
2023-07-17 18:30:30 +0200

    Added warning logger for pot backend

--- nncf/openvino/pot/quantization/quantize_model.py
+++ nncf/openvino/pot/quantization/quantize_model.py
@@ -335,6 +335,11 @@ def quantize_impl(
     if advanced_parameters is None:
         advanced_parameters = AdvancedQuantizationParameters()
 
+    if model_type == ModelType.TRANSFORMER and advanced_parameters.smooth_quant_alpha > 0:
+        nncf_logger.warning(
+            'IMPORTANT. The advanced parameter "smooth_quant_alpha" IS NOT SUPPORTED for the POT backend!'
+        )
+
     algorithm_parameters = _create_quantization_config(
         preset, target_device, subset_size, fast_bias_correction, model_type, ignored_scope, advanced_parameters
     )
@@ -432,6 +437,11 @@ def quantize_with_accuracy_control_impl(
 
     if advanced_quantization_parameters is None:
         advanced_quantization_parameters = AdvancedQuantizationParameters()
+
+    if model_type == ModelType.TRANSFORMER and advanced_quantization_parameters.smooth_quant_alpha > 0:
+        nncf_logger.warning(
+            'IMPORTANT. The advanced parameter "smooth_quant_alpha" IS NOT SUPPORTED for the POT backend!'
+        )
 
     if advanced_quantization_parameters.disable_bias_correction:
         raise ValueError(
