58049c602481ecc868b56ce96b232a4492be1422
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2023-08-17 17:51:53 +0300

    rev

--- tests/openvino/native/models.py
+++ tests/openvino/native/models.py
@@ -541,6 +541,7 @@ class SplitConcatModel(OVReferenceModel):
         return model
 
 
+@SYNTHETIC_MODELS.register()
 class IntegerModel(OVReferenceModel):
     def _create_ov_model(self):
         input_1 = opset.parameter([1, 192, 1], name="Input")
