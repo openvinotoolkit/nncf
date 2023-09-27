9807d5e874d0dfee949ff20608da7357628181a6
Nikita Savelyev <nikita.savelyev@intel.com>
2023-09-27 15:52:56 +0200

    Removed not used import

--- nncf/common/logging/track_progress.py
+++ nncf/common/logging/track_progress.py
@@ -8,7 +8,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from datetime import timedelta
+
 from typing import Callable, Iterable, List, Optional, Sequence, Union
 
 from rich.console import Console
