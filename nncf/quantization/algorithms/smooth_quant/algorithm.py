feec533b01f1058d375fd36d91b231c844132b31
Nikita Malinin <nikita.malinin@intel.com>
2023-10-26 15:05:32 +0200

    Rollback default alpha for matmul

--- nncf/quantization/algorithms/smooth_quant/algorithm.py
+++ nncf/quantization/algorithms/smooth_quant/algorithm.py
@@ -42,7 +42,7 @@ from nncf.quantization.algorithms.algorithm import Algorithm
 TModel = TypeVar("TModel")
 TTensor = TypeVar("TTensor")
 STATISTIC_BRANCH_KEY = "abs_max"
-ALPHA_MAP = {"convolution": 0.05, "matmul": 0.05}
+ALPHA_MAP = {"convolution": 0.05, "matmul": 0.95}
 
 
 class SmoothQuant(Algorithm):
