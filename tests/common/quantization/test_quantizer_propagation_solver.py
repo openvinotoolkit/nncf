620b37e00da76368eb422cd802e0878dd477798e
Vasily Shamporov <vasily.shamporov@intel.com>
2023-10-27 13:10:53 +0200

    Lint

--- tests/common/quantization/test_quantizer_propagation_solver.py
+++ tests/common/quantization/test_quantizer_propagation_solver.py
@@ -8,6 +8,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import abc
 from collections import Counter
 from collections import namedtuple
 from dataclasses import dataclass
@@ -154,7 +155,7 @@ class TwoInputModelWithQuantizableConcat(GraphProvider):
         return graph
 
 
-class ThreeInputGraphWithConcatAndBinaryOp(GraphProvider, ABC):
+class ThreeInputGraphWithConcatAndBinaryOp(GraphProvider, abc.ABC):
     BINARY_OP_NAME = "binary_op"
     CONCAT_NODE_NAME = "concat"
 
