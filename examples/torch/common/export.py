openvinotoolkit/nncf

commit e00f6b779428404dfcfd803a5dc3bbb9bbb1b1d9

Graph build from dataloader (#2196)
### Changes
Extends `ModelInputInfo` mechanism used to specify inputs to
`NNCFNetwork` for graph building/exporting - now the input info can be
specified either as `FillerInputInfo`, which functions pretty much the
same as before and uses NNCF config file as the source of specification
for the input tensors, or as `ExactInputInfo`, which allows to specify
exact forward arguments for graph building. The latter is used to build
the model graph based on outputs of dataloaders attached to `NNCFConfig`
in the QAT API if the "input_info" field is not specified in
`NNCFConfig`, and also in the PTQ API flow to build the graph based on
the output of the calibration dataset.

### Reason for changes
Previously the PTQ API had to specify own `wrap_inputs_fn`,
`wrap_outputs_fn`, `dummy_forward_fn` to make NNCFNetwork build its
graph based on the outputs of the calibration dataloader - these
functions had to be mostly copy-pasted from the QAT approach to preserve
basic NNCF PT functionality such as traced tensor expiry, same tensor
replication etc. The new approach allows code reuse. Also the QAT use
cases where the init dataloaders are specified are made easier since
"input_info" fields in the NNCFConfig may now be omitted.

### Related tickets
N/A

### Tests

tests.torch.test_graph_building.test_input_info_args_are_passed_into_forward
tests.torch.test_graph_building.test_filler_input_info_arg_generation

tests.torch.test_graph_building.test_compressed_model_creation_can_build_exact_input_infos_from_dataloader_in_config

tests.torch.ptq.test_quantize_model_helpers.test_create_nncf_network_with_nncf_dataset

examples/torch/object_detection/main.py
@@ -60,7 +60,7 @@ from nncf.config.structures import ModelEvaluationArgs
 from nncf.config.utils import is_accuracy_aware_training
 from nncf.torch import create_compressed_model
 from nncf.torch import load_state
-from nncf.torch.dynamic_graph.graph_tracer import create_input_infos
+from nncf.torch.dynamic_graph.io_handling import FillerInputInfo
 from nncf.torch.initialization import register_default_init_args
 from nncf.torch.utils import is_main_process
 
@@ -363,8 +363,8 @@ def create_dataloaders(config):
 
 
 def create_model(config: SampleConfig):
-    input_info_list = create_input_infos(config.nncf_config)
-    image_size = input_info_list[0].shape[-1]
+    input_info = FillerInputInfo.from_nncf_config(config.nncf_config)
+    image_size = input_info[0].shape[-1]
     ssd_net = build_ssd(config.model, config.ssd_params, image_size, config.num_classes, config)
     weights = config.get("weights")
     if weights:

nncf/torch/exporter.py
@@ -8,7 +8,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from copy import copy
 from functools import partial
 from typing import Any, Tuple
 
@@ -20,7 +19,6 @@ from nncf.common.logging import nncf_logger
 from nncf.telemetry import tracked_function
 from nncf.telemetry.events import NNCF_PT_CATEGORY
 from nncf.torch.dynamic_graph.graph_tracer import create_dummy_forward_fn
-from nncf.torch.dynamic_graph.graph_tracer import create_mock_tensor
 from nncf.torch.nested_objects_traversal import objwalk
 from nncf.torch.utils import get_model_device
 from nncf.torch.utils import is_tensor
@@ -34,7 +32,7 @@ def generate_output_names_list(num_outputs: int):
     return [f"output.{idx}" for idx in range(0, num_outputs)]
 
 
-def count_tensors(model_retval: Any) -> int:
+def count_tensors(obj: Any) -> int:
     count = 0
 
     def counter_fn(x: torch.Tensor) -> torch.Tensor:
@@ -42,7 +40,7 @@ def count_tensors(model_retval: Any) -> int:
         count += 1
         return x
 
-    objwalk(model_retval, is_tensor, counter_fn)
+    objwalk(obj, is_tensor, counter_fn)
     return count
 
 
@@ -127,38 +125,40 @@ class PTExporter(Exporter):
         """
         original_device = get_model_device(self._model)
         model = self._model.eval().cpu()
-        input_tensor_list = []
-        for info in self._model.nncf.input_infos:
-            single_batch_info = copy(info)
-            input_shape = tuple([1] + list(info.shape)[1:])
-            single_batch_info.shape = input_shape
-            input_tensor_list.append(create_mock_tensor(single_batch_info, "cpu"))
 
-        full_arg_forward = model.nncf.get_original_forward()
-        args = self._model_args[:-1]
-        kwargs = self._model_args[-1]
-        partial_forward = partial(full_arg_forward, *args, **kwargs)
-        with model.nncf.temporary_bound_original_forward(partial_forward):
-            if self._input_names is not None:
-                input_names = self._input_names
+        args, kwargs = self._model.nncf.input_infos.get_forward_inputs()
+
+        if self._model_args is not None:
+            args = tuple(list(args) + list(self._model_args[:-1]))
+            kwargs.update(**self._model_args[-1])
+
+        def to_single_batch_tensors(obj: torch.Tensor):
+            return obj[0:1]
+
+        args = objwalk(args, is_tensor, to_single_batch_tensors)
+        kwargs = objwalk(kwargs, is_tensor, to_single_batch_tensors)
+        export_args = (*args, kwargs)  # according to a variant of passing kwargs in torch.onnx.export doc
+
+        if self._input_names is not None:
+            input_names = self._input_names
+        else:
+            input_names = generate_input_names_list(count_tensors(export_args))
+
+        with torch.no_grad():
+            # Should call this, otherwise the operations executed during export will end up in the graph.
+            model.nncf.disable_dynamic_graph_building()
+
+            if self._output_names is not None:
+                output_names = self._output_names
             else:
-                input_names = generate_input_names_list(len(input_tensor_list))
+                # Will have to run a dummy forward call in order to determine the number of outputs.
+                dummy_forward = create_dummy_forward_fn(self._model.nncf.input_infos)
+                retval = dummy_forward(self._model)
+                output_names = generate_output_names_list(count_tensors(retval))
 
-            with torch.no_grad():
-                # Should call this, otherwise the operations executed during export will end up in the graph.
-                model.nncf.disable_dynamic_graph_building()
+            self._torch_export_call(model, export_args, save_path, input_names, output_names, opset_version)
 
-                if self._output_names is not None:
-                    output_names = self._output_names
-                else:
-                    # Will have to run a dummy forward call in order to determine the number of outputs.
-                    dummy_forward = create_dummy_forward_fn(self._model.nncf.input_infos)
-                    retval = dummy_forward(self._model)
-                    output_names = generate_output_names_list(count_tensors(retval))
-
-                self._torch_export_call(model, input_tensor_list, save_path, input_names, output_names, opset_version)
-
-                model.nncf.enable_dynamic_graph_building()
+            model.nncf.enable_dynamic_graph_building()
         model.to(original_device)
 
     def _torch_export_call(self, model, input_tensor_list, save_path, input_names, output_names, opset_version):

examples/torch/classification/main.py
@@ -73,7 +73,7 @@ from nncf.common.utils.tensorboard import prepare_for_tensorboard
 from nncf.config.utils import is_accuracy_aware_training
 from nncf.torch import create_compressed_model
 from nncf.torch.checkpoint_loading import load_state
-from nncf.torch.dynamic_graph.graph_tracer import create_input_infos
+from nncf.torch.dynamic_graph.io_handling import FillerInputInfo
 from nncf.torch.initialization import default_criterion_fn
 from nncf.torch.initialization import register_default_init_args
 from nncf.torch.structures import ExecutionParameters
@@ -450,8 +450,8 @@ def create_datasets(config):
     elif dataset_config in ["mock_32x32", "mock_299x299"]:
         normalize = transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
 
-    input_info_list = create_input_infos(config)
-    image_size = input_info_list[0].shape[-1]
+    input_info = FillerInputInfo.from_nncf_config(config)
+    image_size = input_info.elements[0].shape[-1]
     size = int(image_size / 0.875)
     if dataset_config in ["cifar10", "cifar100_224x224", "cifar100"]:
         list_val_transforms = [transforms.ToTensor(), normalize]

nncf/common/exporter.py
@@ -47,7 +47,7 @@ class Exporter(ABC):
         self._model = model
         self._input_names = input_names
         self._output_names = output_names
-        self._model_args = model_args if model_args else ({},)
+        self._model_args = model_args
 
     @abstractmethod
     def export_model(self, save_path: str, save_format: Optional[str] = None) -> None:

examples/torch/object_detection/dataset.py
@@ -21,7 +21,7 @@ from examples.torch.object_detection.datasets.coco import COCODataset
 from examples.torch.object_detection.datasets.voc0712 import VOCAnnotationTransform
 from examples.torch.object_detection.datasets.voc0712 import VOCDetection
 from examples.torch.object_detection.utils.augmentations import SSDAugmentation
-from nncf.torch.dynamic_graph.graph_tracer import create_input_infos
+from nncf.torch.dynamic_graph.io_handling import FillerInputInfo
 
 VOC_MEAN = (0.406, 0.456, 0.485)
 VOC_STD = (0.255, 0.224, 0.229)
@@ -45,8 +45,8 @@ def get_training_dataset(dataset_name, path_to_annotations, path_to_imgs, config
     # for VOC path_to_imgs = path_to_annotations = voc_root
     assert dataset_name in ["voc", "coco"]
     preprocessing = get_preprocessing(config)
-    input_info_list = create_input_infos(config)
-    image_size = input_info_list[0].shape[-1]
+    input_info = FillerInputInfo.from_nncf_config(config)
+    image_size = input_info.elements[0].shape[-1]
     ssd_transform = SSDAugmentation(image_size, preprocessing.mean, preprocessing.std, preprocessing.normalize_coef)
     if dataset_name == "voc":
         training_dataset = VOCDetection(
@@ -72,8 +72,8 @@ def get_testing_dataset(dataset_name, path_to_annotations, path_to_imgs, config)
     # for VOC path_to_imgs = path_to_annotations = voc_root
     assert dataset_name in ["voc", "coco"]
     preprocessing = get_preprocessing(config)
-    input_info_list = create_input_infos(config)
-    image_size = input_info_list[0].shape[-1]
+    input_info = FillerInputInfo.from_nncf_config(config)
+    image_size = input_info.elements[0].shape[-1]
     transform = BaseTransform(image_size, preprocessing.mean, preprocessing.std, preprocessing.normalize_coef)
     if dataset_name == "voc":
         testing_dataset = VOCDetection(

nncf/common/graph/graph.py
@@ -143,7 +143,7 @@ class NNCFGraphEdge:
         self.to_node = to_node
         self.input_port_id = input_port_id
         self.output_port_id = output_port_id
-        self.tensor_shape = tensor_shape
+        self.tensor_shape: Tuple[int] = tuple(tensor_shape)
         self.dtype = dtype
         self.parallel_input_port_ids = parallel_input_port_ids
 

nncf/config/config.py
@@ -80,7 +80,7 @@ class NNCFConfig(dict):
     def has_extra_struct(self, struct_cls: Type[NNCFExtraConfigStruct]) -> NNCFExtraConfigStruct:
         return struct_cls.get_id() in self.__nncf_extra_structs
 
-    def get_all_extra_structs_for_copy(self) -> List[NNCFExtraConfigStruct]:
+    def get_all_extra_structs(self) -> List[NNCFExtraConfigStruct]:
         return list(self.__nncf_extra_structs.values())
 
     def get_redefinable_global_param_value_for_algo(self, param_name: str, algo_name: str) -> Optional[str]:

nncf/config/extractors.py
@@ -223,3 +223,7 @@ def extract_accuracy_aware_training_params(config: NNCFConfig) -> Dict[str, obje
     validate_accuracy_aware_schema(config, params)
 
     return params
+
+
+def has_input_info_field(config: NNCFConfig) -> bool:
+    return config.get("input_info") is not None

commit 8b46c0cb1f201d56e23cf9db0fb10ec58b35453c

Fix object detection sample and bump datasets version (#2250)
### Changes
Fixed a regression introduced in #2196 for the object detection samples
and bumped the `datasets` version for the movement sparsity tests to fix
a `Loading a dataset cached in a LocalFileSystem is not supported` error
in the associated test cases.

### Reason for changes
Torch nightly tests fail otherwise.

### Related tickets
N/A

### Tests
torch_nightly

examples/torch/common/export.py
@@ -11,7 +11,9 @@
 import torch
 
 from nncf.api.compression import CompressionAlgorithmController
+from nncf.torch.exporter import count_tensors
 from nncf.torch.exporter import generate_input_names_list
+from nncf.torch.exporter import get_export_args
 
 
 def export_model(ctrl: CompressionAlgorithmController, save_path: str, no_strip_on_export: bool) -> None:
@@ -26,11 +28,9 @@ def export_model(ctrl: CompressionAlgorithmController, save_path: str, no_strip_
     model = ctrl.model if no_strip_on_export else ctrl.strip()
 
     model = model.eval().cpu()
-    input_names = generate_input_names_list(len(model.nncf.input_infos))
-    input_tensor_list = []
-    for info in model.nncf.input_infos:
-        input_shape = tuple([1] + list(info.shape)[1:])
-        input_tensor_list.append(torch.rand(input_shape))
+
+    export_args = get_export_args(model)
+    input_names = generate_input_names_list(count_tensors(export_args))
 
     with torch.no_grad():
-        torch.onnx.export(model, tuple(input_tensor_list), save_path, input_names=input_names)
+        torch.onnx.export(model, export_args, save_path, input_names=input_names)

examples/torch/object_detection/main.py
@@ -364,7 +364,7 @@ def create_dataloaders(config):
 
 def create_model(config: SampleConfig):
     input_info = FillerInputInfo.from_nncf_config(config.nncf_config)
-    image_size = input_info[0].shape[-1]
+    image_size = input_info.elements[0].shape[-1]
     ssd_net = build_ssd(config.model, config.ssd_params, image_size, config.num_classes, config)
     weights = config.get("weights")
     if weights:

nncf/torch/exporter.py
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from functools import partial
-from typing import Any, Tuple
+from typing import Any, Optional, Tuple
 
 import torch
 from torch.onnx import OperatorExportTypes
@@ -20,6 +20,7 @@ from nncf.telemetry import tracked_function
 from nncf.telemetry.events import NNCF_PT_CATEGORY
 from nncf.torch.dynamic_graph.graph_tracer import create_dummy_forward_fn
 from nncf.torch.nested_objects_traversal import objwalk
+from nncf.torch.nncf_network import NNCFNetwork
 from nncf.torch.utils import get_model_device
 from nncf.torch.utils import is_tensor
 
@@ -42,6 +43,21 @@ def count_tensors(obj: Any) -> int:
 
     objwalk(obj, is_tensor, counter_fn)
     return count
+
+
+def get_export_args(model: NNCFNetwork, model_args: Optional[Tuple[Any, ...]] = None) -> Tuple:
+    args, kwargs = model.nncf.input_infos.get_forward_inputs()
+
+    if model_args is not None:
+        args = tuple(list(args) + list(model_args[:-1]))
+        kwargs.update(**model_args[-1])
+
+    def to_single_batch_tensors(obj: torch.Tensor):
+        return obj[0:1]
+
+    args = objwalk(args, is_tensor, to_single_batch_tensors)
+    kwargs = objwalk(kwargs, is_tensor, to_single_batch_tensors)
+    return *args, kwargs  # according to a variant of passing kwargs in torch.onnx.export doc
 
 
 class PTExportFormat:
@@ -126,18 +142,7 @@ class PTExporter(Exporter):
         original_device = get_model_device(self._model)
         model = self._model.eval().cpu()
 
-        args, kwargs = self._model.nncf.input_infos.get_forward_inputs()
-
-        if self._model_args is not None:
-            args = tuple(list(args) + list(self._model_args[:-1]))
-            kwargs.update(**self._model_args[-1])
-
-        def to_single_batch_tensors(obj: torch.Tensor):
-            return obj[0:1]
-
-        args = objwalk(args, is_tensor, to_single_batch_tensors)
-        kwargs = objwalk(kwargs, is_tensor, to_single_batch_tensors)
-        export_args = (*args, kwargs)  # according to a variant of passing kwargs in torch.onnx.export doc
+        export_args = get_export_args(self._model, model_args=self._model_args)
 
         if self._input_names is not None:
             input_names = self._input_names

tests/torch/quantization/test_sanity_sample.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from typing import Dict
 
 import pytest
+import torch
 from torch import nn
 
 from nncf import NNCFConfig
@@ -314,6 +315,7 @@ class ExportSampleValidator(PrecisionSampleValidator):
 
         ctrl_mock = mocker.MagicMock(spec=QuantizationController)
         model_mock = mocker.MagicMock(spec=nn.Module)
+        mocker.patch("examples.torch.common.export.get_export_args", return_value=((torch.Tensor([1, 1]),), {}))
         create_model_location = sample_location + ".create_compressed_model"
         create_model_patch = mocker.patch(create_model_location)
 