395d93f90cf7dd2855d50887c70304ef163657c2
Nikita Malinin <nikita.malinin@intel.com>
2024-01-25 09:18:51 +0100

    Bump NNCF version

--- nncf/version.py
+++ nncf/version.py
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "2.8.0"
+__version__ = "2.9.0"
 
 BKC_TORCH_VERSION = "2.1.2"
 BKC_TORCHVISION_VERSION = "0.16.2"
