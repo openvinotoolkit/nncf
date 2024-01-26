0499ac48a822b226ac1c5275baa26975f36b24f9
Nikita Savelyev <nikita.savelyev@intel.com>
2024-01-26 15:54:44 +0100

    Remove import

--- nncf/quantization/fake_quantize.py
+++ nncf/quantization/fake_quantize.py
@@ -9,7 +9,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import warnings
 from dataclasses import dataclass
 from typing import Tuple
 
