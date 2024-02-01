c0b0196a28c7a075cb83f9c1b223cda5c43c8342
andreyanufr <andrey.anufriev@intel.com>
2024-02-01 11:09:41 +0100

    Proposal for stateful models. (#2440)
    ### Changes
    
    reset_state before model inference
    
    ### Reason for changes
    
    Support statefull model inference in `OVCompiledModelEngine`
    
    ### Related tickets
    
    131141
    
    ### Tests
    
    TBD

--- nncf/openvino/engine.py
+++ nncf/openvino/engine.py
@@ -29,6 +29,8 @@ class OVCompiledModelEngine(Engine):
 
     def __init__(self, model: ov.CompiledModel):
         self.compiled_model = model
+        self.infer_request = model.create_infer_request()
+        self.reset_state = hasattr(self.infer_request, "reset_state")
         self.input_tensor_names = set()
         self.number_of_inputs = len(model.inputs)
         for model_input in model.inputs:
@@ -63,7 +65,11 @@ class OVCompiledModelEngine(Engine):
         :return output_data: Model's output.
         """
         self._check_input_data_format(input_data)
-        model_outputs = self.compiled_model(input_data)
+
+        if self.reset_state:
+            self.infer_request.reset_state()
+
+        model_outputs = self.infer_request.infer(input_data, share_inputs=True)
 
         output_data = {}
         for tensor, value in model_outputs.items():
