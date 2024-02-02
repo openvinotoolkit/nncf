fa4bfb2c332bf794c533cfc94618dd4b9a2c5a0b
Nikita Malinin <nikita.malinin@intel.com>
2024-02-02 13:30:07 +0100

    Added test

--- tests/openvino/native/test_model_utils.py
+++ tests/openvino/native/test_model_utils.py
@@ -9,14 +9,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import networkx as nx
 import numpy as np
 import pytest
 
+from nncf.common.factory import NNCFGraphFactory
+from nncf.common.insertion_point_graph import InsertionPointGraph
 from nncf.openvino.graph.metatypes.openvino_metatypes import OVConvolutionMetatype
+from nncf.openvino.graph.model_utils import remove_friendly_name_duplicates
 from nncf.openvino.graph.node_utils import create_bias_tensor
 from tests.common.quantization.mock_graphs import NodeWithType
 from tests.common.quantization.mock_graphs import create_mock_graph
 from tests.common.quantization.mock_graphs import get_nncf_graph_from_mock_nx_graph
+from tests.openvino.native.models import DuplicatedNamesModel
 
 
 def get_nncf_graph_for_test(edge_shape, dtype):
@@ -45,3 +50,27 @@ def test_create_bias_tensor(edge_shape, dtype, ref_shape):
     val = create_bias_tensor(graph.get_node_by_name("/Conv_1_0"), graph, 5)
     assert val.shape == ref_shape
     assert np.equal(val, np.full(ref_shape, 5)).all()
+
+
+@pytest.mark.parametrize(
+    "model_instance,remove_duplicates,unique_layer_numbers",
+    [
+        (DuplicatedNamesModel().ov_model, False, 5),
+        (DuplicatedNamesModel().ov_model, True, 6),
+    ],
+)
+def test_remove_friendly_name_duplicates(model_instance, remove_duplicates, unique_layer_numbers):
+    if remove_duplicates:
+        model_instance = remove_friendly_name_duplicates(model_instance)
+
+    model_uniqie_names = set([op.get_friendly_name() for op in model_instance.get_ops()])
+    assert len(model_uniqie_names) == unique_layer_numbers
+
+    nncf_graph = NNCFGraphFactory.create(model_instance)
+    assert len(nncf_graph.get_all_nodes()) == unique_layer_numbers
+
+    try:
+        _ = InsertionPointGraph(nncf_graph)
+        assert remove_duplicates
+    except nx.NetworkXUnfeasible:
+        assert not remove_duplicates
