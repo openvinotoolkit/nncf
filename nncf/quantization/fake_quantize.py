ce329b284873a62c4ba19261f1937b1d669425c7
Nikita Savelyev <nikita.savelyev@intel.com>
2024-02-16 11:33:45 +0100

    Reverted

--- nncf/quantization/fake_quantize.py
+++ nncf/quantization/fake_quantize.py
@@ -9,6 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import warnings
 from dataclasses import dataclass
 from typing import Tuple
 
@@ -121,8 +122,11 @@ def tune_range(
         qval = fns.round(fval)
 
     ra = fns.where(qval < level_high, qval / (qval - level_high) * right_border, left_border)
-    qval_ = fns.where(qval == 0.0, 1.0, qval)
-    rb = fns.where(qval > 0.0, (qval - level_high) / qval_ * left_border, right_border)
+    with warnings.catch_warnings():
+        # If `qval` is 0 `rb` will equal `right_border`, and we don't want to show an unnecessary division by 0 warning
+        warnings.simplefilter("ignore")
+        rb_then_result = (qval - level_high) / qval * left_border
+    rb = fns.where(qval > 0.0, rb_then_result, right_border)
 
     range_a = right_border - ra
     range_b = rb - left_border
