401d41441de273421f80e43d534a3636a62bc748
Nikita Malinin <nikita.malinin@intel.com>
2024-02-28 16:18:45 +0100

    Added CPU device

--- tests/post_training/pipelines/image_classification_timm.py
+++ tests/post_training/pipelines/image_classification_timm.py
@@ -133,7 +133,7 @@ class ImageClassificationTimm(PTQTestPipeline):
             core.set_property("CPU", properties={"CPU_THREADS_NUM": str(cpu_threads_num)})
 
         ov_model = core.read_model(self.path_compressed_ir)
-        compiled_model = core.compile_model(ov_model)
+        compiled_model = core.compile_model(ov_model, "CPU")
 
         jobs = int(os.environ.get("NUM_VAL_THREADS", DEFAULT_VAL_THREADS))
         infer_queue = ov.AsyncInferQueue(compiled_model, jobs)
