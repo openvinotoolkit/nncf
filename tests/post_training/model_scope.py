f5111a82e06d74ab5f8bc127039c7cd8cd2bfd39
Andrei Anufriev <andrey.anufriev@intel.com>
2024-03-22 09:03:28 +0100

    Stateless scale estimation test.

--- tests/post_training/model_scope.py
+++ tests/post_training/model_scope.py
@@ -320,6 +320,19 @@ WEIGHT_COMPRESSION_MODELS = [
         "backends": [BackendType.OV],
     },
     {
+        "reported_name": "tinyllama_data_aware_awq_scale_estimation",
+        "model_id": "tinyllama/tinyllama-1.1b-step-50k-105b",
+        "pipeline_cls": LMWeightCompression,
+        "compression_params": {
+            "group_size": 64,
+            "ratio": 0.8,
+            "mode": CompressWeightsMode.INT4_SYM,
+            "awq": True,
+            "scale_estimation": True,
+        },
+        "backends": [BackendType.OV],
+    },
+    {
         "reported_name": "tinyllama_data_aware_awq_scale_estimation_stateful",
         "model_id": "tinyllama/tinyllama-1.1b-step-50k-105b",
         "pipeline_cls": LMWeightCompression,
