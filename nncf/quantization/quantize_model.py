795742d2e8bfa511fe356b359851f4f4313b7d3e
andreyanufr <andrey.anufriev@intel.com>
2024-04-26 14:38:30 +0200

    Update nncf/quantization/quantize_model.py
    Co-authored-by: Daniil Lyakhov <daniil.lyakhov@intel.com>

--- nncf/quantization/quantize_model.py
+++ nncf/quantization/quantize_model.py
@@ -418,7 +418,7 @@ def compress_weights(
     if backend == BackendType.OPENVINO:
         from nncf.openvino.quantization.quantize_model import compress_weights_impl as ov_compress_weights_impl
 
-        if True in [awq, scale_estimation] and (dataset is None or mode == CompressWeightsMode.NF4 or group_size == -1):
+        if any((awq, scale_estimation)) and (dataset is None or mode == CompressWeightsMode.NF4 or group_size == -1):
             raise AttributeError(
                 "Scale estimation or AWQ algorithm defined, but dataset is None or mode is NF4 or group_size < 0."
             )
