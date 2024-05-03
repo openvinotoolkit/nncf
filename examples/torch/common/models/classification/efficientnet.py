bc34af3f7ddbe4622ca0a4fdcc125dfe376705b8
Aleksei Kashapov <aleksei.kashapov@intel.com>
2024-05-03 14:01:02 +0200

    hide import of efficientnet_pytorch

--- examples/torch/common/models/classification/efficientnet.py
+++ examples/torch/common/models/classification/efficientnet.py
@@ -10,16 +10,16 @@
 # limitations under the License.
 # Efficient Net implementation from: https://github.com/lukemelas/EfficientNet-PyTorch
 
-from efficientnet_pytorch import EfficientNet
-from efficientnet_pytorch.utils import Conv2dStaticSamePadding
 
 from nncf.torch import register_module
 
-wrapper = register_module()
-wrapper(Conv2dStaticSamePadding)
-
 
 def efficient_net(pretrained=True, num_classes=1000, **kwargs):
+    from efficientnet_pytorch import EfficientNet
+    from efficientnet_pytorch.utils import Conv2dStaticSamePadding
+
+    wrapper = register_module()
+    wrapper(Conv2dStaticSamePadding)
     if pretrained:
         return EfficientNet.from_pretrained(num_classes=num_classes, **kwargs)
     return EfficientNet.from_name(num_classes=num_classes, **kwargs)
