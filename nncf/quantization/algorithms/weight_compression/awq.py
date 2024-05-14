54b8ec58de2d53ff499678c97829785bb8978667
Andrei Anufriev <andrey.anufriev@intel.com>
2024-05-14 09:09:06 +0200

    Extend AWQ for Act->MatMul and Act->Multiply->MatMul patterns.

--- nncf/quantization/algorithms/weight_compression/awq.py
+++ nncf/quantization/algorithms/weight_compression/awq.py
@@ -137,11 +137,6 @@ class AWQ(Algorithm):
         for _, pattern_graph in self._patterns.items():
             matches.extend(find_subgraphs_matching_pattern(nx_graph, pattern_graph(), strict=False))
 
-        if len(matches) == 0:
-            return model
-
-        target_node_names = []
-        merge_node_names = []
         awq_data = {}
         name_mapping = {wp.weight_name: idx for idx, wp in enumerate(self._all_weight_params)}
 
@@ -149,20 +144,31 @@ class AWQ(Algorithm):
             nncf_node = graph.get_node_by_key(match[-1])
             if not self._backend_entity.is_node_with_weights(nncf_node, graph):
                 continue
-
+            
+            target_node_names = []
             for weight_op_friendly_name, _ in self._backend_entity.get_weight_names_and_port_ids(nncf_node, graph):
                 target_node_names.append(weight_op_friendly_name)
 
-            nncf_node = graph.get_node_by_key(match[0])
-            for weight_op_friendly_name, _ in self._backend_entity.get_weight_names_and_port_ids(nncf_node, graph):
-                merge_node_names.append(weight_op_friendly_name)
-
-            assert len(target_node_names) == len(merge_node_names)
             weight_params = self._all_weight_params[name_mapping[target_node_names[-1]]]
+
             if weight_params.compression_config.num_bits != 4:
                 continue
             target_node = self._nodes_to_compress[name_mapping[target_node_names[-1]]]
-            merge_node = self._nodes_to_compress[name_mapping[merge_node_names[-1]]]
+
+            # avoid matching different patterns for the same node
+            if target_node.node_name in awq_data:
+                continue
+
+            nncf_node = graph.get_node_by_key(match[0])
+
+            if self._backend_entity.is_node_with_weights(nncf_node, graph): # pattern MatMul->Multiply->MatMul
+                merge_node_names = []
+                for weight_op_friendly_name, _ in self._backend_entity.get_weight_names_and_port_ids(nncf_node, graph):
+                    merge_node_names.append(weight_op_friendly_name)
+                assert len(target_node_names) == len(merge_node_names)
+                merge_node = self._nodes_to_compress[name_mapping[merge_node_names[-1]]]
+            else: # pattern Act->MatMul or Act->Multiply->MatMul
+                merge_node = nncf_node
 
             awq_data[target_node.node_name] = AWQCompressionInfo(weight_params, target_node, merge_node)
 
@@ -268,15 +274,181 @@ class AWQ(Algorithm):
             scaled_weight = weight * w_scale
             self._backend_entity.set_weight(wp.node_with_weight, weight_port_id, model, graph, scaled_weight)
 
-            for _, port_id in self._backend_entity.get_weight_names_and_port_ids(merge_node, graph):
-                merge_weight = self._backend_entity.get_weight(merge_node, port_id, model, graph)
-                merge_weight = merge_weight * a_scale
-                self._backend_entity.set_weight(merge_node, port_id, model, graph, merge_weight)
+            if merge_node.node_type is 'MatMul':
+                for _, port_id in self._backend_entity.get_weight_names_and_port_ids(merge_node, graph):
+                    merge_weight = self._backend_entity.get_weight(merge_node, port_id, model, graph)
+                    merge_weight = merge_weight * a_scale
+                    self._backend_entity.set_weight(merge_node, port_id, model, graph, merge_weight)
+                a_scale = fns.transpose(a_scale)
+            else:
+                a_scale = fns.transpose(a_scale)
+                nonlinear_node = self.name_to_node_mapping[merge_node.node_name]
+                self._backend_entity.insert_scale_after_node(nonlinear_node, a_scale.data, merge_node.node_name)
 
             # update activations for next usage
-            a_scale_t = fns.transpose(a_scale)
             for i, stat in enumerate(self._activations[k]):
-                stat = stat * a_scale_t
+                stat = stat * a_scale
+                self._activations[k][i] = stat
+
+        return model
+
+
+    def _apply_nonlinear_pattern(
+        self,
+        model: TModel,
+        graph: NNCFGraph,
+        statistic_points: Optional[StatisticPointsContainer] = None,
+        dataset: Optional[Dataset] = None,
+    ) -> TModel:
+        """
+        Applies the algorithm to the model.
+
+        :param model: Model for applying algorithm.
+        :param graph: Model graph.
+        :param statistic_points: Statistic points with collected statistics values.
+        :param dataset: A representative dataset for the calibration process.
+        :return: A resulting model.
+        """
+        matches = []
+
+        inference_nncf_graph = transform_to_inference_graph(deepcopy(graph), [], [], [])
+        nx_graph = inference_nncf_graph.get_nx_graph_copy()
+        for _, pattern_graph in self._patterns_ext.items():
+            matches.extend(find_subgraphs_matching_pattern(nx_graph, pattern_graph(), strict=False))
+
+        if len(matches) == 0:
+            return model
+
+        target_node_names = []
+        merge_node_names = []
+        awq_data = {}
+        name_mapping = {wp.weight_name: idx for idx, wp in enumerate(self._all_weight_params)}
+
+        for match in matches:
+            target_node = graph.get_node_by_key(match[-1])
+            if not self._backend_entity.is_node_with_weights(target_node, graph):
+                continue
+            target_node_names = []
+            for weight_op_friendly_name, _ in self._backend_entity.get_weight_names_and_port_ids(target_node, graph):
+                target_node_names.append(weight_op_friendly_name)
+
+            merge_node = graph.get_node_by_key(match[0])
+
+            weight_params = self._all_weight_params[name_mapping[target_node_names[-1]]]
+            if weight_params.compression_config.num_bits != 4:
+                continue
+
+            awq_data[target_node.node_name] = AWQCompressionInfo(weight_params, target_node, merge_node)
+
+        alpha_step = (self._alpha_max - self._alpha_min) / self._steps
+
+        for k, awq_data_item in track(awq_data.items(), description="Applying AWQ"):
+            wp = awq_data_item.weight_params
+            target_node = awq_data_item.target_node
+            merge_node = awq_data_item.merge_node
+
+            weight_data = self._backend_entity.get_weight_names_and_port_ids(wp.node_with_weight, graph)
+            if len(weight_data) != 1:  # not supported by the algorithm
+                continue
+            _, weight_port_id = weight_data[0]
+
+            config = wp.compression_config
+
+            stats = self._activations[k]
+            X = fns.stack([fns.mean(stat, axis=0) for stat in stats])
+            X = fns.transpose(X)
+
+            s = fns.max(fns.abs(X), axis=1)
+
+            if X.shape[1] > self._subset_size:
+                lens = [stat.shape[0] for stat in stats]
+                step = X.shape[1] // self._subset_size
+                idxs = [i[0] for i in sorted(enumerate(lens), key=lambda x: -x[1])][::step]
+                X = X[:, idxs]
+
+            top_k = max(int(s.shape[0] * self._percent_to_apply), 1)
+            topk_idxs = fns.argsort(-s)[:top_k]
+
+            group_size = config.group_size
+            if group_size == -1:
+                group_size = s.shape[0]
+
+            groups_to_correct = set()
+            for idx in topk_idxs:
+                groups_to_correct.add(idx.data // group_size)
+
+            groups_to_correct = list(groups_to_correct)
+
+            weight = self._backend_entity.get_weight(
+                wp.node_with_weight, weight_port_id, model, graph
+            )  # get_const_value(wp.weight_node)
+            assert isinstance(wp.reduction_axes, tuple) and len(wp.reduction_axes) == 1
+            reduction_axis = wp.reduction_axes[0]
+
+            if reduction_axis == 0:
+                weight = fns.transpose(weight)
+                reduction_axis = 1
+
+            shape_vector = fns.mean(X, axis=1)
+            scale = fns.ones_like(shape_vector)
+
+            awq_config = deepcopy(config)
+            awq_config.group_size = -1
+
+            for gi in groups_to_correct:
+                offset = gi * group_size
+                gscale = s[offset : offset + group_size]
+
+                a_min = fns.quantile(gscale, 0.1)
+                a_max = 1e2
+                gscale = fns.clip(gscale, a_min=a_min, a_max=a_max)
+
+                gweight = weight[:, offset : offset + group_size]
+                gacts = X[offset : offset + group_size, :]
+
+                fp32_out = fns.matmul(gweight, gacts)
+                min_diff = fns.max(fns.abs(fp32_out))
+                best_scale = None
+
+                alpha = self._alpha_min
+                for _ in range(self._steps):
+                    cur_scale = gscale**alpha
+
+                    g_compressed_weighs, g_c_scale, g_c_zp = do_integer_quantization(
+                        gweight * cur_scale, reduction_axis, awq_config
+                    )
+                    g_decompressed_weighs = do_dequantization(g_compressed_weighs, g_c_scale, g_c_zp)
+                    sacts = gacts / fns.unsqueeze(cur_scale, 1)
+
+                    cur_out = fns.matmul(g_decompressed_weighs, sacts)
+                    cur_diff = fns.mean(fns.abs(cur_out - fp32_out))
+                    if cur_diff < min_diff:
+                        min_diff = cur_diff
+                        best_scale = cur_scale
+                    alpha += alpha_step
+
+                if best_scale is not None:
+                    scale.data[offset : offset + group_size] = best_scale.data
+
+            a_scale = scale
+            w_scale = scale
+            if wp.reduction_axes[0] == 0:
+                w_scale = fns.unsqueeze(w_scale, 1)
+                a_scale = fns.unsqueeze(1.0 / a_scale, 1)
+            else:
+                w_scale = fns.unsqueeze(w_scale, 0)
+                a_scale = fns.unsqueeze(1.0 / a_scale, 0)
+
+            scaled_weight = weight * w_scale
+            self._backend_entity.set_weight(wp.node_with_weight, weight_port_id, model, graph, scaled_weight)
+
+            nonlinear_node = self.name_to_node_mapping[merge_node.node_name]
+            self._backend_entity.insert_scale_after_node(nonlinear_node, a_scale.data, merge_node.node_name)
+
+            # update activations for next usage
+            #a_scale_t = fns.transpose(a_scale)
+            for i, stat in enumerate(self._activations[k]):
+                stat = stat * a_scale
                 self._activations[k][i] = stat
 
         return model
