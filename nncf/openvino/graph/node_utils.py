86849f536648716a4ca84af4878f7f5d90175aae
Aleksandr Suslov <alexander.suslov@intel.com>
2024-05-23 11:53:54 +0400

    make ruff happy

--- nncf/openvino/graph/node_utils.py
+++ nncf/openvino/graph/node_utils.py
@@ -109,7 +109,7 @@ def get_const_value(const_node: ov.Node, dtype: Optional[np.dtype] = None) -> np
     :param dtype: Destination type.
     :return: The constant value.
     """
-    if dtype == None:
+    if dtype is None:
         return const_node.data
     return const_node.get_data(dtype=dtype)
 
