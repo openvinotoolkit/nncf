70483071b961575b94b684186d3bb395aee75515
Aleksei Kashapov <aleksei.kashapov@intel.com>
2024-06-18 12:00:38 +0200

    fix

--- tests/openvino/native/common.py
+++ tests/openvino/native/common.py
@@ -103,7 +103,7 @@ def get_actual_reference_for_current_openvino(rel_path: Path) -> Path:
     :return: Path to a reference file.
     """
     root_dir = OPENVINO_NATIVE_TEST_ROOT / "data"
-    current_ov_version = version.parse(get_openvino_version())
+    current_ov_version = get_openvino_version()
 
     def is_valid_version(dir_path: Path) -> bool:
         try:
@@ -114,10 +114,10 @@ def get_actual_reference_for_current_openvino(rel_path: Path) -> Path:
 
     ref_versions = filter(is_valid_version, root_dir.iterdir())
     ref_versions = sorted(ref_versions, key=lambda x: version.parse(x.name), reverse=True)
-    ref_versions = filter(lambda x: version.parse(x.name) <= current_ov_version, ref_versions)
+    ref_versions = filter(lambda x: version.parse(x.name) <= version.parse(current_ov_version), ref_versions)
 
     for root_version in ref_versions:
         file_name = root_version / rel_path
         if file_name.is_file():
             return file_name
-    return current_ov_version / rel_path
+    return root_dir / current_ov_version / rel_path
