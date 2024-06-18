45f4eee506daf0c8257402dfaec31c21f7e7f84a
Nikita Malinin <nikita.malinin@intel.com>
2024-06-18 14:19:51 +0200

    Bump OV/NNCF version (#2740)
    ### Changes
    
    - Bumped OpenVINO version.
    - Bumped NNCF version.
    - Fixed issue with cheery-pick from #2709.
    
    ### Reason for changes
    
    - NNCF v2.11 release.
    
    ### Related tickets
    
    - 142565
    
    ### Tests
    
    - N/A
    
    ---------
    
    Co-authored-by: Alexander Dokuchaev <alexander.dokuchaev@intel.com>

--- nncf/version.py
+++ nncf/version.py
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "2.11.0"
+__version__ = "2.12.0"
 
 BKC_TORCH_SPEC = "==2.3.*"
 BKC_TF_SPEC = "==2.12.*"
