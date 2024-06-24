6b702f5369bb2abd6398e56c0b1f178f847d7b05
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2024-06-24 23:06:46 +0300

    PTScaledDotProductAttentionMetatype

--- nncf/quantization/algorithms/min_max/torch_backend.py
+++ nncf/quantization/algorithms/min_max/torch_backend.py
@@ -105,7 +105,7 @@ class PTMinMaxAlgoBackend(MinMaxAlgoBackend):
 
     @property
     def scaled_dot_product_attention_metatypes(self) -> List[OperatorMetatype]:
-        return []
+        return [om.PTScaledDotProductAttentionMetatype]
 
     @property
     def scales_unification_map(self) -> Dict[OperatorMetatype, OperatorMetatype]:
