87e30471722d9c3b34fb3f31e811e684c87c4cfd
Nikita Malinin <nikita.malinin@intel.com>
2024-06-27 16:38:13 +0200

    Fix fix ONNX

--- tests/onnx/test_layer_attributes.py
+++ tests/onnx/test_layer_attributes.py
@@ -99,7 +99,8 @@ def get_one_layer_model(op_name: str, node_creator: ONNXNodeCreator, input_shape
         (
             ONNXConvCreator,
             ONNXLayerAttributes(
-                weight_attrs={1: {"name": "Conv1_W", "shape": [3, 3, 1, 1]}}, bias_attrs={"name": "Conv1_B"},
+                weight_attrs={1: {"name": "Conv1_W", "shape": [3, 3, 1, 1]}},
+                bias_attrs={"name": "Conv1_B"},
             ),
         ),
     ],
