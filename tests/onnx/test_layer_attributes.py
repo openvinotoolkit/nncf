99b060ee0e6cdf009d846b3f396048cd4253ed45
Nikita Malinin <nikita.malinin@intel.com>
2024-06-27 16:30:29 +0200

    Fix fix ONNX

--- tests/onnx/test_layer_attributes.py
+++ tests/onnx/test_layer_attributes.py
@@ -95,12 +95,11 @@ def get_one_layer_model(op_name: str, node_creator: ONNXNodeCreator, input_shape
 @pytest.mark.parametrize(
     "node_creator, ref_layer_attrs",
     [
-        (ONNXIdentityCreator, ONNXLayerAttributes(node_attrs={"inputs": ["X"]})),
+        (ONNXIdentityCreator, ONNXLayerAttributes()),
         (
             ONNXConvCreator,
             ONNXLayerAttributes(
-                weight_attrs={1: {"name": "Conv1_W", "shape": [3, 3, 1, 1]}},
-                bias_attrs={"name": "Conv1_B"},
+                weight_attrs={1: {"name": "Conv1_W", "shape": [3, 3, 1, 1]}}, bias_attrs={"name": "Conv1_B"},
             ),
         ),
     ],
