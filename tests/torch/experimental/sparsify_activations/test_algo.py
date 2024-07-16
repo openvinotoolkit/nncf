ff00a1e93c3b03a80d669457486fae57858aba4a
Pan, Yujie <yujie.pan@intel.com>
2024-07-16 16:39:08 +0800

    re-enable graph comparison for dummy llama

--- tests/torch/experimental/sparsify_activations/test_algo.py
+++ tests/torch/experimental/sparsify_activations/test_algo.py
@@ -152,9 +152,7 @@ class TestSparsifyActivationsAlgorithm:
         file_name = f"{desc.name}_compressed_weights" if self.compress_weights else desc.name
         ref_dot_path = Path(TEST_ROOT, "torch", "data", "experimental", "sparsify_activations", f"{file_name}.dot")
         graph = model.nncf.get_graph().get_graph_for_structure_analysis()
-        if desc.name != "dummy_llama":
-            # TODO(yujie): full graph comparison is fragile
-            compare_nx_graph_with_reference(graph, ref_dot_path)
+        compare_nx_graph_with_reference(graph, ref_dot_path)
 
     def test_export_openvino(self):
         model: NNCFNetwork = self.model
