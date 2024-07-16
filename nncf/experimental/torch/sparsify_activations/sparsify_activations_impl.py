a2b6f944444ddf5750b5ce56654556febed917a9
Pan, Yujie <yujie.pan@intel.com>
2024-07-16 16:39:03 +0800

    can work except  ignored_scope

--- nncf/experimental/torch/sparsify_activations/sparsify_activations_impl.py
+++ nncf/experimental/torch/sparsify_activations/sparsify_activations_impl.py
@@ -1,64 +1,67 @@
 from abc import ABC
 from abc import abstractmethod
 from pathlib import Path
-from typing import List, Tuple, TypeVar
-
-import torch
-import torch.nn as nn
+from typing import Dict, List, Optional, TypeVar
 
 import nncf
+from nncf.common import factory
 from nncf.common.factory import NNCFGraphFactory
 from nncf.common.graph.graph import NNCFGraph
 from nncf.common.graph.graph import NNCFNode
 from nncf.common.graph.operator_metatypes import OperatorMetatype
-from nncf.common.graph.transformations.commands import TargetType
-from nncf.common.graph.transformations.layout import TransformationLayout
+from nncf.common.logging.track_progress import track
+from nncf.common.scopes import matches_any
 from nncf.common.scopes import should_consider_scope
-from nncf.common.tensor_statistics.statistic_point import StatisticPointsContainer
 from nncf.common.utils.backend import BackendType
 from nncf.common.utils.backend import get_backend
 from nncf.data import Dataset
-from nncf.experimental.tensor.tensor import Tensor
-from nncf.quantization.algorithms.algorithm import Algorithm
-from nncf.torch.graph import operator_metatypes as om
-from nncf.torch.graph.transformations.commands import PTInsertionCommand
-from nncf.torch.graph.transformations.commands import PTSharedFnInsertionCommand
-from nncf.torch.graph.transformations.commands import PTTargetPoint
+from nncf.scopes import IgnoredScope
+from nncf.scopes import get_ignored_node_names_from_ignored_scope
 from nncf.torch.model_creation import is_wrapped_model
 from nncf.torch.model_creation import wrap_model
-from nncf.torch.model_graph_manager import find_const_node_in_constant_subgraph
-from nncf.torch.model_graph_manager import get_const_node
-from nncf.torch.model_graph_manager import get_module_by_name
-from nncf.torch.model_graph_manager import split_const_name
-from nncf.torch.model_transformer import PTModelTransformer
-from nncf.torch.utils import is_tracing_state
 
 TModel = TypeVar("TModel")
 
 
-def node_name_matches_module_name(node_name, module_name):
-    parts = module_name.split('.')
-    return all(f'[{p}]' in node_name for p in parts)
+class SparsifyActivationsAlgoBackend(ABC):
 
+    def do_inference(self, model: TModel, dataset: Dataset):
+        engine = factory.EngineFactory.create(model)
+        for input_data in track(
+            dataset.get_inference_data(),
+            total=dataset.get_length(),
+            description="Activation sparsifier calibration",
+        ):
+            engine.infer(input_data)
 
-class SparsifyActivationsAlgorithmBackend(ABC):
     @property
     @abstractmethod
     def supported_metatypes(self) -> List[OperatorMetatype]:
         pass
 
     @abstractmethod
-    def do_sparsification(self, model, graph: NNCFGraph, nodes_to_sparsify: List[NNCFNode]):
+    def insert_sparsifiers(self, model: TModel, target_sparsity_by_node: Dict[NNCFNode, float]) -> TModel:
         pass
 
-    def transform_model(self,):
+    @abstractmethod
+    def calibrate_sparsifiers(self, model: TModel, dataset: Dataset) -> TModel:
+        pass
+
+    @abstractmethod
+    def freeze_sparsifiers(self, model: TModel) -> TModel:
         pass
 
 
 class SparsifyActivationsAlgorithm:
 
-    def __init__(self, sparse_config: dict[str, float]):
-        self.sparse_config = sparse_config
+    def __init__(
+        self,
+        target_sparsity_by_scope: dict[str, float],
+        ignored_scope: IgnoredScope,
+    ):
+        self._target_sparsity_by_scope = target_sparsity_by_scope
+        self._ignored_scope = ignored_scope
+        self._backend_entity = None
 
     @property
     def available_backends(self) -> List[BackendType]:
@@ -72,14 +75,14 @@ class SparsifyActivationsAlgorithm:
         """
         model_backend = get_backend(model)
         if model_backend == BackendType.TORCH:
-            from nncf.experimental.torch.sparsify_activations.torch_backend import PTPruneActivationAlgorithmBackend
-            self._backend_entity = PTPruneActivationAlgorithmBackend()
+            from nncf.experimental.torch.sparsify_activations.torch_backend import PTSparsifyActivationsAlgoBackend
+            self._backend_entity = PTSparsifyActivationsAlgoBackend()
         else:
             raise nncf.UnsupportedBackendError(
                 f"{model_backend.value} backend is not supported for `sparsify_activations`."
             )
 
-    def _get_nodes_to_sparsify(self, nncf_graph: NNCFGraph) -> List[NNCFNode]:
+    def _get_target_sparsity_by_node(self, nncf_graph: NNCFGraph) -> Dict[NNCFNode, float]:
         """
         Collects nodes in the model's graph corresponding to the layers for weight compression.
 
@@ -87,26 +90,37 @@ class SparsifyActivationsAlgorithm:
         :return: List with the data for each layer.
         """
         supported_metatypes = self._backend_entity.supported_metatypes
-        ordered_nodes_to_sparsify = []
+        ignored_names = get_ignored_node_names_from_ignored_scope(
+            self._ignored_scope, nncf_graph, strict=self._ignored_scope.validate
+        )
+        print(ignored_names)
+        target_sparsity_by_node = {}
         for node in nncf_graph.topological_sort():
-            if node.metatype in supported_metatypes:
-                ordered_nodes_to_sparsify.append(node)
-        return ordered_nodes_to_sparsify
+            print(node.metatype, node.node_name, ignored_names,
+                  'should_consider_scope=',
+                  should_consider_scope(node.node_name, ignored_names))
+            if node.metatype not in supported_metatypes or not should_consider_scope(node.node_name, ignored_names):
+                continue
+            for scope, target_sparsity in self._target_sparsity_by_scope.items():
+                if matches_any(node.node_name, scope):
+                    if node.node_name in target_sparsity_by_node:
+                        raise nncf.ValidationError(
+                            f'"{node.node_name}" is matched by multiple items in `target_sparsity_by_scope`.')
+                    target_sparsity_by_node[node] = target_sparsity
+        return target_sparsity_by_node
 
-    def _get_activation_node_and_port(self, node: NNCFNode, nncf_graph: NNCFGraph) -> Tuple[NNCFNode, int]:
-        """
-        This method returns the activation layer and corresponding port id for the node.
-
-        :param node: NNCFGraph node for which the activation is sought.
-        :param nncf_graph: NNCFGraph instance with the node.
-        :return: Tuple with the activation node and port id.
-        """
-        activation_port = self._backend_entity.get_activation_port_id(
-            node, nncf_graph)
-        activation_edge = nncf_graph.get_input_edges(node)[activation_port]
-        activation_node = activation_edge.from_node
-        port_id = activation_edge.output_port_id
-        return activation_node, port_id
+    def do_sparsification(
+        self,
+        model: TModel,
+        graph: NNCFGraph,
+        target_sparsity_by_node: Dict[NNCFNode, float],
+        dataset: Dataset,
+    ):
+        model = self._backend_entity.insert_sparsifiers(
+            model, graph, target_sparsity_by_node)
+        model = self._backend_entity.calibrate_sparsifiers(model, dataset)
+        model = self._backend_entity.freeze_sparsifiers(model)
+        return model
 
     def apply(
         self,
@@ -115,23 +129,18 @@ class SparsifyActivationsAlgorithm:
         dataset: Dataset,
     ) -> TModel:
         self._set_backend_entity(model)
-        nodes_to_sparsify = self._get_nodes_to_sparsify(graph)
-        transformed_model, activation_sparsifiers = self._backend_entity.do_sparsification(
-            model, graph, nodes_to_sparsify
+        target_sparsity_by_node = self._get_target_sparsity_by_node(graph)
+        sparsified_model = self.do_sparsification(
+            model, graph, target_sparsity_by_node, dataset,
         )
-        for sparsifier in activation_sparsifiers:
-            sparsifier.reset_running_stats()
-        self._backend_entity.inference(transformed_model, dataset)
-        for sparsifier in activation_sparsifiers:
-            sparsifier.freeze(True)
-        transformed_model.nncf.rebuild_graph()
-        return transformed_model
+        return sparsified_model
 
 
 def sparsify_activations(
     model: TModel,
     dataset: Dataset,
-    sparsity_config: dict[str, float],
+    target_sparsity_by_scope: Dict[str, float],
+    ignored_scope: Optional[IgnoredScope] = None,
     debug_folder=None,
 ) -> TModel:
     """
@@ -147,7 +156,12 @@ def sparsify_activations(
             trace_parameters=True,
         )
 
-    algorithm = SparsifyActivationsAlgorithm(sparsity_config)
+    if ignored_scope is None:
+        ignored_scope = IgnoredScope()
+
+    algorithm = SparsifyActivationsAlgorithm(
+        target_sparsity_by_scope, ignored_scope)
+
     graph = NNCFGraphFactory.create(model)
     if debug_folder:
         graph.dump_graph(
--- nncf/experimental/torch/sparsify_activations/torch_backend.py
+++ nncf/experimental/torch/sparsify_activations/torch_backend.py
@@ -1,36 +1,20 @@
-from pathlib import Path
 from typing import List, Tuple, TypeVar
 
 import torch
 import torch.nn as nn
 
-import nncf
-import nncf.experimental.torch.sparsify_activations
-from nncf.common.factory import NNCFGraphFactory
 from nncf.common.graph.graph import NNCFGraph
 from nncf.common.graph.graph import NNCFNode
 from nncf.common.graph.operator_metatypes import OperatorMetatype
 from nncf.common.graph.transformations.commands import TargetType
 from nncf.common.graph.transformations.layout import TransformationLayout
-from nncf.common.tensor_statistics.statistic_point import StatisticPointsContainer
-from nncf.common.utils.backend import BackendType
-from nncf.common.utils.backend import get_backend
 from nncf.data import Dataset
-from nncf.experimental.tensor.tensor import Tensor
-from nncf.experimental.torch.sparsify_activations.sparsify_activations_impl import SparsifyActivationsAlgorithmBackend
-from nncf.quantization.algorithms.algorithm import Algorithm
+from nncf.experimental.torch.sparsify_activations.sparsify_activations_impl import SparsifyActivationsAlgoBackend
 from nncf.torch.graph import operator_metatypes as om
-from nncf.torch.graph.transformations.commands import PTInsertionCommand
 from nncf.torch.graph.transformations.commands import PTSharedFnInsertionCommand
 from nncf.torch.graph.transformations.commands import PTTargetPoint
-from nncf.torch.model_creation import is_wrapped_model
-from nncf.torch.model_creation import wrap_model
-from nncf.torch.model_graph_manager import find_const_node_in_constant_subgraph
-from nncf.torch.model_graph_manager import get_const_node
-from nncf.torch.model_graph_manager import get_module_by_name
-from nncf.torch.model_graph_manager import split_const_name
 from nncf.torch.model_transformer import PTModelTransformer
-from nncf.torch.utils import is_tracing_state
+from nncf.torch.nncf_network import NNCFNetwork
 
 TModel = TypeVar("TModel")
 
@@ -44,18 +28,17 @@ class ActivationSparsifier(nn.Module):
         self.register_buffer("num_batches_tracked", torch.tensor(0))
         self.running_threshold: torch.Tensor
         self.num_batches_tracked: torch.Tensor
-        self._frozen = False
+        self._frozen = True
 
     def forward(self, x):
         threshold = None
         if not self._frozen:
             threshold = self._calculate_quantile(x.abs(), self.target_sparsity)
             self._update(threshold)
-        assert self.num_batches_tracked > 0
         mask = torch.le(x.abs(), self.running_threshold)
-        if '[1]' in self.node_name and 'up_proj' in self.node_name:
-            print('sparsity', mask.float().mean(), 'cur_threshold',
-                  threshold, 'threshold', self.running_threshold)
+        # if '[1]' in self.node_name and 'up_proj' in self.node_name:
+        #     print('sparsity', mask.float().mean(), 'cur_threshold',
+        #           threshold, 'threshold', self.running_threshold)
         x = torch.masked_fill(x, mask, 0.)
         return x
 
@@ -67,10 +50,10 @@ class ActivationSparsifier(nn.Module):
         self._frozen = freeze
 
     def extra_repr(self) -> str:
-        return f"target_sparsity={self.target_sparsity.item()}"
+        return f"target_sparsity={self.target_sparsity.item()},{self.running_threshold:=},{self.num_batches_tracked}"
 
     def _calculate_quantile(self, x: torch.Tensor, target_sparsity: float):
-        return x.view(-1).quantile(q=target_sparsity, dim=-1)
+        return x.float().view(-1).quantile(q=target_sparsity, dim=-1)
 
     def _update(self, threshold: torch.Tensor):
         beta = 1.0 - self.alpha
@@ -84,26 +67,52 @@ class ActivationSparsifier(nn.Module):
         return self.running_threshold
 
 
-def node_name_matches_module_name(node_name, module_name):
-    parts = module_name.split('.')
-    return all(f'[{p}]' in node_name for p in parts)
-
-
-class PTPruneActivationAlgorithmBackend(SparsifyActivationsAlgorithmBackend):
+class PTSparsifyActivationsAlgoBackend(SparsifyActivationsAlgoBackend):
     SUPPORTED_METATYPES = [om.PTLinearMetatype]
-
-    def __init__(self) -> None:
-        pass
-
-    def do_sparsification(self):
-        pass
 
     @property
     def supported_metatypes(self) -> List[OperatorMetatype]:
-        return PTPruneActivationAlgorithmBackend.SUPPORTED_METATYPES
+        return PTSparsifyActivationsAlgoBackend.SUPPORTED_METATYPES
 
-    @staticmethod
-    def get_activation_port_id(node: NNCFNode, graph: NNCFGraph) -> NNCFNode:
+    def insert_sparsifiers(self, model, graph: NNCFGraph, target_sparsity_by_node: dict[NNCFNode, float]) -> NNCFNetwork:
+        transformation_layout = TransformationLayout()
+        for node, target_sparsity in target_sparsity_by_node.items():
+            act_node, act_port_id = self._get_activation_node_and_port(
+                node, graph)
+            sparsifier = ActivationSparsifier(target_sparsity=target_sparsity)
+            sparsifier_name = f"activation_sparsifier_{node.node_name.replace('.', '_')}"
+            transformation_layout.register(PTSharedFnInsertionCommand(
+                [
+                    PTTargetPoint(
+                        TargetType.PRE_LAYER_OPERATION,
+                        target_node_name=node.node_name,
+                        input_port_id=act_port_id)
+                ],
+                sparsifier,
+                sparsifier_name,
+            ))
+
+        transformed_model = PTModelTransformer(
+            model).transform(transformation_layout)
+        return transformed_model
+
+    def get_sparsifiers(self, model: NNCFNetwork) -> List[ActivationSparsifier]:
+        return [m for m in model.nncf.modules() if isinstance(m, ActivationSparsifier)]
+
+    def calibrate_sparsifiers(self, model: NNCFNetwork, dataset: Dataset) -> NNCFNetwork:
+        for sparsifier in self.get_sparsifiers(model):
+            sparsifier.reset_running_stats()
+            sparsifier.freeze(False)
+        self.do_inference(model, dataset)
+        return model
+
+    def freeze_sparsifiers(self, model: NNCFNetwork) -> NNCFNetwork:
+        for sparsifier in self.get_sparsifiers(model):
+            sparsifier.freeze(True)
+        model.nncf.rebuild_graph()
+        return model
+
+    def _get_activation_port_id(self, node: NNCFNode, graph: NNCFGraph) -> NNCFNode:
         activation_ports = []
         for prev_node in graph.get_previous_nodes(node):
             if 'weight' in prev_node.node_name.lower() or 'bias' in prev_node.node_name:
@@ -114,43 +123,16 @@ class PTPruneActivationAlgorithmBackend(SparsifyActivationsAlgorithmBackend):
         assert len(activation_ports) == 1
         return activation_ports[0]
 
-    @staticmethod
-    def inference(model: nn.Module, dataset: Dataset) -> None:
-        model = model.eval()
-        with torch.no_grad():
-            for batch in dataset.get_inference_data():
-                model(**batch)
+    def _get_activation_node_and_port(self, node: NNCFNode, nncf_graph: NNCFGraph) -> Tuple[NNCFNode, int]:
+        """
+        This method returns the activation layer and corresponding port id for the node.
 
-    def do_sparsification(self, model, graph: NNCFGraph, nodes_to_sparsify: List[NNCFNode]):
-        transformation_layout = TransformationLayout()
-        activation_sparsifiers = []
-
-        for node in nodes_to_sparsify:
-            activation_node, activation_port_id = self._get_activation_node_and_port(
-                node, graph)
-            target_sparsity = None
-            for module_name in self.sparse_config:
-                if node_name_matches_module_name(node.node_name, module_name):
-                    target_sparsity = self.sparse_config[module_name]
-                    break
-            if target_sparsity is None:
-                continue
-            activation_sparsifier = ActivationSparsifier(
-                target_sparsity=target_sparsity)
-            activation_sparsifier.node_name = node.node_name
-            activation_sparsifiers.append(activation_sparsifier)
-            activation_sparsifier_name = f"activation_sparsifier_{node.node_name.replace('.', '_')}"
-            transformation_layout.register(PTSharedFnInsertionCommand(
-                [
-                    PTTargetPoint(
-                        TargetType.PRE_LAYER_OPERATION,
-                        target_node_name=node.node_name,
-                        input_port_id=activation_port_id)
-                ],
-                activation_sparsifier,
-                activation_sparsifier_name,
-            ))
-
-        transformed_model = PTModelTransformer(
-            model).transform(transformation_layout)
-        return transformed_model, activation_sparsifiers
+        :param node: NNCFGraph node for which the activation is sought.
+        :param nncf_graph: NNCFGraph instance with the node.
+        :return: Tuple with the activation node and port id.
+        """
+        activation_port = self._get_activation_port_id(node, nncf_graph)
+        activation_edge = nncf_graph.get_input_edges(node)[activation_port]
+        activation_node = activation_edge.from_node
+        port_id = activation_edge.output_port_id
+        return activation_node, port_id
