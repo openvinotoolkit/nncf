1a706e38c8819070177212d5105773f25e94728b
Riffat Khan <83776178+rk119@users.noreply.github.com>
2024-07-30 21:21:57 +0400

    Import fix

--- tests/torch/fx/test_statistics_aggregator.py
+++ tests/torch/fx/test_statistics_aggregator.py
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Callable, List, Type
+from typing import List, Type
 
 import numpy as np
 import pytest
