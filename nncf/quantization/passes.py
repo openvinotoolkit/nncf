687e690d202323ddd12024625abc240e59518122
Andrey Churkin <andrey.churkin@intel.com>
2024-08-16 14:06:52 +0100

    Add transform_graph pass

--- nncf/quantization/passes.py
+++ nncf/quantization/passes.py
@@ -10,7 +10,8 @@
 # limitations under the License.
 
 import collections
-from typing import List, TypeVar
+from enum import Enum
+from typing import Dict, List, TypeVar
 
 from nncf.common.graph.graph import NNCFGraph
 from nncf.common.graph.graph import NNCFNode
@@ -18,6 +19,151 @@ from nncf.common.graph.layer_attributes import Dtype
 from nncf.common.graph.operator_metatypes import OperatorMetatype
 
 TModel = TypeVar("TModel")
+
+
+class NodeMarker(Enum):
+    """
+    Markers are used to classify graph nodes.
+
+    - Node marker `DEPENDS_ON_INPUTS` means that the node depends on input data (nodes that are
+      designated as graph inputs).
+    - Node marker `CONSTANT_SUBGRAPH` means that the node is part of a constant subgraph, i.e.,
+      its output tensor does not depend on any input data.
+    """
+
+    DEPENDS_ON_INPUT = "DEPENDS_ON_INPUT"
+    CONSTANT_SUBGRAPH = "CONSTANT_SUBGRAPH"
+
+
+def classify_nodes(
+    graph: NNCFGraph,
+    start_nodes: List[NNCFNode],
+    constant_metatypes: List[OperatorMetatype],
+) -> Dict[NNCFNode, NodeMarker]:
+    """
+    Assigns a marker to each node in the input graph.
+
+    :param graph: The input graph to be analyzed.
+    :param start_nodes: A list of nodes designated as graph inputs. These nodes are used to identify
+        which nodes depend on input data.
+    :param constant_metatypes: A list of metatypes representing backend-specific constant operations.
+    :return: A dictionary mapping each node to its assigned marker.
+    """
+    # NOTE: All nodes in the graph that have no input edges should either be
+    # in `start_nodes` or have a constant metatype.
+
+    # TODO(andrey-churkin): It seems possible to identify a ShapeOf subgraph using
+    # this approach. Suppose we have a marker `SHAPEOF_SUBGRAPH`. If a node has this marker,
+    # it means that the node is part of a ShapeOf subgraph. Naturally, all nodes with the
+    # ShapeOf metatype have this marker. Next, for each node, if all input edges have integer type
+    # and all producers are marked using `SHAPEOF_SUBGRAPH` marker, then we assign the `SHAPEOF_SUBGRAPH`
+    # marker to the node.
+
+    sorted_nodes = graph.topological_sort()
+
+    node_markers: Dict[NNCFNode, NodeMarker] = {}
+    for node in sorted_nodes:
+        producer_nodes = graph.get_previous_nodes(node)
+        if producer_nodes:
+            # NOTE: If there are producer nodes, the `node` marker is determined using the markers
+            # of the producer nodes according to the following rule: If all producer nodes
+            # have the `CONSTANT_SUBGRAPH` marker, then the marker for `node` is also `CONSTANT_SUBGRAPH`.
+            # Otherwise, the marker for node is `DEPENDS_ON_INPUT`.
+
+            # NOTE: It is guaranteed that all producers already have their markers because the nodes
+            # are considered in topological order.
+            node_markers[node] = (
+                NodeMarker.CONSTANT_SUBGRAPH
+                if all(node_markers[v] == NodeMarker.CONSTANT_SUBGRAPH for v in producer_nodes)
+                else NodeMarker.DEPENDS_ON_INPUT
+            )
+        elif node in start_nodes:
+            node_markers[node] = NodeMarker.DEPENDS_ON_INPUT
+        elif node.metatype in constant_metatypes:
+            node_markers[node] = NodeMarker.CONSTANT_SUBGRAPH
+        else:
+            raise ValueError(
+                "The input graph contains a node {node} that does not have input edges but is not classified "
+                " as a constant node or a start node."
+            )
+
+    return node_markers
+
+
+def remove_constant_subgraphs(
+    graph: NNCFGraph,
+    initial_node_markers: Dict[NNCFNode, NodeMarker],
+    current_node_markers: Dict[NNCFNode, NodeMarker],
+) -> NNCFGraph:
+    """
+    :param graph:
+    :param initial_node_markers:
+    :param current_node_markers:
+    :return:
+    """
+    constant_subgraph_nodes = set()
+
+    node_to_input_port_ids: Dict[NNCFNode, List[int]] = {}
+    for node in graph.get_all_nodes():
+        # Collect all nodes with `CONSTANT_SUBGRAPH` marker
+        if current_node_markers[node] == NodeMarker.CONSTANT_SUBGRAPH:
+            constant_subgraph_nodes.add(node)
+
+        for e in graph.get_input_edges(node):
+            initial_marker = initial_node_markers[e.from_node]
+            current_marker = current_node_markers[e.from_node]
+            if initial_marker == NodeMarker.DEPENDS_ON_INPUT and current_marker == NodeMarker.CONSTANT_SUBGRAPH:
+                node_to_input_port_ids.setdefault(node, []).append(e.input_port_id)
+
+    preserved_nodes = set(node_to_input_port_ids)
+    queue = collections.deque(node_to_input_port_ids)
+    while queue:
+        node = queue.pop()
+        port_ids = node_to_input_port_ids.get(node)
+        if port_ids:
+            producers = [graph.get_input_edge_by_port_id(node, port_id).from_node for port_id in port_ids]
+        else:
+            producers = graph.get_previous_nodes(node)
+
+        for v in producers:
+            if v not in preserved_nodes:
+                queue.append(v)
+                preserved_nodes.add(v)
+
+    constant_subgraph_nodes = constant_subgraph_nodes.difference(preserved_nodes)
+    graph.remove_nodes_from(constant_subgraph_nodes)
+
+    return graph
+
+
+def transform_graph(
+    graph: NNCFGraph,
+    start_nodes: List[NNCFNode],
+    shapeof_metatypes: List[OperatorMetatype],
+    dropout_metatypes: List[OperatorMetatype],
+    constant_metatypes: List[OperatorMetatype],
+) -> NNCFGraph:
+    """
+    Applies the following ordered list of transformations to the provided graph:
+        1. Removes all shapeof subgraphs.
+        2. Removes constant subgraphs while preserving some of them.
+        3. Removes all dropout operations and connects their producers with consumers.
+           It assumes that each dropout node has only one producer and one consumer.
+
+    :param graph: The input graph to be transformed.
+    :param start_nodes: A list of nodes designated as graph inputs. These nodes are used to identify
+        which nodes depend on input data.
+    :param shapeof_metatypes: A list of metatypes representing backend-specific shapeof operations.
+    :param dropout_metatypes: A list of metatypes representing backend-specific dropout operations.
+    :param constant_metatypes: A list of metatypes representing backend-specific constant operations.
+    :return: A transformed graph.
+    """
+    initial_node_markers = classify_nodes(graph, start_nodes, constant_metatypes)
+    remove_shapeof_subgraphs(graph, shapeof_metatypes, start_nodes)
+    current_node_markers = classify_nodes(graph, start_nodes, constant_metatypes)
+    remove_constant_subgraphs(graph, initial_node_markers, current_node_markers)
+    remove_nodes_and_reconnect_graph(graph, dropout_metatypes)
+    return graph
 
 
 def transform_to_inference_graph(
