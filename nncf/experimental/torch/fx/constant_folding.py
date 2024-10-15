2539929a2774868549c5e026748da858dcbc302a
dlyakhov <daniil.lyakhov@intel.com>
2024-10-15 13:47:02 +0200

    Comments

--- nncf/experimental/torch/fx/constant_folding.py
+++ nncf/experimental/torch/fx/constant_folding.py
@@ -240,15 +240,17 @@ class ConstantFolder(torch.fx.Interpreter):
 
 def constant_fold(
     gm: torch.fx.GraphModule,
-    constraint_fn: Optional[Callable[[torch.fx.Node], bool]] = None,
 ) -> None:
+    """
+    Calcualtes constant subgraphs values and replaces them with a constan node inplace.
+
+    :param gm: Given graph model.
+    """
     with torch.utils._python_dispatch._disable_current_modes():
         cf = ConstantFolder(gm, skip_constructors=True)
         cf.run()
 
         for node, constant in cf.node_replacements.items():
-            if constraint_fn is not None and not constraint_fn(node):
-                continue
             replace_node_with_constant(gm, node, constant)
 
         erased_params = []
