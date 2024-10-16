504102f7cd635bafeabffdabd3e84e9266d84c24
Aleksei Kashapov <aleksei.kashapov@intel.com>
2024-10-16 21:28:28 +0200

    fix fx

--- nncf/experimental/torch/fx/statistics/aggregator.py
+++ nncf/experimental/torch/fx/statistics/aggregator.py
@@ -21,6 +21,7 @@ from nncf.common.graph.transformations.layout import TransformationLayout
 from nncf.common.tensor_statistics.aggregator import StatisticPointsContainer
 from nncf.common.tensor_statistics.aggregator import StatisticsAggregator
 from nncf.experimental.common.tensor_statistics.collectors import TensorCollector
+from nncf.experimental.common.tensor_statistics.statistics import TensorStatistic
 from nncf.experimental.torch.fx.commands import FXApplyTransformationCommand
 from nncf.experimental.torch.fx.transformations import leaf_module_insertion_transformation_builder
 from nncf.tensor import Tensor
@@ -121,3 +122,14 @@ class FXStatisticsAggregator(StatisticsAggregator):
     @staticmethod
     def _process_outputs(outputs: Dict[str, np.ndarray]) -> Dict[str, Tensor]:
         return outputs
+
+    def _get_statistics_key(self, statistics: TensorStatistic, target_point: PTTargetPoint) -> str:
+        """
+        Returns key of statistics.
+
+        :param statistics: Statistics value.
+        :param target_point: Statistics target point.
+        :return: Statistics key.
+        """
+        target_point_id = f"{target_point.target_node_name}_{target_point.type}_{target_point.input_port_id}"
+        return f"{statistics.__class__.__name__}_{target_point_id}"
