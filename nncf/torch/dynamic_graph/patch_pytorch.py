7e4b771ad930cd194c8cadfd3e1569b296c4983a
Aamir Nazir <aamir.nazir@intel.com>
2024-10-30 12:08:29 +0400

    minor fix of import

--- nncf/torch/dynamic_graph/patch_pytorch.py
+++ nncf/torch/dynamic_graph/patch_pytorch.py
@@ -12,8 +12,8 @@
 import functools
 import inspect
 from contextlib import contextmanager
+from contextlib import suppress
 from typing import Callable, List, Union
-import sys
 
 import torch
 import torch.utils.cpp_extension
@@ -306,16 +306,16 @@ def patch_torch_jit():
 
 
 def patch_namespace_opname(namespace, op_info: PatchedOperatorInfo):
-    # This is required since torchvision changes a dictionary inside of pytorch mapping  
+    # This is required since torchvision changes a dictionary inside of pytorch mapping
     # different ops and their role in torch fx graph. Once the nncf mapping is done, it is
-    # represented as a different custom operation which is how it is changed in 
-    # the said mapping. refer to `unwrap_with_attr_name_if_wrapper()` in 
+    # represented as a different custom operation which is how it is changed in
+    # the said mapping. refer to `unwrap_with_attr_name_if_wrapper()` in
     # torch/_dynamo/utils.py and torch/_dynamo/tracer_rules.py `lookup_inner()`
     # function where this dictionary is referenced.
-    try:
+
+    with suppress(ModuleNotFoundError):
         import torchvision  # noqa
-    except:
-        pass
+
     op_name = op_info.name
     if hasattr(namespace, op_name):
         orig = getattr(namespace, op_name)
