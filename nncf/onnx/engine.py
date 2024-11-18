35aff71c9aa2d629ff5404b29aca6a219b6b9a8d
Aleksei Kashapov <aleksei.kashapov@intel.com>
2024-11-18 11:59:31 +0100

    add comment

--- nncf/onnx/engine.py
+++ nncf/onnx/engine.py
@@ -38,7 +38,13 @@ class ONNXEngine(Engine):
         :param input_data: inputs for the model
         :return output_data: models outputs
         """
-        output_tensors = self.sess.run([], {k: v for k, v in input_data.items() if k in self.input_names})
+        output_tensors = self.sess.run([], input_data)
         model_outputs = self.sess.get_outputs()
 
-        return {output.name: np.copy(tensor) for tensor, output in zip(output_tensors, model_outputs)}
+        outputs_safe = {}
+        for tensor, output in zip(output_tensors, model_outputs):
+            # Some outputs overlap with inputs and may share memory
+            # To avoid modifications to the input tensors, make a copy
+            outputs_safe[output.name] = tensor.copy() if output.name in self.input_names else tensor
+
+        return outputs_safe
