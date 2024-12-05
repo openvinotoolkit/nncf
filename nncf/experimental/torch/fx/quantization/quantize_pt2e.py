869bc8fd121cfb4ade900c23705ed90c7e8892a1
dlyakhov <daniil.lyakhov@intel.com>
2024-12-05 16:44:13 +0100

    batchwise_statistics

--- nncf/experimental/torch/fx/quantization/quantize_pt2e.py
+++ nncf/experimental/torch/fx/quantization/quantize_pt2e.py
@@ -22,6 +22,7 @@ from torch.ao.quantization.quantizer import Quantizer
 from torch.fx import GraphModule
 from torch.fx.passes.infra.pass_manager import PassManager
 
+import nncf
 from nncf.common.factory import NNCFGraphFactory
 from nncf.common.logging import nncf_logger
 from nncf.data import Dataset
@@ -43,22 +44,49 @@ def quantize_pt2e(
     quantizer: Quantizer,
     calibration_dataset: Dataset,
     subset_size: int = 300,
-    fast_bias_correction: bool = True,
+    fast_bias_correction: Optional[bool] = True,
     smooth_quant: bool = False,
     bias_correction_params: Optional[AdvancedBiasCorrectionParameters] = None,
     smooth_quant_params: Optional[AdvancedSmoothQuantParameters] = None,
     activations_range_estimator_params: Optional[RangeEstimatorParameters] = None,
     weights_range_estimator_params: Optional[RangeEstimatorParameters] = None,
-    fold_quantize: Optional[bool] = False,
+    batchwise_statistics: bool = False,
+    fold_quantize: bool = False,
 ) -> torch.fx.GraphModule:
     """
-    Implementation of the `quantize()` method for the Torch FX backend.
+    Applies post-training quantization to the torch.fx.GraphModule provided model
+    using provided torch.ao quantizer.
+
+    :param quantizer: Torch ao quantizer to annotate nodes in the graph with quantization setups
+        to convey the desired way of quantization.
+    :param subset_size: Size of a subset to calculate activations
+        statistics used for quantization.
+    :param fast_bias_correction: Setting this option to `False` enables a different
+        bias correction method which is more accurate, in general, and takes
+        more time but requires less memory. None disables the bias correction algorithm.
+    :param smooth_quant: Setting this option to `True` enables the SmoothQuant algorithm.
+    :param bias_correction_params: Contains advanced parameters for fine-tuning bias correction algorithm.
+    :param smooth_quant_params: Contains advanced alpha parameters for SmoothQuant algorithm.
+    :param activations_range_estimator_params: Contains parameters for estimating the range
+        of activations of the model.
+    :param weights_range_estimator_params: Contains parameters for estimating the range
+        of weights of the model.
+    :param batchwise_statistics: Determines whether quantizer statistics should be calculated
+        for each item of the batch or for the entire batch, default is False.
+    :param fold_quantize: Boolean flag for whether fold the quantize op or not.
     """
     nncf_logger.warning(
         "Experimental Torch FX quantization backend is being used for the given torch.fx.GraphModule model."
         " Torch FX PTQ is an experimental feature, consider using Torch or OpenVino PTQ backends"
         " in case of errors or a poor model performance."
     )
+
+    if subset_size < 1:
+        raise nncf.ValidationError("Subset size must be positive.")
+
+    batch_size = calibration_dataset.get_batch_size()
+    if batch_size is not None and batch_size > 1 and batchwise_statistics is not None:
+        batchwise_statistics = True
 
     original_graph_meta = model.meta
 
@@ -83,6 +111,7 @@ def quantize_pt2e(
         smooth_quant_params=smooth_quant_params,
         activations_range_estimator_params=activations_range_estimator_params,
         weights_range_estimator_params=weights_range_estimator_params,
+        batchwise_statistics=batchwise_statistics,
     )
 
     nncf_graph = NNCFGraphFactory.create(copied_model)
