20147abcbacad487969f24c6ac0ee139b26bffb8
dlyakhov <daniil.lyakhov@intel.com>
2024-12-23 22:49:32 +0100

    Comments

--- nncf/experimental/quantization/algorithms/post_training/algorithm.py
+++ nncf/experimental/quantization/algorithms/post_training/algorithm.py
@@ -16,8 +16,8 @@ from nncf import Dataset
 from nncf.common.graph.graph import NNCFGraph
 from nncf.common.tensor_statistics.statistic_point import StatisticPointsContainer
 from nncf.common.utils.backend import BackendType
-from nncf.experimental.common.quantization.algorithms.post_training.pipeline import experimental_create_ptq_pipeline
-from nncf.experimental.common.quantization.algorithms.quantizer.base_quantizer import NNCFQuantizer
+from nncf.experimental.quantization.algorithms.post_training.pipeline import experimental_create_ptq_pipeline
+from nncf.experimental.quantization.algorithms.quantizer.base_quantizer import Quantizer as NNCFQuantizer
 from nncf.quantization.advanced_parameters import AdvancedBiasCorrectionParameters
 from nncf.quantization.advanced_parameters import AdvancedSmoothQuantParameters
 from nncf.quantization.advanced_parameters import RangeEstimatorParameters
