7d811dfea1c2d942c255b99239532bba2a3c3a32
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2025-01-07 16:07:43 +0200

    NotImplementedError

--- nncf/tensorflow/sparsity/rb/algorithm.py
+++ nncf/tensorflow/sparsity/rb/algorithm.py
@@ -193,4 +193,4 @@ class RBSparsityController(BaseSparsityController):
         self._scheduler = StubCompressionScheduler()
 
     def compression_stage(self) -> CompressionStage:
-        return None
+        raise NotImplementedError
