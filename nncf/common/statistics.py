d0ab1d92671a4332ab43056aa4da9e41794ee0ca
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2025-01-11 00:33:33 +0200

    statistics

--- nncf/common/statistics.py
+++ nncf/common/statistics.py
@@ -9,7 +9,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional
+from dataclasses import dataclass
+from dataclasses import fields
+from typing import Iterator, Optional, Tuple
 
 from nncf.api.statistics import Statistics
 from nncf.common.pruning.statistics import FilterPruningStatistics
@@ -22,116 +24,45 @@ from nncf.common.utils.api_marker import api
 
 
 @api()
-class NNCFStatistics(Statistics):
+@dataclass
+class NNCFStatistics:
     """
     Groups statistics for all available NNCF compression algorithms.
     Statistics are present only if the algorithm has been started.
     """
 
-    def __init__(self):
-        """
-        Initializes nncf statistics.
-        """
-        self._storage = {}
+    const_sparsity: Optional[ConstSparsityStatistics] = None
+    filter_pruning: Optional[FilterPruningStatistics] = None
+    magnitude_sparsity: Optional[MagnitudeSparsityStatistics] = None
+    movement_sparsity: Optional[MovementSparsityStatistics] = None
+    quantization: Optional[QuantizationStatistics] = None
+    rb_sparsity: Optional[RBSparsityStatistics] = None
 
-    @property
-    def magnitude_sparsity(self) -> Optional[MagnitudeSparsityStatistics]:
-        """
-        Returns statistics of the magnitude sparsity algorithm. If statistics
-        have not been collected, `None` will be returned.
-
-        :return: Instance of the `MagnitudeSparsityStatistics` class.
-        """
-        return self._storage.get("magnitude_sparsity")
-
-    @property
-    def rb_sparsity(self) -> Optional[RBSparsityStatistics]:
-        """
-        Returns statistics of the RB-sparsity algorithm. If statistics
-        have not been collected, `None` will be returned.
-
-        :return: Instance of the `RBSparsityStatistics` class.
-        """
-        return self._storage.get("rb_sparsity")
-
-    @property
-    def movement_sparsity(self) -> Optional[MovementSparsityStatistics]:
-        """
-        Returns statistics of the movement sparsity algorithm. If statistics
-        have not been collected, `None` will be returned.
-
-        :return: Instance of the `MovementSparsityStatistics` class.
-        """
-        return self._storage.get("movement_sparsity")
-
-    @property
-    def const_sparsity(self) -> Optional[ConstSparsityStatistics]:
-        """
-        Returns statistics of the const sparsity algorithm. If statistics
-        have not been collected, `None` will be returned.
-
-        :return: Instance of the `ConstSparsityStatistics` class.
-        """
-        return self._storage.get("const_sparsity")
-
-    @property
-    def quantization(self) -> Optional[QuantizationStatistics]:
-        """
-        Returns statistics of the quantization algorithm. If statistics
-        have not been collected, `None` will be returned.
-
-        :return: Instance of the `QuantizationStatistics` class.
-        """
-        return self._storage.get("quantization")
-
-    @property
-    def filter_pruning(self) -> Optional[FilterPruningStatistics]:
-        """
-        Returns statistics of the filter pruning algorithm. If statistics
-        have not been collected, `None` will be returned.
-
-        :return: Instance of the `FilterPruningStatistics` class.
-        """
-        return self._storage.get("filter_pruning")
-
-    def register(self, algorithm_name: str, stats: Statistics):
+    def register(self, algorithm_name: str, stats: Statistics) -> None:
         """
         Registers statistics for the algorithm.
 
         :param algorithm_name: Name of the algorithm. Should be one of the following
-            * magnitude_sparsity
-            * rb_sparsity
             * const_sparsity
-            * quantization
             * filter_pruning
+            * magnitude_sparsity
+            * movement_sparsity
+            * quantization
+            * rb_sparsity
 
         :param stats: Statistics of the algorithm.
         """
-
-        available_algorithms = [
-            "magnitude_sparsity",
-            "rb_sparsity",
-            "movement_sparsity",
-            "const_sparsity",
-            "quantization",
-            "filter_pruning",
-        ]
+        available_algorithms = [f.name for f in fields(self)]
         if algorithm_name not in available_algorithms:
             raise ValueError(
                 f"Can not register statistics for the algorithm. Unknown name of the algorithm: {algorithm_name}."
             )
 
-        self._storage[algorithm_name] = stats
+        setattr(self, algorithm_name, stats)
 
     def to_str(self) -> str:
-        """
-        Calls `to_str()` method for all registered statistics of the algorithm and returns
-        a sum-up string.
-
-        :return: A representation of the NNCF statistics as a human-readable string.
-        """
-        pretty_string = "\n\n".join([stats.to_str() for stats in self._storage.values()])
+        pretty_string = "\n\n".join([str(x[1].to_str()) for x in self])
         return pretty_string
 
-    def __iter__(self):
-        return iter(self._storage.items())
+    def __iter__(self) -> Iterator[Tuple[str, Statistics]]:
+        return iter([(f.name, getattr(self, f.name)) for f in fields(self) if getattr(self, f.name) is not None])
