6bb18d80199f1a0a5647a6b949ff59ba1284efa8
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2025-01-17 18:40:32 +0200

    if

--- tests/torch/test_pytorch_patch.py
+++ tests/torch/test_pytorch_patch.py
@@ -114,7 +114,7 @@ def test_jit_script_exception_preserves_patching():
     run_pytest_case_function_in_separate_process(test_jit_script_exception_preserves_patching_isolated)
 
 
-@pytest.mark.skip(is_windows(), reason="https://github.com/pytorch/pytorch/issues/122094")
+@pytest.mark.skipif(is_windows(), reason="https://github.com/pytorch/pytorch/issues/122094")
 @pytest.mark.parametrize("compile_forward", [False, True])
 def test_torch_compile(compile_forward):
     # Run test case in a separate process to track patching of torch by NNCF
