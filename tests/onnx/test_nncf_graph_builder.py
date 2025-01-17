5ee716d85c3c92d5e7d25dd30c0f9879e616e91c
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2025-01-17 17:29:06 +0200

    build models in test

--- tests/onnx/test_nncf_graph_builder.py
+++ tests/onnx/test_nncf_graph_builder.py
@@ -41,56 +41,51 @@ def test_compare_nncf_graph_synthetic_models(model_cls_to_test):
     compare_nx_graph_with_reference(nx_graph, path_to_dot, check_edge_attrs=True)
 
 
+def model_builder(model_name):
+    if model_name == "resnet18":
+        return models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
+    if model_name == "mobilenet_v2":
+        return models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1)
+    if model_name == "mobilenet_v3_small":
+        return models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.IMAGENET1K_V1)
+    if model_name == "inception_v3":
+        return models.inception_v3(weights=models.Inception_V3_Weights.IMAGENET1K_V1)
+    if model_name == "googlenet":
+        return models.googlenet(weights=models.GoogLeNet_Weights.IMAGENET1K_V1)
+    if model_name == "vgg16":
+        return models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
+    if model_name == "shufflenet_v2_x1_0":
+        return models.shufflenet_v2_x1_0(weights=models.ShuffleNet_V2_X1_0_Weights.IMAGENET1K_V1)
+    if model_name == "squeezenet1_0":
+        return models.squeezenet1_0(weights=models.SqueezeNet1_0_Weights.IMAGENET1K_V1)
+    if model_name == "densenet121":
+        return models.densenet121(weights=models.DenseNet121_Weights.IMAGENET1K_V1)
+    if model_name == "mnasnet0_5":
+        return models.mnasnet0_5(weights=models.MNASNet0_5_Weights.IMAGENET1K_V1)
+    raise ValueError(f"Unknown model name {model_name}")
+
+
 CLASSIFICATION_MODEL_DEF_AND_OBJ = [
-    (
-        ModelToTest("resnet18", [1, 3, 224, 224]),
-        models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1),
-    ),
-    (
-        ModelToTest("mobilenet_v2", [1, 3, 224, 224]),
-        models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1),
-    ),
-    (
-        ModelToTest("mobilenet_v3_small", [1, 3, 224, 224]),
-        models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.IMAGENET1K_V1),
-    ),
-    (
-        ModelToTest("inception_v3", [1, 3, 224, 224]),
-        models.inception_v3(weights=models.Inception_V3_Weights.IMAGENET1K_V1),
-    ),
-    (
-        ModelToTest("googlenet", [1, 3, 224, 224]),
-        models.googlenet(weights=models.GoogLeNet_Weights.IMAGENET1K_V1),
-    ),
-    (
-        ModelToTest("vgg16", [1, 3, 224, 224]),
-        models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1),
-    ),
-    (
-        ModelToTest("shufflenet_v2_x1_0", [1, 3, 224, 224]),
-        models.shufflenet_v2_x1_0(weights=models.ShuffleNet_V2_X1_0_Weights.IMAGENET1K_V1),
-    ),
-    (
-        ModelToTest("squeezenet1_0", [1, 3, 224, 224]),
-        models.squeezenet1_0(weights=models.SqueezeNet1_0_Weights.IMAGENET1K_V1),
-    ),
-    (
-        ModelToTest("densenet121", [1, 3, 224, 224]),
-        models.densenet121(weights=models.DenseNet121_Weights.IMAGENET1K_V1),
-    ),
-    (
-        ModelToTest("mnasnet0_5", [1, 3, 224, 224]),
-        models.mnasnet0_5(weights=models.MNASNet0_5_Weights.IMAGENET1K_V1),
-    ),
+    ModelToTest("resnet18", [1, 3, 224, 224]),
+    ModelToTest("mobilenet_v2", [1, 3, 224, 224]),
+    ModelToTest("mobilenet_v3_small", [1, 3, 224, 224]),
+    ModelToTest("inception_v3", [1, 3, 224, 224]),
+    ModelToTest("googlenet", [1, 3, 224, 224]),
+    ModelToTest("vgg16", [1, 3, 224, 224]),
+    ModelToTest("shufflenet_v2_x1_0", [1, 3, 224, 224]),
+    ModelToTest("squeezenet1_0", [1, 3, 224, 224]),
+    ModelToTest("densenet121", [1, 3, 224, 224]),
+    ModelToTest("mnasnet0_5", [1, 3, 224, 224]),
 ]
 
 
 @pytest.mark.parametrize(
-    ("model_to_test", "model"),
+    ("model_to_test"),
     CLASSIFICATION_MODEL_DEF_AND_OBJ,
-    ids=[x.model_name for x, _ in CLASSIFICATION_MODEL_DEF_AND_OBJ],
+    ids=[x.model_name for x in CLASSIFICATION_MODEL_DEF_AND_OBJ],
 )
-def test_compare_nncf_graph_classification_real_models(tmp_path, model_to_test, model):
+def test_compare_nncf_graph_classification_real_models(tmp_path, model_to_test):
+    model = model_builder(model_to_test.model_name)
     onnx_model_path = tmp_path / (model_to_test.model_name + ".onnx")
     x = torch.randn(model_to_test.input_shape, requires_grad=False)
     torch.onnx.export(model, x, onnx_model_path, opset_version=13)
@@ -115,7 +110,7 @@ DETECTION_MODELS = [
 
 
 @pytest.mark.parametrize(("model_to_test"), DETECTION_MODELS, ids=[x.model_name for x in DETECTION_MODELS])
-def test_compare_nncf_graph_detection_real_models(tmp_path, model_to_test):
+def test_compare_nncf_graph_detection_real_models(model_to_test):
     onnx_model_dir = TEST_ROOT / "onnx" / "data" / "models"
     onnx_model_path = onnx_model_dir / (model_to_test.model_name + ".onnx")
     if not os.path.isdir(onnx_model_dir):
--- tests/onnx/test_weightless_model.py
+++ tests/onnx/test_weightless_model.py
@@ -22,11 +22,10 @@ from tests.onnx.weightless_model import save_model_without_tensors
 
 @pytest.mark.parametrize(
     ("model_to_test", "model"),
-    [
-        (ModelToTest("resnet18", [1, 3, 224, 224]), models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)),
-    ],
+    [ModelToTest("resnet18", [1, 3, 224, 224])],
 )
-def test_save_weightless_model(tmp_path, model_to_test, model):
+def test_save_weightless_model(tmp_path, model_to_test):
+    model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
     onnx_model_path = tmp_path / (model_to_test.model_name + ".onnx")
     x = torch.randn([1, 3, 224, 224], requires_grad=False)
     torch.onnx.export(model, x, onnx_model_path)
