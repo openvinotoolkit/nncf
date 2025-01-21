026a0ed8ff57efefc94f162d5d8d56c9162445fc
Aleksei Kashapov <aleksei.kashapov@intel.com>
2025-01-21 14:07:30 +0100

    add tinyllama_scale_estimation_group_size_64

--- tests/post_training/model_scope.py
+++ tests/post_training/model_scope.py
@@ -531,6 +531,21 @@ WEIGHT_COMPRESSION_MODELS = [
         },
         "backends": [BackendType.OV],
     },
+    {
+        "reported_name": "tinyllama_scale_estimation_group_size_64",
+        "model_id": "tinyllama/tinyllama-1.1b-step-50k-105b",
+        "pipeline_cls": LMWeightCompression,
+        "compression_params": {
+            "group_size": 64,
+            "ratio": 0.8,
+            "mode": CompressWeightsMode.INT4_SYM,
+            "scale_estimation": True,
+            "advanced_parameters": AdvancedCompressionParameters(
+                scale_estimation_params=AdvancedScaleEstimationParameters(32, 5, 10, 1.0)
+            ),
+        },
+        "backends": [BackendType.OV, BackendType.TORCH],
+    },
 ]
 
 
