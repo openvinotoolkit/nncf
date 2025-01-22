b2f516544d7ad6207729dcd247d6abfcf7c21de8
dlyakhov <daniil.lyakhov@intel.com>
2025-01-22 20:04:07 +0100

    OpenVINOQuantizer: SharedQuantizationSpec support

--- nncf/experimental/quantization/quantizers/openvino_quantizer.py
+++ nncf/experimental/quantization/quantizers/openvino_quantizer.py
@@ -10,14 +10,17 @@
 # limitations under the License.
 
 from collections import defaultdict
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import torch.fx
 from torch.ao.quantization.observer import HistogramObserver
 from torch.ao.quantization.observer import PerChannelMinMaxObserver
+from torch.ao.quantization.quantizer.quantizer import EdgeOrNode
 from torch.ao.quantization.quantizer.quantizer import QuantizationAnnotation as TorchAOQuantizationAnnotation
 from torch.ao.quantization.quantizer.quantizer import QuantizationSpec as TorchAOQuantizationSpec
+from torch.ao.quantization.quantizer.quantizer import QuantizationSpecBase as TorchAOQuantizationSpecBase
 from torch.ao.quantization.quantizer.quantizer import Quantizer as TorchAOQuantizer
+from torch.ao.quantization.quantizer.quantizer import SharedQuantizationSpec as TorchAOSharedQuantizationSpec
 
 from nncf.common.graph.graph import NNCFGraph
 from nncf.common.quantization.quantizer_propagation.solver import QuantizerPropagationRule
@@ -25,7 +28,6 @@ from nncf.common.quantization.quantizer_setup import QuantizationPointBase
 from nncf.common.quantization.quantizer_setup import SingleConfigQuantizerSetup
 from nncf.common.quantization.structs import QuantizationPreset
 from nncf.common.quantization.structs import QuantizationScheme
-from nncf.common.quantization.structs import QuantizerConfig as NNCFQuantizerConfig
 from nncf.experimental.torch.fx.nncf_graph_builder import GraphConverter
 from nncf.experimental.torch.fx.node_utils import get_graph_node_by_name
 from nncf.experimental.torch.fx.transformations import fold_constant_except_qdq
@@ -103,49 +105,153 @@ class OpenVINOQuantizer(TorchAOQuantizer):
         return self._min_max_algo.find_quantization_setup(model, nncf_graph)
 
     def annotate(self, model: torch.fx.GraphModule) -> torch.fx.GraphModule:
-        nncf_grpah = GraphConverter.create_nncf_graph(model)
-        quantization_setup = self.get_quantization_setup(model, nncf_grpah)
-        target_node_vs_qp: Dict[str, List[QuantizationPointBase]] = defaultdict(list)
+        nncf_graph = GraphConverter.create_nncf_graph(model)
+        quantization_setup = self.get_quantization_setup(model, nncf_graph)
+
+        non_unified_quantization_points = quantization_setup.quantization_points.copy()
+
         graph = model.graph
-        for qp in quantization_setup.quantization_points.values():
-            target_node_vs_qp[qp.insertion_point.target_node_name].append(qp)
+        node_vs_torch_annotation = defaultdict(TorchAOQuantizationAnnotation)
+        for quantizer_ids in quantization_setup.unified_scale_groups.values():
 
-        for target_node_name, qps in target_node_vs_qp.items():
-            input_qspec_map = dict()
-            output_qspec = None
-            target_node = get_graph_node_by_name(graph, target_node_name)
-            for qp in qps:
-                ip = qp.insertion_point
-                if qp.is_activation_quantization_point():
-                    inductor_qspec = self._convert_nncf_qspec_to_inductor_qspec(qp.qconfig, is_weight=False)
-                    if ip.input_port_id is None:
-                        output_qspec = inductor_qspec
-                    else:
-                        node = target_node.all_input_nodes[ip.input_port_id]
-                        input_qspec_map[node] = inductor_qspec
-                else:
-                    inductor_qspec = self._convert_nncf_qspec_to_inductor_qspec(qp.qconfig, is_weight=True)
-                    weight_node = target_node.all_input_nodes[1]
-                    input_qspec_map[weight_node] = inductor_qspec
+            root_quantizer_id = self._get_unified_scales_root_quantizer_id(
+                nncf_graph, quantizer_ids, quantization_setup
+            )
+            qp = quantization_setup.quantization_points[root_quantizer_id]
+            root_edge_or_node, annotation = self._get_edge_or_node_and_annotation(graph, qp, node_vs_torch_annotation)
+            qspec = self._get_inductor_qspec_from_qp(qp)
+            self._fill_torch_ao_annotation(root_edge_or_node, qspec, annotation)
 
-            annotation = TorchAOQuantizationAnnotation(input_qspec_map=input_qspec_map, output_qspec=output_qspec)
-            assert QUANT_ANNOTATION_KEY not in target_node.meta
-            target_node.meta[QUANT_ANNOTATION_KEY] = annotation
+            while quantizer_ids:
+                quantizer_id = quantizer_ids.pop()
+                del non_unified_quantization_points[quantizer_id]
 
-    def _convert_nncf_qspec_to_inductor_qspec(
-        self, qspec: NNCFQuantizerConfig, is_weight: bool
-    ) -> TorchAOQuantizationSpec:
+                if quantizer_id == root_quantizer_id:
+                    continue
+
+                qspec = TorchAOSharedQuantizationSpec(root_edge_or_node)
+                qp = quantization_setup.quantization_points[quantizer_id]
+                edge_or_node, annotation = self._get_edge_or_node_and_annotation(graph, qp, node_vs_torch_annotation)
+
+                self._fill_torch_ao_annotation(edge_or_node, qspec, annotation)
+
+        for qp in non_unified_quantization_points.values():
+            edge_or_node, annotation = self._get_edge_or_node_and_annotation(graph, qp, node_vs_torch_annotation)
+            qspec = self._get_inductor_qspec_from_qp(qp)
+            self._fill_torch_ao_annotation(edge_or_node, qspec, annotation)
+
+        for node, annotation in node_vs_torch_annotation.items():
+            assert QUANT_ANNOTATION_KEY not in node.meta
+            node.meta[QUANT_ANNOTATION_KEY] = annotation
+
+    @staticmethod
+    def _get_unified_scales_root_quantizer_id(
+        nncf_graph: NNCFGraph, quantizer_ids: List[int], quantizer_setup: SingleConfigQuantizerSetup
+    ) -> int:
+        """
+        Identifies the earliest quantizer node ID based on the corresponding `nncf_node.node_id`
+        in the given NNCFGraph. This is required by the `_get_obs_or_fq_map` function.
+        Refer to: https://github.com/pytorch/pytorch/blob/main/torch/ao/quantization/pt2e/prepare.py#L291
+
+        :param nncf_graph: The NNCFGraph instance.
+        :param quantizer_ids: The list of quantizer IDs to evaluate.
+        :param quantizer_setup: The instance of SingleConfigQuantizerSetup.
+        :return: The ID of the earliest quantizer node in terms of `nncf_node.node_id`.
+        """
+        nncf_node_quantizer_id = None
+        root_quantizer_id = None
+        for quantizer_id in quantizer_ids:
+            target_node_name = quantizer_setup.quantization_points[quantizer_id].insertion_point.target_node_name
+            nncf_node = nncf_graph.get_node_by_name(target_node_name)
+            if nncf_node_quantizer_id is None or nncf_node.node_id < nncf_node_quantizer_id:
+                root_quantizer_id = quantizer_id
+                nncf_node_quantizer_id = nncf_node.node_id
+        return root_quantizer_id
+
+    @staticmethod
+    def _get_edge_or_node_and_annotation(
+        graph: torch.fx.Graph,
+        qp: QuantizationPointBase,
+        node_vs_torch_annotation: Dict[torch.fx.Node, TorchAOQuantizationAnnotation],
+    ) -> Tuple[EdgeOrNode, TorchAOQuantizationAnnotation]:
+        """
+        Retrieves the edge or node and its corresponding TorchAOQuantizationAnnotation based on the given graph,
+        quantization point, and node-to-annotation mapping.
+
+        :param graph: torch.fx.Graph instance.
+        :param qp: QuantizationPointBase instance.
+        :param node_vs_torch_annotation: A dictionary mapping torch.fx.GraphNode objects to their respective
+            TorchAOQuantizationAnnotations.
+        :return: A tuple containing the EdgeOrNode and its associated TorchAOQuantizationAnnotation.
+        """
+        target_node = get_graph_node_by_name(graph, qp.insertion_point.target_node_name)
+        annotation = node_vs_torch_annotation[target_node]
+        edge_or_node = OpenVINOQuantizer._get_edge_or_node(target_node, qp)
+        return edge_or_node, annotation
+
+    @staticmethod
+    def _get_edge_or_node(target_node: torch.fx.Node, qp: QuantizationPointBase) -> EdgeOrNode:
+        """
+        Returns the edge or node based on the given target node and quantization point.
+
+        :param target_node: The target node instance.
+        :param qp: The QuantizationPointBase instance.
+        :return: The corresponding EdgeOrNode derived from the target node and quantization point.
+        """
+        ip = qp.insertion_point
+        if qp.is_weight_quantization_point():
+            weight_node = target_node.all_input_nodes[1]
+            return (weight_node, target_node)
+
+        if ip.input_port_id is None:
+            return target_node
+
+        node = target_node.all_input_nodes[ip.input_port_id]
+        return (node, target_node)
+
+    @staticmethod
+    def _fill_torch_ao_annotation(
+        edge_or_node: EdgeOrNode,
+        qspec: TorchAOQuantizationSpecBase,
+        annotation_to_update: TorchAOQuantizationAnnotation,
+    ) -> None:
+        """
+        Helper method to update the annotation_to_update based on the specified edge_or_node and qspec.
+
+        :param edge_or_node: The target EdgeOrNode to be used for the update.
+        :param qspec: An instance of TorchAOQuantizationSpecBase representing the quantization specification to apply.
+        :param annotation_to_update: The annotation to update based on the edge_or_node and qspec.
+        """
+        if isinstance(edge_or_node, torch.fx.Node):
+            annotation_to_update.output_qspec = qspec
+        else:
+            annotation_to_update.input_qspec_map[edge_or_node[0]] = qspec
+
+    @staticmethod
+    def _get_inductor_qspec_from_qp(qp: QuantizationPointBase) -> TorchAOQuantizationSpec:
+        """
+        Retrieves the quantization configuration from the given quantization point and
+        converts it into a TorchAOQuantizationSpec.
+
+        :param qp: An instance of QuantizationPointBase.
+        :return: A TorchAOQuantizationSpec retrieved and converted from the quantization point.
+        """
         # Eps value is borrowed from
         # https://github.com/pytorch/pytorch/blob/main/torch/ao/quantization/quantizer/x86_inductor_quantizer.py
         # get_default_x86_inductor_quantization_config
         extra_args = {"eps": 2**-12}
-        if qspec.per_channel:
+        qconfig = qp.qconfig
+        is_weight = qp.is_weight_quantization_point()
+
+        if qconfig.per_channel:
             torch_qscheme = (
-                torch.per_channel_symmetric if qspec.mode is QuantizationScheme.SYMMETRIC else torch.per_channel_affine
+                torch.per_channel_symmetric
+                if qconfig.mode is QuantizationScheme.SYMMETRIC
+                else torch.per_channel_affine
             )
         else:
             torch_qscheme = (
-                torch.per_tensor_symmetric if qspec.mode is QuantizationScheme.SYMMETRIC else torch.per_tensor_affine
+                torch.per_tensor_symmetric if qconfig.mode is QuantizationScheme.SYMMETRIC else torch.per_tensor_affine
             )
         if is_weight:
             observer = PerChannelMinMaxObserver
@@ -161,7 +267,7 @@ class OpenVINOQuantizer(TorchAOQuantizer):
             )
             quant_min = 0
             quant_max = 255
-            dtype = torch.int8 if qspec.signedness_to_force else torch.uint8
+            dtype = torch.int8 if qconfig.signedness_to_force else torch.uint8
             channel_axis = 1  # channel dim for activations
         return TorchAOQuantizationSpec(
             dtype=dtype,
