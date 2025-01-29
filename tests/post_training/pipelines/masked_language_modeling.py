553b21ba82f182e4cc9fcfd7d500e608ef2a61a9
Lyalyushkin Nikolay <nikolay.lyalyushkin@intel.com>
2025-01-29 11:20:37 +0100

    xfail for bug with f16 zp (#3220)
    ### Changes
    
    Mark as xfail PTWC torch tests counting number of int4/int8 ops. 
    
    ### Reason for changes
    
    starting from OV 2025 ZP is represented as f16 in some cases (bug:
    160006)
    
    ### Related tickets
    
    159993
    160006
    
    ### Tests
    
    - [x] openvino-nightly/job/post_training_weight_compression/73
    
    ![image](https://github.com/user-attachments/assets/bac6b5f0-0d8a-4890-9558-3ce6028ca56c)
    - [x] manual/job/post_training_weight_compression/303
    
    ![image](https://github.com/user-attachments/assets/0d76fa7a-baf6-4515-bdd5-7bdda11b046e)

--- tests/post_training/pipelines/masked_language_modeling.py
+++ tests/post_training/pipelines/masked_language_modeling.py
@@ -106,6 +106,3 @@ class MaskedLanguageModelingHF(PTQTestPipeline):
             self.calibration_dataset = calibration_dataset
         else:
             self.calibration_dataset = nncf.Dataset(calibration_dataset, self.get_transform_calibration_fn())
-
-    def _validate(self):
-        pass
