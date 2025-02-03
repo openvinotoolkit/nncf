e92e247c35c2d74d22c79c00b59d95e33ca4eae1
Nikita Savelyev <nikita.savelyev@intel.com>
2025-02-03 21:11:19 +0100

    [Release] Set environment variable on LNL CPUs before compression model compilation (#3233)
    ### Changes
    
    Set `export DNNL_MAX_CPU_ISA=AVX2_VNNI` during weights compression on
    LNL CPUs.
    
    ### Reason for changes
    
    Fix compression on LNL CPUs.
    
    ### Related tickets
    
    161336
    
    ### Tests
    - https://github.com/openvinotoolkit/nncf/actions/runs/13075940656 ✅ 
    - https://github.com/openvinotoolkit/nncf/actions/runs/13075938396 ✅ 
    - NNCF/job/nightly/job/sdl/job/nightly_trigger/426/ ✅
    
    ---------
    
    Co-authored-by: Alexander Suslov <alexander.suslov@intel.com>

--- nncf/common/utils/backend.py
+++ nncf/common/utils/backend.py
@@ -12,6 +12,8 @@ from copy import deepcopy
 from enum import Enum
 from typing import Any, Callable, TypeVar, cast
 
+from packaging import version
+
 import nncf
 from nncf.experimental.common.check_feature import is_experimental_torch_tracing_enabled
 
@@ -19,6 +21,7 @@ try:
     import openvino  # type: ignore # noqa: F401
 
     _OPENVINO_AVAILABLE = True
+    _OPENVINO_VERSION = version.parse(openvino.__version__.split("-")[0])
 except ImportError:
     _OPENVINO_AVAILABLE = False
 
@@ -185,3 +188,16 @@ def is_openvino_available() -> bool:
     :return: True if openvino package is installed, False otherwise.
     """
     return _OPENVINO_AVAILABLE
+
+
+def is_openvino_at_least(version_str: str) -> bool:
+    """
+    Check if OpenVINO version is at least the specified one.
+
+    :param version_str: The version string to compare with the installed OpenVINO version. For example "2025.1".
+    :return: True if the installed OpenVINO version is at least the specified one, False otherwise.
+    """
+    if not _OPENVINO_AVAILABLE:
+        return False
+
+    return version.parse(version_str) <= _OPENVINO_VERSION
