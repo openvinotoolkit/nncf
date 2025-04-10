4d242a9b4438aec01ef5617bb7c602b60646bb4c
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2025-04-10 13:25:08 +0300

    remove xfail for test_mixed_precision_e2m1

--- tests/openvino/native/quantization/test_weights_compression.py
+++ tests/openvino/native/quantization/test_weights_compression.py
@@ -975,8 +975,6 @@ def test_call_gptq_with_dataset_scale_estimation_neg_group_size(mode):
     compress_weights(model, mode=mode, ratio=1.0, group_size=-1, dataset=dataset, gptq=True, scale_estimation=True)
 
 
-# TODO(andreyanufr) Waiting for the e2m1 in OV release
-@pytest.mark.xfail
 @pytest.mark.parametrize(
     ("mode", "all_layers", "ratio", "ref_ids"),
     (
