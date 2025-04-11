be7e640e756f020c5f5d72348a04d2dd62d8a904
Nikita Malinin <nikita.malinin@intel.com>
2025-04-11 16:22:07 +0200

    Rollback

--- nncf/torch/quantization/quantize_functions.py
+++ nncf/torch/quantization/quantize_functions.py
@@ -133,6 +133,7 @@ class QuantizeSymmetricTorch(torch.autograd.Function):
 
         original_shape = input_.shape
         input_ = input_.reshape(input_shape)
+
         output = RQ.Quantize_forward(input_, input_low, input_range, levels)
 
         ctx.save_for_backward(input_, input_low, input_range)
@@ -195,6 +196,7 @@ class QuantizeAsymmetricTorch(torch.autograd.Function):
         input_shape = input_.shape
         orig_shape = grad_output.shape
         grad_output = grad_output.reshape(input_shape)
+
         output = RQ.Quantize_forward(input_, input_low, input_range, levels)
         grad_input, grad_low, grad_range = RQ.Quantize_backward(
             grad_output, input_, input_low, input_range, output, level_low, level_high
