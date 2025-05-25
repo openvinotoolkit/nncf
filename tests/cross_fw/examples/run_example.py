e2f3de422ab287d488542ec2de7ba422c0bd7c5e
J. Pablo Muñoz <pablo.munoz@intel.com>
2025-05-25 13:56:16 -0700

    Add num of min loss configs to test
    Signed-off-by: J. Pablo Muñoz <pablo.munoz@intel.com>

--- tests/cross_fw/examples/run_example.py
+++ tests/cross_fw/examples/run_example.py
@@ -234,6 +234,7 @@ def llm_compression_qat_with_nls() -> float:
         "--task=arc_challenge",
         "--epochs=2",
         "--batch_size=16",
+        "--num_min_loss_configs=5",
         "--lr=5e-4",
         "--lora_rank_space",
         "16",
