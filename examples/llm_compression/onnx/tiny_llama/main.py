44ed6c45489d7951f5e9e36b563fdffbdb4a98be
Peng, Bo <bo.peng@intel.com>
2025-07-23 11:48:48 +0800

    remove main_npu, use latest onnx and convert_version to convert opset

--- examples/llm_compression/onnx/tiny_llama/main.py
+++ examples/llm_compression/onnx/tiny_llama/main.py
@@ -9,13 +9,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
 import time
 from pathlib import Path
 
 import onnx
 from optimum.intel.openvino import OVModelForCausalLM
 from optimum.onnxruntime import ORTModelForCausalLM
-from optimum.exporters.onnx import main_export
 from transformers import AutoTokenizer
 
 import nncf
@@ -24,7 +24,6 @@ from nncf.onnx.quantization.backend_parameters import BackendParameters
 ROOT = Path(__file__).parent.resolve()
 
 
-COMPRESS_FOR_NPU = False
 MODEL_ID = "PY007/TinyLlama-1.1B-Chat-v0.3"
 OUTPUT_DIR = ROOT / "tinyllama_compressed"
 
@@ -42,10 +41,17 @@ def main():
     # the directory containing the model’s external data files.
     onnx_model = onnx.load(OUTPUT_DIR / "model.onnx", load_external_data=False)
 
+    # Convert model to the target opset version if necessary.
+    opset = 21 if len(sys.argv) < 2 else int(sys.argv[1])
+    if onnx_model.opset_import[0].version != opset:
+        onnx_model = onnx.version_converter.convert_version(onnx_model, opset)
+
     compressed_onnx_model = nncf.compress_weights(
         onnx_model,
         mode=nncf.CompressWeightsMode.INT4_SYM,
-        ratio=0.8,
+        ratio=1.0,
+        all_layers=True,
+        ignored_scope=nncf.IgnoredScope(types=["Gather"]),
         advanced_parameters=nncf.AdvancedCompressionParameters(
             backend_params={BackendParameters.EXTERNAL_DATA_DIR: OUTPUT_DIR}
         ),
@@ -70,50 +76,5 @@ def main():
     return output_text
 
 
-# This is a workaround for the NPU to handle quantized models correctly.
-def main_npu():
-    # In current nncf:
-    #   model with opset < 21  will use MatMulNBits OP, which is not supported by NPU.
-    #   model with opset >= 21 will use DequantizeLinear OP, which is supported by NPU.
-    # The main_export function will export the model with opset 20, and then modified to 21.
-    # This is a temporary solution until the NPU supports the MatMulNBits OP.
-    main_export(MODEL_ID, OUTPUT_DIR, task="text-generation-with-past", opset=20, no_post_process=True)
-    onnx_model = onnx.load(OUTPUT_DIR / "model.onnx", load_external_data=False)
-    onnx_model.ir_version = 10
-    onnx_model.opset_import[0].version = 21
-
-    compressed_onnx_model = nncf.compress_weights(
-        onnx_model,
-        mode=nncf.CompressWeightsMode.INT4_SYM,
-        ratio=1.0,
-        all_layers=True,
-        ignored_scope=nncf.IgnoredScope(types=["Gather"]),
-        advanced_parameters=nncf.AdvancedCompressionParameters(
-            backend_params={BackendParameters.EXTERNAL_DATA_DIR: OUTPUT_DIR}
-        ),
-    )
-
-    # Replace the original model with the compressed model.
-    onnx.save(compressed_onnx_model, OUTPUT_DIR / "model.onnx", save_as_external_data=True)
-
-    # Infer Model.
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
-
-    ov_model = OVModelForCausalLM.from_pretrained(OUTPUT_DIR, from_onnx=True)
-
-    input_ids = tokenizer("What is PyTorch?", return_tensors="pt").to(device="cpu")
-
-    start_t = time.time()
-    output = ov_model.generate(**input_ids, max_new_tokens=100)
-    print("Elapsed time: ", time.time() - start_t)
-
-    output_text = tokenizer.decode(output[0])
-    print(output_text)
-    return output_text
-
-
 if __name__ == "__main__":
-    if COMPRESS_FOR_NPU:
-        main_npu()
-    else:
-        main()
+    main()
