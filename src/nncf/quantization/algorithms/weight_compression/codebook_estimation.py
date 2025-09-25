812cbed354039e27ee322dd03020ff06253225eb
Andrei Anufriev <andrey.anufriev@intel.com>
2025-09-25 18:21:01 +0200

    Remove unused code.

--- src/nncf/quantization/algorithms/weight_compression/codebook_estimation.py
+++ src/nncf/quantization/algorithms/weight_compression/codebook_estimation.py
@@ -42,62 +42,6 @@ from nncf.quantization.algorithms.weight_compression.constants import CB4_QUANTI
 TModel = TypeVar("TModel")
 
 
-f8e4m3_data = np.array(
-    [-4.4800000e+02, -4.1600000e+02, -3.8400000e+02, -3.5200000e+02
-    , -3.2000000e+02, -2.8800000e+02, -2.5600000e+02, -2.4000000e+02
-    , -2.2400000e+02, -2.0800000e+02, -1.9200000e+02, -1.7600000e+02
-    , -1.6000000e+02, -1.4400000e+02, -1.2800000e+02, -1.2000000e+02
-    , -1.1200000e+02, -1.0400000e+02, -9.6000000e+01, -8.8000000e+01
-    , -8.0000000e+01, -7.2000000e+01, -6.4000000e+01, -6.0000000e+01
-    , -5.6000000e+01, -5.2000000e+01, -4.8000000e+01, -4.4000000e+01
-    , -4.0000000e+01, -3.6000000e+01, -3.2000000e+01, -3.0000000e+01
-    , -2.8000000e+01, -2.6000000e+01, -2.4000000e+01, -2.2000000e+01
-    , -2.0000000e+01, -1.8000000e+01, -1.6000000e+01, -1.5000000e+01
-    , -1.4000000e+01, -1.3000000e+01, -1.2000000e+01, -1.1000000e+01
-    , -1.0000000e+01, -9.0000000e+00, -8.0000000e+00, -7.5000000e+00
-    , -7.0000000e+00, -6.5000000e+00, -6.0000000e+00, -5.5000000e+00
-    , -5.0000000e+00, -4.5000000e+00, -4.0000000e+00, -3.7500000e+00
-    , -3.5000000e+00, -3.2500000e+00, -3.0000000e+00, -2.7500000e+00
-    , -2.5000000e+00, -2.2500000e+00, -2.0000000e+00, -1.8750000e+00
-    , -1.7500000e+00, -1.6250000e+00, -1.5000000e+00, -1.3750000e+00
-    , -1.2500000e+00, -1.1250000e+00, -1.0000000e+00, -9.3750000e-01
-    , -8.7500000e-01, -8.1250000e-01, -7.5000000e-01, -6.8750000e-01
-    , -6.2500000e-01, -5.6250000e-01, -5.0000000e-01, -4.6875000e-01
-    , -4.3750000e-01, -4.0625000e-01, -3.7500000e-01, -3.4375000e-01
-    , -3.1250000e-01, -2.8125000e-01, -2.5000000e-01, -2.3437500e-01
-    , -2.1875000e-01, -2.0312500e-01, -1.8750000e-01, -1.7187500e-01
-    , -1.5625000e-01, -1.4062500e-01, -1.2500000e-01, -1.1718750e-01
-    , -1.0937500e-01, -1.0156250e-01, -9.3750000e-02, -7.8125000e-02
-    , -7.0312500e-02, -5.8593750e-02, -5.0781250e-02, -3.9062500e-02
-    , -2.9296875e-02, -1.9531250e-02, -9.7656250e-03, 0.0000000e+00
-    , 9.7656250e-03, 1.9531250e-02, 2.9296875e-02, 3.9062500e-02
-    , 5.0781250e-02, 5.8593750e-02, 7.0312500e-02, 7.8125000e-02
-    , 9.3750000e-02, 1.0156250e-01, 1.0937500e-01, 1.1718750e-01
-    , 1.2500000e-01, 1.4062500e-01, 1.5625000e-01, 1.7187500e-01
-    , 1.8750000e-01, 2.0312500e-01, 2.1875000e-01, 2.3437500e-01
-    , 2.5000000e-01, 2.8125000e-01, 3.1250000e-01, 3.4375000e-01
-    , 3.7500000e-01, 4.0625000e-01, 4.3750000e-01, 4.6875000e-01
-    , 5.0000000e-01, 5.6250000e-01, 6.2500000e-01, 6.8750000e-01
-    , 7.5000000e-01, 8.1250000e-01, 8.7500000e-01, 9.3750000e-01
-    , 1.0000000e+00, 1.1250000e+00, 1.2500000e+00, 1.3750000e+00
-    , 1.5000000e+00, 1.6250000e+00, 1.7500000e+00, 1.8750000e+00
-    , 2.0000000e+00, 2.2500000e+00, 2.5000000e+00, 2.7500000e+00
-    , 3.0000000e+00, 3.2500000e+00, 3.5000000e+00, 3.7500000e+00
-    , 4.0000000e+00, 4.5000000e+00, 5.0000000e+00, 5.5000000e+00
-    , 6.0000000e+00, 6.5000000e+00, 7.0000000e+00, 7.5000000e+00
-    , 8.0000000e+00, 9.0000000e+00, 1.0000000e+01, 1.1000000e+01
-    , 1.2000000e+01, 1.3000000e+01, 1.4000000e+01, 1.5000000e+01
-    , 1.6000000e+01, 1.8000000e+01, 2.0000000e+01, 2.2000000e+01
-    , 2.4000000e+01, 2.6000000e+01, 2.8000000e+01, 3.0000000e+01
-    , 3.2000000e+01, 3.6000000e+01, 4.0000000e+01, 4.4000000e+01
-    , 4.8000000e+01, 5.2000000e+01, 5.6000000e+01, 6.0000000e+01
-    , 6.4000000e+01, 7.2000000e+01, 8.0000000e+01, 8.8000000e+01
-    , 9.6000000e+01, 1.0400000e+02, 1.1200000e+02, 1.2000000e+02
-    , 1.2800000e+02, 1.4400000e+02, 1.6000000e+02, 1.7600000e+02
-    , 1.9200000e+02, 2.0800000e+02, 2.2400000e+02, 2.4000000e+02
-    , 2.5600000e+02, 2.8800000e+02, 3.2000000e+02, 3.5200000e+02
-    , 3.8400000e+02, 4.1600000e+02, 4.4800000e+02]
-)
 
 def fp8_convert(in_shape):
     input = opset.parameter(
@@ -156,6 +100,7 @@ class CodebookEstimation:
         all_weight_params: list[WeightCompressionParameters],
         statistics: dict[str, WCTensorStatistic],
         backend_entity: Optional[WeightCompressionAlgoBackend] = None,
+        debug=False
     ) -> dict[str, CompressedWeight]:
         """
         Estimates better codebook.
@@ -194,9 +139,10 @@ class CodebookEstimation:
             _, weight_port_id = weight_data[0]
 
             weight = self._backend_entity.get_weight(wp.node_with_weight, weight_port_id, model, graph)
-            
-            qw = float_quantize_dequantize_weight(weight, config, wp.reduction_axes)
-            print("Initial diff:", np.mean(np.abs(weight.data - qw.data)))
+
+            if debug:
+                qw = float_quantize_dequantize_weight(weight, config, wp.reduction_axes)
+                print("Initial diff:", np.mean(np.abs(weight.data - qw.data)))
 
             codebook, scale, indexes = self.calculate_codebook(
                 stats,
@@ -207,8 +153,10 @@ class CodebookEstimation:
             )
             res[weight_name] = CompressedWeight(indexes, scale, None, codebook)
             config.codebook_values = codebook
-            qw = float_quantize_dequantize_weight(weight, config, wp.reduction_axes)
-            print("kmeans diff:", np.mean(np.abs(weight.data - qw.data)))
+  
+            if debug:
+                qw = float_quantize_dequantize_weight(weight, config, wp.reduction_axes)
+                print("kmeans diff:", np.mean(np.abs(weight.data - qw.data)))
 
         return res
 
@@ -245,18 +193,9 @@ class CodebookEstimation:
 
         converter = fp8_convert(codebook.shape)
         indexes = indexes.reshape(orig_shape)
-        
-        fp8_scales = np.unique(np.abs(f8e4m3_data))
-        fp8_scales = fp8_scales[fp8_scales >= 1.0]
+
         
         best_codebook = converter(codebook)[0]
-        #print("Best codebook:", best_codebook)
-
-        min_diff = float("inf")
-        best_scale = 1.0
-        
-        #weight = weight.as_numpy_tensor().data
-        #scale = scale.as_numpy_tensor().data
         
         fp_outs = fns.matmul(weight, X)
         diff = float('inf')
@@ -271,42 +210,19 @@ class CodebookEstimation:
             qw = float_quantize_dequantize_weight(weight, config, wp.reduction_axes)
             q_outs = fns.matmul(qw, X)
             
-            cur_diff = np.mean(np.abs(fp_outs.data - q_outs.data))
+            cur_diff = fns.mean(fns.abs(fp_outs - q_outs)).item()
             if cur_diff < diff:
                 diff = cur_diff
                 best_codebook = var
                 best_i = i_var
-            # else:
-            #     print("Was skip: ", diff, cur_diff)
-                #print("Best codebook:", best_codebook)
             
         print("Best codebook:", best_codebook, "diff:", diff, "best_i:", best_i)
 
-        # min_diffs = []
-        # for fp8_scale in fp8_scales:
-        #     scaled_codebook = codebook * fp8_scale
-        #     scaled_codebook = converter(scaled_codebook)[0]
-            
-            
-        #     dequantized_weight = scaled_codebook[indexes]
-        #     dequantized_weight = dequantized_weight * scale
-        #     dequantized_weight = dequantized_weight / fp8_scale
-            
-        #     diff = np.mean(np.abs(weight - dequantized_weight))
-            
-        #     if diff < min_diff:
-        #         min_diff = diff
-        #         best_codebook = deepcopy(scaled_codebook)
-        #         best_scale = fp8_scale
-        #         min_diffs.append(min_diff)
-        
-        #print("\t", min_diffs)
-        return Tensor(best_codebook), None, None #Tensor(scale / best_scale), Tensor(indexes)
+        return Tensor(best_codebook), None, None
 
 
 def round(quantiles, values):
     center_of_quantiles = 0.5 * (quantiles[1:] + quantiles[:-1])
-    
     return np.searchsorted(center_of_quantiles, values, side='left', sorter=None)
 
 
@@ -335,15 +251,13 @@ class KMeansWeighted:
                 next_ = values[np.where(n_frequencies <= quants[i + 1])[0][-1]].item()
                 res.append((prev + next_) / 2)
 
-        res = np.array(res)#.reshape(1, -1)
+        res = np.array(res)
         return res
 
     @staticmethod
     def create_histogramm(data, granularity=0.01):
         centers = []
         step = granularity
-        
-        #granularity = granularity * (data.max() - data.min())
 
         data_range=(data.min().item(), data.max().item())
         prev = data_range[0]
@@ -381,11 +295,7 @@ class KMeansWeighted:
         ranges = []
         step = data_.max().item() * granularity / 3.5
 
-        #granularity = granularity * (data.max() - data.min())
-
         data = np.array([data_, importance])
-
-        #data = np.sort(data, axis=1)
         
         data = data[:, data[0, :].argsort()]
 
@@ -411,20 +321,14 @@ class KMeansWeighted:
         for i in range(centers.size):
             res[0].append(centers[i])
             if i == 0:
-                # res[1].append(np.sum(data[0, :ranges_idxs[1]]))
-                # res[2].append(ranges_idxs[1])
                 KMeansWeighted.add_weighted_data_and_weights(res, data[:, :ranges_idxs[1]])
             elif i == centers.size - 1:
-                # res[1].append(np.sum(data[ranges_idxs[-2]:]))
-                # res[2].append(len(data) - ranges_idxs[-2])
                 KMeansWeighted.add_weighted_data_and_weights(res, data[:, ranges_idxs[-2]:])
             else:
                 idx = 2 * i
-                # res[1].append(np.sum(data[ranges_idxs[idx - 1]:ranges_idxs[idx + 1]]))
-                # res[2].append(ranges_idxs[idx + 1] - ranges_idxs[idx - 1] - 1)
                 KMeansWeighted.add_weighted_data_and_weights(res, data[:, ranges_idxs[idx - 1]:ranges_idxs[idx + 1]])
 
-        res[0] = np.array(res[0])#.reshape(-1, 1)
+        res[0] = np.array(res[0])
         res[1] = np.array(res[1])
         res[2] = np.array(res[2])
 
@@ -434,18 +338,8 @@ class KMeansWeighted:
         if self.max_iter == 1:
             self.centroids = deepcopy(init)
             return
-
-        # start = time.time()
-        # self.hist = self.create_histogramm(X_train)
-        # end = time.time()
-        # print("create_histogramm", end - start)
         
-        start = time.time()
         self.hist = KMeansWeighted.create_histogramm_sorted(X_train, importance)
-        end = time.time()
-        #print("create_histogramm_sorted", end - start)
-        
-        start = time.time()
 
         init_by_hist = self.get_init(self.hist[0], self.hist[2], self.n_clusters)
         init_by_hist[0] = init[0]
@@ -480,9 +374,7 @@ class KMeansWeighted:
                 break
         
         self.variants.append(deepcopy(self.centroids))
-        end = time.time()
-        #print("rest", end - start)
-        #print(self.centroids)
+
 
     def evaluate(self, X):
         centroid_idxs = round(self.centroids, X)
@@ -501,17 +393,11 @@ def weights_clusterization_k_means(weight, importance, n_centroids=2**4):
     n_init = [0, 0]
     n_init[0] = weight.min()
     n_init[-1] = weight.max()
-    #print("n_init:", n_init)
 
     kmeans = KMeansWeighted(n_centroids, max_iter=70)
-    
-    #n_init = kmeans.get_init(weight, n_init, n_centroids)
-    
-    #kmeans.fit(weight.reshape(-1, 1), n_init.reshape(1, -1), fixed=[0, 7, 15])
+
     kmeans.fit(weight, importance, n_init, fixed=[0, 7, 15])
     codebook, indexes = kmeans.evaluate(weight.reshape(-1, 1))
-    # codebook = kmeans.cluster_centers_.flatten()
-    # indexes  = kmeans.labels_
 
     indexes = np.reshape(indexes, orig_shape)
 
