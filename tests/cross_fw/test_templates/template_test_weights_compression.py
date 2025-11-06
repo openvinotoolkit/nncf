6184f82afe7fa438fba08d211420876f5c54f5be
Nikita Savelyev <nikita.savelyev@intel.com>
2025-11-06 14:25:32 +0100

    Add comment

--- tests/cross_fw/test_templates/template_test_weights_compression.py
+++ tests/cross_fw/test_templates/template_test_weights_compression.py
@@ -521,6 +521,7 @@ class TemplateWeightCompression(ABC):
                     "int4_asym, group size 16  │ 25% (1 / 9)                 │ 25% (1 / 9)",
                 ]
                 for row in table_rows:
+                    # On Windows "|" is printed instead of "│"
                     assert any(row in msg.replace("|", "│") for msg in info_messages), "\n".join(info_messages)
 
     @pytest.mark.parametrize(
