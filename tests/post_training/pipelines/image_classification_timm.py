da2dd23dc3277f933a75cf6affc4af1ecfbaf493
Andrey Churkin <andrey.churkin@intel.com>
2025-11-12 10:48:59 +0000

    update opset version

--- tests/post_training/pipelines/image_classification_timm.py
+++ tests/post_training/pipelines/image_classification_timm.py
@@ -52,7 +52,7 @@ class ImageClassificationTimm(ImageClassificationBase):
                 self.dummy_tensor,
                 onnx_path,
                 export_params=True,
-                opset_version=13,
+                opset_version=18,
                 **additional_kwargs,
             )
             self.model = onnx.load(onnx_path)
