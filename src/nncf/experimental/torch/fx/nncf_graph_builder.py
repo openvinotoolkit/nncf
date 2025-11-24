54c2c107c78c3594fa54f22f0d9f19383e0534be
anzr299 <aamir.nazir@intel.com>
2025-11-24 21:29:25 +0400

    fix

--- src/nncf/experimental/torch/fx/nncf_graph_builder.py
+++ src/nncf/experimental/torch/fx/nncf_graph_builder.py
@@ -196,20 +196,16 @@ class GraphConverter:
                 source_node.meta["val"], (tuple, list)
             ):
                 tensor = source_node.meta["val"][0]
-            elif (
-                source_nncf_node.metatype
-                in [
-                    om.PTSplitMetatype,
-                    om.PTMaxMetatype,
-                    om.PTMinMetatype,
-                    om.PTMedianMetatype,
-                    om.PTAdaptiveMaxPool1dMetatype,
-                    om.PTAdaptiveMaxPool2dMetatype,
-                    om.PTAdaptiveMaxPool3dMetatype,
-                    om.PTAtenEmbeddingBagMetatype,
-                ]
-                and len(source_node.users) > 1
-            ):
+            elif source_nncf_node.metatype in [
+                om.PTSplitMetatype,
+                om.PTMaxMetatype,
+                om.PTMinMetatype,
+                om.PTMedianMetatype,
+                om.PTAdaptiveMaxPool1dMetatype,
+                om.PTAdaptiveMaxPool2dMetatype,
+                om.PTAdaptiveMaxPool3dMetatype,
+                om.PTAtenEmbeddingBagMetatype,
+            ] and isinstance(source_node.meta["val"], (tuple, list)):
                 tensor = source_node.meta["val"][output_idx]
                 # Assume every outputs corresponds to an unique output_port_id
                 output_port_id = output_idx
