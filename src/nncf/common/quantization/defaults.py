4fbc5e1a5bbaf0f4824675d9c5f658a2b0c2d831
dlyakhov <daniil.lyakhov@intel.com>
2026-01-02 10:58:46 +0100

    Happy New Year 2026!

--- src/nncf/common/quantization/defaults.py
+++ src/nncf/common/quantization/defaults.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 Intel Corporation
+# Copyright (c) 2026 Intel Corporation
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -9,7 +9,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Copyright (c) 2025 Intel Corporation
+# Copyright (c) 2026 Intel Corporation
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
