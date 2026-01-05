2c4aaae49ad91cda193b850d8e70a0389a354057
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2026-01-05 19:38:26 +0400

    [PT] prune + quantize (#3811)
    ### Changes
    
    - Support quantization after pruning
    - Renamed `apply_magnitude_sparsity_binary_mask` to
    `apply_magnitude_binary_mask`
    - `wrap_model` don not raise error if model already wrapped
    - `find_const_node_in_constant_subgraph` can find weight after pruning
    operations
    
    ### Related tickets
    
    CVS-171577
    
    ### Tests

--- src/nncf/torch/function_hook/strip.py
+++ src/nncf/torch/function_hook/strip.py
@@ -164,7 +164,15 @@ def apply_compression_in_place(model: TModel) -> TModel:
     for hook_name, hook_module in hook_storage.named_hooks():
         if not isinstance(
             hook_module,
-            (RBPruningMask, UnstructuredPruningMask, SymmetricQuantizer, AsymmetricQuantizer, BaseWeightsDecompressor),
+            (
+                RBPruningMask,
+                UnstructuredPruningMask,
+                SymmetricQuantizer,
+                AsymmetricQuantizer,
+                BaseWeightsDecompressor,
+                RBPruningMask,
+                UnstructuredPruningMask,
+            ),
         ):
             continue
 
