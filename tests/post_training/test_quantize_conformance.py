68b192358005a6c97313647ef3400e03348459c9
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2026-01-21 15:05:27 +0200

    Remove WA

--- tests/post_training/test_quantize_conformance.py
+++ tests/post_training/test_quantize_conformance.py
@@ -36,12 +36,6 @@ from tests.post_training.pipelines.base import RunInfo
 DATA_ROOT = Path(__file__).parent / "data"
 
 
-# TODO(AlexanderDokuchaev): WA for https://github.com/huggingface/optimum-intel/issues/1498
-from optimum.exporters.tasks import TasksManager  # noqa: E402
-
-TasksManager._TRANSFORMERS_TASKS_TO_MODEL_LOADERS["image-text-to-text"] = "AutoModelForImageTextToText"
-
-
 @pytest.fixture(scope="function", name="use_avx2")
 def fixture_use_avx2():
     old_value = os.environ.get("ONEDNN_MAX_CPU_ISA")
