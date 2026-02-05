ce6aa04610dbf592f5b8903abe044c1998d5cfec
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2026-02-05 14:18:38 +0400

    [POC] GPTQModel convertor (#3848)
    ### Changes
    
    Add convertor linear moduole to gptqmodel format. 
    Currently support only triton kernrel. 
    
    ### Tests
    
    https://github.com/openvinotoolkit/nncf/actions/runs/21706864333

--- tests/cross_fw/examples/run_example.py
+++ tests/cross_fw/examples/run_example.py
@@ -270,7 +270,6 @@ def llm_compression_qat_with_nls() -> float:
 def post_training_quantization_torch_fx_resnet18():
     from examples.post_training_quantization.torch_fx.resnet18.main import main as resnet18_main
 
-    # Set manual seed and determenistic cuda mode to make the test determenistic
     results = resnet18_main()
 
     return {
@@ -285,7 +284,6 @@ def post_training_quantization_torch_fx_resnet18():
 def quantization_aware_training_torch_resnet18():
     from examples.quantization_aware_training.torch.resnet18.main import main as resnet18_main
 
-    # Set manual seed and determenistic cuda mode to make the test determenistic
     set_torch_cuda_seed()
     results = resnet18_main()
 
@@ -306,7 +304,6 @@ def quantization_aware_training_torch_resnet18():
 def magnitude_pruning_torch_resnet18():
     from examples.pruning.torch.resnet18.main import main as pruning_resnet18_main
 
-    # Set manual seed and determenistic cuda mode to make the test determenistic
     set_torch_cuda_seed()
     results = pruning_resnet18_main()
 
@@ -317,9 +314,9 @@ def magnitude_pruning_torch_resnet18():
 
 def set_torch_cuda_seed(seed: int = 42):
     """
-    Sets torch, cuda and python random module to determenistic mode with
+    Sets torch, cuda and python random module to deterministic mode with
     given seed.
-    :param seed: Seed to use for determenistic run.
+    :param seed: Seed to use for deterministic run.
     """
     import random
 
@@ -347,7 +344,6 @@ def quantization_aware_training_torch_anomalib(data: Union[str, None]):
         with tarfile.open(tar_file_path) as tar_file:
             tar_file.extractall(dataset_path)
 
-    # Set manual seed and determenistic cuda mode to make the test determenistic
     set_torch_cuda_seed()
     results = anomalib_main()
 
@@ -363,6 +359,14 @@ def quantization_aware_training_torch_anomalib(data: Union[str, None]):
         "int8_model_size": results[6],
         "model_compression_rate": results[5] / results[6],
     }
+
+
+def llm_compression_torch_gptqmodel_convertor():
+    from examples.llm_compression.torch.gptq_model_convertor.main import main as gptqmodel_convertor_main
+
+    result = gptqmodel_convertor_main()
+
+    return {"word_count": len(result.split())}
 
 
 def main(argv):
