4a218bac0ed967880415a896f239f44b6263c73b
Alexander Dokuchaev <alexander.dokuchaev@intel.com>
2026-09-23 15:03:46 +0300

    common

--- tests/common/conftest.py
+++ tests/common/conftest.py
@@ -9,5 +9,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
+from pytest import Config
+from pytest import Parser
 
 from tests.cross_fw.shared.logging import nncf_caplog  # noqa: F401
+
+
+def pytest_addoption(parser: Parser):
+    parser.addoption(
+        "--regen-ref-data",
+        action="store_true",
+        default=False,
+        help="If specified, the reference files will be regenerated using the current state of the repository.",
+    )
+
+
+def pytest_configure(config: Config) -> None:
+    regen_dot = config.getoption("--regen-ref-data", False)
+    if regen_dot:
+        os.environ["NNCF_TEST_REGEN_DOT"] = "1"
